be3622244473e685464f1779a935916e3bd0db1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:39 +0000

    Add package skil/0.2.3/skil-0.2.3.tar.gz

---
+++ packages/skil/skil-0.2.3.tar.gz/skil-0.2.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='skil',
+    version='0.2.3',
+    packages=find_packages(),
+    install_requires=['skil_client', 'requests'],
+    extras_require={
+        'tests': ['pytest', 'pytest-pep8', 'pytest-cov', 'mock']
+    },
+    include_package_data=True,
+    license='Apache',
+    description='Deploy your Python models with SKIL',
+    long_description='Deploy your Python models with SKIL',
+    author='Max Pumperla',
+    author_email='max@skymind.io',
+    url='https://github.com/SkymindIO/skil-python',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+    ]
+)
