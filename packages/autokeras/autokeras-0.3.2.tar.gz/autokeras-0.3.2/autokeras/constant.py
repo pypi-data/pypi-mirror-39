ce2228243be4b3fdacf4c636882ae367db974620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:18 +0000

    Add package autokeras/0.3.2/autokeras-0.3.2.tar.gz

---
+++ packages/autokeras/autokeras-0.3.2.tar.gz/autokeras-0.3.2/autokeras/constant.py
@@ -0,0 +1,64 @@
+class Constant:
+    # Data
+
+    VALIDATION_SET_SIZE = 0.08333
+    CUTOUT_HOLES = 1
+    CUTOUT_RATIO = 0.5
+
+    # Searcher
+
+    MAX_MODEL_NUM = 1000
+    BETA = 2.576
+    KERNEL_LAMBDA = 1.0
+    T_MIN = 0.0001
+    N_NEIGHBOURS = 8
+    MAX_MODEL_SIZE = (1 << 25)
+    MAX_LAYER_WIDTH = 4096
+    MAX_LAYERS = 500
+
+    # Model Defaults
+
+    DENSE_DROPOUT_RATE = 0.5
+    CONV_DROPOUT_RATE = 0.25
+    MLP_DROPOUT_RATE = 0.25
+    CONV_BLOCK_DISTANCE = 2
+    DENSE_BLOCK_DISTANCE = 1
+    MODEL_LEN = 3
+    MLP_MODEL_LEN = 3
+    MLP_MODEL_WIDTH = 5
+    MODEL_WIDTH = 64
+    POOLING_KERNEL_SIZE = 2
+
+    # ModelTrainer
+
+    DATA_AUGMENTATION = True
+    MAX_ITER_NUM = 200
+    MIN_LOSS_DEC = 1e-4
+    MAX_NO_IMPROVEMENT_NUM = 5
+    MAX_BATCH_SIZE = 128
+    LIMIT_MEMORY = False
+    SEARCH_MAX_ITER = 200
+
+    # text preprocessor
+
+    EMBEDDING_DIM = 100
+    MAX_SEQUENCE_LENGTH = 400
+    MAX_NB_WORDS = 5000
+    EXTRACT_PATH = "glove/"
+    STORE_PATH = ''
+
+    # Download file name
+
+    FILE_PATH = "glove.zip"
+    PRE_TRAIN_FILE_LINK = "http://nlp.stanford.edu/data/glove.6B.zip"
+    PRE_TRAIN_FILE_NAME = "glove.6B.100d.txt"
+
+    # Image Resize
+
+    MAX_IMAGE_SIZE = 128 * 128
+
+    # SYS Constant
+
+    SYS_LINUX = 'linux'
+    SYS_WINDOWS = 'windows'
+    SYS_GOOGLE_COLAB = 'goog_colab'
---
+++ packages/autokeras/autokeras-0.3.2.tar.gz/autokeras-0.3.2/autokeras/net_transformer.py
@@ -0,0 +1,121 @@
+from copy import deepcopy
+from operator import itemgetter
+from random import randrange, sample
+
+from autokeras.nn.graph import NetworkDescriptor
+
+from autokeras.constant import Constant
+from autokeras.nn.layer_transformer import init_dense_weight, init_conv_weight, init_bn_weight
+from autokeras.nn.layers import is_layer, StubDense, get_dropout_class, StubReLU, get_conv_class, \
+    get_batch_norm_class, get_pooling_class
+
+
+def to_wider_graph(graph):
+    weighted_layer_ids = graph.wide_layer_ids()
+    weighted_layer_ids = list(filter(lambda x: graph.layer_list[x].output.shape[-1], weighted_layer_ids))
+    wider_layers = sample(weighted_layer_ids, 1)
+
+    for layer_id in wider_layers:
+        layer = graph.layer_list[layer_id]
+        if is_layer(layer, 'Conv'):
+            n_add = layer.filters
+        else:
+            n_add = layer.units
+
+        graph.to_wider_model(layer_id, n_add)
+    return graph
+
+
+def to_skip_connection_graph(graph):
+    # The last conv layer cannot be widen since wider operator cannot be done over the two sides of flatten.
+    weighted_layer_ids = graph.skip_connection_layer_ids()
+    descriptor = graph.extract_descriptor()
+    sorted_skips = sorted(descriptor.skip_connections, key=itemgetter(2, 0, 1))
+    p = 0
+    valid_connection = []
+    for skip_type in sorted([NetworkDescriptor.ADD_CONNECT, NetworkDescriptor.CONCAT_CONNECT]):
+        for index_a in range(len(weighted_layer_ids)):
+            for index_b in range(len(weighted_layer_ids))[index_a + 1:]:
+                if p < len(sorted_skips) and sorted_skips[p] == (index_a + 1, index_b + 1, skip_type):
+                    p += 1
+                else:
+                    valid_connection.append((index_a, index_b, skip_type))
+
+    if len(valid_connection) < 1:
+        return graph
+    # n_skip_connection = randint(1, len(valid_connection))
+    # for index_a, index_b, skip_type in sample(valid_connection, n_skip_connection):
+    for index_a, index_b, skip_type in sample(valid_connection, 1):
+        a_id = weighted_layer_ids[index_a]
+        b_id = weighted_layer_ids[index_b]
+        if skip_type == NetworkDescriptor.ADD_CONNECT:
+            graph.to_add_skip_model(a_id, b_id)
+        else:
+            graph.to_concat_skip_model(a_id, b_id)
+    return graph
+
+
+def create_new_layer(input_shape, n_dim):
+    dense_deeper_classes = [StubDense, get_dropout_class(n_dim), StubReLU]
+    conv_deeper_classes = [get_conv_class(n_dim), get_batch_norm_class(n_dim), StubReLU]
+    if len(input_shape) == 1:
+        # It is in the dense layer part.
+        layer_class = sample(dense_deeper_classes, 1)[0]
+    else:
+        # It is in the conv layer part.
+        layer_class = sample(conv_deeper_classes, 1)[0]
+
+    if layer_class == StubDense:
+        new_layer = StubDense(input_shape[0], input_shape[0])
+
+    elif layer_class == get_dropout_class(n_dim):
+        new_layer = layer_class(Constant.DENSE_DROPOUT_RATE)
+
+    elif layer_class == get_conv_class(n_dim):
+        new_layer = layer_class(input_shape[-1], input_shape[-1], sample((1, 3, 5), 1)[0], stride=1)
+
+    elif layer_class == get_batch_norm_class(n_dim):
+        new_layer = layer_class(input_shape[-1])
+
+    elif layer_class == get_pooling_class(n_dim):
+        new_layer = layer_class(sample((1, 3, 5), 1)[0])
+
+    else:
+        new_layer = layer_class()
+
+    return new_layer
+
+
+def to_deeper_graph(graph):
+    weighted_layer_ids = graph.deep_layer_ids()
+    if len(weighted_layer_ids) >= Constant.MAX_LAYERS:
+        return None
+
+    deeper_layer_ids = sample(weighted_layer_ids, 1)
+
+    for layer_id in deeper_layer_ids:
+        layer = graph.layer_list[layer_id]
+        new_layer = create_new_layer(layer.output.shape, graph.n_dim)
+        graph.to_deeper_model(layer_id, new_layer)
+    return graph
+
+
+def transform(graph):
+    graphs = []
+    for i in range(Constant.N_NEIGHBOURS * 2):
+        a = randrange(3)
+        temp_graph = None
+        if a == 0:
+            temp_graph = to_deeper_graph(deepcopy(graph))
+        elif a == 1:
+            temp_graph = to_wider_graph(deepcopy(graph))
+        elif a == 2:
+            temp_graph = to_skip_connection_graph(deepcopy(graph))
+
+        if temp_graph is not None and temp_graph.size() <= Constant.MAX_MODEL_SIZE:
+            graphs.append(temp_graph)
+
+        if len(graphs) >= Constant.N_NEIGHBOURS:
+            break
+
+    return graphs
