69713acb551db8e8839faf415859a8b5395178ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:29 +0000

    Add package jupyter-enterprise-gateway/1.1.1/jupyter_enterprise_gateway-1.1.1.tar.gz

---
+++ packages/jupyter-enterprise-gateway/jupyter_enterprise_gateway-1.1.1.tar.gz/jupyter_enterprise_gateway-1.1.1/enterprise_gateway/_version.py
@@ -0,0 +1,13 @@
+"""kernel_gateway version info"""
+
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+version_info = (
+    1,
+    1,
+    1
+
+)
+
+__version__ = '.'.join(map(str, version_info))
