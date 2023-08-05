944530c895bf3dce2afff34b795c3db222056e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:00 +0000

    Add package protosync/0.0.3/protosync-0.0.3.tar.gz

---
+++ packages/protosync/protosync-0.0.3.tar.gz/protosync-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="protosync",
+    version="0.0.3",
+    author="Edward Liu",
+    author_email="edwardliu573@gmail.com",
+    description="Sync code with a remote server in real time",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yl573/protosync",
+    entry_points={
+        'console_scripts': ['protosync=protosync:main'],
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
