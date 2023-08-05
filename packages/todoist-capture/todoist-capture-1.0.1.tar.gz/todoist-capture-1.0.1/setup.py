9423e0979ca07bafbf51c9dca708aee5966f84f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:59 +0000

    Add package todoist-capture/1.0.1/todoist-capture-1.0.1.tar.gz

---
+++ packages/todoist-capture/todoist-capture-1.0.1.tar.gz/todoist-capture-1.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read()
+
+setup(
+    name='todoist-capture',
+    version='1.0.1',
+    description='Capture tasks to your Inbox from command line',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Ege Güneş',
+    author_email='egegunes@gmail.com',
+    url='https://github.com/egegunes/todoist-capture',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': ['capture=capture.capture:run']
+    }
+)
