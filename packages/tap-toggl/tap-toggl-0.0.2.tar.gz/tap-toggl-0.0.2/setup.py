f5a2b6c905bfa72cec5c280d984c4e0d20facbfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:21 +0000

    Add package tap-toggl/0.0.2/tap-toggl-0.0.2.tar.gz

---
+++ packages/tap-toggl/tap-toggl-0.0.2.tar.gz/tap-toggl-0.0.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tap-toggl',
+      version='0.0.2',
+      description='Singer.io tap for extracting data from the Toggl API',
+      author='lambtron',
+      author_email="andyjiang@gmail.com",
+      url='https://andyjiang.com',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_toggl'],
+      install_requires=[
+          'singer-python==5.1.5',
+          'requests==2.20.0'
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-toggl=tap_toggl:main
+      ''',
+      packages=['tap_toggl'],
+      package_data = {
+          "schemas": ["tap_toggl/schemas/*.json"]
+      },
+      include_package_data=True,
+)
