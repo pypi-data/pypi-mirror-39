76554da35687b02076559510726b5e889c5cdccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:04 +0000

    Add package thinc/7.0.0.dev4/thinc-7.0.0.dev4.tar.gz

---
+++ packages/thinc/thinc-7.0.0.dev4.tar.gz/thinc-7.0.0.dev4/thinc/compat.py
@@ -0,0 +1,11 @@
+try:
+    basestring = basestring
+except NameError:
+    basestring = str
+
+try:
+    from StringIO import StringIO as BytesIO
+except ImportError:
+    from io import BytesIO
+
+from pathlib import Path
