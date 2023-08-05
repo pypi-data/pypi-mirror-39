8d37ffcf8d3c26fe9d65f1d510b661e5635e6374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:39:52 +0000

    Add package djangocms-link/2.2.2/djangocms-link-2.2.2.tar.gz

---
+++ packages/djangocms-link/djangocms-link-2.2.2.tar.gz/djangocms-link-2.2.2/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+from djangocms_link import __version__
+
+
+REQUIREMENTS = [
+    'django-cms>=3.4.5',
+    'djangocms-attributes-field>=0.4.0',
+]
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+
+setup(
+    name='djangocms-link',
+    version=__version__,
+    description='Adds a link plugin to django CMS',
+    author='Divio AG',
+    author_email='info@divio.ch',
+    url='https://github.com/divio/djangocms-link',
+    license='BSD',
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite='tests.settings.run',
+)
