0185200071c7c1ba8884662a250792411963311c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:33 +0000

    Add package sa-insert-helper/0.0.1/sa-insert-helper-0.0.1.tar.gz

---
+++ packages/sa-insert-helper/sa-insert-helper-0.0.1.tar.gz/sa-insert-helper-0.0.1/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+        name='sa-insert-helper',
+        version='0.0.1',
+        keywords='bulk insert',
+        description='a library for bulk insert',
+        long_description=open('README.md').read(),
+        license='MIT License',
+        url='https://github.com/sunurdy/sa_insert_helper',
+        author='sunurdy',
+        author_email='sunurdy@hotmail.com',
+        packages=find_packages()
+)
