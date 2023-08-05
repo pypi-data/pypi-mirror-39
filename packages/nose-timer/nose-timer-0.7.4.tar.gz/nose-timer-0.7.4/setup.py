f9019a21b09fc81d805f6267afa51aa6b70d03e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:47 +0000

    Add package nose-timer/0.7.4/nose-timer-0.7.4.tar.gz

---
+++ packages/nose-timer/nose-timer-0.7.4.tar.gz/nose-timer-0.7.4/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='nose-timer',
+    version='0.7.4',
+    description='A timer plugin for nosetests',
+    long_description=open('README.rst').read(),
+    author=', '.join([
+        'Alister Cordiner',
+        'Andres Riancho',
+        'Anton Egorov',
+        'Arthur Kulik',
+        'Corey Goldberg',
+        'Dmitry Sandalov',
+        'Harald Nordgren',
+        'Ivan Kolodyazhny',
+        'Juan Pedro Fisanotti',
+        'Kevin Burke',
+        'Mahmoud Abdelkader',
+        'Raoul Snyman',
+        'Stanislav Kudriashev',
+    ]),
+    url='https://github.com/mahmoudimus/nose-timer',
+    packages=['nosetimer', ],
+    install_requires=[
+        'nose',
+    ],
+    license='MIT',
+    entry_points={
+        'nose.plugins.0.10': [
+            'nosetimer = nosetimer.plugin:TimerPlugin',
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Testing',
+        'Environment :: Console',
+    ],
+)
