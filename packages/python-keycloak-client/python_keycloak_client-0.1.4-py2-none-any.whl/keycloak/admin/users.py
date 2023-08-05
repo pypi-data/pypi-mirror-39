f1ed608c64d4e57f3934b7831f22ecfe2d2a8923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:12 +0000

    Add package python-keycloak-client/0.1.4/python_keycloak_client-0.1.4-py2-none-any.whl

---
+++ packages/python-keycloak-client/python_keycloak_client-0.1.4-py2-none-any.whl/keycloak/admin/users.py
@@ -0,0 +1,55 @@
+import json
+
+from collections import OrderedDict
+
+from keycloak.admin import KeycloakAdminBase
+
+
+class Users(KeycloakAdminBase):
+
+    _paths = {
+        'collection': '/auth/admin/realms/{realm}/users'
+    }
+
+    _realm_name = None
+
+    def __init__(self, realm_name, *args, **kwargs):
+        self._realm_name = realm_name
+        super(Users, self).__init__(*args, **kwargs)
+
+    def create(self, username, **kwargs):
+        """
+        Create a user in Keycloak
+
+        http://www.keycloak.org/docs-api/3.4/rest-api/index.html#_users_resource
+
+        :param str username:
+        :param object credentials: (optional)
+        :param str first_name: (optional)
+        :param str last_name: (optional)
+        :param str email: (optional)
+        :param boolean enabled: (optional)
+        """
+        payload = OrderedDict(username=username)
+
+        if 'credentials' in kwargs:
+            payload['credentials'] = [kwargs['credentials']]
+
+        if 'first_name' in kwargs:
+            payload['firstName'] = kwargs['first_name']
+
+        if 'last_name' in kwargs:
+            payload['lastName'] = kwargs['last_name']
+
+        if 'email' in kwargs:
+            payload['email'] = kwargs['email']
+
+        if 'enabled' in kwargs:
+            payload['enabled'] = kwargs['enabled']
+
+        self._client.post(
+            url=self._client.get_full_url(
+                self.get_path('collection', realm=self._realm_name)
+            ),
+            data=json.dumps(payload)
+        )
