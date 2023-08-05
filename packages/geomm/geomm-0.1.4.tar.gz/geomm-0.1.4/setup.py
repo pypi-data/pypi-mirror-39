04def4c1e4ec94eb29fbdc8321582f22f88d5e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:57 +0000

    Add package geomm/0.1.4/geomm-0.1.4.tar.gz

---
+++ packages/geomm/geomm-0.1.4.tar.gz/geomm-0.1.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+
+import numpy as np
+
+setup(
+    name='geomm',
+    version='0.1.4',
+    py_modules=['geomm'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+    ],
+    include_dirs=[np.get_include()],
+    ext_modules = cythonize("geomm/pyqcprot.pyx")
+)
