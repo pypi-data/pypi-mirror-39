b2a84dd3085760bce27352cabf173fe6755c3b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:03 +0000

    Add package sixel/0.0.1/sixel-0.0.1.tar.gz

---
+++ packages/sixel/sixel-0.0.1.tar.gz/sixel-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='sixel',
+	version='0.0.1',
+	author='mat',
+	author_email='fake@email.lol',
+	description='Draw pixels and stuff yay',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://repl.it/@mat1/Sixel',
+	packages=setuptools.find_packages(),
+	install_requires='Pillow',
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+	],
+)
