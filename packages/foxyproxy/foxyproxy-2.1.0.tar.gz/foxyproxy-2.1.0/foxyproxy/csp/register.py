547d20286d7ddd573eb2ee940b3e9c0cf214c0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:47 +0000

    Add package foxyproxy/2.1.0/foxyproxy-2.1.0.tar.gz

---
+++ packages/foxyproxy/foxyproxy-2.1.0.tar.gz/foxyproxy-2.1.0/foxyproxy/csp/register.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+***
+Module:
+***
+
+ Copyright (C) Smart Arcs Ltd, registered in the United Kingdom.
+ This file is owned exclusively by Smart Arcs Ltd.
+ Unauthorized copying of this file, via any medium is strictly prohibited
+ Proprietary and confidential
+ Written by Smart Arcs <support@smartarchitects.co.uk>, May 2018
+"""
+__author__ = "Smart Arcs"
+__copyright__ = 'Smart Arcs Ltd'
+__email__ = 'support@smartarchitects.co.uk'
+__status__ = 'Development'
+
+from ica import CryptoICA
+from pgp import CryptoPGP
+from postsignum import CryptoPostSignum
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Register(object):
+
+    @staticmethod
+    def get_service(cmd_params=None):
+        if cmd_params['signer'].lower() == 'ica':
+            return CryptoICA(cmd_params)
+        elif cmd_params['signer'].lower() == 'pgp':
+            return CryptoPGP(cmd_params)
+        elif cmd_params['signer'].lower() == 'postsignum':
+            return CryptoPostSignum(cmd_params)
+        else:
+            logger.error("Unknown signing service requested: %s" % cmd_params['signer'])
+            exit(3)
+
