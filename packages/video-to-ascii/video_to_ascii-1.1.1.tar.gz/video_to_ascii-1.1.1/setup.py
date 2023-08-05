1112e02eea25dffa0241116ced06e4261570b78e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:45 +0000

    Add package video-to-ascii/1.1.1/video_to_ascii-1.1.1.tar.gz

---
+++ packages/video-to-ascii/video_to_ascii-1.1.1.tar.gz/video_to_ascii-1.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="video_to_ascii",
+    version="1.1.1",
+    author="Joel Ibaceta",
+    author_email="mail@joelibaceta.com",
+    description="A simple tool to play a video using ascii characters",
+    url="https://github.com/joelibaceta/video-to-ascii",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['bin/video-to-ascii'],
+)
