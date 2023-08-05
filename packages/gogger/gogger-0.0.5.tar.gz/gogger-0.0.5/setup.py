80e1c5c6fc14b71d5ba6b8fb23c1f45e71df6954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:13:38 +0000

    Add package gogger/0.0.5/gogger-0.0.5.tar.gz

---
+++ packages/gogger/gogger-0.0.5.tar.gz/gogger-0.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+	name='gogger',
+	version='0.0.5',
+	packages=['gogger'],
+	url='',
+	license='',
+	author='peixiaohan',
+	author_email='xiaohan@bigonelab.com',
+	description='Logger creator'
+	)
