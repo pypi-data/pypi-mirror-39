47cbca2bd906f9f803d036bd82674ea2e9264028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:53 +0000

    Add package insteonplm/0.15.2/insteonplm-0.15.2.tar.gz

---
+++ packages/insteonplm/insteonplm-0.15.2.tar.gz/insteonplm-0.15.2/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Setup for insteonplm module."""
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    print('Skipping md->rst conversion for long_description')
+    long_description = 'Error converting Markdown from git repo'
+
+if len(long_description) < 100:
+    print("\n***\n***\nWARNING: %s\n***\n***\n" % long_description)
+
+setup(
+    name='insteonplm',
+    version='0.15.2',
+    author='David McNett',
+    author_email='nugget@macnugget.org',
+    url='https://github.com/nugget/python-insteonplm',
+    license="LICENSE",
+    packages=find_packages(),
+    scripts=[],
+    description='Python API for controlling Insteon PowerLinc Modems',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'pyserial==3.2.0',
+        'pyserial-asyncio',
+        'async_timeout',
+        'aiohttp'
+    ],
+    entry_points={
+        'console_scripts': ['insteonplm_monitor = insteonplm.tools:monitor',
+                            'insteonplm_interactive = '
+                            'insteonplm.tools:interactive']
+    }
+)
