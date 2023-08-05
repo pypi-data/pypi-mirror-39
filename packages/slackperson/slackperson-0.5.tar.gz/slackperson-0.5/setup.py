bb9c06f701ba8c0da408d88b4d0f4b79f007f70a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:40 +0000

    Add package slackperson/0.5/slackperson-0.5.tar.gz

---
+++ packages/slackperson/slackperson-0.5.tar.gz/slackperson-0.5/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+modules = \
+['slackperson']
+install_requires = \
+['attrs>=18.2,<19.0', 'nameparser>=1.0,<2.0']
+
+setup_kwargs = {
+    'name': 'slackperson',
+    'version': '0.5',
+    'description': 'Simple class to store slack profile information',
+    'long_description': None,
+    'author': 'Rick Henry',
+    'author_email': 'fredericmhenry@gmail.com',
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.4,<4.0',
+}
+
+
+setup(**setup_kwargs)
