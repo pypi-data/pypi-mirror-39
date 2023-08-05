aafc3b9a4dbfa83c90428b81aea78e1ab4fe0f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:17 +0000

    Add package slack-entities/0.0.25/slack_entities-0.0.25.tar.gz

---
+++ packages/slack-entities/slack_entities-0.0.25.tar.gz/slack_entities-0.0.25/slack_entities/entities/outcoming_message.py
@@ -0,0 +1,34 @@
+from slack_entities.client.client import get_client
+from .channel import Channel
+
+
+class OutcomingMessage:
+    """
+    Describes message sent to Slack
+    """
+    def __init__(self, channel: Channel, attachments: list=None, text: str='', token: str=None):
+        self.channel = channel
+        self.token = token
+        self.text = text
+        self.attachments = attachments if attachments else []
+
+    def send(self):
+        return get_client(token=self.token).api_call(
+            'chat.postMessage',
+            channel=self.channel.id,
+            text=self.text,
+            attachments=self.attachments
+        )
+
+    def update(self, ts):
+        """
+        Updates the message with the timestamp
+        :param ts: Timestamp of the message to update
+        """
+        return get_client(token=self.token).api_call(
+            'chat.update',
+            channel=self.channel.id,
+            ts=ts,
+            text=self.text,
+            attachments=self.attachments
+        )
