c86d52b8a3201e37a42848fa27b75cc83845c83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:08 +0000

    Add package PyNewHope/0.3/PyNewHope-0.3.tar.gz

---
+++ packages/PyNewHope/PyNewHope-0.3.tar.gz/PyNewHope-0.3/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+
+from setuptools import setup
+
+
+def read_(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
+setup(
+    name="PyNewHope",
+    version="0.3",
+    packages=["pynewhope"],
+    author="Scott Wyman Neagle, Svetlin Nakov",
+    description=(
+        "An experimental implementation of the NewHope post-quantum key exchange algorithm"),
+    license="MIT",
+    keywords=["NewHope", "PQC", "post-quantum-cryptography", "key-exchange", "cryptography"],
+    url="https://github.com/nakov/PyNewHope",
+    download_url = "https://github.com/nakov/PyNewHope",
+    long_description=read_("README.md")
+)
