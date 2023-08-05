82886718f9cbb6b5783f8ec6812dafdae45499e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package SimpleDicomToolkit/1.1/SimpleDicomToolkit-1.1.tar.gz

---
+++ packages/SimpleDicomToolkit/SimpleDicomToolkit-1.1.tar.gz/SimpleDicomToolkit-1.1/SimpleDicomToolkit/dicom_tags.py
@@ -0,0 +1,104 @@
+# =============================================================================
+#  Patient Level
+# =============================================================================
+PATIENTNAME             = 'PatientName'
+PATIENTID               = 'PatientID'
+PATIENTBIRTHDATE        = 'PatientBirthDate'
+PATIENTSEX              = 'PatientSex'
+PATIENTWEIGHT           = 'PatientWeight'
+
+# =============================================================================
+# Study Level
+# =============================================================================
+STUDYDESCRIPTION        = 'StudyDescription'
+STUDYINSTANCEUID        = 'StudyInstanceUID'
+ACCESSIONNUMBER         = 'AccessionNumber'
+STUDYDATE               = 'StudyDate'
+STUDYTIME               = 'StudyTime'
+STUDYDATETIME           = 'StudyDateTime'
+
+# =============================================================================
+# Series Level
+# =============================================================================
+SERIESDESCRIPTION       = 'SeriesDescription'
+SERIESINSTANCEUID       = 'SeriesInstanceUID'
+SERIESDATE              = 'SeriesDate'
+SERIESTIME              = 'SeriesTime'
+SERIESDATETIME          = 'SeriesDateTime'
+MODALITY                = 'Modality'
+ACQUISITIONNUMBER       = 'AcquisitionNumber'
+
+# =============================================================================
+# Instance Level
+# =============================================================================
+SOPINSTANCEUID          = 'SOPInstanceUID'
+SOPCLASSUID             = 'SOPClassUID'
+INSTANCENUMBER          = 'InstanceNumber'
+
+# =============================================================================
+# Various Meta Data
+# =============================================================================
+SLICELOCATION                 = 'SliceLocation'
+ACQUISITIONTIME               = 'AcquisitionTime'
+ACQUISITIONDATE               = 'AcquisitionDate'
+ACQUISITIONDATETIME           = 'AcquisitionDateTime'
+REALWORLDVALUEMAPPINGSEQUENCE = 'RealWorldValueMappingSequence'
+RESCALEINTERCEPT              = 'RescaleIntercept'
+RESCALESLOPE                  = 'RescaleSlope'
+RADIOPHARMACEUTICALINFORMATIONSEQUENCE = 'RadiopharmaceuticalInformationSequence'
+
+
+PIXELSPACING            = 'PixelSpacing'
+IMAGEORIENTATIONPATIENT = 'ImageOrientationPatient'
+IMAGEPOSITIONPATIENT    = 'ImagePositionPatient'
+SLICETHICKNESS          = 'SliceThickness'
+
+COLUMNS = 'Columns'
+ROWS = 'Rows'
+BITSALLOCATED = 'BitsAllocated'
+BITSSTORED = 'BitsStored'
+HIGHBIT = 'HighBit'
+PIXELREPRESENTATION = 'PixelRepresentation'
+SAMPLESPERPIXEL = 'SamplesPerPixel'
+PHOTOMETRICINTERPRETATION = 'PhotoMetricInterpretation'
+
+FRAMEREFERENCETIME = 'FrameReferenceTime'
+ACQUISITIONDATE = 'AcquisitionDate'
+AcquisitionTime = 'AcquisitionTime'
+ACTUALFRAMEDURATION = 'ActualFrameDuration'
+DECAYFACTOR = 'DecayFactor'
+
+IMAGETYPE = 'ImageType'
+RESCALETYPE = 'RescaleType'
+KVP = 'KVP'
+ACQUISITIONNUMBER = 'AcquisitionNumber'
+
+MANUFACTURER = 'Manufacturer'
+
+FRAMEOFREFERENCEUID = 'FrameOfReferenceUID'
+POSITIONREFERENCEINDICATOR = 'PositionReferenceIndicator'
+
+SERIESNUMBER = 'SeriesNumber'
+PATIENTPOSITION = 'PatientPosition'
+
+STUDYINSTANCEUID = 'StudyInstanceUID'
+STUDYDATE = 'StudyDate'
+STUDYTIME = 'StudyTime'
+REFERRINGPHYSICIANNAME = 'ReferringPhysicianName'
+STUDYID = 'StudyID'
+ACCESSIONNUMBER = 'AccessionNumber'
+
+SERIESDATE = 'SeriesDate'
+SERIESTIME = 'SeriesTime'
+UNITS = 'Units'
+COUNTSSOURCE = 'CountsSource'
+SERIESTYPE = 'SeriesType'
+NUMBEROFSLICES = 'NumberOfSlices'
+CORRECTEDIMAGE = 'CorrectedImage'
+DECAYCORRECTION = 'DecayCorrection'
+COLLIMATORTYPE = 'CollimatorType'
+PixelData = 'PixelData'
+
+RADIONUCLIDECODESEQUENCE = 'RadioNuclideCodeSequence'
+
+CODESCHEMEDESIGNATOR = 'CodeSchemeDesignator'
