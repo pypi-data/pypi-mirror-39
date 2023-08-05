c68a0596e93d9026736541233ac9c256be88b6df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:51 +0000

    Add package mkit/1.4/mkit-1.4.tar.gz

---
+++ packages/mkit/mkit-1.4.tar.gz/mkit-1.4/mizlicai/product.py
@@ -0,0 +1,45 @@
+import datetime
+import time
+
+import requests
+import json
+
+from kit.dingding.notify import get_access_token, get_dept_member, get_user, send_text_to_users
+from mizlicai import new_product_url, unique_id, recommend, total_amount, sold_amount, rate, status, name, term
+from kit.db.db_helper import insert, query
+from mizlicai.model import Product
+
+
+def dd_notify(message):
+    token = get_access_token()
+    for user_id in get_dept_member(token):
+        # print(get_user(token, user_id))
+        send_text_to_users(token, [user_id], message)
+        return
+    pass
+
+
+def check_new_product(conn):
+    result = requests.get(new_product_url)
+    jsonObj = json.loads(result.text, encoding='utf-8')
+    products = jsonObj['normalProductOnsell']
+
+    for product in products:
+        lastProduct = Product()
+        lastProduct.id = product[unique_id]
+        lastProduct.recommend = product[recommend]
+        lastProduct.amount = int(product[total_amount]) - int(product[sold_amount])
+        lastProduct.rate = float(product[rate])
+        lastProduct.status = product[status]
+        lastProduct.name = product[name]
+        lastProduct.term = product[term]
+        has_notify = False
+        if query(lastProduct, conn) is None:
+            if insert(lastProduct, conn):
+                dd_notify(lastProduct.__str__())
+                has_notify = True
+        else:
+            print(lastProduct.__str__() + " already exist")
+        if not has_notify:
+            dd_notify(time.time())
+
