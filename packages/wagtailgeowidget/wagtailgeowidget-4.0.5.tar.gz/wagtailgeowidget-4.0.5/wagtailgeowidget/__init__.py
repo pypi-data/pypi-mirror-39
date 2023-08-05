f396fa86ee20d06f26bb936cd96d0e56aef25ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:28 +0000

    Add package wagtailgeowidget/4.0.5/wagtailgeowidget-4.0.5.tar.gz

---
+++ packages/wagtailgeowidget/wagtailgeowidget-4.0.5.tar.gz/wagtailgeowidget-4.0.5/wagtailgeowidget/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+wagtailgeowidget
+----------
+A Google Maps widget for Wagtail that supports both GeoDjango PointField, StreamField and the standard CharField.
+"""
+
+__title__ = "wagtailgeowidget"
+__version__ = "4.0.5"
+__build__ = 405
+__author__ = "Martin Sandström"
+__license__ = "MIT"
+__copyright__ = "Copyright 2015-2018 Fröjd Interactive"
