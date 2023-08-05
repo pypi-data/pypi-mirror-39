c52cbce12849a9802a8be82f7f8c9716b2d8dff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:58 +0000

    Add package bleak/0.2.3/bleak-0.2.3.tar.gz

---
+++ packages/bleak/bleak-0.2.3.tar.gz/bleak-0.2.3/HISTORY.rst
@@ -0,0 +1,32 @@
+=======
+History
+=======
+
+0.2.3 (2018-11-28)
+------------------
+
+* Fix for issue #51: ``dpkg-query not found on all Linux systems```
+
+0.2.2 (2018-11-08)
+------------------
+
+* Made it compliant with Python 3.5 by removing f-strings
+
+0.2.1 (2018-06-28)
+------------------
+
+* Improved logging on .NET discover method
+* Some type annotation fixes in .NET code
+
+0.2.0 (2018-04-26)
+------------------
+
+* Project added to Github
+* First version on PyPI.
+* Working Linux (BlueZ DBus API) backend.
+* Working Windows (UWP Bluetooth API) backend.
+
+0.1.0 (2017-10-23)
+------------------
+
+* Bleak created.
---
+++ packages/bleak/bleak-0.2.3.tar.gz/bleak-0.2.3/tests/test_imports.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `bleak` package."""
+
+import os
+import platform
+
+import pytest
+
+_IS_CI = os.environ.get('CI', "false").lower() == "true"
+_IS_TRAVIS = os.environ.get('TRAVIS', "false").lower() == "true"
+_IS_APPVEYOR = os.environ.get('APPVEYOR', "false").lower() == "true"
+
+
+@pytest.mark.skipif(
+    condition=_IS_CI and (_IS_TRAVIS or _IS_APPVEYOR),
+    reason="Cannot run on Travis CI (has BlueZ 4.101 <= 5.43) or on Appveyor (runs Windows < 10)."
+)
+def test_import():
+    """Test by importing the client and assert correct client by OS."""
+    if platform.system() == 'Linux':
+        from bleak import BleakClient
+        assert BleakClient.__name__ == 'BleakClientBlueZDBus'
+    elif platform.system() == 'Windows':
+        from bleak import BleakClient
+        assert BleakClient.__name__ == 'BleakClientDotNet'
+    elif platform.system() == 'Darwin':
+        from bleak import BleakClient
+        assert BleakClient.__name__ == 'BleakClientCoreBluetooth'
