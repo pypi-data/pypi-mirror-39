09259e78fb6d4b1cefb6b109fda362cf38c084b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:33 +0000

    Add package nesttest/1.0.0/nesttest-1.0.0.tar.gz

---
+++ packages/nesttest/nesttest-1.0.0.tar.gz/nesttest-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'nesttest',
+        version = '1.0.0',
+        py_modules =['nesttest'],
+        author = 'wangdongrong',
+        author_email = '52723058@qq.com',
+        url = 'https://upload.pypi.org/legacy/',
+        description = 'A simple printer of nest lists',
+    )
