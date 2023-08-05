7c76074872839cd258a247d239a3c6cb2ff2bfb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:31 +0000

    Add package vyper/0.1.0b5/vyper-0.1.0b5.tar.gz

---
+++ packages/vyper/vyper-0.1.0b5.tar.gz/vyper-0.1.0b5/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='vyper',
+    # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
+    version='0.1.0-beta.5',
+    description='Vyper Programming Language for Ethereum',
+    long_description_markdown_filename='README.md',
+    author='Vitalik Buterin',
+    author_email='',
+    url='https://github.com/ethereum/vyper',
+    license="MIT",
+    keywords='ethereum',
+    include_package_data=True,
+    packages=find_packages(exclude=('tests', 'docs')),
+    python_requires='>=3.6',
+    py_modules=['vyper'],
+    install_requires=[
+        'pycryptodome>=3.5.1,<4',
+    ],
+    setup_requires=[
+        'pytest-runner'
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'py-evm==0.2.0a32',
+        'eth-tester[py-evm]==0.1.0b32',
+        'web3==4.8.2',
+    ],
+    scripts=[
+        'bin/vyper',
+        'bin/vyper-serve',
+        'bin/vyper-lll'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
