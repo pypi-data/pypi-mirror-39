d5915fd64ff07eadfed15d109beaee0694109c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:00 +0000

    Add package sphinx-jekyll-builder/0.1.5/sphinx_jekyll_builder-0.1.5-py2.py3-none-any.whl

---
+++ packages/sphinx-jekyll-builder/sphinx_jekyll_builder-0.1.5-py2.py3-none-any.whl/sphinx_jekyll_builder/jekyll_writer.py
@@ -0,0 +1,35 @@
+from sphinx_markdown_builder.markdown_writer import MarkdownTranslator, MarkdownWriter
+from munch import munchify
+import yaml
+
+class JekyllTranslator(MarkdownTranslator):
+    visited_title = False
+    title = None
+
+    def visit_document(self, node):
+        self.title = getattr(self.builder, 'current_docname')
+        MarkdownTranslator.visit_document(self, node)
+
+    def depart_document(self, node):
+        variables = munchify({
+            'current_docname': getattr(self.builder, 'current_docname'),
+            'images': getattr(self.builder, 'images'),
+            'title': self.title,
+            'versioning_method': getattr(self.builder, 'versioning_method')
+        })
+        variables_yaml = yaml.safe_dump(variables)
+        frontmatter = '---\n' + variables_yaml + '---\n'
+        self.add(frontmatter, section='head')
+        MarkdownTranslator.depart_document(self, node)
+
+    def visit_title(self, node):
+        if not self.visited_title:
+            self.title = node.astext()
+        self.visited_title = True
+        MarkdownTranslator.visit_title(self, node)
+
+    def depart_title(self, node):
+        MarkdownTranslator.depart_title(self, node)
+
+class JekyllWriter(MarkdownWriter):
+    translator_class = JekyllTranslator
