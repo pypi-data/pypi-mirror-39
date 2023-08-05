3652d7aa23ec50dca764aa4b4b110745cd6d1b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:30 +0000

    Add package mbddns/0.1.2/mbddns-0.1.2.tar.gz

---
+++ packages/mbddns/mbddns-0.1.2.tar.gz/mbddns-0.1.2/mbddns/__init__.py
@@ -0,0 +1,34 @@
+import asyncio
+import aiohttp
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+async def update(domain, password, host, ttl=60, session=None):
+    data = {
+        'domain': domain,
+        'password': password,
+        'command': "REPLACE {} {} A DYNAMIC_IP".format(host, ttl)
+    }
+
+    async with session or aiohttp.ClientSession() as session:
+        try:
+            resp = await asyncio.gather(session.post('https://dnsapi4.mythic-beasts.com/',data=data))
+            body = await resp[0].text()
+
+            if body.startswith("REPLACE"):
+                _LOGGER.debug("Updating Mythic Beasts successful: %s", body)
+                return True
+
+            if body.startswith("ERR"):
+                _LOGGER.error("Updating Mythic Beasts failed: %s",
+                              body.partition(' ')[2])
+
+            if body.startswith("NREPLACE"):
+                _LOGGER.warning("Updating Mythic Beasts failed: %s",
+                                body.partition(';')[2])
+
+        except Exception as e:
+            _LOGGER.error("Updating Mythic Beasts failed: %s", e)
+
+        return False
