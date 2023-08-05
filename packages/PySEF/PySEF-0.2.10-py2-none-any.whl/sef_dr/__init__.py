071ab3800ba61fc507326bf0867520f24105316c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:13:21 +0000

    Add package PySEF/0.2.10/PySEF-0.2.10-py2-none-any.whl

---
+++ packages/PySEF/PySEF-0.2.10-py2-none-any.whl/sef_dr/__init__.py
@@ -0,0 +1,10 @@
+# License: MIT License https://github.com/passalis/sef/blob/master/LICENSE.txt
+
+from .targets import *
+
+from .linear import LinearSEF
+from .kernel import KernelSEF
+
+from sef_dr import classification, datasets
+
+from .version import __version__
