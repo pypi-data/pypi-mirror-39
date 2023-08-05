65032cebb7a4ebc53c9e7bda1feb8f67a4457ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:15 +0000

    Add package cleartest/0.904/cleartest-0.904.tar.gz

---
+++ packages/cleartest/cleartest-0.904.tar.gz/cleartest-0.904/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='cleartest',
+    version='0.904',
+    description='Lightweight test framework for Python 2.7',
+    long_description='Lightweight test framework for Python 2.7',
+    py_modules=['cleartest'],
+    author='Steve',
+    author_email='teamcleartest@gmail.com',
+    license='MIT',
+    scripts=['runtests'],
+    url='https://github.com/sbarba/cleartest'
+)
