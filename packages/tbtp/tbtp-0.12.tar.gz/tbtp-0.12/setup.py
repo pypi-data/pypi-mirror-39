af0254e43cb716ac4a9f3252be3c8118e1adcb31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:54 +0000

    Add package tbtp/0.12/tbtp-0.12.tar.gz

---
+++ packages/tbtp/tbtp-0.12.tar.gz/tbtp-0.12/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tbtp',
+      version='0.12',
+      description='Thinact BackTest Platform',
+      url='https://github.com/grantfang/tbtp',
+      author='Grant Fang',
+      author_email='grant.fang@thinact.com',
+      license='MIT',
+      packages=['tbtp'],
+      zip_safe=False)
