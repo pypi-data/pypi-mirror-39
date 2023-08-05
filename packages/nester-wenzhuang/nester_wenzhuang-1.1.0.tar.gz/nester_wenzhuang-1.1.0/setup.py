9e709142ac159f1218dc8365fb661779bb07a558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:28 +0000

    Add package nester-wenzhuang/1.1.0/nester_wenzhuang-1.1.0.tar.gz

---
+++ packages/nester-wenzhuang/nester_wenzhuang-1.1.0.tar.gz/nester_wenzhuang-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name           = 'nester_wenzhuang',
+	version        = '1.1.0',
+	py_modules     = ['nester_wenzhuang'],
+	author         = 'liuwenzhuang',
+	author_email   = 'liuwenzhuang@corp.netease.com',
+	url            = 'http://liuwenzhuang.github.io',
+	description    = 'A sample printer of nested list'
+    )
