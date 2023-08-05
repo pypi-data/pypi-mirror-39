cbe22df432331a36dc4f9c5bbd9b7c33140449a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:22 +0000

    Add package fnal-column-analysis-tools/0.0.1.dev1/fnal-column-analysis-tools-0.0.1.dev1.tar.gz

---
+++ packages/fnal-column-analysis-tools/fnal-column-analysis-tools-0.0.1.dev1.tar.gz/fnal-column-analysis-tools-0.0.1.dev1/fnal_column_analysis_tools/striped/ColumnGroup2JaggedTable.py
@@ -0,0 +1,13 @@
+from ..analysis_objects.JaggedDecoratedFourVector import JaggedWithLorentz, JaggedDecoratedFourVector
+from StripedColumnTransformer import PhysicalColumnGroup
+import awkward
+
+def jaggedFromColumnGroup(cgroup):
+    if isinstance(cgroup,PhysicalColumnGroup):
+        return JaggedDecoratedFourVector.fromcounts(counts = cgroup.counts(),
+                                                    p4 = cgroup.p4Column(),
+                                                    **cgroup.otherColumns())
+    else:
+        return awkward.JaggedArray.fromcounts(cgroup.counts(),
+                                              awkward.Table(cgroup.columns()))
+                                                    
