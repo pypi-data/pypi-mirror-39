7fec56c4b5ef520fa5c824d40049d70cbcc70d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:05 +0000

    Add package lp-backup/0.1.10/lp_backup-0.1.10.tar.gz

---
+++ packages/lp-backup/lp_backup-0.1.10.tar.gz/lp_backup-0.1.10/lp_backup/exceptions.py
@@ -0,0 +1,17 @@
+class LoginFailed(Exception):
+    pass
+
+
+class ConfigurationError(Exception):
+    pass
+
+
+class InvalidKey(Exception):
+    pass
+
+
+class BackupFailed(Exception):
+    pass
+
+class NoWebdav(Exception):
+    pass
