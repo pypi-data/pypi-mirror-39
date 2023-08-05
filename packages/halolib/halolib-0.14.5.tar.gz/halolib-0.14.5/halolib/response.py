c5a996c2bf0a37fb1feebf6c8e82d80ad04c36f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:54 +0000

    Add package halolib/0.14.5/halolib-0.14.5.tar.gz

---
+++ packages/halolib/halolib-0.14.5.tar.gz/halolib-0.14.5/halolib/response.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class HaloResponse:
+    payload = 'this is HaloResponse'
+    code = 200
+    headers = []
