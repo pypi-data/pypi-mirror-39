5198a65be70f0f5d039fd936c00624ec58f314c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:18 +0000

    Add package lightnovel-crawler/2.5.3/lightnovel_crawler-2.5.3-py3-none-any.whl

---
+++ packages/lightnovel-crawler/lightnovel_crawler-2.5.3-py3-none-any.whl/lightnovel_crawler/__init__.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Interactive value input"""
+import os
+import logging
+import requests
+from colorama import init as init_colorama
+from .app import start_app
+from .app.arguments import get_args, build_parser
+from .app.display import description, epilog, debug_mode
+from .assets.version import get_value as get_version
+
+from .lnmtl import LNMTLCrawler
+from .webnovel import WebnovelCrawler
+from .wuxia import WuxiaCrawler
+from .wuxiac import WuxiaCoCrawler
+from .wuxiaonline import WuxiaOnlineCrawler
+from .boxnovel import BoxNovelCrawler
+from .readln import ReadLightNovelCrawler
+from .novelplanet import NovelPlanetCrawler
+from .lnindo import LnindoCrawler
+from .idqidian import IdqidianCrawler
+from .utils.crawler import Crawler
+from .tests.crawler_app_test import run_tests
+
+crawler_list = {
+    'https://lnmtl.com/': LNMTLCrawler,
+    'https://www.webnovel.com/': WebnovelCrawler,
+    'https://wuxiaworld.online/': WuxiaOnlineCrawler,
+    'https://www.wuxiaworld.com/': WuxiaCrawler,
+    'https://www.wuxiaworld.co/': WuxiaCoCrawler,
+    'https://boxnovel.com/': BoxNovelCrawler,
+    'https://novelplanet.com/': NovelPlanetCrawler,
+    'https://www.readlightnovel.org/': ReadLightNovelCrawler,
+    'https://lnindo.org/': LnindoCrawler,
+    'https://www.idqidian.us/': IdqidianCrawler,
+}
+
+
+def main():
+    init_colorama()
+
+    os.environ['version'] = get_version()
+
+    description()
+    build_parser()
+
+    args = get_args()
+    if args.log:
+        os.environ['debug_mode'] = 'true'
+        levels = [None, logging.WARN, logging.INFO, logging.DEBUG]
+        logging.basicConfig(level=levels[args.log])
+        debug_mode(args.log)
+        print(args)
+    # end if
+    requests.urllib3.disable_warnings(
+        requests.urllib3.exceptions.InsecureRequestWarning)
+    # end if
+
+    try:
+        if args.test:
+            run_tests()
+        else:
+            start_app(crawler_list)
+        # end if
+    except Exception as err:
+        if args.log == 3:
+            raise err
+        # end if
+    # end try
+
+    epilog()
+# end def
+
+
+if __name__ == '__main__':
+    main()
+# end if
---
+++ packages/lightnovel-crawler/lightnovel_crawler-2.5.3-py3-none-any.whl/lightnovel_crawler/wuxia.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Crawler for [WuxiaWorld](http://www.wuxiaworld.com/).
+"""
+import json
+import logging
+import re
+from bs4 import BeautifulSoup
+from .utils.crawler import Crawler
+
+logger = logging.getLogger('WUXIA_WORLD')
+
+
+class WuxiaCrawler(Crawler):
+    @property
+    def supports_login(self):
+        '''Whether the crawler supports login() and logout method'''
+        return False
+    # end def
+
+    def login(self, email, password):
+        pass
+    # end def
+
+    def logout(self):
+        pass
+    # end def
+
+    def read_novel_info(self):
+        '''Get novel title, autor, cover etc'''
+        logger.debug('Visiting %s', self.novel_url)
+        response = self.get_response(self.novel_url)
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        self.novel_title = soup.select_one('.section-content  h4').text
+        logger.info('Novel title: %s', self.novel_title)
+
+        try:
+            self.novel_cover = self.absolute_url(
+                soup.select_one('img.media-object')['src'])
+            logger.info('Novel cover: %s', self.novel_cover)
+        except Exception as ex:
+            logger.debug('Failed to get cover: %s', ex)
+        # end try
+
+        self.novel_author = soup.select_one('.media-body dl dt').text
+        self.novel_author += soup.select_one('.media-body dl dd').text
+        logger.info('Novel author: %s', self.novel_author)
+
+        for panel in soup.select('#accordion .panel-default'):
+            vol_id = int(panel.select_one('h4.panel-title .book').text)
+            vol_title = panel.select_one('h4.panel-title .title a').text
+            self.volumes.append({
+                'id': vol_id,
+                'title': vol_title,
+            })
+            for a in panel.select('ul.list-chapters li.chapter-item a'):
+                chap_id = len(self.chapters) + 1
+                self.chapters.append({
+                    'id': chap_id,
+                    'volume': vol_id,
+                    'url': self.absolute_url(a['href']),
+                    'title': a.text.strip() or ('Chapter %d' % chap_id),
+                })
+            # end def
+        # end def
+
+        logger.debug(self.chapters)
+        logger.debug('%d chapters found', len(self.chapters))
+    # end def
+
+    def download_chapter_body(self, chapter):
+        '''Download body of a single chapter and return as clean html format.'''
+        logger.info('Downloading %s', chapter['url'])
+        response = self.get_response(chapter['url'])
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        self.blacklist_patterns = [
+            r'^<span>(...|\u2026)</span>$',
+            r'^translat(ed by|or)',
+            r'(volume|chapter) .?\d+',
+        ]
+        body_parts = soup.select_one('.panel-default .fr-view')
+        body = self.extract_contents(body_parts.contents)
+        return '<p>' + '</p><p>'.join(body) + '</p'
+    # end def
+# end class
