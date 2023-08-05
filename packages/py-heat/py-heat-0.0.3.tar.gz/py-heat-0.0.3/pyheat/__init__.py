626520dc990cebf2729d30e8a2d8c462318ca76a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:23 +0000

    Add package py-heat/0.0.3/py-heat-0.0.3.tar.gz

---
+++ packages/py-heat/py-heat-0.0.3.tar.gz/py-heat-0.0.3/pyheat/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+#                  __               __
+#     ____  __  __/ /_  ___  ____ _/ /_
+#    / __ \/ / / / __ \/ _ \/ __ `/ __/
+#   / /_/ / /_/ / / / /  __/ /_/ / /_
+#  / .___/\__, /_/ /_/\___/\__,_/\__/
+# /_/    /____/
+
+"""
+pyheat module
+~~~~~~~~~~~~~
+
+Usage of PyHeat class:
+
+    >>> from pyheat import PyHeat
+    >>> ph = PyHeat(<file_path>)
+    >>> ph.create_heatmap()
+    >>> ph.show_heatmap()
+
+:copyright: (c) 2017 by Vishwas B Sharma.
+:license: MIT, see LICENSE for more details.
+"""
+
+from .pyheat import PyHeat
