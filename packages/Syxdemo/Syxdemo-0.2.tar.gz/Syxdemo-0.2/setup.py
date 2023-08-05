967a1185a239c88e9c4c64591eeaeb0731a0ca5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:26 +0000

    Add package Syxdemo/0.2/Syxdemo-0.2.tar.gz

---
+++ packages/Syxdemo/Syxdemo-0.2.tar.gz/Syxdemo-0.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='Syxdemo',
+    version='0.2',
+    py_modules=['Syxdemo'],
+    author='Runtian',
+    author_email='runtian@amaresun.com',
+    long_description_content_type='量化模型工具',
+    url='https://github.com/RomanceCode/Syxdemo'
+    )
+
+
+
+
+
