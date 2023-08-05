8d540376f7d526772b3674bc7388e1d46c3b7fbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:38 +0000

    Add package happytrans/0.6/happytrans-0.6.tar.gz

---
+++ packages/happytrans/happytrans-0.6.tar.gz/happytrans-0.6/happytrans/translate_model.py
@@ -0,0 +1,14 @@
+import codecs,jieba
+from tqdm import tqdm
+from vanish_model_dir import van_model
+
+class Model:
+    def __init__(self):
+        self.use_replace_model(p2c='./data/p2c_happy.py',c2p='./data/c2p_happy.py')
+    def use_replace_model(self,p2c,c2p):
+        self.model = van_model.van_m(p2c=p2c,c2p=c2p)
+    def convert(self,sentence):
+        return self.model.convert(sentence)
+
+model = Model()
+print(model.convert('我是个杀手，我没有感情。'))
