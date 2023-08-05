f8eb74a708f65bcb6a2ef1897d0a13714280aaa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:37 +0000

    Add package xuzhao-markdown-editor/1.1.1/xuzhao-markdown-editor-1.1.1.tar.gz

---
+++ packages/xuzhao-markdown-editor/xuzhao-markdown-editor-1.1.1.tar.gz/xuzhao-markdown-editor-1.1.1/README.md
@@ -0,0 +1,5 @@
+# xuzhao-markdown-editor
+
+[![pypi-version](https://img.shields.io/pypi/v/pip.svg)](https://pypi.org/project/xuzhao-markdown-editor)
+[![Build Status](https://travis-ci.org/twtyjvkg/xuzhao-markdown-editor.svg?branch=master)](https://travis-ci.org/twtyjvkg/xuzhao-markdown-editor)
+[![Documentation Status](https://readthedocs.org/projects/xuzhao-markdown-editor/badge/?version=latest)](https://xuzhao-markdown-editor.readthedocs.io/en/latest/?badge=latest)
---
+++ packages/xuzhao-markdown-editor/xuzhao-markdown-editor-1.1.1.tar.gz/xuzhao-markdown-editor-1.1.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = "xuzhao"
+__email__ = "contact@xuzhao.xin"
+__file__ = "setup.py"
+__description__ = ""
+__created_time__ = "2018/11/30 16:39"
+
+import setuptools
+
+with open('README.md', 'r', encoding='utf8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='xuzhao-markdown-editor',
+                 version='1.1.1',
+                 description='xuzhao markdown editor',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 author='xuzhao',
+                 author_email='markdown@felinae.net',
+                 url='https://markdown.felinae.net',
+                 keywords='django markdown editor editormd',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False,
+                 install_requires=[],
+                 include_package_data=True,
+                 classifiers=(
+                     "Programming Language :: Python",
+                     "Development Status :: 4 - Beta",
+                     "Operating System :: OS Independent",
+                     "License :: OSI Approved :: Apache Software License",
+                     "Framework :: Django"
+                 ))
