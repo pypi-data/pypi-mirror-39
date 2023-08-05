7b230de558dcca2bb2a5baf3017c84c11b4a9e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:59 +0000

    Add package drfserializerbuilder/1.0.0/drfserializerbuilder-1.0.1-py3.6.egg

---
+++ packages/drfserializerbuilder/drfserializerbuilder-1.0.1-py3.6.egg/drf_serializer_builder/__init__.py
@@ -0,0 +1,14 @@
+__title__ = 'Django REST framework serializer builder'
+__version__ = '1.0.0'
+__author__ = 'Eugene Savchenko'
+__license__ = 'BSD 2-Clause'
+__copyright__ = 'Copyright 2018 Eugene Savchenko'
+
+# Version synonym
+VERSION = __version__
+
+# Header encoding (see RFC5987)
+HTTP_HEADER_ENCODING = 'iso-8859-1'
+
+# Default datetime input and output formats
+ISO_8601 = 'iso-8601'
