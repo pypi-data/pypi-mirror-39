957d8927c013f4fc139bceedf7525f39b18fd836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:09 +0000

    Add package redisGroupMsg/0.0.1/redisGroupMsg-0.0.1.tar.gz

---
+++ packages/redisGroupMsg/redisGroupMsg-0.0.1.tar.gz/redisGroupMsg-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(name='redisGroupMsg',
+      version='0.0.1',
+      description='This is a packet that broadcasts redis multiple queues',
+      url='https://github.com/zhenruyan/redisGroupMsg',
+      author='zhenruyan',
+      author_email='baiyangwangzhan@hotmail.com',
+      license='WTFPL',
+      packages=find_packages(),
+      zip_safe=False,
+      platforms=["all"],
+      long_description=open('README.md').read(),
+classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],install_requires=[
+        'redis==3.0.1'
+    ]
+      )
