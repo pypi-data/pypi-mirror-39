3132f040bb2bf8b47cf48103d0cdcf9b2f0a06b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:34 +0000

    Add package pantomime/0.3.2/pantomime-0.3.2.tar.gz

---
+++ packages/pantomime/pantomime-0.3.2.tar.gz/pantomime-0.3.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pantomime',
+    version='0.3.2',
+    description="MIME type normalisation and labels.",
+    long_description="",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='mime mimetypes file types',
+    author='Journalism Development Network, Inc.',
+    author_email='data@occrp.org',
+    url='http://github.com/alephdata/pantomime',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
+    namespace_packages=[],
+    package_data={},
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='nose.collector',
+    install_requires=[
+        'six >= 1.11.0',
+        'banal >= 0.3.5',
+        'normality >= 0.5.6',
+    ],
+    tests_require=['nose'],
+    entry_points={}
+)
