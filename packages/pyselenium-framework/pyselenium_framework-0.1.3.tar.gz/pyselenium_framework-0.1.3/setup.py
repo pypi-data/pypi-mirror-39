c7136436dea52e90528e4ac773fc9576b0aa6d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:45 +0000

    Add package pyselenium-framework/0.1.3/pyselenium_framework-0.1.3.tar.gz

---
+++ packages/pyselenium-framework/pyselenium_framework-0.1.3.tar.gz/pyselenium_framework-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+version = '0.1.3'
+
+with open('README.md') as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name='pyselenium_framework',
+    version=version,
+    packages=find_packages(exclude=['AUT', 'Execution'],
+                           include=['selenium_framework.driver_binary_files.*']),
+    author=u'Wally Yu',
+    install_requires=['selenium==3.14.1'],
+    url='https://github.com/wally-yu/selenium-framework',
+    include_package_data=True,
+    license='MIT License',
+    description='A Python Selenium Framework Which Makes Code More Easy to Maintain and Read',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+      )
+
+# build: python3 setup.py sdist bdist_wheel
