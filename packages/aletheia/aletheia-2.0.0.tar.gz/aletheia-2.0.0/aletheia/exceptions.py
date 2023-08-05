3a66c9a821763f82c62f3152591261cae02d8a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:25 +0000

    Add package aletheia/2.0.0/aletheia-2.0.0.tar.gz

---
+++ packages/aletheia/aletheia-2.0.0.tar.gz/aletheia-2.0.0/aletheia/exceptions.py
@@ -0,0 +1,22 @@
+class UnknownFileTypeError(Exception):
+    pass
+
+
+class PublicKeyNotExistsError(Exception):
+    pass
+
+
+class UnparseableFileError(Exception):
+    pass
+
+
+class DependencyMissingError(Exception):
+    pass
+
+
+class UnacceptableLocationError(Exception):
+    pass
+
+
+class UnrecognisedKey(Exception):
+    pass
