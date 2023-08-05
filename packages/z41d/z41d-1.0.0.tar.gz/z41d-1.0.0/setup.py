2eba584af60e16a9f138d5e93870f1c134f867b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:39 +0000

    Add package z41d/1.0.0/z41d-1.0.0.tar.gz

---
+++ packages/z41d/z41d-1.0.0.tar.gz/z41d-1.0.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+        name         = 'z41d',
+        version      = '1.0.0',
+        py_modules   = ['nester'],
+        author       = 'zaid',
+        author_email = 'shanaszaid@gmail.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+)
