2022d093a03795abb5271128d3ca5c7b68e9b4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:57 +0000

    Add package PyUpload/0.1.3/PyUpload-0.1.3.tar.gz

---
+++ packages/PyUpload/PyUpload-0.1.3.tar.gz/PyUpload-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyUpload',
+    version='0.1.3',
+    packages=find_packages(),
+    author='yukinotenshi',
+    author_email='gabriel.bentara@gmail.com',
+    license='MIT',
+    url='https://github.com/yukinotenshi/pyupload',
+    install_requires=[
+        'requests',
+        'requests-toolbelt',
+        'click'
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    entry_points={
+        'console_scripts': ['pyupload=pyupload.main:upload']
+    }
+)
