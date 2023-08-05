5f8cd8af856415f42fb2228b230bfcc69ac274f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:17 +0000

    Add package OpenVisus/1.2.121/OpenVisus-1.2.121-cp36-none-win_amd64.whl

---
+++ packages/OpenVisus/OpenVisus-1.2.121-cp36-none-win_amd64.whl/OpenVisus/visus.bat
@@ -0,0 +1,11 @@
+cd /d %~dp0
+
+if EXIST %cd%\win32\Python (set PYTHON_DIR=%cd%\win32\Python) else (set PYTHON_DIR=%cd%\..\..\..\) 
+%PYTHON_DIR%\python.exe -m pip install --user --upgrade numpy
+
+set PATH=%PYTHON_DIR%;%cd%\bin;%PATH%
+set PYTHONPATH=%PYTHON_DIR%\lib;%PYTHON_DIR%\DLLs
+
+
+
+bin\visus.exe %*
