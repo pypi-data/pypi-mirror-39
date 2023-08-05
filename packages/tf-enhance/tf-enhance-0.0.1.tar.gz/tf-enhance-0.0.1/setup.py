3661cd5c338d9a00a0ea79bd4df27497ec8ccbfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:49 +0000

    Add package tf-enhance/0.0.1/tf-enhance-0.0.1.tar.gz

---
+++ packages/tf-enhance/tf-enhance-0.0.1.tar.gz/tf-enhance-0.0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding=UTF-8 -*-
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__name__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_desc = f.read()
+
+setup(name='tf-enhance',
+      version='0.0.1',
+      description='An easy enhancement framework for TensorFlow.',
+      long_description=long_desc,
+      long_description_content_type='text/markdown',
+      url='https://github.com/LovelyLazyCat/tf-enhance',
+      author='LazyCat',
+      author_email='lazycat7703@gmail.com',
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Programming Language :: Python :: 3.6'],
+      keywords='TensorFlow',
+      packages=find_packages(exclude=['train', 'models', 'eval', 'app']),
+      install_requires=['numpy', 'tensorflow', 'matplotlib', 'flask'])
