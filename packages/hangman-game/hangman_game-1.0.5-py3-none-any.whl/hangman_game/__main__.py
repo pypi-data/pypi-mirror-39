7cad5868d04e469a75d72369cab6ed0da7d11d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:19 +0000

    Add package hangman-game/1.0.5/hangman_game-1.0.5-py3-none-any.whl

---
+++ packages/hangman-game/hangman_game-1.0.5-py3-none-any.whl/hangman_game/__main__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Imports
+from .engine import start_game
+from .helpers import print_message
+
+
+def main():
+    # Start the game
+    try:
+        start_game(True)
+    # Catch exceptions
+    except Exception as e:
+        print_message("An error has occurred during the game: %s" % e)
+    except KeyboardInterrupt:
+        print("")
+        print("\n    Game closed. See you later :)")
+        print("")
+        exit(0)
+
+
+# Call main
+main()
