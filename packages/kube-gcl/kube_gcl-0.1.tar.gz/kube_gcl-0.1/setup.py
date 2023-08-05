03f48ac3bd6af242e492f62063f59c4b63b5409e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:46 +0000

    Add package kube-gcl/0.1/kube_gcl-0.1.tar.gz

---
+++ packages/kube-gcl/kube_gcl-0.1.tar.gz/kube_gcl-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kube_gcl',
+      version='0.1',
+      description='Kube GCL lets you write simple easy configs to autogenerate kubernetes yaml configs.',
+      url='https://https://github.com/Hello1024/kube_gcl',
+      author='Oliver Mattos',
+      author_email='omattos@gmail.com',
+      license='MIT',
+      packages=['kube_gcl'],
+      zip_safe=False)
