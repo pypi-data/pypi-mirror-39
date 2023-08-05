d8638e19d87d04b8397f3fba32c05e3f2543d8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:39 +0000

    Add package etherscan/0.1.3/etherscan-0.1.3.tar.gz

---
+++ packages/etherscan/etherscan-0.1.3.tar.gz/etherscan-0.1.3/etherscan/etherscan.py
@@ -0,0 +1,105 @@
+# coding:utf-8
+import requests
+
+
+class Client():
+
+    def __init__(self,
+                 api_key: str,
+                 network=None,
+                 ):
+
+        # API URL
+        self._api_url = 'https://api.etherscan.io/api'
+
+        # API Key
+        self._api_key = api_key
+
+        # Network
+        if network:
+            if network not in ['ropsten', 'kovan', 'rinkeby']:
+                raise Exception('network could only be None(mainnet) /ropsten/kovan/rinkeby')
+
+            self._api_url = 'https://api-{network}.etherscan.io/api'.format(
+                network=network
+            )
+
+        # params
+        self._params = {
+            'apikey': self._api_key,
+        }
+
+    def _req(self):
+        r = requests.post(url=self._api_url, data=self._params).json()
+
+        if '1' == r['status']:
+            return r['result']
+        else:
+            # todo: handle errors
+            print(r)
+
+        return r
+
+    def _proxy_req(self):
+        self._params['module'] = 'proxy'
+
+        # get, json
+        r = requests.get(url=self._api_url, data=self._params).json()
+
+        # todo: handle exceptions
+
+        return r['result']
+
+    def get_eth_price(self):
+        self._params['module'] = 'stats'
+        self._params['action'] = 'ethprice'
+
+        r = self._req()
+
+        return {
+            'ethbtc': float(r['ethbtc']),
+            'ethbtc_timestamp': int(r['ethbtc_timestamp']),
+            'ethusd': float(r['ethusd']),
+            'ethusd_timestamp': int(r['ethbtc_timestamp']),
+        }
+
+    def get_eth_supply(self):
+        self._params['module'] = 'stats'
+        self._params['action'] = 'ethsupply'
+
+        return int(self._req())
+
+    def get_eth_balance(self, address: str):
+        self._params['module'] = 'account'
+        self._params['action'] = 'balance'
+        self._params['address'] = address
+
+        return int(self._req())
+
+    def get_eth_balances(self, addresses: list):
+        self._params['module'] = 'account'
+        self._params['action'] = 'balancemulti'
+        self._params['address'] = ','.join(addresses)
+
+        balances = {}
+        for row in self._req():
+            balances[row['account']] = int(row['balance'])
+
+        return balances
+
+    def get_gas_price(self):
+        self._params['action'] = 'eth_gasPrice'
+
+        return int(self._proxy_req(), 16)
+
+    def get_block_number(self):
+        self._params['action'] = 'eth_blockNumber'
+
+        return int(self._proxy_req(), 16)
+
+    def get_block_by_number(self, block_number):
+        self._params['action'] = 'eth_getBlockByNumber'
+        self._params['tag'] = hex(block_number)
+        self._params['boolean'] = True
+
+        return self._proxy_req()
