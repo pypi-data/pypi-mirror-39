970d4093d2ed9fbc9d52e6c02076f3abca629e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:49 +0000

    Add package splotch/0.2.3/splotch-0.2.3.tar.gz

---
+++ packages/splotch/splotch-0.2.3.tar.gz/splotch-0.2.3/setup.py
@@ -0,0 +1,26 @@
+import atexit
+from setuptools import setup
+from setuptools.command.install import install
+
+#def _post_install():
+#	import splot
+#	splot.copy_style()
+
+#class new_install(install):
+#	def __init__(self, *args, **kwargs):
+#		super(new_install, self).__init__(*args, **kwargs)
+#		atexit.register(_post_install)
+
+setup(name='splotch',
+		version='0.2.3',
+		description='Simple PLOTs, Contours and Histograms is a small package with wrapper functions designed to simplify plotting calls from matplotlib.',
+		url='https://github.com/MBravoS/splot',
+		author='Matías A. Bravo Santa Cruz',
+		author_email='matias.bravo@icrar.org',
+		license='BSD-3-Clause',
+		packages=['splotch'],
+		#cmdclass={'install':new_install},
+		#package_data={'splot/styles':['splot/styles/astro.mplstyle',]},
+		install_requires=['numpy>=1.10','matplotlib>=2.2.0','scipy>=1.0.0'],
+		zip_safe=False)
+
