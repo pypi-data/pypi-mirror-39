b245b5e4c4c2a18cb1974d6b7eb5a05926f7aa7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package stock-analysis-engine/1.3.40/stock_analysis_engine-1.3.40-py2.py3-none-any.whl

---
+++ packages/stock-analysis-engine/stock_analysis_engine-1.3.40-py2.py3-none-any.whl/analysis_engine/start_worker.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from celery import signals
+from spylunking.log.setup_logging import build_colorized_logger
+from analysis_engine.work_tasks.get_celery_app import get_celery_app
+from analysis_engine.consts import APP_NAME
+from analysis_engine.consts import LOG_CONFIG_PATH
+from analysis_engine.consts import WORKER_BROKER_URL
+from analysis_engine.consts import WORKER_BACKEND_URL
+from analysis_engine.consts import WORKER_TASKS
+from analysis_engine.consts import WORKER_CELERY_CONFIG_MODULE
+from analysis_engine.consts import INCLUDE_TASKS
+
+
+# Disable celery log hijacking
+# https://github.com/celery/celery/issues/2509
+@signals.setup_logging.connect
+def setup_celery_logging(**kwargs):
+    pass
+
+
+log = build_colorized_logger(
+    name=APP_NAME,
+    log_config_path=LOG_CONFIG_PATH)
+
+log.info(
+    'start - {}'.format(
+        APP_NAME))
+
+log.info(
+    'broker={} backend={} '
+    'config={} include_tasks={}'.format(
+        WORKER_BROKER_URL,
+        WORKER_BACKEND_URL,
+        WORKER_CELERY_CONFIG_MODULE,
+        WORKER_TASKS))
+
+# Get the Celery app from the project's get_celery_app module
+app = get_celery_app(
+    name=APP_NAME,
+    path_to_config_module=WORKER_CELERY_CONFIG_MODULE,
+    auth_url=WORKER_BROKER_URL,
+    backend_url=WORKER_BACKEND_URL,
+    include_tasks=INCLUDE_TASKS)
+
+log.info('starting celery')
+app.start()
+
+log.info(
+    'end - {}'.format(
+        APP_NAME))
---
+++ packages/stock-analysis-engine/stock_analysis_engine-1.3.40-py2.py3-none-any.whl/analysis_engine/utils.py
@@ -0,0 +1,107 @@
+"""
+Date utils
+"""
+
+import datetime
+from functools import lru_cache
+from analysis_engine.consts import COMMON_DATE_FORMAT
+from analysis_engine.consts import COMMON_TICK_DATE_FORMAT
+
+
+@lru_cache(1)
+def last_close():
+    """last close
+
+    Get Last Trading Close Date
+
+    .. note:: does not detect holidays and non-trading
+              days yet
+
+    last_close - get the last close trading date
+    """
+    today = datetime.date.today()
+    close = datetime.datetime(
+        year=today.year,
+        month=today.month,
+        day=today.day,
+        hour=16)
+
+    if today.weekday() == 5:
+        return close - datetime.timedelta(days=1)
+    elif today.weekday() == 6:
+        return close - datetime.timedelta(days=2)
+    else:
+        if datetime.datetime.now().hour < 16:
+            close -= datetime.timedelta(days=1)
+            if close.weekday() == 5:  # saturday
+                return close - datetime.timedelta(days=1)
+            elif close.weekday() == 6:  # sunday
+                return close - datetime.timedelta(days=2)
+            return close
+        return close
+    # if/else
+# end of last_close
+
+
+def get_last_close_str(
+        fmt=COMMON_DATE_FORMAT):
+    """get_last_close_str
+
+    Get the Last Trading Close Date as a string
+    with default formatting COMMON_DATE_FORMAT
+    (YYYY-MM-DD)
+
+    :param fmt: optional output format (default
+                COMMON_DATE_FORMAT)
+    """
+    return last_close().strftime(fmt)
+# end of get_last_close_str
+
+
+def utc_now_str(
+        fmt=COMMON_TICK_DATE_FORMAT):
+    """utc_now_str
+
+    Get the UTC now as a string
+    with default formatting COMMON_TICK_DATE_FORMAT
+    (YYYY-MM-DD HH:MM:SS)
+
+    :param fmt: optional output format (default
+                COMMON_TICK_DATE_FORMAT)
+    """
+    return datetime.datetime.utcnow().strftime(
+        fmt)
+# end of utc_now_str
+
+
+def utc_date_str(
+        fmt=COMMON_DATE_FORMAT):
+    """utc_date_str
+
+    Get the UTC date as a string
+    with default formatting ``COMMON_DATE_FORMAT``
+
+    :param fmt: optional output format (default
+                COMMON_DATE_FORMAT ``YYYY-MM-DD``)
+    """
+    return datetime.datetime.utcnow().strftime(
+        fmt)
+# end of utc_date_str
+
+
+def get_date_from_str(
+        date_str,
+        fmt=COMMON_TICK_DATE_FORMAT):
+    """get_date_from_str
+
+    Convert a date to a string where the
+    default date formatting is ``COMMON_TICK_DATE_FORMAT``
+
+    :param date_str: string date value with a format of ``fmt``
+    :param fmt: date format ``YYYY-MM-DD HH:MM:SS`` by default
+    """
+
+    return datetime.datetime.strptime(
+        date_str,
+        fmt)
+# end of get_date_from_str
