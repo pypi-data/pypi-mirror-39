6e4669ba9d6d8ce8b8f88b5cdf63c454dc1d2e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:01 +0000

    Add package qdpy/0.0.2/qdpy-0.0.2-py3-none-any.whl

---
+++ packages/qdpy/qdpy-0.0.2-py3-none-any.whl/qdpy/collections.py
@@ -0,0 +1,64 @@
+#    This file is part of qdpy.
+#
+#    qdpy is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 3 of
+#    the License, or (at your option) any later version.
+#
+#    qdpy is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with qdpy. If not, see <http://www.gnu.org/licenses/>.
+
+"""TODO"""
+
+
+# TODO : contains genome, features scores, other infos ?
+class Individual(object):
+    """TODO"""
+
+    # TODO
+    def __init__(self):
+        pass
+
+
+# TODO
+class Container(object):
+    """TODO"""
+
+    # TODO
+    def __init__(self):
+        pass
+
+    # TODO
+    def add(self, ind):
+        pass
+
+    # TODO
+    def novelty_score(self, ind, k):
+        pass
+
+    # TODO
+    def stddev(self, ind):
+        pass
+
+
+# TODO
+class Archive(Container):
+    """TODO"""
+    pass
+
+
+# TODO
+class Grid(Container):
+    """TODO"""
+    pass
+
+
+
+# MODELINE	"{{{1
+# vim:expandtab:softtabstop=4:shiftwidth=4:fileencoding=utf-8
+# vim:foldmethod=marker
