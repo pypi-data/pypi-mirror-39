0364a2646464548c32955f80742bfba54c38e01b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:26 +0000

    Add package smb-lib/0.0.11/smb-lib-0.0.11.tar.gz

---
+++ packages/smb-lib/smb-lib-0.0.11.tar.gz/smb-lib-0.0.11/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name="smb-lib",
+    version='0.0.11',
+    author = "Mohamed Chennouf",
+    author_email = "mohamed.chennouf@etu.unice.fr",
+    url = "https://github.com/clement-alexandre/TotemBionet",
+    description = "smbionet librairie python",
+    long_description = open("README.rst").read(),
+    install_requires = ["py4j"],
+    license="WTFPL",
+    include_package_data = True,
+    packages = find_packages(),
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    keywords="jupyter, computational systems biology",
+)
