c7cc25924ddea22440b4a924e171df6535e78c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:30 +0000

    Add package parselglossy/0.1.0/parselglossy-0.1.0.tar.gz

---
+++ packages/parselglossy/parselglossy-0.1.0.tar.gz/parselglossy-0.1.0/parselglossy/cli.py
@@ -0,0 +1,45 @@
+#
+# parselglossy -- Generic input parsing library, speaking in tongues
+# Copyright (C) 2018 Roberto Di Remigio, and contributors.
+#
+# This file is part of parselglossy.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# Roberto Di Remigio, and contributors. OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# For information on the complete list of contributors to the
+# parselglossy library, see: <http://parselglossy.readthedocs.io/>
+#
+
+# -*- coding: utf-8 -*-
+"""Console script for parselglossy."""
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for parselglossy."""
+    click.echo("Replace this message by putting your code into "
+               "parselglossy.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
