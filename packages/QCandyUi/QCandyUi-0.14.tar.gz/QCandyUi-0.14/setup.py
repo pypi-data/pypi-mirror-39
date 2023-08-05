ef7ff124d3aaed6de61d56db750e525d671c1d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:57 +0000

    Add package QCandyUi/0.14/QCandyUi-0.14.tar.gz

---
+++ packages/QCandyUi/QCandyUi-0.14.tar.gz/QCandyUi-0.14/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="QCandyUi",       
+    version="0.14",   
+    description="quick beautify your pyQt5 app",
+    keywords='python pyQt ui',
+    author='shuoGG',
+    author_email='328893769@qq.com',
+    url='https://github.com/shuoGG1239/QCandyUi',
+    packages=['QCandyUi'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires= [
+        'pywin32'
+    ]
+)
