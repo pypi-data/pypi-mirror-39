b39e18b7f8f97bb9fbbe5a5a88a5b3a82b5c22d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:12 +0000

    Add package Misskey.py/0.5.0/Misskey.py-0.5.0-py3-none-any.whl

---
+++ packages/Misskey.py/Misskey.py-0.5.0-py3-none-any.whl/Misskey/Exceptions.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+class MisskeyInitException(Exception):
+    pass
+
+class MisskeyArgumentException(Exception):
+    pass
+
+class MisskeyBadRequestException(Exception):
+    pass
+
+class MisskeyIsntAdminException(Exception):
+    pass
+
+class MisskeyPermissionException(Exception):
+    pass
+
+class MisskeyForbiddenException(Exception):
+    pass
+
+class MisskeyResponseException(Exception):
+    pass
+
+class MisskeyUnknownException(Exception):
+    pass
