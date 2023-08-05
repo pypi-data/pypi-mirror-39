f3575d9eb1125c69436fa4c168867f04b2a1b8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:20 +0000

    Add package pack64/2.0.1/pack64-2.0.1.tar.gz

---
+++ packages/pack64/pack64-2.0.1.tar.gz/pack64-2.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pack64',
+    version = '2.0.1',
+    maintainer='Luminoso Technologies, Inc.',
+    maintainer_email='info@luminoso.com',
+    url = 'http://github.com/LuminosoInsight/pack64',
+    license = 'MIT',
+    platforms = ['any'],
+    description = 'A library for representing floating point vectors in a compact, base64-like format',
+    py_modules=['pack64'],
+    install_requires=['numpy >= 1.9.0'],
+    tests_require=['pytest']
+)
