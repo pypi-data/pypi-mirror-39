664ec7e1470be1ebc2a137191e83c2bc3b53bf0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:30 +0000

    Add package nesterHFPbyPB/1.1.0/nesterHFPbyPB-1.1.0.tar.gz

---
+++ packages/nesterHFPbyPB/nesterHFPbyPB-1.1.0.tar.gz/nesterHFPbyPB-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name          = 'nesterHFPbyPB',
+        version       = '1.1.0',
+        py_modules    = ['nesterHFPbyPB'],
+        author        = 'atkirakosian',
+        author_email  = 'antranik.kirakosian@gmail.com',
+        url           = 'https://www.linkedin.com/in/antranik-tony-kirakosian/',
+        description   = 'A simple function for printing nested lists. First version uploaded was supposed to be 1.0.0, not 3.6.5',
+        )
