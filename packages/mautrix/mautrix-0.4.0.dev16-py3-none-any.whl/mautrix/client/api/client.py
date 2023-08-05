738ec9f9608eaa4a4afabd6a7502ec107e5ebe9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:40 +0000

    Add package mautrix/0.4.0.dev16/mautrix-0.4.0.dev16-py3-none-any.whl

---
+++ packages/mautrix/mautrix-0.4.0.dev16-py3-none-any.whl/mautrix/client/api/client.py
@@ -0,0 +1,15 @@
+from .authentication import ClientAuthenticationMethods
+from .filtering import FilteringMethods
+from .events import EventMethods
+from .rooms import RoomMethods
+from .user_data import UserDataMethods
+from .modules import ModuleMethods
+
+
+class ClientAPI(ClientAuthenticationMethods, FilteringMethods, EventMethods, RoomMethods,
+                UserDataMethods, ModuleMethods):
+    """
+    ClientAPI is a medium-level wrapper around the HTTPAPI that provides many easy-to-use
+    functions for accessing the client-server API.
+    """
+    pass
