100cbc105ccf2ba7a19ed4fe255573e29b1106b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:02 +0000

    Add package tetrationcli/0.0.1.dev20181129182554/tetrationcli-0.0.1.dev20181129182554.tar.gz

---
+++ packages/tetrationcli/tetrationcli-0.0.1.dev20181129182554.tar.gz/tetrationcli-0.0.1.dev20181129182554/tetrationcli/controllers/swiches.py
@@ -0,0 +1,33 @@
+from cement import Controller, ex
+import json
+
+class Switches(Controller):
+
+    class Meta:
+        label = 'switches'
+        stacked_type = 'nested'
+        # stacked_on = 'base'
+
+    @ex(help='list hardware sensors')
+    def list(self):
+        restclient = self.app.tetpyclient
+        response = restclient.get('/switches')
+        self.app.log.debug('command returned: %s' % response.status_code)
+        data = {
+            'results': json.loads(response.content.decode("utf-8"))
+        }
+        
+        self.app.log.debug('data returned: %s' % data)
+        self.app.render(data, 'switches_list.jinja2')
+
+    @ex(help='delete')
+    def delete(self):
+        self.app.log.error('FEATURE NOT IMPLEMENTED YET, OPEN A ISSUE')
+
+    # @ex(help='create')
+    # def create(self):
+    #     self.app.log.error('FEATURE NOT IMPLEMENTED YET, OPEN A ISSUE')
+
+    # @ex(help='update')
+    # def update(self):
+    #     self.app.log.error('FEATURE NOT IMPLEMENTED YET, OPEN A ISSUE')
