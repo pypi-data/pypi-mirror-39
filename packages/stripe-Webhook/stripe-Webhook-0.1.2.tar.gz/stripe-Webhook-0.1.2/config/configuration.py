43aa46fc9da32728fa7176a9435234363c8e10a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:28 +0000

    Add package stripe-Webhook/0.1.2/stripe-Webhook-0.1.2.tar.gz

---
+++ packages/stripe-Webhook/stripe-Webhook-0.1.2.tar.gz/stripe-Webhook-0.1.2/config/configuration.py
@@ -0,0 +1,7 @@
+class BaseConfig(object):
+
+    # the ngrok conf for get webhooks
+    NGROk_ADDR = 'localhost'
+    NGROK_PORT = '80'
+    WEBHOOK_SECRET = 'whsec_ockkw25Bge0wgoIkXHhGb6tYZYiNoLzl'
+    STRIPE_KEY_TEST = "sk_test_T7ugYRCXZwOezUViefmVlD2E"
