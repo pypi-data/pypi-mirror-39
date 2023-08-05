7fd2d83fe87f9ded6e88410954f9e9486f00920b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:59 +0000

    Add package cepton-sdk/1.9.2/cepton_sdk-1.9.2.tar.gz

---
+++ packages/cepton-sdk/cepton_sdk-1.9.2.tar.gz/cepton_sdk-1.9.2/setup.py
@@ -0,0 +1,45 @@
+import platform
+import sys
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="cepton_sdk",
+        version="1.9.2",
+        description="Cepton Python SDK",
+        long_description=open("README.md").read(),
+        url="https://github.com/ceptontech/cepton_sdk_redist",
+        author="Cepton Technologies",
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Programming Language :: Python :: 3",
+        ],
+        keywords="cepton sdk",
+        python_requires=">=3.3",
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        install_requires=[
+            "numpy",
+            "pyserial",
+        ],
+        extras_require={
+            "capture": [
+                "netifaces",
+            ],
+            "export": [
+                "laspy",
+                "uuid",
+                "plyfile",
+            ],
+            "plot": [
+                "pyqt5",
+                "vispy",
+            ],
+        },
+        scripts=[
+            "samples/cepton_export",
+            "samples/cepton_export_sora",
+            "samples/cepton_list_sensors",
+        ]
+    )
