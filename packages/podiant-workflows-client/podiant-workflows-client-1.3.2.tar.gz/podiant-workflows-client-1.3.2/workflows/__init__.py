724404d05999dd08a39ccf36076e0a6f4be870c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:40 +0000

    Add package podiant-workflows-client/1.3.2/podiant-workflows-client-1.3.2.tar.gz

---
+++ packages/podiant-workflows-client/podiant-workflows-client-1.3.2.tar.gz/podiant-workflows-client-1.3.2/workflows/__init__.py
@@ -0,0 +1,14 @@
+from logging.config import dictConfig
+from . import settings
+
+
+__version__ = '1.3.2'
+__all__ = [
+    'endpoint',
+    'operation',
+    'setup_logging',
+]
+
+
+def setup_logging():
+    dictConfig(settings.LOGGING)
