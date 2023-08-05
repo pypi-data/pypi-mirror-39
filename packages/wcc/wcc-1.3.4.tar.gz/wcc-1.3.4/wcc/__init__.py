c93644ed7978d02581245e18e515e3b0a72e3984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:50 +0000

    Add package wcc/1.3.4/wcc-1.3.4.tar.gz

---
+++ packages/wcc/wcc-1.3.4.tar.gz/wcc-1.3.4/wcc/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '1.3.4'
+#from . import models, exceptions
+from . import osskey
+# from . import wcc
+# from .wcc import Wcc
+#从本目录下api.py里import Service和Bucket这个类
+#from .api import Service, Bucket
+from .osskey import Osskey
+from .utils import *
+from .req   import getpage
+from .req   import get_cookie
