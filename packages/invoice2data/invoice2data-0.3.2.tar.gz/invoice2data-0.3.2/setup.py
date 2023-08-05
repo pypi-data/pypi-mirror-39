8a77918fdc4b1377e8333ee0616e2fdc1fba45a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:48 +0000

    Add package invoice2data/0.3.2/invoice2data-0.3.2.tar.gz

---
+++ packages/invoice2data/invoice2data-0.3.2.tar.gz/invoice2data-0.3.2/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from os import path
+import sys
+
+setup(
+    name='invoice2data',
+    version='0.3.2',
+    author='Manuel Riel',
+    author_email='github@snapdragon.cc',
+    url='https://github.com/m3nu/invoice2data',
+    description='Python parser to extract data from pdf invoice',
+    license="MIT",
+    long_description=open(path.join(path.dirname(__file__), 'README.rst')).read(),
+    package_data = {
+        'invoice2data.extract': [
+            'templates/com/*.yml',
+            'templates/de/*.yml',
+            'templates/es/*.yml',
+            'templates/fr/*.yml',
+            'templates/nl/*.yml',
+            'templates/ch/*.yml'],
+        'invoice2data.test': ['pdfs/*.pdf']
+        },
+    packages=find_packages(),
+    install_requires=[
+        r.strip() for r in open(
+            path.join(path.dirname(__file__), 'requirements.txt')
+                ).read().splitlines() if not r.startswith('#')
+        ],
+    zip_safe=False,
+    entry_points = {
+              'console_scripts': [
+                  'invoice2data = invoice2data.main:main',
+              ],
+          },
+)
