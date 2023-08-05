ada682a4f0b2bd4a83a3d5e70e2d271cd497e25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:06 +0000

    Add package digsandpaper/0.3.6/digsandpaper-0.3.6.tar.gz

---
+++ packages/digsandpaper/digsandpaper-0.3.6.tar.gz/digsandpaper-0.3.6/setup.py
@@ -0,0 +1,32 @@
+
+from distutils.core import setup
+from setuptools import find_packages
+import os
+
+
+ES_MAJOR_VERSION = int(os.environ.get("ES_MAJOR_VERSION", 2))
+ES_VERSION = 'elasticsearch>={}.0.0,<{}.0.0'.format(ES_MAJOR_VERSION,
+                                                    ES_MAJOR_VERSION + 1)
+ES_DSL_VERSION = 'elasticsearch-dsl>={}.0.0,<{}.0.0'.format(ES_MAJOR_VERSION,
+                                                            ES_MAJOR_VERSION + 1)
+
+with open('requirements.txt', 'r') as f:
+    install_requires = list()
+    for line in f:
+        re = line.strip()
+        if re:
+            install_requires.append(re)
+
+setup(
+    name='digsandpaper',
+    version='0.3.6',
+    description='digsandpaper',
+    author='Jason Slepicka',
+    author_email='jasonslepicka@gmail.com',
+    url='https://github.com/usc-isi-i2/dig-sandpaper',
+    download_url='https://github.com/usc-isi-i2/dig-sandpaper',
+    packages=find_packages(),
+    keywords=['ir', 'search'],
+    include_package_data=True,
+    install_requires=install_requires
+)
