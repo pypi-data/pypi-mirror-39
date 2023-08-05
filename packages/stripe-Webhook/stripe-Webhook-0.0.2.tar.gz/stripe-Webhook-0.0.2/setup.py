948e814656c4761e414d6eb3416f826fc58fe657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:47 +0000

    Add package stripe-Webhook/0.0.2/stripe-Webhook-0.0.2.tar.gz

---
+++ packages/stripe-Webhook/stripe-Webhook-0.0.2.tar.gz/stripe-Webhook-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stripe-Webhook",
+    version="0.0.2",
+    author="maximeV",
+    author_email="vermande.maxime@gmail.com",
+    description="a package for stripe webhook",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="https://gitlab.com/maximeV/nowhatstripet",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
