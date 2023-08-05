3ff97873fbcadd5f6f53955f17ef3a002b9729d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:43 +0000

    Add package grocker/6.5/grocker-6.5-py2.py3-none-any.whl

---
+++ packages/grocker/grocker-6.5-py2.py3-none-any.whl/grocker/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) Polyconseil SAS. All rights reserved.
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import pkg_resources
+
+__version__ = pkg_resources.get_distribution('grocker').version
+__copyright__ = '2015, Polyconseil'
