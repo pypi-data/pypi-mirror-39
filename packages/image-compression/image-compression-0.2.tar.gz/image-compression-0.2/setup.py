7c29de1b3107c5e977b20490164c5b09ccd23a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:19 +0000

    Add package image-compression/0.2/image-compression-0.2.tar.gz

---
+++ packages/image-compression/image-compression-0.2.tar.gz/image-compression-0.2/setup.py
@@ -0,0 +1,34 @@
+
+from distutils.core import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+  name = 'image-compression',        
+  packages = ['image-compression'],   
+  version = '0.2',     
+  license='MIT',        
+  description = 'compress image without losing quality',  
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Saurabh Gharat',                   
+  author_email = 'saurabhhgharat9@gmail.com',      
+  url = 'https://github.com/saurabhgharat',   
+  download_url = 'https://github.com/saurabhgharat/image-compression/archive/0.2.tar.gz',   
+  keywords = ['compression', 'image', 'python','django'],   
+  install_requires=[            
+          'pillow'
+          
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
