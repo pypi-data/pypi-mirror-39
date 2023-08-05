6a4fa254e5e7933807c1706df901c63f904f667e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:04 +0000

    Add package prayer-times-calculator/0.0.3/prayer_times_calculator-0.0.3.tar.gz

---
+++ packages/prayer-times-calculator/prayer_times_calculator-0.0.3.tar.gz/prayer_times_calculator-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='prayer_times_calculator',
+      version='0.0.3',
+      description='Prayer Times Calculator',
+      url='https://github.com/uchagani/prayer-times-calculator',
+      author='Umair Chagani',
+      author_email='umair.chagani@gmail.com',
+      license='MIT',
+      packages=['prayer_times_calculator'],
+      zip_safe = False)
