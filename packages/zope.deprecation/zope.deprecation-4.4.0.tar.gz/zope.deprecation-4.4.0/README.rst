890e549e9e38569e24b3e0c4531f3864eeee26b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:46 +0000

    Add package zope.deprecation/4.4.0/zope.deprecation-4.4.0.tar.gz

---
+++ packages/zope.deprecation/zope.deprecation-4.4.0.tar.gz/zope.deprecation-4.4.0/README.rst
@@ -0,0 +1,27 @@
+======================
+ ``zope.deprecation``
+======================
+
+.. image:: https://img.shields.io/pypi/v/zope.deprecation.svg
+        :target: https://pypi.python.org/pypi/zope.deprecation/
+        :alt: Latest release
+
+.. image:: https://img.shields.io/pypi/pyversions/zope.deprecation.svg
+        :target: https://pypi.org/project/zope.deprecation/
+        :alt: Supported Python versions
+
+.. image:: https://travis-ci.org/zopefoundation/zope.deprecation.svg?branch=master
+        :target: https://travis-ci.org/zopefoundation/zope.deprecation
+
+.. image:: https://coveralls.io/repos/github/zopefoundation/zope.deprecation/badge.svg?branch=master
+        :target: https://coveralls.io/github/zopefoundation/zope.deprecation?branch=master
+
+.. image:: https://readthedocs.org/projects/zopedeprecation/badge/?version=latest
+        :target: https://zopedeprecation.readthedocs.io/en/latest/
+        :alt: Documentation Status
+
+
+This package provides a simple function called ``deprecated(names, reason)``
+to mark deprecated modules, classes, functions, methods and properties.
+
+Please see https://zopedeprecation.readthedocs.io for the documentation.
---
+++ packages/zope.deprecation/zope.deprecation-4.4.0.tar.gz/zope.deprecation-4.4.0/setup.py
@@ -0,0 +1,73 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.deprecation package
+"""
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+setup(
+    name='zope.deprecation',
+    version='4.4.0',
+    url='https://github.com/zopefoundation/zope.deprecation',
+    license='ZPL 2.1',
+    description='Zope Deprecation Infrastructure',
+    author='Zope Corporation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=(
+        read('README.rst')
+        + '\n\n' +
+        read('CHANGES.rst')
+    ),
+    keywords=["deprecation", "deprecated", "warning"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Framework :: Zope :: 3",
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    namespace_packages=['zope',],
+    install_requires=[
+        'setuptools',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='zope.deprecation',
+    extras_require={
+        'docs': ['Sphinx'],
+        'test': [
+            'zope.testrunner',
+        ],
+    },
+)
