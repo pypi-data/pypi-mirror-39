7accad5fe1e9e7785d72c83f188a59d644d057df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:23 +0000

    Add package validate-it/0.3.8/validate-it-0.3.8.tar.gz

---
+++ packages/validate-it/validate-it-0.3.8.tar.gz/validate-it-0.3.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+version = "0.3.8"
+
+setup(
+    name="validate-it",
+    packages=find_packages(),
+    version=version,
+    description="Yet another schema validator",
+    long_description="Yet another schema validator",
+    author="Ruslan Roskoshnyj",
+    author_email="i.am.yarger@gmail.com",
+    url="https://github.com/ruslux/validate_it",
+    download_url="https://github.com/ruslux/validate_it/archive/{}.tar.gz".format(version),
+    keywords=["schema", "validator", "json"],
+    classifiers=[],
+    python_requires=">3.7.0",
+    platforms=["OS Independent"],
+    license="LICENSE.txt",
+    install_requires=[],
+    extras_require={
+        "tests": [
+            "pytest (==3.4.0)",
+            "coverage (==4.5)",
+            "pytest-cov (==2.5.1)",
+            "python-box (==3.2.0)",
+        ],
+        "docs": [
+            "sphinx >= 1.4",
+            "aiohttp_theme"
+        ]
+    }
+)
