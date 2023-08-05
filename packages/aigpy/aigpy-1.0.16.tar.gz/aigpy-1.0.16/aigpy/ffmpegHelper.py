69b0f430505244e09654138388c65702b146aeb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:56 +0000

    Add package aigpy/1.0.16/aigpy-1.0.16.tar.gz

---
+++ packages/aigpy/aigpy-1.0.16.tar.gz/aigpy-1.0.16/aigpy/ffmpegHelper.py
@@ -0,0 +1,31 @@
+import subprocess
+import os
+
+def mergerByM3u8(url, filepath, showshell=False):
+    try:
+        cmd = "ffmpeg -safe 0 -i " + url + " -c copy -bsf:a aac_adtstoasc \"" + filepath + "\""
+        res = subprocess.call(cmd, shell=showshell)
+        if res != 0:
+            return False
+        return True
+    except:
+        return False
+
+def mergerByFiles(srcfilepaths, filepath, showshell=False):
+    result = True
+    tmpfile = filepath + "TMP.txt"
+    try:
+        with open(tmpfile, 'w') as fd:
+            for item in srcfilepaths:
+                fd.write('file \'' + item + '\'')
+
+        cmd = "ffmpeg -f concat -safe 0 -i \"" + tmpfile + "\" -c copy \"" + filepath + "\""
+        res = subprocess.call(cmd, shell=showshell)
+        if res != 0:
+            result = False
+    except:
+        result = False
+
+    if os.access(tmpfile,0):
+        os.remove(tmpfile)
+    return result
