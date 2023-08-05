af68ac9f1c3ee8248b60b750cbee5ed8262c2b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:47 +0000

    Add package pytorchcv/0.0.22/pytorchcv-0.0.22.tar.gz

---
+++ packages/pytorchcv/pytorchcv-0.0.22.tar.gz/pytorchcv-0.0.22/pytorchcv/models/common.py
@@ -0,0 +1,277 @@
+"""
+    Common routines for models in PyTorch.
+"""
+
+__all__ = ['conv1x1', 'ChannelShuffle', 'ChannelShuffle2', 'SEBlock', 'DualPathSequential', 'Concurrent',
+           'ParametricSequential', 'ParametricConcurrent']
+
+import torch
+import torch.nn as nn
+
+
+def conv1x1(in_channels,
+            out_channels,
+            stride=1,
+            bias=False):
+    """
+    Convolution 1x1 layer.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    stride : int or tuple/list of 2 int, default 1
+        Strides of the convolution.
+    bias : bool, default False
+        Whether the layer uses a bias vector.
+    """
+    return nn.Conv2d(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=1,
+        stride=stride,
+        bias=bias)
+
+
+def channel_shuffle(x,
+                    groups):
+    """
+    Channel shuffle operation from 'ShuffleNet: An Extremely Efficient Convolutional Neural Network for Mobile Devices,'
+    https://arxiv.org/abs/1707.01083.
+
+    Parameters:
+    ----------
+    x : Tensor
+        Input tensor.
+    groups : int
+        Number of groups.
+
+    Returns
+    -------
+    Tensor
+        Resulted tensor.
+    """
+    batch, channels, height, width = x.size()
+    # assert (channels % groups == 0)
+    channels_per_group = channels // groups
+    x = x.view(batch, groups, channels_per_group, height, width)
+    x = torch.transpose(x, 1, 2).contiguous()
+    x = x.view(batch, channels, height, width)
+    return x
+
+
+class ChannelShuffle(nn.Module):
+    """
+    Channel shuffle layer. This is a wrapper over the same operation. It is designed to save the number of groups.
+
+    Parameters:
+    ----------
+    channels : int
+        Number of channels.
+    groups : int
+        Number of groups.
+    """
+    def __init__(self,
+                 channels,
+                 groups):
+        super(ChannelShuffle, self).__init__()
+        # assert (channels % groups == 0)
+        if channels % groups != 0:
+            raise ValueError('channels must be divisible by groups')
+        self.groups = groups
+
+    def forward(self, x):
+        return channel_shuffle(x, self.groups)
+
+
+def channel_shuffle2(x,
+                     groups):
+    """
+    Channel shuffle operation from 'ShuffleNet: An Extremely Efficient Convolutional Neural Network for Mobile Devices,'
+    https://arxiv.org/abs/1707.01083. The alternative version.
+
+    Parameters:
+    ----------
+    x : Tensor
+        Input tensor.
+    groups : int
+        Number of groups.
+
+    Returns
+    -------
+    Tensor
+        Resulted tensor.
+    """
+    batch, channels, height, width = x.size()
+    # assert (channels % groups == 0)
+    channels_per_group = channels // groups
+    x = x.view(batch, channels_per_group, groups, height, width)
+    x = torch.transpose(x, 1, 2).contiguous()
+    x = x.view(batch, channels, height, width)
+    return x
+
+
+class ChannelShuffle2(nn.Module):
+    """
+    Channel shuffle layer. This is a wrapper over the same operation. It is designed to save the number of groups.
+    The alternative version.
+
+    Parameters:
+    ----------
+    channels : int
+        Number of channels.
+    groups : int
+        Number of groups.
+    """
+    def __init__(self,
+                 channels,
+                 groups):
+        super(ChannelShuffle2, self).__init__()
+        # assert (channels % groups == 0)
+        if channels % groups != 0:
+            raise ValueError('channels must be divisible by groups')
+        self.groups = groups
+
+    def forward(self, x):
+        return channel_shuffle2(x, self.groups)
+
+
+class SEBlock(nn.Module):
+    """
+    Squeeze-and-Excitation block from 'Squeeze-and-Excitation Networks,' https://arxiv.org/abs/1709.01507.
+
+    Parameters:
+    ----------
+    channels : int
+        Number of channels.
+    reduction : int, default 16
+        Squeeze reduction value.
+    """
+    def __init__(self,
+                 channels,
+                 reduction=16):
+        super(SEBlock, self).__init__()
+        mid_cannels = channels // reduction
+
+        self.pool = nn.AdaptiveAvgPool2d(output_size=1)
+        self.conv1 = conv1x1(
+            in_channels=channels,
+            out_channels=mid_cannels,
+            bias=True)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv1x1(
+            in_channels=mid_cannels,
+            out_channels=channels,
+            bias=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        w = self.pool(x)
+        w = self.conv1(w)
+        w = self.relu(w)
+        w = self.conv2(w)
+        w = self.sigmoid(w)
+        x = x * w
+        return x
+
+
+class DualPathSequential(nn.Sequential):
+    """
+    A sequential container for modules with dual inputs/outputs.
+    Blocks will be executed in the order they are added.
+
+    Parameters:
+    ----------
+    return_two : bool, default True
+        Whether to return two output after execution.
+    first_ordinals : int, default 0
+        Number of the first modules with single input/output.
+    last_ordinals : int, default 0
+        Number of the final modules with single input/output.
+    dual_path_scheme : function
+        Scheme of dual path response for a module.
+    dual_path_scheme_ordinal : function
+        Scheme of dual path response for an ordinal module.
+    """
+    def __init__(self,
+                 return_two=True,
+                 first_ordinals=0,
+                 last_ordinals=0,
+                 dual_path_scheme=(lambda module, x1, x2: module(x1, x2)),
+                 dual_path_scheme_ordinal=(lambda module, x1, x2: (module(x1), x2))):
+        super(DualPathSequential, self).__init__()
+        self.return_two = return_two
+        self.first_ordinals = first_ordinals
+        self.last_ordinals = last_ordinals
+        self.dual_path_scheme = dual_path_scheme
+        self.dual_path_scheme_ordinal = dual_path_scheme_ordinal
+
+    def forward(self, x1, x2=None):
+        length = len(self._modules.values())
+        for i, module in enumerate(self._modules.values()):
+            if (i < self.first_ordinals) or (i >= length - self.last_ordinals):
+                x1, x2 = self.dual_path_scheme_ordinal(module, x1, x2)
+            else:
+                x1, x2 = self.dual_path_scheme(module, x1, x2)
+        if self.return_two:
+            return x1, x2
+        else:
+            return x1
+
+
+class Concurrent(nn.Sequential):
+    """
+    A container for concatenation of modules on the base of the sequential container.
+
+    Parameters:
+    ----------
+    axis : int, default 1
+        The axis on which to concatenate the outputs.
+    """
+    def __init__(self, axis=1):
+        super(Concurrent, self).__init__()
+        self.axis = axis
+
+    def forward(self, x):
+        out = []
+        for module in self._modules.values():
+            out.append(module(x))
+        out = torch.cat(tuple(out), dim=self.axis)
+        return out
+
+
+class ParametricSequential(nn.Sequential):
+    """
+    A sequential container for modules with parameters.
+    Blocks will be executed in the order they are added.
+    """
+    def __init__(self, *args):
+        super(ParametricSequential, self).__init__(*args)
+
+    def forward(self, x, **kwargs):
+        for module in self._modules.values():
+            x = module(x, **kwargs)
+        return x
+
+
+class ParametricConcurrent(nn.Sequential):
+    """
+    A container for concatenation of modules with parameters.
+
+    Parameters:
+    ----------
+    axis : int, default 1
+        The axis on which to concatenate the outputs.
+    """
+    def __init__(self, axis=1):
+        super(ParametricConcurrent, self).__init__()
+        self.axis = axis
+
+    def forward(self, x, **kwargs):
+        out = []
+        for module in self._modules.values():
+            out.append(module(x, **kwargs))
+        out = torch.cat(tuple(out), dim=self.axis)
+        return out
