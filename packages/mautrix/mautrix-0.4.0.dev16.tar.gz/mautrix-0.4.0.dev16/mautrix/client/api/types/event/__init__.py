1e256cc4d5d7668d7cc7c68a85b3f8f5587b02c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:40 +0000

    Add package mautrix/0.4.0.dev16/mautrix-0.4.0.dev16.tar.gz

---
+++ packages/mautrix/mautrix-0.4.0.dev16.tar.gz/mautrix-0.4.0.dev16/mautrix/client/api/types/event/__init__.py
@@ -0,0 +1,14 @@
+from .base import EventType
+from .redaction import RedactionEventContent, RedactionEvent
+from .message import (MessageEvent, MessageEventContent, MessageUnsigned, MediaMessageEventContent,
+                      LocationMessageEventContent, LocationInfo, InReplyTo, MessageType, Format,
+                      MediaInfo, FileInfo, AudioInfo, VideoInfo, ImageInfo, ThumbnailInfo,
+                      TextMessageEventContent, BaseMessageEventContent, MatchedCommand, RelatesTo,
+                      MatchedPassiveCommand, BaseFileInfo)
+from .state import (PowerLevelStateEventContent, Membership, MemberStateEventContent, StateEvent,
+                    AliasesStateEventContent, CanonicalAliasStateEventContent, StrippedStateEvent,
+                    RoomNameStateEventContent, RoomTopicStateEventContent, StateUnsigned,
+                    RoomAvatarStateEventContent, StateEventContent)
+from .account_data import (AccountDataEvent, AccountDataEventContent, RoomTagInfo,
+                           RoomTagAccountDataEventContent)
+from .generic import Event
