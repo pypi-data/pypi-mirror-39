91300dfab3d39968cfaf1bbcc2c49e8a60f24b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:15 +0000

    Add package django-geonames-api/0.8.1/django_geonames_api-0.8.1-py3-none-any.whl

---
+++ packages/django-geonames-api/django_geonames_api-0.8.1-py3-none-any.whl/geography/autocomplete.py
@@ -0,0 +1,163 @@
+r""" User autocomplete
+"""
+
+# Moills imports
+from django.db.models import Q
+
+from .models import (
+    Address,
+    City,
+    Conurbation,
+    Department,
+    Region,
+    Country,
+    Timezone
+)
+
+# Other imports
+from dal import autocomplete
+
+
+class AddressAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+
+        if not self.request.user.is_authenticated:
+            return Address.objects.none()
+
+        qs = Address.objects.select_related('city').all()
+
+        if self.q:
+            qs = qs.filter(
+                Q(street__icontains=self.q)
+                | Q(complement__icontains=self.q)
+                | Q(city__name__icontains=self.q)
+            )
+
+        return qs
+
+
+class TimezoneAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+
+        if not self.request.user.is_authenticated:
+            return Timezone.objects.none()
+
+        qs = Timezone.objects.all()
+
+        if self.q:
+            qs = qs.filter(name__icontains=self.q)
+
+        return qs
+
+
+class CityAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+
+        if not self.request.user.is_authenticated:
+            return City.objects.none()
+
+        qs = City.objects.all()
+
+        country = self.forwarded.get('country', None)
+        if country:
+            qs = qs.filter(country=country)
+
+        region = self.forwarded.get('region', None)
+        if region:
+            qs = qs.filter(region=region)
+
+        department = self.forwarded.get('department', None)
+        if department:
+            qs = qs.filter(department=department)
+
+        conurbation = self.forwarded.get('conurbation', None)
+        if conurbation:
+            qs = qs.filter(conurbation=conurbation)
+
+        if self.q:
+            qs = qs.filter(
+                Q(name__icontains=self.q) |
+                Q(code__istartswith=self.q)
+            )
+
+        return qs
+
+
+class ConurbationAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+
+        if not self.request.user.is_authenticated:
+            return Conurbation.objects.none()
+
+        qs = Conurbation.objects.all()
+
+        country = self.forwarded.get('country', None)
+        if country:
+            qs = qs.filter(country=country)
+
+        region = self.forwarded.get('region', None)
+        if region:
+            qs = qs.filter(region=region)
+
+        department = self.forwarded.get('department', None)
+        if department:
+            qs = qs.filter(department=department)
+
+        if self.q:
+            qs = qs.filter(name__icontains=self.q)
+
+        return qs
+
+
+class DepartmentAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+
+        if not self.request.user.is_authenticated:
+            return Department.objects.none()
+
+        qs = Department.objects.all()
+
+        country = self.forwarded.get('country', None)
+        if country:
+            qs = qs.filter(country=country)
+
+        region = self.forwarded.get('region', None)
+        if region:
+            qs = qs.filter(region=region)
+
+        if self.q:
+            qs = qs.filter(name__icontains=self.q)
+
+        return qs
+
+
+class RegionAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+
+        if not self.request.user.is_authenticated:
+            return Region.objects.none()
+
+        qs = Region.objects.all()
+
+        country = self.forwarded.get('country', None)
+        if country:
+            qs = qs.filter(country=country)
+
+        if self.q:
+            qs = qs.filter(name__icontains=self.q)
+
+        return qs
+
+
+class CountryAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+
+        if not self.request.user.is_authenticated:
+            return Country.objects.none()
+
+        qs = Country.objects.all()
+
+        if self.q:
+            qs = qs.filter(name__icontains=self.q)
+
+        return qs
