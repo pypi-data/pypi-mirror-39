acf671201656f6113407697b6bb81f4c815804e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:26 +0000

    Add package funniest-bw/0.5/funniest_bw-0.5.tar.gz

---
+++ packages/funniest-bw/funniest_bw-0.5.tar.gz/funniest_bw-0.5/funniest_bw/__init__.py
@@ -0,0 +1,6 @@
+def joke():
+    return "hello world haha bucuo"
+
+
+def main():
+    print joke()
---
+++ packages/funniest-bw/funniest_bw-0.5.tar.gz/funniest_bw-0.5/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='funniest_bw',
+    version='0.5',
+    description='a simple python project',
+    long_description=long_description,
+    url='http://www.sogou.com',
+    author='wb',
+    author_email='biaowang_88@163.com',
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='sample setuptools development',
+    packages=['funniest_bw'],
+    install_requires=[],
+    extras_require={},
+    package_data={},
+    data_files=[],
+    entry_points={
+        'console_scripts': [
+            'funniest_bw=funniest_bw:main',
+        ]
+    }
+)
