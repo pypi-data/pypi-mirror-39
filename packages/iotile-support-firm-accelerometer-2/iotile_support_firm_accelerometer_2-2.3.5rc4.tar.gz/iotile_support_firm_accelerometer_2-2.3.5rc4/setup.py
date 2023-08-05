fa94c2e7edb0ec5d71d5a53c9b45caface5405db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:46 +0000

    Add package iotile-support-firm-accelerometer-2/2.3.5rc4/iotile_support_firm_accelerometer_2-2.3.5rc4.tar.gz

---
+++ packages/iotile-support-firm-accelerometer-2/iotile_support_firm_accelerometer_2-2.3.5rc4.tar.gz/iotile_support_firm_accelerometer_2-2.3.5rc4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="iotile_support_firm_accelerometer_2",
+    packages=find_packages(include=["iotile_support_firm_accelerometer_2.*", "iotile_support_firm_accelerometer_2"]),
+    version="2.3.5rc4",
+    install_requires=[u'monotonic', u'bitstring ~= 3.1'],
+    entry_points={'iotile.proxy': ['accel1_proxy = iotile_support_firm_accelerometer_2.accel1_proxy']},
+    author="Arch",
+    author_email="info@arch-iot.com"
+)
