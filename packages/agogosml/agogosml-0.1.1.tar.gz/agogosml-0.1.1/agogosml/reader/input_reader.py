1ab169c5c9646fb31e97c1bf5408a2127e53673b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:25 +0000

    Add package agogosml/0.1.1/agogosml-0.1.1.tar.gz

---
+++ packages/agogosml/agogosml-0.1.1.tar.gz/agogosml-0.1.1/agogosml/reader/input_reader.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+""" InputReader """
+
+from agogosml.common.abstract_streaming_client import AbstractStreamingClient
+from agogosml.common.message_sender import MessageSender
+
+
+class InputReader:  # pylint: disable=too-few-public-methods
+    """Accepts incoming messages and routes them to a configured output"""
+
+    def __init__(self, streaming_client: AbstractStreamingClient, message_sender: MessageSender):
+        """
+        :param streaming_client: A client that can stream data out
+        :param message_sender: A message sender that can send messages onwards
+        """
+        self.message_sender = message_sender
+        self.messaging_client = streaming_client
+
+    def start_receiving_messages(self):
+        """Start receiving messages from streaming endpoint"""
+        self.messaging_client.start_receiving(self.on_message_received)
+
+    def stop_incoming_messages(self):
+        """Stop incoming messages from streaming endpoint"""
+        self.messaging_client.stop()
+
+    def on_message_received(self, message):
+        """Send messages onwards
+
+        :param message: a message to process
+        """
+        self.message_sender.send(message)
---
+++ packages/agogosml/agogosml-0.1.1.tar.gz/agogosml-0.1.1/tests/client_mocks.py
@@ -0,0 +1,68 @@
+from agogosml.common.abstract_streaming_client import AbstractStreamingClient
+from agogosml.common.listener_client import ListenerClient
+from agogosml.common.message_sender import MessageSender
+
+
+class ClientMessagingMock(AbstractStreamingClient):
+    def __init__(self):
+        self.sent = False
+        self.receiving = False
+        pass
+
+    def send(self, *args, **kwargs):
+        self.sent = True
+        pass
+
+    def stop(self, *args, **kwargs):
+        pass
+
+    def start_receiving(self, callback):
+        self.receiving = True
+        self.callback = callback
+        pass
+
+    def get_sent(self):
+        return self.sent
+
+    def get_receiving(self):
+        return self.receiving
+
+    def mock_incoming_message_event(self, msg):
+        self.callback(msg)
+
+
+class ListenerClientMock(ListenerClient):
+    def __init__(self, port):
+        self.callback = None
+        self.startCalled = False
+        self.stopCalled = False
+
+    def start(self, on_message_received):
+        self.callback = on_message_received
+        self.startCalled = True
+        pass
+
+    def stop(self):
+        self.stopCalled = True
+        pass
+
+    def mock_new_incoming_message(self):
+        self.callback("{'some':'json'}")
+
+    def get_started(self):
+        return self.startCalled
+
+    def get_stopped(self):
+        return self.stopCalled
+
+
+class MessageSenderMock(MessageSender):
+    def __init__(self):
+        pass
+
+    def send(self, msg):
+        self.msg = msg
+        pass
+
+    def get_last_msg(self):
+        return self.msg
