90c942dfb8e3fe48f79c1324ea9135aa7cda3653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:31 +0000

    Add package wcc/1.4.8/wcc-1.4.8.tar.gz

---
+++ packages/wcc/wcc-1.4.8.tar.gz/wcc-1.4.8/wcc/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '1.4.8'
+#从本目录下api.py里import Service和Bucket这个类
+#from .api import Service, Bucket
+from .urllibx import *
+from .utils import *
+from .oss   import *
+from .req   import getpage
+from .req   import getcookie
