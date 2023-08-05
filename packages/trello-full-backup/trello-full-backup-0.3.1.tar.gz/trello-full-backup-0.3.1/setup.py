8c3de1dbb69564af17752e0a41985421240dd254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:33 +0000

    Add package trello-full-backup/0.3.1/trello-full-backup-0.3.1.tar.gz

---
+++ packages/trello-full-backup/trello-full-backup-0.3.1.tar.gz/trello-full-backup-0.3.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='trello-full-backup',
+    packages=find_packages(),
+    version='0.3.1',
+    author='Jeremy Tuloup',
+    author_email='jerem@jtp.io',
+    url='https://github.com/jtpio/trello-full-backup',
+    description='Backup everything from Trello',
+    long_description=long_description,
+    license='MIT',
+    classifiers=[
+        'Topic :: System :: Archiving :: Backup',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    install_requires=['requests'],
+    python_requires='>=3.4',
+    entry_points={
+        'console_scripts': [
+            'trello-full-backup = trello_full_backup:main'
+        ]
+    }
+)
