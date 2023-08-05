b7c1956e2749145b9fe335a712fb6b40987b4867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:19 +0000

    Add package punica/0.0.18/punica-0.0.18-py3-none-any.whl

---
+++ packages/punica/punica-0.0.18-py3-none-any.whl/punica/cli/box_cmd.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import click
+
+from .main import main
+
+from punica.box.repo_box import Box
+from ontology.exception.exception import SDKException
+from punica.exception.punica_exception import PunicaException
+
+
+@main.command('unbox')
+@click.argument('box_name', nargs=1)
+@click.pass_context
+def unbox_cmd(ctx, box_name):
+    """
+    Download a Punica Box, a pre-built Ontology DApp project.
+    """
+    project_dir = ctx.obj['PROJECT_DIR']
+    try:
+        Box.unbox(box_name, project_dir)
+    except (PunicaException, SDKException) as e:
+        print('An error occur...')
+        print(e.args)
+        exit(1)
+
+
+@main.command('boxes')
+@click.pass_context
+def boxes_cmd(ctx):
+    """
+    List all available punica box.
+    """
+    boxes = Box.list_boxes()
+    print('Various punica boxes are waiting for your:')
+    for box in boxes:
+        print('\t', box)
