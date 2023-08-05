def6ce319fa51ccfb9e2637e748948ca2f3513b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:31 +0000

    Add package mpython/1.0/mpython-1.0.tar.gz

---
+++ packages/mpython/mpython-1.0.tar.gz/mpython-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'mpython',      
+    version = '1.0',
+    py_modules = ['mpython'],
+    author = 'Zhang KaiHua',        
+    author_email = 'apple_eat@126.com',
+    url = 'http://www.1zlab.com',
+    description = 'mpython is a micropython module'   
+    )
