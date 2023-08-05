b52d85226d46e9418f052cdc62a4a46f15e5d210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:31 +0000

    Add package dash-devextreme/0.10.1/dash_devextreme-0.10.1.tar.gz

---
+++ packages/dash-devextreme/dash_devextreme-0.10.1.tar.gz/dash_devextreme-0.10.1/dash_devextreme/_imports_.py
@@ -0,0 +1,26 @@
+from .DataGrid import DataGrid
+from .PieChart import PieChart
+from .PivotGrid import PivotGrid
+from .Popover import Popover
+from .Popup import Popup
+from .SelectBox import SelectBox
+from .TabPanel import TabPanel
+from .Tabs import Tabs
+from .TextBox import TextBox
+from .Tooltip import Tooltip
+from .TreeList import TreeList
+
+
+__all__ = [
+    "DataGrid",
+    "PieChart",
+    "PivotGrid",
+    "Popover",
+    "Popup",
+    "SelectBox",
+    "TabPanel",
+    "Tabs",
+    "TextBox",
+    "Tooltip",
+    "TreeList",
+]
