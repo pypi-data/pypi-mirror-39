65fc78088592840e9698d3751bd7d45663d518e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:21 +0000

    Add package ecs-manager/0.1.4.4/ecs_manager-0.1.4.4.tar.gz

---
+++ packages/ecs-manager/ecs_manager-0.1.4.4.tar.gz/ecs_manager-0.1.4.4/setup.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+import os
+import sys
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+setup(
+    name='ecs_manager',
+    version='0.1.4.4',
+    url='https://github.com/pistatium/ecs_service_manager',
+    author='pistatium',
+    description='Manage ecs service',
+    packages=find_packages(),
+    install_requires=[
+        'boto3',
+        'click',
+        'future'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ecs_manager=ecs_manager.cmd:main'
+        ]
+    },
+)
+
