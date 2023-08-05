4c0c2c6460f3a17db817a5703f48fbe99dac9097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:02 +0000

    Add package edx-proctoring/1.5.0b3/edx_proctoring-1.5.0b3-py2.py3-none-any.whl

---
+++ packages/edx-proctoring/edx_proctoring-1.5.0b3-py2.py3-none-any.whl/edx_proctoring/__init__.py
@@ -0,0 +1,9 @@
+"""
+The exam proctoring subsystem for the Open edX platform.
+"""
+
+from __future__ import absolute_import
+
+__version__ = '1.5.0b3'
+
+default_app_config = 'edx_proctoring.apps.EdxProctoringConfig'  # pylint: disable=invalid-name
