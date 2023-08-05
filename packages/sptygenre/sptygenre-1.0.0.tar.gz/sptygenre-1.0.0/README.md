f2cdc18070fcc9631589807666eb787813d2817f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:53 +0000

    Add package sptygenre/1.0.0/sptygenre-1.0.0.tar.gz

---
+++ packages/sptygenre/sptygenre-1.0.0.tar.gz/sptygenre-1.0.0/README.md
@@ -0,0 +1,19 @@
+# sptygenre
+Analyse spotify playlists for genres.
+The genres are displayed in a wordcloud plot.
+Code works but is now "beta" so use with precaution.
+Please report bugs via Issues.
+
+For installation make sure python is installed and install with:
+```
+pip install sptygenre
+```
+or for python3:
+```
+pip3 install sptygenre
+```
+
+After successful installation you can run to generate a wordcloud for given playlist:
+```
+sptygenre [username]  [playlist uri]
+```
---
+++ packages/sptygenre/sptygenre-1.0.0.tar.gz/sptygenre-1.0.0/sptygenre/auth/auth.py
@@ -0,0 +1,28 @@
+import spotipy
+from spotipy import oauth2
+import spotipy.util as util
+from sptygenre import exceptions
+
+def authorize(username):
+    scope = 'playlist-read-private'
+    token = util.prompt_for_user_token(username, scope, client_id='3dab3c84034247168f7021f1f1128754', client_secret='3240734543f14ac2b389d2b6b7878290', redirect_uri='http://localhost:5000/callback/')
+    if token:
+        return spotipy.Spotify(auth=token)
+    else:
+        raise exceptions.TokenException(username)
+
+# returns auth url
+def get_auth_url(username):
+    _client_id = ''
+    _client_secret = ''
+    _redirect_uri = ''
+    scope = 'playlist-read-private'
+    cache_path = ".cache-" + username
+    sp_oauth = oauth2.SpotifyOAuth(_client_id, _client_secret, _redirect_uri, scope=scope, cache_path=cache_path)
+    return sp_oauth.get_authorize_url()
+
+# returns token for response url
+def get_token(sp_oauth, response_url):
+    code = oauth2.parse_response_code(response_url)
+    token = sp_oauth.get_access_token(code)
+    return token['access_token']
