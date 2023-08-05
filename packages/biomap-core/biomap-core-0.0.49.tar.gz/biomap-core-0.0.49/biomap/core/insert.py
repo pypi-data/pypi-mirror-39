27b9d5f766507ff20953035c1dec9d27b8728c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:26 +0000

    Add package biomap-core/0.0.49/biomap-core-0.0.49.tar.gz

---
+++ packages/biomap-core/biomap-core-0.0.49.tar.gz/biomap-core-0.0.49/biomap/core/insert.py
@@ -0,0 +1,16 @@
+'''
+
+'''
+import abc
+
+class MapInserter(abc.ABC):
+    '''
+
+    '''
+    @abc.abstractmethod
+    def mapper_data(self):
+        pass
+
+    @abc.abstractmethod
+    def mapper_definition(self):
+        pass
