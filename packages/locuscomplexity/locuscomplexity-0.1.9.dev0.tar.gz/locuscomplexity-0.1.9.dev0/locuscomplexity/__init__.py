f2ab1e84e914004aedcb93dc5ded47c2a20c0991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:46 +0000

    Add package locuscomplexity/0.1.9.dev0/locuscomplexity-0.1.9.dev0.tar.gz

---
+++ packages/locuscomplexity/locuscomplexity-0.1.9.dev0.tar.gz/locuscomplexity-0.1.9.dev0/locuscomplexity/__init__.py
@@ -0,0 +1,3 @@
+import locuscomplexity.fitness, locuscomplexity.complexity, locuscomplexity.color_code
+
+__all__ = ["color_code", "fitness", "complexity"]
---
+++ packages/locuscomplexity/locuscomplexity-0.1.9.dev0.tar.gz/locuscomplexity-0.1.9.dev0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='locuscomplexity',
+    version='0.1.9dev0',
+    packages = ['locuscomplexity'],
+    url='https://github.com/LocusAnalytics/EconCmplx',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+          'pandas','networkx', 'matplotlib'],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
