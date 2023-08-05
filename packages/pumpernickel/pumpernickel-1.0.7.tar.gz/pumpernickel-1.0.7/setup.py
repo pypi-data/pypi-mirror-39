5904323ec3b63eddd921590745aa5cc02ab90f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:31 +0000

    Add package pumpernickel/1.0.7/pumpernickel-1.0.7.tar.gz

---
+++ packages/pumpernickel/pumpernickel-1.0.7.tar.gz/pumpernickel-1.0.7/setup.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+try:
+    import setuptools
+    from setuptools import setup, find_packages
+except ImportError:
+    print("Please install setuptools.")
+
+setup(
+    name  = 'pumpernickel',
+    version = '1.0.7',
+    license = 'MIT',
+    author = 'hiroyam',
+    author_email = 'hogehoge@gmail.com',
+    url = 'https://github.com/hiroyam/pumpernickel',
+    packages = find_packages(),
+    install_requires = ['numpy'],
+)
