abebf55541681e5ef747da2aa195c90bea0788aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:56 +0000

    Add package python-schema/0.2/python_schema-0.2.tar.gz

---
+++ packages/python-schema/python_schema-0.2.tar.gz/python_schema-0.2/python_schema/exception.py
@@ -0,0 +1,32 @@
+class BasePythonSchemaError(Exception):
+    """Base class in order to catch all python-schema easily (if needed)
+    """
+
+
+class ValueNotSetError(BasePythonSchemaError):
+    """Happens when we try to read a field that was never loaded
+    """
+
+
+class NormalisationError(BasePythonSchemaError):
+    """Base error for all the normalisation issues
+    """
+
+    def __init__(self, message, *args, **kwargs):
+        self.errors = [message]
+
+        super().__init__(message, *args, **kwargs)
+
+
+class NoneNotAllowedError(NormalisationError):
+    """Happens when we try to load field with None, but it's not allowed
+    """
+
+
+class ValidationError(BasePythonSchemaError):
+    """Exception happens when one or more of validators fails.
+    """
+    def __init__(self, errors, *args, **kwargs):
+        self.errors = errors
+
+        super().__init__(*args, **kwargs)
