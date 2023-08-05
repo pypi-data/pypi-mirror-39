34cda079995d0d7f254381f772faf96a97aa992e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:56 +0000

    Add package pythonGroupMsg/0.0.3/pythonGroupMsg-0.0.3.tar.gz

---
+++ packages/pythonGroupMsg/pythonGroupMsg-0.0.3.tar.gz/pythonGroupMsg-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pythonGroupMsg',
+      version='0.0.3',
+      description='This is a packet that broadcasts redis multiple queues',
+      url='https://github.com/zhenruyan/pythonGroupMsg',
+      author='zhenruyan',
+      author_email='baiyangwangzhan@hotmail.com',
+      license='WTFPL',
+      packages=find_packages(),
+      zip_safe=False,
+      platforms=["linux"],
+      long_description=open('README.rst').read(),
+classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ],include_package_data=True,
+          )
