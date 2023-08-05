a0c02c0857d45edafe0f5babe4b96c4f35b8e365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:05 +0000

    Add package displayer/0.2/displayer-0.2.tar.gz

---
+++ packages/displayer/displayer-0.2.tar.gz/displayer-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='displayer',
+    version='0.2',
+    description='Batch display library for images in a directory using a web browser',
+    url='https://github.com/enomotokenji/image-displayer',
+    author='Kenji Enomoto',
+    author_email='kenji.enomoto.abc.123@gmail.com',
+    license='MIT',
+    packages=[
+        "displayer",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points={
+        'console_scripts': ['displayer=displayer.displayer:displayer']}
+)
