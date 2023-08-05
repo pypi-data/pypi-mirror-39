f231d5667ec29b1fa0069d16afc709174b5dab17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:54 +0000

    Add package xptools/0.35/xptools-0.35.tar.gz

---
+++ packages/xptools/xptools-0.35.tar.gz/xptools-0.35/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name='xptools',
+      version='0.35',
+      description='Image and video analysis tools for experimental sciences',
+      url='https://github.com/hlgirard/xptools',
+      author='Henri-Louis Girard',
+      author_email='hl.girard@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'numpy',
+          'pandas',
+          'scikit-image',
+          'joblib',
+          'matplotlib',
+          'av',
+          'tqdm'
+      ],
+      scripts=[
+          'bin/analyze_front',
+          'bin/display_image_matrix',
+          'bin/analyze_bubbles'
+      ],
+      zip_safe=False)
