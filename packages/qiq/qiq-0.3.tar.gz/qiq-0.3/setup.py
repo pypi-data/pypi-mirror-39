993d86facadf7ce88d6a6425960e508204275a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:00 +0000

    Add package qiq/0.3/qiq-0.3.tar.gz

---
+++ packages/qiq/qiq-0.3.tar.gz/qiq-0.3/setup.py
@@ -0,0 +1,45 @@
+import sys
+
+import setuptools  # Don't delete, hook for distutils
+
+from distutils.core import setup
+
+sys.path.append("src/qiq")
+
+from qiq import qiq
+
+setup(
+	name="qiq",
+	version=qiq.__version__,
+	package_dir={"qiq": "src/qiq"},
+	entry_points={
+        "console_scripts": [
+            "qiq = qiq.qiq:main",
+        ],
+    },
+	packages=["qiq"],
+    author="Alexander Ptakhin",
+    author_email="me@aptakhin.name",
+    description="qiq is the thin layer over pip with a few rules.",
+    license="MIT",
+    keywords="pip qiq",
+    url="https://gitlab.com/aptakhin/qiq",
+    project_urls={
+        "Source Code": "https://gitlab.com/aptakhin/qiq",
+    },
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"Environment :: Console",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: MacOS",
+		"Operating System :: Microsoft",
+		"Operating System :: POSIX",
+		"Operating System :: Unix",
+		"Programming Language :: Python",
+		"Topic :: Software Development",
+		"Topic :: Software Development :: Testing",
+		"Topic :: Software Development :: Version Control",
+		"Topic :: Utilities",
+	],
+)
