379e2a9f4303c5bfae1c780ab48b2db96616b692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:28 +0000

    Add package transistor/0.2.1/transistor-0.2.1-py3-none-any.whl

---
+++ packages/transistor/transistor-0.2.1-py3-none-any.whl/tests/books_toscrape/test_books_toscrape.py
@@ -0,0 +1,270 @@
+# -*- coding: utf-8 -*-
+"""
+transistor.tests.books_toscrape.test_books_toscrape
+~~~~~~~~~~~~
+This module implements testing of the books_toscrape example in order to
+check Transistor source code.
+
+:copyright: Copyright (C) 2018 by BOM Quote Limited
+:license: The MIT License, see LICENSE for more details.
+~~~~~~~~~~~~
+"""
+from gevent import monkey
+monkey.patch_all()
+import pytest
+from requests import Response
+from bs4 import BeautifulSoup
+from pkgutil import get_data
+from pathlib import Path
+from os.path import dirname as d
+from os.path import abspath
+from transistor import StatefulBook, WorkGroup
+from transistor.persistence.newt_db.collections import SpiderLists
+from transistor.persistence.exporters.exporters import CsvItemExporter
+from ..conftest import get_job_results, delete_job
+from examples.books_to_scrape.persistence.newt_db import ndb
+from examples.books_to_scrape.scraper import BooksToScrapeScraper
+from examples.books_to_scrape.manager import BooksWorkGroupManager
+from examples.books_to_scrape.persistence.serialization import (
+    BookItems, BookItemsLoader)
+
+root_dir = d(d(abspath(__file__)))
+
+
+def get_html(filename):
+    """
+    Get the appropriate html testfile and return it. Filename should include
+    the folder the file is in.
+
+    :param filename: ex. -> "digidog/digidog_china_multiple.html"
+    """
+    data_folder = Path(root_dir)
+    file_to_open = data_folder / filename
+    f = open(file_to_open, encoding='utf-8')
+    return f.read()
+
+
+def get_file_path(filename):
+    """
+    Find the book_title excel file path.
+    """
+    root_dir = d(d(abspath(__file__)))
+    root = Path(root_dir)
+    filepath = root / 'books_toscrape' / filename
+    return r'{}'.format(filepath)
+
+
+@pytest.fixture(scope='function')
+def bts_static_scraper(test_dict):
+    """
+    A BooksToScrapeScraper static test fixture.
+    """
+    book_title = 'Soumission'
+    page = get_html("books_toscrape/books_toscrape_index.html")
+    test_dict['_test_page_text'] = page
+    test_dict['url'] = 'http://books.toscrape.com'
+    scraper = BooksToScrapeScraper(book_title=book_title, **test_dict)
+    scraper.start_http_session()
+    return scraper
+
+
+@pytest.fixture(scope='function')
+def bts_live_scraper():
+    """
+    A BooksToScrapeScraper live fixture for TestLiveBooksToScrape.
+    """
+    scraper = BooksToScrapeScraper(book_title='Black Dust')
+    scraper.start_http_session(url='http://books.toscrape.com')
+    return scraper
+
+
+@pytest.fixture(scope='function')
+def bts_manager(_BooksToScrapeGroup, _BooksWorker):
+    """
+    A BooksToScrape Manager test fixture for live network call.
+    """
+    # ensure to open this file in binary mode
+    book_data_file = open('c:/tmp/book_data.csv', 'a+b')
+    exporters = [
+        CsvItemExporter(
+            fields_to_export=['book_title', 'stock', 'price'],
+            file=book_data_file
+        )
+    ]
+
+    file = get_file_path('book_titles.xlsx')
+    trackers = ['books.toscrape.com']
+    tasks = StatefulBook(file, trackers, keywords='titles', autorun=True)
+
+    groups = [
+        WorkGroup(
+            name='books.toscrape.com',
+            url='http://books.toscrape.com/',
+            spider=BooksToScrapeScraper,
+            worker=_BooksWorker,
+            items=BookItems,
+            loader=BookItemsLoader,
+            exporters=exporters,
+            workers=3,  # this creates 3 scrapers and assigns each a book as a task
+            kwargs={'timeout': (3.0, 20.0)})
+    ]
+    manager = BooksWorkGroupManager('books_scrape', tasks, groups=groups, pool=5)
+
+    return manager
+
+
+class TestStaticBooksToScrapeScraper:
+    """
+    Unit test some BooksToScrapeScraper methods using a static html page to hit a
+    few methods which can be easily tested without live network request.
+    """
+    def test_stock(self, bts_static_scraper):
+        """
+        Test that the stock attribute has been properly set
+        """
+        assert bts_static_scraper.stock == 'In stock'
+
+    def test_price(self, bts_static_scraper):
+        """
+        Test that the price attribute has been properly set
+        """
+        assert bts_static_scraper.price == '£50.10'
+
+    def test_next_page(self, bts_static_scraper):
+        """
+        Test that the _next_page method returns the expected url
+        """
+        next_page = bts_static_scraper._next_page()
+        assert next_page == r'http://books.toscrape.com/catalogue/page-2.html'
+
+    def test_LUA_SOURCE(self, bts_static_scraper):
+        """
+        Test the LUA_SOURCE returns as expected.
+        """
+        source = bts_static_scraper.LUA_SOURCE
+        expected = get_data(
+            'transistor',
+            'scrapers/scripts/basic_splash.lua').decode('utf-8')
+        assert source == expected
+
+
+class TestStaticSplashBrowser:
+    """
+    Unit test some SplashBrowser methods, using the BooksToScrapeScraper test fixture.
+    Many of the methods depend on making a live network request. At the moment,
+    we are only using a static html page to hit a few methods.
+    """
+
+    def test_ucontent(self, bts_static_scraper):
+        """
+        Test ucontent method returns as expected.
+        """
+        assert bts_static_scraper.browser.ucontent.startswith('<!DOCTYPE html>')
+
+    def test_resp_content(self, bts_static_scraper):
+        """
+        Test resp_content returns an empty dict.
+        """
+        assert bts_static_scraper.browser.resp_content == {}
+
+    def test_get_current_page_is_soup(self, bts_static_scraper):
+        """
+        Test get_current_page() returns a beautifulsoup4 object.
+        """
+        page = bts_static_scraper.browser.get_current_page()
+        assert type(page) == BeautifulSoup
+
+    def test_get_current_page_to_string(self, bts_static_scraper):
+        """
+        Test get_current_page() cast to string is roughly similar to our html.
+        """
+        get_page = str(bts_static_scraper.browser.get_current_page())
+        html = get_html("books_toscrape/books_toscrape_index.html")
+        assert get_page[:100] == html[:100]
+
+
+class TestLiveBooksToScrape:
+    """
+    Run through a live scrape of books.toscrape.com, save to newt_db,
+    and check the newt_db results are what we expect.
+    """
+
+    def test_live_manager(self, bts_manager):
+        """
+        Test that the stock attribute has been properly set
+        """
+
+        # todo: move this to a setup fixture
+        # first, setup newt.db for testing
+        ndb.root._spiders = SpiderLists()
+        ndb.commit()
+
+        # now, perform the scrape
+        bts_manager.main()
+
+        # when the scrape is completed then check the results
+
+        result = get_job_results('books_scrape')
+
+        book_titles = []
+        prices = []
+        stocks = []
+        for r in result:
+            book_titles.append(r['book_title'])
+            prices.append(r['price'])
+            stocks.append(r['stock'])
+
+        assert len(book_titles) == 3
+        assert len(prices) == 3
+        assert len(stocks) == 3
+
+        assert 'Soumission' in book_titles
+        assert 'Rip it Up and Start Again' in book_titles
+        assert 'Black Dust' in book_titles
+        assert 'UK £50.10' in prices
+        assert 'In stock' in stocks
+        assert None not in prices
+        assert None not in stocks
+
+        assert result[0]['har']['log']['browser']['comment'] == 'PyQt 5.9, Qt 5.9.1'
+        assert result[0]['png']
+
+        # the below should currently return None if not using Crawlera
+        assert result[0]['endpoint_status'] is None
+        assert result[0]['crawlera_session'] is None
+        assert result[0]['resp_content_type_header'] is None
+
+        # todo: move this to a teardown fixture
+        delete_job('books_scrape')
+        del ndb.root._spiders
+        ndb.commit()
+
+    def test_live_scraper_browser_open(self, bts_live_scraper):
+        """Test the scraper.browser.open method returns Response"""
+
+        link = 'http://books.toscrape.com/catalogue/black-dust_976/index.html'
+        bts_live_scraper.splash_args = {
+            'lua_source': bts_live_scraper.LUA_SOURCE,
+            'url': link,
+            'crawlera_user': bts_live_scraper.crawlera_user,
+            # set Splash to cache the lua script, to avoid sending it every request
+            'cache_args': 'lua_source',
+            'timeout': 10.0,
+            'session_id': 'create',
+            'referrer': bts_live_scraper.referrer if not None else
+            "https://www.google.com",
+            'searchurl': bts_live_scraper.searchurl,
+            'keyword': None,  # can be used in the LUA script to submit a form
+            'cookies': bts_live_scraper.cookies,
+            'user_agent': bts_live_scraper.user_agent,
+            'splash_wait': bts_live_scraper.splash_wait,
+            'js_source': ";" if not bts_live_scraper.js_source else
+            bts_live_scraper.js_source,
+            'script': 0 if not bts_live_scraper.js_source else 1
+        }
+        page = bts_live_scraper.browser.open('http://localhost:8050/execute',
+                                             json=bts_live_scraper.splash_args,
+                                             timeout=(3.0, 10.0),
+                                             verify=bts_live_scraper._crawlera_ca,
+                                             stream=True)
+        assert type(page) == Response
---
+++ packages/transistor/transistor-0.2.1-py3-none-any.whl/transistor/managers/base_manager.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+"""
+transistor.managers.base_manager
+~~~~~~~~~~~~
+This module implements BaseWorkGroupManager as a fully functional base class
+which can assign tasks and conduct a scrape job across an
+arbitrary number of WorkGroups. Each WorkGroup can contain an arbitrary number of
+Worker/Scrapers.
+
+Although this class is fully functional as-is. The monitor() method provides an
+excellent hook point for post-scrape Worker manipulation. A more robust implementation
+will subclass BaseManager and override the monitor method for customization.
+
+
+:copyright: Copyright (C) 2018 by BOM Quote Limited
+:license: The MIT License, see LICENSE for more details.
+~~~~~~~~~~~~
+"""
+
+import gevent
+from gevent.queue import Queue, Empty
+from gevent.pool import Pool
+
+
+class BaseWorkGroupManager:
+    """
+    Base class for a WorkGroupManager.
+    """
+    __attrs__ = [
+        'book', 'exporter', 'job_id', 'groups', 'trackers', 'pool', 'qitems',
+        'workgroups',
+    ]
+
+    def __init__(self, job_id, book, groups:list, pool:int=20):
+        """
+
+        Create the instance.
+
+        :param job_id: will save the result of the workers Scrapes to `job_id` list.
+        If this job_id is "NONE" then it will pass on the save.
+        :param book:  a StatefulBook instance
+        :param pool: size of the greenlets pool. If you want to utilize all the
+        workers concurrently, it should be at least the total number
+        of all workers + 1 for the manager. Otherwise, the pool is useful to
+        constrain concurrency to help stay within Crawlera subscription limits.
+        :param groups: a list of class: `WorkGroup()` objects.
+        Example:
+            >>> groups = [
+            >>> WorkGroup(class_=MouseKeyGroup, workers=5, kwargs={"china":True}),
+            >>> WorkGroup(class_=MouseKeyGroup, workers=5, kwargs={})
+            >>> ]
+        :param pool: number of greenlets to create
+        """
+        self.job_id = job_id
+        self.book = book
+        self.trackers = self.book.trackers
+        self.groups = groups
+        self.pool = Pool(pool)
+        self.qitems = {}
+        self.workgroups = {}
+        self._init_tasks()
+
+    def _init_tasks(self):
+        """
+        Create individual task queues for the workers. The tracker names
+        should be extracted from self.book.to_do() and the tracker
+        name should match the worker name.
+
+        Extract the tracker name and then create the qitems. Example:
+
+        >>> qitems = {}
+        >>> for tracker in book.to_do():
+        >>>    qitems.[self.tracker.name] = tracker.to_do()
+        returns:
+        deque([<TaskTracker(name=mousekey.cn)>, <TaskTracker(name=mousekey.com)>])
+        """
+
+        for tracker in self.book.to_do():
+            # set the name of qitems key to tracker.name
+            self.qitems[tracker.name] = Queue(items=tracker.to_do())
+
+        self._init_workers()
+
+    def _init_workers(self):
+        """
+        Create the WorkGroups by tracker name and assign them by name to the
+        workgroups dict.
+
+        :return:
+        """
+        # first, build a list from tracker names per qitems.keys()
+        names = [name for name in self.qitems.keys()]
+        for name in names:
+            for group in self.groups:
+                # match the tracker name to the group name
+                if group.name == name:
+                    # assumes `group` is a WorkGroup namedtuple
+
+                    # add the name to group.kwargs dict so it can be passed down
+                    # to the group/worker/spider and assigned as an attr
+                    group.kwargs['name'] = name
+                    group.kwargs['url'] = group.url
+                    group.kwargs['spider'] = group.spider
+                    group.kwargs['worker'] = group.worker
+                    group.kwargs['items'] = group.items
+                    group.kwargs['loader'] = group.loader
+                    # exporters is a list of exporter instances
+                    group.kwargs['exporters'] = group.exporters
+                    workergroup = group.group(
+                        staff=group.workers, job_id=self.job_id, **group.kwargs)
+                    # now that attrs assigned, init the workers on the workgroup
+                    workergroup.init_workers()
+                    # lastly, after calling init_workers, assign the workgroup instance
+                    # with workers instance to the workgroups dict with key = `name`
+                    self.workgroups[name] = workergroup
+
+    def spawn_list(self):
+        """"
+        The spawn() method begins a new greenlet with the given arguments
+        (which are passed to the greenlet constructor) and adds it to the
+        collection of greenlets this group is monitoring.
+
+        We return a list of the newly started greenlets, used in a later
+        'joinall` call.
+
+        :return: A list of the newly started greenlets.
+        """
+
+        # workgroups is a list of BaseGroup objects
+        workgroups = [val for val in self.workgroups.values()]
+
+        spawn_list = [self.pool.spawn(self.monitor, worker) for work_group in
+                      workgroups for worker in work_group]
+
+        # we get a blocking error if we spawn the manager first, so spawn it last
+        spawn_list.append(self.pool.spawn(self.manage))
+
+        return spawn_list
+
+    def monitor(self, target):
+        """
+        This method actually spawns the spider and then the purpose is to allow
+        some additional final actions to be performed on the spider object after
+        the worker completes the spider's job, but before it shuts down and the
+        object instance is lost.
+
+        The simplest example which must be implemented:
+
+        def monitor(self, target):
+            '''
+            The only absolute requirement is to start the spider with
+            target.spawn_spider() and then call gevent.sleep(0)
+            '''
+            target.spawn_spider()
+            gevent.sleep(0)
+
+        A more useful example:
+
+        def monitor(self, target):
+            '''
+            More useful, would be to hook in some post-scrape logic between
+            spawn_spider() and gevent.sleep(0).
+            '''
+            target.spawn_spider()
+            # /start --> YOUR POST-SCRAPE HOOK IS HERE, ADD LOGIC AS REQUIRED.
+            for event in target.events:
+                # .event is a simple list() as a class attribute, in the scraper object
+                # we could apply some transformation to an object in event, now.
+                print(f'THIS IS A MONITOR EVENT - > {event}')
+            # /end --> YOUR POST SCRAPE HOOK LOGIC. Finally, call gevent.sleep()
+            gevent.sleep(0)
+
+        :param target: a worker
+        :return:
+        """
+        target.spawn_spider()
+        gevent.sleep(0)
+
+    def manage(self):
+        """"
+        Manage will hand out work when the appropriate Worker is free.
+        The manager timeout must be less than worker timeout, or else, the workers
+        will be idled and shutdown.
+        """
+        try:
+            while True:
+                for name, workgroup in self.workgroups.items():
+                    for qname, q in self.qitems.items():
+                        if name == qname: # workgroup name must match the tracker name
+                            for worker in workgroup:
+                                one_task = q.get(timeout=0.5)
+                                worker.tasks.put(one_task)
+                gevent.sleep(0)
+        except Empty:
+            print(f'Assigned all {name} work!')
+
+    def main(self):
+        spawny = self.spawn_list()
+        gevent.pool.joinall(spawny)
+        gevent.sleep(0)
