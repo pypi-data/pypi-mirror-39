cf6a5eaf65e95fa09c2466957d29fdffe6918817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:16 +0000

    Add package pixelate-redactor/0.3/pixelate_redactor-0.3-py2-none-any.whl

---
+++ packages/pixelate-redactor/pixelate_redactor-0.3-py2-none-any.whl/pixelate_redactor/pixelate_redactor.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+
+def pixelate_redactor(input_file_path, output_file_path, pixel_size, start_x, start_y, size_x, size_y):
+    original = Image.open(input_file_path)
+    pixelated = original.resize(
+        (original.size[0] // pixel_size, original.size[1] // pixel_size),
+        Image.NEAREST
+    )
+    pixelated = pixelated.resize(
+        (pixelated.size[0] * pixel_size, pixelated.size[1] * pixel_size),
+        Image.NEAREST
+    )
+    box = (start_x, start_y, start_x + size_x, start_y + size_y)
+    original.paste(pixelated.crop(box), box)
+    original.save(output_file_path)
