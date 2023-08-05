fd38da74d82bd2dd02e8cf3050d743de2cc77f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:07 +0000

    Add package osinfo/1.1/osinfo-1.1.tar.gz

---
+++ packages/osinfo/osinfo-1.1.tar.gz/osinfo-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='osinfo', 
+    version='1.1',
+    description='It inherits OS adds info prompts', 
+    url='https://github.com/SuperSystemStudio/os_update', 
+    author='Mryan', 
+    author_email='A2564011261@163.com', 
+    license='Apache-2.0'
+)
