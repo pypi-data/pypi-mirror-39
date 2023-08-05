2eb8f19f2eb00efe945cd42e0a77ef2f2987d02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:42 +0000

    Add package MongoSchemaImportExport/0.2.1/MongoSchemaImportExport-0.2.1.tar.gz

---
+++ packages/MongoSchemaImportExport/MongoSchemaImportExport-0.2.1.tar.gz/MongoSchemaImportExport-0.2.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='MongoSchemaImportExport',
+    version='0.2.1',
+    packages=['mongo_import_export_schema'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    description='Import and export metadata about mongodb databases and collections',
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    author="Jonathan Mackenzie",
+    url="https://github.com/jonnoftw/mongo_import_export_schema",
+    keywords=["pymongo mongodb schema import export"],
+    long_description=open('README.md').read(),
+    requires=['pymongo'],
+    scripts=['mongo_import_export_schema/mongo-schema-export.py',
+             'mongo_import_export_schema/mongo-schema-import.py'],
+)
