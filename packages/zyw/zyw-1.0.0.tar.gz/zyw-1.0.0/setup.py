c2e4676832fb1a4b8362050262128a9c911cb7af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:07 +0000

    Add package zyw/1.0.0/zyw-1.0.0.tar.gz

---
+++ packages/zyw/zyw-1.0.0.tar.gz/zyw-1.0.0/setup.py
@@ -0,0 +1,15 @@
+﻿# author：灵岩（1318215328@qq.com）
+#coding=utf-8
+# 创建于: 11/30 0027 16:26
+
+from distutils.core import setup
+setup(
+    name = 'zyw',
+    version='1.0.0',
+    author = '灵岩',
+    author_email='1318215328@qq.com',
+    # py_modules = 'zyw',
+    url = 'http://www.xxxxx.com',
+    description='这是一个测试打包的过程',
+    py_modules = ['zyw']
+)
---
+++ packages/zyw/zyw-1.0.0.tar.gz/zyw-1.0.0/zyw.py
@@ -0,0 +1,3 @@
+# author：灵岩（1318215328@qq.com）
+# 创建于: 11/30 0030 17:02
+print("hello world")
