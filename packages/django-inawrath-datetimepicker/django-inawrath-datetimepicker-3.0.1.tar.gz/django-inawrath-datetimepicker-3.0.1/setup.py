b63487737ab1c5116b501e6fd67796f0cf11f9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:25 +0000

    Add package django-inawrath-datetimepicker/3.0.1/django-inawrath-datetimepicker-3.0.1.tar.gz

---
+++ packages/django-inawrath-datetimepicker/django-inawrath-datetimepicker-3.0.1.tar.gz/django-inawrath-datetimepicker-3.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='django-inawrath-datetimepicker',
+    packages=['datetimepicker',],
+    package_data={'datetimepicker': ['static/datetimepicker/css/*.css',
+                                     'static/datetimepicker/js/*.js',
+                                     'static/datetimepicker/vendor/js/jquery.min.js']},
+    include_package_data=True,
+    version='3.0.1',
+    description='Datetimepicker for Django projects. Fork to fix issues',
+    long_description=long_description,
+    author='Inawrath',
+    author_email='inawrathc@gmail.com',
+    url='https://github.com/inawrath/django-datetimepicker.git',
+    license='Apache License 2.0',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+    ],
+)
