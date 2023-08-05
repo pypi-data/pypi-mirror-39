7d0e172d82b6faa253be180a65d1c246d25037b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:07 +0000

    Add package leprikon/1.18.0/leprikon-1.18.0.tar.gz

---
+++ packages/leprikon/leprikon-1.18.0.tar.gz/leprikon-1.18.0/leprikon/models/__init__.py
@@ -0,0 +1,5 @@
+from . import (  # noqa
+    account, agegroup, citizenship, courses, department, events, fields,
+    leprikonsite, messages, place, printsetup, question, roles, school,
+    schoolyear, startend, subjects, timesheets, useragreement, utils,
+)
