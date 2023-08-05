38cac095d3f5b3cd9802e76c881784733ef2494d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:39:51 +0000

    Add package ebook-crawler/2.1.5/ebook_crawler-2.1.5-py3-none-any.whl

---
+++ packages/ebook-crawler/ebook_crawler-2.1.5-py3-none-any.whl/ebook_crawler/__init__.py
@@ -0,0 +1,16 @@
+def main():
+    print('''
+    +=+=+=+===========+=+=+=+=+=+=+=+=+=+===============+=+=+=+
+    | * * *           * [ N O T I C E ] *               * * * |
+    |-+-+-+-----------=-=-=-=-=-=-=-=-=-=---------------+-+-+-|
+    |                                                         |
+    | This project was renamed to "lightnovel-crawler"        |
+    | You can install the latest using:                       |
+    |                                                         |
+    | $ python3 -m pip install -U --user lightnovel-crawler   |
+    |                                                         |
+    | Check out my repository for latest updates:             |
+    | > https://github.com/dipu-bd/lightnovel-crawler         |
+    |                                                         |
+    +=+=+=+===========-=-=-=-=-=-=-=-=-=-===============+=+=+=+
+    ''')
