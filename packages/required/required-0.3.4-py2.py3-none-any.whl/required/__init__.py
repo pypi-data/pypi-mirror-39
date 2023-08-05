db2d98309045296b7fb47d11c574408eb94bab24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:40 +0000

    Add package required/0.3.4/required-0.3.4-py2.py3-none-any.whl

---
+++ packages/required/required-0.3.4-py2.py3-none-any.whl/required/__init__.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, division, print_function
+
+from .requires import Requires, RequirementError, validate, empty
+from .expressions import R, Func
+
+__version__ = "0.3.4"
+
+__title__ = "required"
+__description__ = "A easy dependency validator"
+__uri__ = "https://github.com/shezadkhan137/required"
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "Shezad Khan"
+__email__ = "shezadkhan137@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2018 Shezad Khan"
+
+__all__ = [
+    "Requires",
+    "RequirementError",
+    "R",
+    "Func",
+    "empty",
+    "validate",
+]
