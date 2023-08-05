3efdd77e62750bb46e212bbcb453521d30cfb2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:07 +0000

    Add package dateutil-br/0.0.4/dateutil-br-0.0.4.tar.gz

---
+++ packages/dateutil-br/dateutil-br-0.0.4.tar.gz/dateutil-br-0.0.4/README.md
@@ -0,0 +1,36 @@
+# Dateutil-br
+
+Implementation to python-dateutil with to Pt-br language. "(>_&lt;)"
+
+This implementation rewrites the constants used in the [dateutil](https://github.com/dateutil/dateutil) library's date parse.
+
+You can see the repository `pypi` on: https://pypi.org/project/dateutil-br/
+
+## Install
+
+```sh
+pip install dateutil-br
+```
+
+## Usage Exemples
+
+```python
+>>> from dateutil_br import BrParserInfo, parser
+
+>>> print(parser(BrParserInfo()).parse('08/04/2018'))
+"2018-04-08 00:00:00"
+
+>>> print(parser(BrParserInfo()).parse('10 de abril de 2018'))
+"2018-04-10 00:00:00"
+
+>>> print(parser(BrParserInfo()).parse('15 horas e 13 min de 10 de abril de 2018'))
+"2018-04-10 15:13:00"
+```
+
+## Maintainer
+
+This code is sustained by Magrathea Labs, in the repository https://github.com/magrathealabs/dateutil-br
+
+## License
+
+The package is available as open source under the terms of the MIT License.
---
+++ packages/dateutil-br/dateutil-br-0.0.4.tar.gz/dateutil-br-0.0.4/setup.py
@@ -0,0 +1,43 @@
+import codecs
+import os
+
+from setuptools import setup
+
+requires = [
+    'python-dateutil>=2.7.5',
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='dateutil-br',
+    version='0.0.4',
+    description='Implementation to Pt-br of python lib python-dateutil',
+    long_description=long_description,
+    url='http://github.com/magrathealabs/dateutil-br',
+    author='Magrathea Labs',
+    author_email='contact@magrathealabs.com',
+    license=license,
+    packages=['.'],
+    zip_safe=False,
+    keywords=['date parser python', 'date parser',
+              'dateutil', 'dateutil-br', 'data em português'],
+    install_requires=requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ]
+)
