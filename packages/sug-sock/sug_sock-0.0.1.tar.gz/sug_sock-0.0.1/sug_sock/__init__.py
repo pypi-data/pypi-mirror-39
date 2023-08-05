1c5d0846ae9c34c69ecf2ef036b6cda35bd5a8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:50 +0000

    Add package sug-sock/0.0.1/sug_sock-0.0.1.tar.gz

---
+++ packages/sug-sock/sug_sock-0.0.1.tar.gz/sug_sock-0.0.1/sug_sock/__init__.py
@@ -0,0 +1,10 @@
+'''
+socket extention api.
+builds on the standart socket library and adds more advance functions,
+making it easier to make more varied sockets.
+'''
+from .sug_sock import *
+
+
+def meme():
+	print("nice")
