1f839fe6e7d99264e4a5edd79170955c140532d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    Add package molurus/0.0.1/molurus-0.0.1.tar.gz

---
+++ packages/molurus/molurus-0.0.1.tar.gz/molurus-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(
+  name = 'molurus',
+  packages = ['molurus'],
+  version = '0.0.1',
+  description = 'A microservice framework for Python',
+  author = 'molurus Team',
+  author_email = 'contact@devebot.com',
+  url = 'https://github.com/molurusio/molurus',
+  download_url = 'https://github.com/molurusio/molurus/archive/0.0.1.tar.gz',
+  keywords = ['molurus', 'microservice'],
+  classifiers = [],
+)
