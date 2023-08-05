27a70524d586fe6295820e60b2df4716532e7b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:43 +0000

    Add package anosql/0.3.0/anosql-0.3.0.tar.gz

---
+++ packages/anosql/anosql-0.3.0.tar.gz/anosql-0.3.0/anosql/__init__.py
@@ -0,0 +1,4 @@
+from anosql.core import load_queries, load_queries_from_string
+from anosql.loaders import register_query_loader, QueryLoader
+from anosql.exceptions import SQLLoadException, SQLParseException
+
