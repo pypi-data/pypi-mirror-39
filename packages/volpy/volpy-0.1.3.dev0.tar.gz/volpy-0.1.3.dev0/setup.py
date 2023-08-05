51f057dae9473cecf5ab0fe644c641eef802a29f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:59 +0000

    Add package volpy/0.1.3.dev0/volpy-0.1.3.dev0.tar.gz

---
+++ packages/volpy/volpy-0.1.3.dev0.tar.gz/volpy-0.1.3.dev0/setup.py
@@ -0,0 +1,28 @@
+# Publish by:
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
+
+from setuptools import setup, find_packages
+
+setup(
+    name='volpy',
+    version='0.1.3dev',
+    packages=find_packages(),
+    install_requires=['numpy',
+                      'pandas',
+                      'utm',
+                      'sympy',
+                      'plotly',],
+
+    package_data={
+        'volpy': ['sample_data/*.csv', 'sample_data/*.gpx', 'sample_data/*.txt', ],
+    },
+
+    author='Andre Guerra',
+    author_email='agu3rra@gmail.com',
+    description='volpy: Volume calculations in Python',
+    long_description='Calculate volumes out of a Digital Elevation Model (DEM) represented by Triangulated Irregular Network',
+    url='https://github.com/agu3rra/volpy/',
+    license='GPL',
+    keywords='gps volume terrain survey'
+)
