02e380112e7ef772616102525b7016981ba9afd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:21 +0000

    Add package edgepy/0.1dev/edgepy-0.1dev.tar.gz

---
+++ packages/edgepy/edgepy-0.1dev.tar.gz/edgepy-0.1dev/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'edgepy',
+	version = '0.1dev',
+	packages = ['edgepy',],
+	license = 'Creative Commons Attribution-Noncommercial-Share Alike license',
+	long_description = open('README.txt').read(),
+)
+
