d04e900b6d6d83a7c44664ae57e049036a1c9c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:30 +0000

    Add package imagedt/0.0.31/imagedt-0.0.31-py2.py3-none-any.whl

---
+++ packages/imagedt/imagedt-0.0.31-py2.py3-none-any.whl/imagedt/file/file_write.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+from __future__ import absolute_import
+from __future__ import print_function
+
+import csv
+import json
+
+
+def write_txt(data, path):
+    with open(path, "w") as text_file:
+        if isinstance(data, (str, unicode)):
+            text_file.write(str(data))
+        elif isinstance(data, list):
+            for line in data:
+                if isinstance(line, (str, unicode)):
+                    text_file.write(str(line) + '\n')
+        else:
+            text_file.write(json.dumps(data))
+
+
+def write_csv(data, path, is_excel=False):
+  with file(path, 'wb') as csvfile:
+    if is_excel:
+      csvfile.write(u"\ufeff")
+    writer = csv.writer(csvfile)
+    for row in data:
+      writer.writerow(row)
+
+
+def readlines(file_path):
+    with open(file_path, 'r') as f:
+        lines = [item.strip() for item in f.readlines()]
+    return lines
