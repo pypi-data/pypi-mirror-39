fde4201c03d31259b9bcf1cc67fafb1764a7a608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:40 +0000

    Add package Flask-GraphQL-Auth/1.1.2/Flask-GraphQL-Auth-1.1.2.tar.gz

---
+++ packages/Flask-GraphQL-Auth/Flask-GraphQL-Auth-1.1.2.tar.gz/Flask-GraphQL-Auth-1.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='Flask-GraphQL-Auth',
+    version='1.1.2',
+    url="https://github.com/devArtoria/flask-graphql-auth",
+    license='MIT',
+    author='devArtoria',
+    author_email='artoria@artoria.us',
+    keywords=['jwt', 'auth', 'graphql'],
+    description="JWT library for Flask-GraphQL",
+    packages=['flask_graphql_auth'],
+    install_requires=[
+        'PyJWT',
+        'Flask_GraphQL',
+        'graphene'
+    ]
+)
