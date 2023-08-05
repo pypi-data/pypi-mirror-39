ddf249f191be622fb1ec039d3363e7d6dfa5639f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:23 +0000

    Add package osensaplantiga-dev/0.1.14/osensaplantiga-dev-0.1.14.tar.gz

---
+++ packages/osensaplantiga-dev/osensaplantiga-dev-0.1.14.tar.gz/osensaplantiga-dev-0.1.14/setup.py
@@ -0,0 +1,29 @@
+# Upload command:
+# python setup.py sdist bdist_wheel upload [-r pypitest] (if .pypirc setup)
+# python setup.py sdist bdist_wheel upload [-r https://test.pypi.org/legacy/]
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='osensaplantiga-dev',
+    packages=find_packages(),
+    install_requires=['pyserial', 'python-dateutil', 'xmodem', 'fastavro'],
+    python_requires='>=3.6',
+    version='0.1.14',
+    description='OSENSA-Plantiga Python library',
+    long_description=long_description,
+    author='OSENSA Innovations',
+    author_email='cng@osensa.com',
+    license='MIT',
+    keywords=[],
+    classifiers=[],
+)
