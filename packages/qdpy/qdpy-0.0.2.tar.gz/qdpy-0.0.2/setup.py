afae41f3f4589123a284e0d28b60ff0321fe9d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:02 +0000

    Add package qdpy/0.0.2/qdpy-0.0.2.tar.gz

---
+++ packages/qdpy/qdpy-0.0.2.tar.gz/qdpy-0.0.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+from setuptools import setup, find_packages
+
+packages = find_packages(exclude=['examples'])
+
+setup(name='qdpy',
+      version='0.0.2',
+      description='Quality-Diversity algorithms in Python',
+      url='https://gitlab.com/leo.cazenille/qdpy',
+      author='Leo Cazenille',
+      author_email='leo.cazenille@gmail.com',
+      license='LGPLv3',
+      packages=packages,
+      install_requires=['numpy', 'scipy', 'sklearn', 'matplotlib'],
+      zip_safe=False)
+
+# MODELINE	"{{{1
+# vim:expandtab:softtabstop=4:shiftwidth=4:fileencoding=utf-8
+# vim:foldmethod=marker
