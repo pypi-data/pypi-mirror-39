b4a5467c75d577a9f8af2c8f2d9e134c0488ab98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package flythings/1.1.0/flythings-1.1.0.zip

---
+++ packages/flythings/flythings-1.1.0.zip/flythings-1.1.0/flythings/__init__.py
@@ -0,0 +1,11 @@
+import os
+try:
+	import requests
+except ImportError:
+	print ("Trying to Install required module: requests\n")
+	os.system('python -m pip install requests')
+
+from flythings.client import search,sendObservation,sendObservations,login,setDevice,setSensor,setServer\
+	,setToken,setTimeout,getObservation,setCustomHeader,setWorkspace,sendSocket,findSeries,sendRecord\
+	,registerAction, registerActionForSeries,startActionListening, stopActionListening, ActionDataTypes\
+	,loadDataByFile, acumulateObs, setBatchEnabled
