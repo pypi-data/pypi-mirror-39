fc1eba6c13b495fef39d08182c6c1f4cfd61d5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:42 +0000

    Add package tasker-bot/0.0.1/tasker_bot-0.0.1.tar.gz

---
+++ packages/tasker-bot/tasker_bot-0.0.1.tar.gz/tasker_bot-0.0.1/README.md
@@ -0,0 +1,3 @@
+# tasker.bot
+
+Comando de control y gestión de tareas.
---
+++ packages/tasker-bot/tasker_bot-0.0.1.tar.gz/tasker_bot-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tasker_bot",
+    version="0.0.1",
+    author="Edwin Ariza",
+    author_email="me@edwinariza.com",
+    description="Bot para gestionar y automatizar la gestión de tareas.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/edwinariza/tasker_bot/src/master/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
