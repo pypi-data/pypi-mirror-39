a24f815c318ea0c93b0511fc3d4b77e6d0cb2174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:11 +0000

    Add package Flask-WX-OAuth/0.1.3/Flask-WX-OAuth-0.1.3.tar.gz

---
+++ packages/Flask-WX-OAuth/Flask-WX-OAuth-0.1.3.tar.gz/Flask-WX-OAuth-0.1.3/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='Flask-WX-OAuth',
+    version='0.1.3',
+    description='Flask Extension for wechat oauth2.0.',
+    long_description=readme,
+    author='codeif',
+    author_email='me@codeif.com',
+    url='https://github.com/codeif/Flask-WX-OAuth',
+    license='MIT',
+    install_requires=['rauth'],
+    packages=find_packages(),
+)
