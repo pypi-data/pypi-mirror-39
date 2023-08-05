d078984fa29e5c7bb269f8ab85596b9892fe5836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:36 +0000

    Add package pyreduce/1.0.1/pyreduce-1.0.1.tar.gz

---
+++ packages/pyreduce/pyreduce-1.0.1.tar.gz/pyreduce-1.0.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup, Extension
+
+NAME = 'pyreduce'
+VERSION = '1.0.1'
+
+
+def main():
+    setup(name=NAME,
+          # url='https://github.com/danishprakash/pyreduce',
+          version=VERSION,
+          description='Reduce Python programs',
+          long_description=open('README.rst', 'r').read(),
+          # author='Danish Prakash',
+          # author_email='danishprakash@outlook.com',
+          platforms='UNIX',
+          license='GPL',
+          keywords=['language'],
+          scripts=['pyreduce'],
+          classifiers=['Development Status :: 4 - Beta',
+                       'Intended Audience :: Developers',
+                       'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                       'Operating System :: POSIX :: Linux',
+                       'Programming Language :: C',
+                       'Programming Language :: Python :: 3.0',
+                       'Programming Language :: Python :: 3.1',
+                       'Programming Language :: Python :: 3.2',
+                       'Programming Language :: Python :: 3.3',
+                       'Programming Language :: Python :: 3.4',
+                       'Programming Language :: Python :: 3.5',
+                       'Programming Language :: Python :: 3.6',
+                       'Programming Language :: Python :: 3.7',
+                       'Topic :: Software Development :: Libraries :: Python Modules',
+                       'Topic :: System :: Filesystems',
+                       'Topic :: System :: Operating System',
+                       'Topic :: System :: Operating System Kernels :: Linux'])
+
+
+if __name__ == '__main__':
+    main()
