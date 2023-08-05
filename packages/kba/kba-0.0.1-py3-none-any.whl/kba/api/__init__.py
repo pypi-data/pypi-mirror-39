7731ffd3c8f82ef97eb0d961acb88819b5c87383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:12 +0000

    Add package kba/0.0.1/kba-0.0.1-py3-none-any.whl

---
+++ packages/kba/kba-0.0.1-py3-none-any.whl/kba/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from kba.api.concepts_api import ConceptsApi
+from kba.api.metadata_api import MetadataApi
+from kba.api.statements_api import StatementsApi
