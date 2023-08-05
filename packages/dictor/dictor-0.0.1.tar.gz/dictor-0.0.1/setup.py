4317d9cb62a43d6b170b6ce1b86474bb562f2c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:00 +0000

    Add package dictor/0.0.1/dictor-0.0.1.tar.gz

---
+++ packages/dictor/dictor-0.0.1.tar.gz/dictor-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dictor',
+      version='0.0.1',
+      description='an elegant dictionary and JSON handler',
+      url='https://github.com/perfecto25/dictor',
+      author='mike.reider',
+      author_email='mike.reider@gmail.com',
+      license='MIT',
+      packages=['dictor'],
+      zip_safe=False)
