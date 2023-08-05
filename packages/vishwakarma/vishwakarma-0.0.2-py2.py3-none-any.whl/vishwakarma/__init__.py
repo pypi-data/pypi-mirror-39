7841b699c3c9eacfb9603ba5a531df1de5add7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:40 +0000

    Add package vishwakarma/0.0.2/vishwakarma-0.0.2-py2.py3-none-any.whl

---
+++ packages/vishwakarma/vishwakarma-0.0.2-py2.py3-none-any.whl/vishwakarma/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+    __init__.py
+    ~~~~~~~~~~~
+
+    no description available
+
+    :copyright: (c) 2018 by Diagram AI.
+    :license: see LICENSE for more details.
+"""
+
+__title__ = 'vishwakarma'
+__version__ = '0.0.2'
+__author__ = 'diagram-ai'
+
+from .pgmplot import pgmplot
