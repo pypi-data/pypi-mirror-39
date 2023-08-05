c2dbb0bdbce6fc235ee396c3649059dab6407fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:59 +0000

    Add package pysradb/0.2.2/pysradb-0.2.2-py2.py3-none-any.whl

---
+++ packages/pysradb/pysradb-0.2.2-py2.py3-none-any.whl/pysradb/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for pysradb."""
+
+__author__ = """Saket Choudhary"""
+__email__ = 'saketkc@gmail.com'
+__version__ = '0.2.2'
+
+from .pysradb import download_sradb_file
+from .pysradb import SRAdb
