cca3a688628d659b97efa8850f08abe0137cd9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:40 +0000

    Add package cmdlr/4.1.3/cmdlr-4.1.3-py3-none-any.whl

---
+++ packages/cmdlr/cmdlr-4.1.3-py3-none-any.whl/cmdlr/info.py
@@ -0,0 +1,9 @@
+"""Cmdlr infomation files."""
+
+VERSION = (4, 1, 3)
+DESCRIPTION = ('Extensible comic subscriber.')
+LICENSE = 'MIT'
+AUTHOR = 'visig'
+AUTHOR_EMAIL = 'visig@protonmail.ch'
+PROJECT_URL = 'https://gitlab.com/visig/cmdlr'
+PROJECT_NAME = 'cmdlr'
