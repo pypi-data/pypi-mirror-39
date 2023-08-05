568985564f27194808a494d2de0ec246df90894c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:32 +0000

    Add package bumplus/0.1.6/bumplus-0.1.6-py2.py3-none-any.whl

---
+++ packages/bumplus/bumplus-0.1.6-py2.py3-none-any.whl/bumplus/errors.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+from .constants import CONF_FILE
+
+
+class BumplusError(Exception):
+    pass
+
+
+class NotBumplusDir(BumplusError):
+    def __init__(self):
+        msg = 'Could not find {0}'.format(CONF_FILE)
+        super(NotBumplusDir, self).__init__(msg)
+
+
+class VersionNotDefined(BumplusError):
+    def __init__(self):
+        msg = 'Version not defined'
+        super(VersionNotDefined, self).__init__(msg)
---
+++ packages/bumplus/bumplus-0.1.6-py2.py3-none-any.whl/bumplus/filters.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+def strftime(value, format='%x'):
+    return value.strftime(format)
+
+
+filters = {
+    'strftime': strftime,
+}
