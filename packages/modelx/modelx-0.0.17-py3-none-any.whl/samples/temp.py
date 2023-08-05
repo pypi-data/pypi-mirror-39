e63f4b96b62f80ab10e1b72a8c7b73ef48b2e27a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:03 +0000

    Add package modelx/0.0.17/modelx-0.0.17-py3-none-any.whl

---
+++ packages/modelx/modelx-0.0.17-py3-none-any.whl/samples/temp.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 25 21:42:26 2018
+
+@author: fumito
+"""
+
+import modelx as mx
+from modelx import defcells
+import modelx.core
+
+m, base = mx.new_model(), mx.new_space(name='base')
+base.new_space('child')
+
+sub = m.new_space(name='sub')
+#sub.new_cells('child')
+
+#m.sub.add_bases(m.base)
+
+# m---base--child
+#   |
+#   +-sub--child
+#
