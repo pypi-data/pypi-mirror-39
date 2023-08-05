ed26fc878c8ef3cd14b83482f742db1e5a7aca36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:46 +0000

    Add package vishwakarma/0.0.3/vishwakarma-0.0.3.tar.gz

---
+++ packages/vishwakarma/vishwakarma-0.0.3.tar.gz/vishwakarma-0.0.3/vishwakarma/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+'''
+    Python visualization library for Probabilistic Graphical Models, and a lot more!
+
+    :copyright: (c) 2018 by Diagram AI.
+    :license: see LICENSE for more details.
+'''
+
+__title__ = 'vishwakarma'
+__version__ = '0.0.3'
+__author__ = 'diagram-ai'
+
+from .pgmplot import pgmplot
