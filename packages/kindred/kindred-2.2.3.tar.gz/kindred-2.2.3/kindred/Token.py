38cc6ff846574d756037116096cde2782feb596f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:03 +0000

    Add package kindred/2.2.3/kindred-2.2.3.tar.gz

---
+++ packages/kindred/kindred-2.2.3.tar.gz/kindred-2.2.3/kindred/Token.py
@@ -0,0 +1,33 @@
+
+class Token:
+	"""
+	Individual word with lemma, part-of-speech and location in text.
+	"""
+	
+	def __init__(self,word,lemma,partofspeech,startPos,endPos):
+		"""
+		Constructor for Token class
+		
+		:param word: Unprocessed word
+		:param lemma: Lemmatized word
+		:param partofspeech: Part-of-speech of word
+		:param startPos: Start position of token in sentence
+		:param endPos: End position of token in sentence
+		:type word: str
+		:type lemma: str
+		:type partofspeech: str
+		:type startPos: int
+		:type endPos: int
+		"""
+
+		self.word = word
+		self.lemma = lemma
+		self.partofspeech = partofspeech
+		self.startPos = startPos
+		self.endPos = endPos
+
+	def __str__(self):
+		return self.word
+		
+	def __repr__(self):
+		return self.__str__()
