cc4a6663601413daa46c78141ca2be12251d1b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:21 +0000

    Add package xqrcode/1.0.0/xqrcode-1.0.0.tar.gz

---
+++ packages/xqrcode/xqrcode-1.0.0.tar.gz/xqrcode-1.0.0/setup.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xqrcode",
+    version="1.0.0",
+    author="@neoctobers",
+    author_email="neoctobers@gmail.com",
+    description="A simply QR-code decoder, from URL or a file.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/neoctobers/xqrcode",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pillow',
+        'pyzbar',
+    ],
+)
