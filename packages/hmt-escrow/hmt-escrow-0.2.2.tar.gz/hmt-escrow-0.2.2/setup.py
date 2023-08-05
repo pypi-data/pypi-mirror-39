b3c717cda3121ef0d29824661a70e5858f325417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:08 +0000

    Add package hmt-escrow/0.2.2/hmt-escrow-0.2.2.tar.gz

---
+++ packages/hmt-escrow/hmt-escrow-0.2.2.tar.gz/hmt-escrow-0.2.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="hmt-escrow",
+    version="0.2.2",
+    author="HUMAN Protocol",
+    description=
+    "A python library to launch escrow contracts to the HUMAN network.",
+    url="https://github.com/hCaptcha/hmt-escrow",
+    include_package_data=True,
+    zip_safe=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent", "Programming Language :: Python"
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "hypothesis==3.75.3",
+        "ipfsapi==0.4.3",
+        "py-solc==3.2.0",
+        "schematics==2.1.0",
+        "trinity==0.1.0a16",
+        "web3==4.4.1",
+    ])
