f4c4537d8e83a94fda9795ac84c73a212a81db73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:42 +0000

    Add package lbsntransform/0.1.510/lbsntransform-0.1.510.tar.gz

---
+++ packages/lbsntransform/lbsntransform-0.1.510.tar.gz/lbsntransform-0.1.510/lbsntransform/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Collection of classes to import, convert and export data
+from and to common lbsn structure
+
+For more info, see [concept](https://gitlab.vgiscience.de/lbsn/concept)
+"""
+
+from .classes.db_connection import DBConnection
+from .classes.helper_functions import HelperFunctions
+from .classes.helper_functions import LBSNRecordDicts
+from .classes.helper_functions import GeocodeLocations
+from .classes.helper_functions import TimeMonitor
+from .classes.field_mapping_twitter import FieldMappingTwitter
+from .classes.submit_data import LBSNTransfer
+from .config.config import BaseConfig
---
+++ packages/lbsntransform/lbsntransform-0.1.510.tar.gz/lbsntransform-0.1.510/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import sys
+
+with open('README.md') as f:
+    long_description = f.read()
+     
+     
+## setuptools dev
+setup(  name = "lbsntransform",
+        version = "0.1.510",
+        description = "Location based social network (LBSN) data structure format & transfer tool",
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author='Alexander Dunkel',
+        author_email='alexander.dunkel@tu-dresden.de',
+        url='https://gitlab.vgiscience.de/lbsn/lbsntransform',
+        license='GNU GPLv3 or any higher',
+        packages=['lbsntransform'],
+        install_requires=[
+            'lbsnstructure>=0.2.6.211',
+            'protobuf',
+            'psycopg2',
+            'ppygis3',
+            'shapely',
+            'emoji'
+        ],
+        entry_points={
+        'console_scripts': [
+            'lbsntransform = lbsntransform.__main__:main'
+        ]
+        })
