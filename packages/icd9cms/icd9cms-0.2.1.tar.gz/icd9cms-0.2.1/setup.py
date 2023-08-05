e0397f34a2462369ae021f44ad5f74db8cdb38ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:06 +0000

    Add package icd9cms/0.2.1/icd9cms-0.2.1.tar.gz

---
+++ packages/icd9cms/icd9cms-0.2.1.tar.gz/icd9cms-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="icd9cms",
+    description='A utility for searching across the full icd9cms (2015) diagnosis hierarchy',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tomolopolis/icd-util",
+    version='0.2.1',
+    author_email='thomas.searle@kcl.ac.uk',
+    author='Tom Searle',
+    packages=['icd9cms'],
+    install_requires=[
+        'pandas',
+        'bs4',
+        'requests',
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
