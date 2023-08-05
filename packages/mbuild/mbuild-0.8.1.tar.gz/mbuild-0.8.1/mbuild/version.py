5e2ef4160daab97815188829e078b50d20ef6623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:31 +0000

    Add package mbuild/0.8.1/mbuild-0.8.1.tar.gz

---
+++ packages/mbuild/mbuild-0.8.1.tar.gz/mbuild-0.8.1/mbuild/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '0.8.1'
+short_version = '0.8.1'
+full_version = '0.8.1'
+git_revision = '29ec2a60381371a269c42fc29d80b66616139b13'
+release = True
