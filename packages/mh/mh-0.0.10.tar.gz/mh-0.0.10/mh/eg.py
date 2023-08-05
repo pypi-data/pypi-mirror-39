bb8801304faac1c142661bf0807d87f4e96adb6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:30 +0000

    Add package mh/0.0.10/mh-0.0.10.tar.gz

---
+++ packages/mh/mh-0.0.10.tar.gz/mh-0.0.10/mh/eg.py
@@ -0,0 +1,16 @@
+# eg.py
+'''
+eg code
+'''
+
+EG_VAR='abcd'
+
+def test():
+    ''' returns a test string '''
+    return 'ok'
+
+class Mh (object):
+    def runMh(self):
+        print ('ok run')
+        return 10
+
