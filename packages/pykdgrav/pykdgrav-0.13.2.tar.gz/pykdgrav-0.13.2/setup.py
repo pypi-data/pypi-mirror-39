b096397e7e9113ee43c5a1eb5befc57ff85e8ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:40 +0000

    Add package pykdgrav/0.13.2/pykdgrav-0.13.2.tar.gz

---
+++ packages/pykdgrav/pykdgrav-0.13.2.tar.gz/pykdgrav-0.13.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pykdgrav',
+      version='0.13.2',
+      description='Fast approximate gravitational force and potential calculations',
+      url='http://github.com/omgspace/pykdgrav',
+      author='Mike Grudic',
+      author_email='mgrudich@caltech.edu',
+      license='MIT',
+      packages=['pykdgrav'],
+      zip_safe=False)
