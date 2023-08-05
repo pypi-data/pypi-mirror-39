9b8e1d7739e81d00cffed370376a681923a0d122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:53 +0000

    Add package bcf/0.23.2/bcf-0.23.2.tar.gz

---
+++ packages/bcf/bcf-0.23.2.tar.gz/bcf-0.23.2/README.md
@@ -0,0 +1,19 @@
+<a href="https://www.bigclown.com/"><img src="https://bigclown.sirv.com/logo.png" width="200" alt="BigClown Logo" align="right"></a>
+
+# Host Application - BigClown Firmware Tool
+
+[![Travis](https://img.shields.io/travis/bigclownlabs/bch-firmware-tool/master.svg)](https://travis-ci.org/bigclownlabs/bch-firmware-tool)
+[![Release](https://img.shields.io/github/release/bigclownlabs/bch-firmware-tool.svg)](https://github.com/bigclownlabs/bch-firmware-tool/releases)
+[![License](https://img.shields.io/github/license/bigclownlabs/bch-firmware-tool.svg)](https://github.com/bigclownlabs/bch-firmware-tool/blob/master/LICENSE)
+[![Twitter](https://img.shields.io/twitter/follow/BigClownLabs.svg?style=social&label=Follow)](https://twitter.com/BigClownLabs)
+
+This repository contains BigClown Firmware Tool.
+
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT/) - see the [LICENSE](LICENSE) file for details.
+
+---
+
+Made with &#x2764;&nbsp; by [**HARDWARIO s.r.o.**](https://www.hardwario.com/) in the heart of Europe.
---
+++ packages/bcf/bcf-0.23.2.tar.gz/bcf-0.23.2/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='bcf',
+    packages=["bcf", "bcf.flasher", "bcf.flasher.serialport", "bcf.log", "bcf.repos", "bcf.firmware"],
+    version='0.23.2',
+    description='BigClown Firmware Tool.',
+    author='HARDWARIO s.r.o.',
+    author_email='karel.blavka@bigclown.com',
+    url='https://github.com/bigclownlabs/bch-firmware-tool',
+    include_package_data=True,
+    install_requires=[
+        'appdirs>=1.4', 
+        'pyserial>=3.0', 
+        'colorama>=0.3', 
+        'PyYAML>=3.11', 
+        'schema>=0.6.7', 
+        'requests>=2.18', 
+        'Click>=6.0', 
+        'intelhex>=2.2.1'
+    ],
+    license='MIT',
+    zip_safe=False,
+    keywords=['BigClown', 'bcf', 'firmware', 'flasher'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+        'Environment :: Console'
+    ],
+    entry_points='''
+        [console_scripts]
+        bcf=bcf.cli:main
+    ''',
+    long_description='''
+BigClown Firmware Tool.
+'''
+)
