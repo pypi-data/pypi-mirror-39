ea849f32a886ec9b217b40eb902917ceff26227f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:12 +0000

    Add package spacy-nightly/2.1.0a3/spacy_nightly-2.1.0a3-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/spacy-nightly/spacy_nightly-2.1.0a3-cp27-cp27mu-manylinux1_x86_64.whl/spacy/cli/__init__.py
@@ -0,0 +1,14 @@
+from .download import download
+from .info import info
+from .link import link
+from .package import package
+from .profile import profile
+from .train import train
+from .pretrain import pretrain
+from .evaluate import evaluate
+from .convert import convert
+from .vocab import make_vocab as vocab
+from .init_model import init_model
+from .validate import validate
+from .ud_train import main as ud_train
+from .conll17_ud_eval import main as ud_evaluate
