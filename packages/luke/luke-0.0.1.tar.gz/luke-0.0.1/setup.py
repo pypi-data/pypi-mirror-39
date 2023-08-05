bb594f093ffae0247c7bfd8c5942b0f3618c0bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:29 +0000

    Add package luke/0.0.1/luke-0.0.1.tar.gz

---
+++ packages/luke/luke-0.0.1.tar.gz/luke-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='luke',
+    version='0.0.1',
+    description='A Python-based simple search',
+    long_description='',
+    author='Ikuya Yamada',
+    author_email='ikuya@ousia.jp',
+    packages=['luke'],
+    license=open('LICENSE').read(),
+    include_package_data=True,
+    keywords=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    install_requires=[],
+    tests_require=[],
+)
