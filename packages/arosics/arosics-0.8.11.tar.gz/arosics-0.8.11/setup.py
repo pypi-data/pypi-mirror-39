3b735c6b0cb84b87bf17baa87d3ef5c17c663e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:07 +0000

    Add package arosics/0.8.11/arosics-0.8.11.tar.gz

---
+++ packages/arosics/arosics-0.8.11.tar.gz/arosics-0.8.11/setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+import warnings
+from importlib import util
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+version = {}
+with open("arosics/version.py") as version_file:
+    exec(version_file.read(), version)
+
+requirements = ['numpy', 'gdal', 'shapely', 'scikit-image', 'matplotlib', 'geopandas', 'geoarray>=0.8.0',
+                'py_tools_ds>=0.12.1', 'plotly', 'cmocean', 'six', 'folium>=0.6.0', 'geojson'
+                # 'pykrige'  # conda install --yes -c conda-forge pykrige
+                # 'pyfftw', # conda install --yes -c conda-forge pyfftw=0.10.4 ; \
+                # 'basemap', # conda install --yes -c conda-forge basemap; \
+                ]
+
+setup_requirements = [
+    # TODO(danschef): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = requirements + ['coverage', 'nose', 'nose-htmloutput', 'rednose']
+
+setup(
+    name='arosics',
+    version=version['__version__'],
+    description="An Automated and Robust Open-Source Image Co-Registration Software for Multi-Sensor Satellite Data",
+    long_description=readme + '\n\n' + history,
+    author="Daniel Scheffler",
+    author_email='daniel.scheffler@gfz-potsdam.de',
+    url='https://gitext.gfz-potsdam.de/danschef/arosics',
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=["bin/arosics_cli.py"],
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    zip_safe=False,
+    keywords='arosics',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
+
+
+# check for pyfftw
+if not util.find_spec('pyfftw'):
+    warnings.warn('You need to install pyfftw manually (see https://pypi.python.org/pypi/pyFFTW) for speeding up '
+                  'the computation. It is not automatically installed.')
+
+# check for basemap
+if not util.find_spec('mpl_toolkits.basemap'):
+    warnings.warn('You need to install basemap manually if you want to plot maps (see www./matplotlib.org/basemap). '
+                  'It is not automatically installed.')
+
+# check for pykrige
+if not util.find_spec('pykrige'):
+    warnings.warn('You need to install pykrige manually if you want to interpolate tie point grids produced by AROSICS '
+                  '(see https://github.com/bsmurphy/PyKrige). It is not automatically installed.')
