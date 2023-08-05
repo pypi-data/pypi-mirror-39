772f00159f35348d81e3a579629fadcd338b0876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:22 +0000

    Add package tkthread/0.3.0/tkthread-0.3.0.zip

---
+++ packages/tkthread/tkthread-0.3.0.zip/tkthread-0.3.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from tkthread._version import __version__
+
+with open('README.md', encoding='utf-8') as fid:
+    LONG_DESCRIPTION = fid.read()
+
+setup(
+    name='tkthread',
+    version=__version__,
+    author='Roger D. Serwy',
+    author_email='roger.serwy@gmail.com',
+    license="Apache Version 2.0",
+    keywords="tkinter threading",
+    url="http://github.com/serwy/tkthread",
+    packages=['tkthread'],
+    description='Easy multithreading with Tkinter',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: User Interfaces',
+    ],
+)
