c9e4b26e7d5a1e73a468cd7921eeb457fc2adeee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:49 +0000

    Add package PDBCOCO/0.2/PDBCOCO-0.2.tar.gz

---
+++ packages/PDBCOCO/PDBCOCO-0.2.tar.gz/PDBCOCO-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='PDBCOCO',version='0.2',
+      description='correcting errors in PDB files',
+      url='https://github.com/jordan078/PDBCOCO',
+      author='Jordan Yang',
+      author_email='haobo_yang@brown.edu',
+      license='Brown',
+      packages=['PDBCOCO'],
+      zip_safe=False)
