90e09eb068dfa1e178fc99bf7bfd2dd077de686c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:37 +0000

    Add package pypi-hello-world/1.0/pypi_hello_world-1.0.tar.gz

---
+++ packages/pypi-hello-world/pypi_hello_world-1.0.tar.gz/pypi_hello_world-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pypi_hello_world',
+      version='1.0',
+      description='My PyPi hello world',
+      url='https://github.com/pasiasty/pypi_hello_world',
+      author='Mariusz Pasek',
+      author_email='pasiasty@gmail.com',
+      license='MIT',
+      packages=['pypi_hello_world'],
+      zip_safe=False)
