4377a25db10576dbf438f7f5f6851f221e457e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:26 +0000

    Add package edgepy/0.1.8dev/edgepy-0.1.8dev.tar.gz

---
+++ packages/edgepy/edgepy-0.1.8dev.tar.gz/edgepy-0.1.8dev/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+REQUIRES = [
+	'biopython>=1.71',
+	'xmltodict',
+	'pandas',
+]
+
+
+setup(
+	name = 'edgepy',
+	version = '0.1.8dev',
+	author = 'Alex Summers',
+	author_email='asummers.edgepy@gmail.com',
+	packages = ['edgepy'],
+	url = 'http://pypi.python.org/pypi/edgepy/',
+	license = 'LICENSE.txt',
+	long_description = open('README.txt').read(),
+	#install_requires = REQUIRES,
+)
+
