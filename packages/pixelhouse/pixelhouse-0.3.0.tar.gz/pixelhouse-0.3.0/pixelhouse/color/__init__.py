c3daa937b113dd611b9c3d2c62bc432a1ba8a067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:42 +0000

    Add package pixelhouse/0.3.0/pixelhouse-0.3.0.tar.gz

---
+++ packages/pixelhouse/pixelhouse-0.3.0.tar.gz/pixelhouse-0.3.0/pixelhouse/color/__init__.py
@@ -0,0 +1,8 @@
+from .colors import NamedColors
+from .palettes import ColorLoversPalette
+
+from .interpolation import RGBa_interpolation
+from .interpolation import LABa_interpolation
+
+matplotlib_colors = NamedColors()
+palette = ColorLoversPalette()
