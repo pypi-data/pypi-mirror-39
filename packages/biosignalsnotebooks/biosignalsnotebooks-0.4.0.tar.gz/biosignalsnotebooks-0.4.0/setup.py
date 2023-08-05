cd34e85e79bb6bc249f2c34b1649fe9847fe4313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:17 +0000

    Add package biosignalsnotebooks/0.4.0/biosignalsnotebooks-0.4.0.tar.gz

---
+++ packages/biosignalsnotebooks/biosignalsnotebooks-0.4.0.tar.gz/biosignalsnotebooks-0.4.0/setup.py
@@ -0,0 +1,41 @@
+"""
+File that specifies package requisites for a correct functioning of all available functions.
+
+"""
+
+from setuptools import setup
+from os import path
+from sys import platform
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README_BSN.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Adjust requirements accordingly to the operating system.
+if "linux" in platform:
+    requirements = ['numpy', 'matplotlib', 'scipy', 'h5py', 'python-magic', 'wget', 'datetime',
+          'libmagic', 'bokeh', 'scipy', 'IPython', 'pandas', 'nbformat', 'ipython', 'requests']
+else:
+    requirements = ['numpy', 'matplotlib', 'scipy', 'h5py', 'python-magic', 'wget', 'datetime',
+          'libmagic', 'bokeh', 'scipy', 'IPython', 'pandas', 'nbformat', 'ipython',
+          'python-magic-bin==0.4.14', 'requests']
+
+
+setup(name='biosignalsnotebooks',
+      version='0.4.0',#major.minor.build_nbr
+      description='A Python package for supporting the external loading and processing of '
+                  'OpenSignals electrophysiological acquisitions.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/biosignalsnotebooks/biosignalsnotebooks',
+      author='Plux Wireless Biosignals',
+      author_email='gramos@plux.info',
+      license='MIT',
+      packages=['biosignalsnotebooks'],
+      setup_requires=['numpy'],
+      install_requires=requirements,
+      zip_safe=False,
+      include_package_data=True)
+
+# 02/11/2018  17h33m :)
