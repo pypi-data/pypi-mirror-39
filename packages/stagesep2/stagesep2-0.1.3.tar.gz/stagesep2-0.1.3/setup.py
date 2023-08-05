1f3d27854dec9a9f7588dfca7337365abd988f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:38 +0000

    Add package stagesep2/0.1.3/stagesep2-0.1.3.tar.gz

---
+++ packages/stagesep2/stagesep2-0.1.3.tar.gz/stagesep2-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='stagesep2',
+    version='0.1.3',
+    description='Analyse, and convert video into useful data.',
+    author='williamfzc',
+    author_email='fengzc@vip.qq.com',
+    url='https://github.com/williamfzc/stagesep2',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        'structlog',
+        'numpy',
+        'jieba',
+        'scikit-image'
+    ]
+)
