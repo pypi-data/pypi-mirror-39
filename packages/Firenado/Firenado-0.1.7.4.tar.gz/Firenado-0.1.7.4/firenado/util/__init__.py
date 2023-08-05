d064ce6c8d748219b25592ed572de15f85642a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:24 +0000

    Add package Firenado/0.1.7.4/Firenado-0.1.7.4.tar.gz

---
+++ packages/Firenado/Firenado-0.1.7.4.tar.gz/Firenado-0.1.7.4/firenado/util/__init__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#
+# Copyright 2015-2018 Flavio Garcia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import (absolute_import, division, print_function,
+                        with_statement)
+
+import random
+import string
+
+
+# Used implementations described on: http://bit.ly/2gHlH9z
+# Recommended here: http://bit.ly/2fm97H3
+# Confirmed here:
+# https://docs.python.org/2/library/random.html#random.SystemRandom
+# TODO: Use that after 3.6 https://bit.ly/2wvubJ6
+def random_string(length=5, upper_chars=True, punctuation=False):
+    """
+    Generates a random string with the size equal to the given length.
+
+    The string is based on random choices from a sequence of ascii lower case
+    characters and digits.
+
+    If length is not informed the string size will be 5.
+    """
+    chars = string.ascii_lowercase + string.digits
+    if upper_chars:
+        chars += string.ascii_uppercase
+    if punctuation:
+        chars += string.punctuation
+    return ''.join(random.SystemRandom().choice(chars) for _ in range(length))
