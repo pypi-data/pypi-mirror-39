5a2485d5a796eeed380623f48ca39d2c865f92f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:09 +0000

    Add package functionTest/1.4.0/functionTest-1.4.0.tar.gz

---
+++ packages/functionTest/functionTest-1.4.0.tar.gz/functionTest-1.4.0/functionTest.py
@@ -0,0 +1,16 @@
+""" This is function.py and it provides one funciton called print_deep_level to print the list
+    \*data*\ is the data source
+     \*need*\ control whether  print Tab 
+     \*level*\ the number of printing Tab
+"""
+
+def print_deep_level(data,need=False,level=0):
+    """ print the list data"""
+    for entity in data:
+        if isinstance(entity,list):
+            print_deep_level(entity,level+1)
+        else:
+            if need:
+                for num in range(level):
+                    print("\t",end='')
+            print(entity)
