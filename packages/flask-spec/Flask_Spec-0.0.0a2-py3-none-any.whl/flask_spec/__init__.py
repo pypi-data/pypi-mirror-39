4c196bdcaf8b69c9f9f748fc1cb03c9696b4f3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:00 +0000

    Add package flask-spec/0.0.0a2/Flask_Spec-0.0.0a2-py3-none-any.whl

---
+++ packages/flask-spec/Flask_Spec-0.0.0a2-py3-none-any.whl/flask_spec/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from .extensions import SpecBlueprint
+from .registars import FlaskSpec
+
+
+__version__ = '0.0.0a2'
+
+__all__ = [
+    'SpecBlueprint',
+    'FlaskSpec',
+]
