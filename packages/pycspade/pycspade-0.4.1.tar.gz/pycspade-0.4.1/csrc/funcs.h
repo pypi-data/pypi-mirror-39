d64a1ff4360eab62312316bf34a61f8a8f4609c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:43 +0000

    Add package pycspade/0.4.1/pycspade-0.4.1.tar.gz

---
+++ packages/pycspade/pycspade-0.4.1.tar.gz/pycspade-0.4.1/csrc/funcs.h
@@ -0,0 +1,15 @@
+//
+// Created by Yukio Fukuzawa on 2/12/18.
+//
+
+#ifndef SPADE_UTILITY_FUNCS_H
+#define SPADE_UTILITY_FUNCS_H
+
+#include "utils.h"
+
+void sequenceFunc(int argc, char **argv);
+void exttposeFunc(int argc, char **argv);
+void getconfFunc(int argc, char **argv);
+void makebinFunc(int argc, char **argv);
+
+#endif //SPADE_UTILITY_FUNCS_H
