e9f0700d4dc7ed24d62bd87d4bc05b4a8b064e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:13:16 +0000

    Add package netease-musicbox/0.2.5.4/NetEase-MusicBox-0.2.5.4.tar.gz

---
+++ packages/netease-musicbox/NetEase-MusicBox-0.2.5.4.tar.gz/NetEase-MusicBox-0.2.5.4/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: omi
+# @Date:   2014-08-24 22:08:33
+# @Last Modified by:   omi
+# @Last Modified time: 2015-03-30 23:36:21
+
+'''
+__   ___________________________________________
+| \  ||______   |   |______|_____||______|______
+|  \_||______   |   |______|     |______||______
+
+________     __________________________  _____ _     _
+|  |  ||     ||______  |  |      |_____]|     | \___/
+|  |  ||_____|______|__|__|_____ |_____]|_____|_/   \_
+
+
++ ------------------------------------------ +
+|   NetEase-MusicBox               320kbps   |
++ ------------------------------------------ +
+|                                            |
+|   ++++++++++++++++++++++++++++++++++++++   |
+|   ++++++++++++++++++++++++++++++++++++++   |
+|   ++++++++++++++++++++++++++++++++++++++   |
+|   ++++++++++++++++++++++++++++++++++++++   |
+|   ++++++++++++++++++++++++++++++++++++++   |
+|                                            |
+|   A sexy cli musicbox based on Python      |
+|   Music resource from music.163.com        |
+|                                            |
+|   Built with love to music by omi          |
+|                                            |
++ ------------------------------------------ +
+
+'''
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+about = {}  # type: dict
+
+with open(os.path.join(here, 'NEMbox', '__version__.py'), 'r') as f:
+    exec(f.read(), about)
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    description=about['__description__'],
+    license=about['__license__'],
+    packages=find_packages(),
+    install_requires=[
+        'requests-cache',
+        'pycryptodomex',
+        'future',
+    ],
+    entry_points={
+        'console_scripts': [
+            'musicbox = NEMbox.__main__:start'
+        ],
+    },
+    keywords=['music', 'netease', 'cli', 'player'],
+)
