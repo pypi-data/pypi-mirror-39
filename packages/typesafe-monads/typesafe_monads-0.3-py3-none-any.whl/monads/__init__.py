fdbe7539d1dc8ccb6d3cd16807ad0316088710b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:36 +0000

    Add package typesafe-monads/0.3/typesafe_monads-0.3-py3-none-any.whl

---
+++ packages/typesafe-monads/typesafe_monads-0.3-py3-none-any.whl/monads/__init__.py
@@ -0,0 +1,7 @@
+from .functor import Functor
+from .applicative import Applicative
+from .monad import Monad
+from .list import List
+from .maybe import Maybe, Just, Nothing
+from .result import Result, Ok, Err
+from .reader import Reader
