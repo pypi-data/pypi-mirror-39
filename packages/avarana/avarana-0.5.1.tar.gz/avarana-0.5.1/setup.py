2c0764d7557d3c559ab09fd1a4f81e4d92cc14c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:37 +0000

    Add package avarana/0.5.1/avarana-0.5.1.tar.gz

---
+++ packages/avarana/avarana-0.5.1.tar.gz/avarana-0.5.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='avarana',
+      version='0.5.1',
+      description='Wrappers and functions for predictive modeling',
+      url='https://github.com/hrajiv',
+      author='Rajiv HS',
+      author_email='rajivhs@gmail.com',
+      license='MIT',
+      packages=['avarana'],
+      zip_safe=False)
