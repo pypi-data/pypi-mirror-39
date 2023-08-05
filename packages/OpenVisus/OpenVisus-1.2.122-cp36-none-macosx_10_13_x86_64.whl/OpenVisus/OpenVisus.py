15792df5893b302f8da96e12ca23c34605f5faf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:28 +0000

    Add package OpenVisus/1.2.122/OpenVisus-1.2.122-cp36-none-macosx_10_13_x86_64.whl

---
+++ packages/OpenVisus/OpenVisus-1.2.122-cp36-none-macosx_10_13_x86_64.whl/OpenVisus/OpenVisus.py
@@ -0,0 +1,14 @@
+import os,sys
+
+__this_dir__=os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+if not __this_dir__ in sys.path:
+  sys.path.append(__this_dir__)
+  
+__bin_dir__=os.path.abspath(__this_dir__+ "/bin")
+if not __bin_dir__ in sys.path:
+  sys.path.append(__bin_dir__)
+
+from NonGuiOpenVisus import *
+
+# prefer not to include this automatically
+# from GuiOpenVisus import *
