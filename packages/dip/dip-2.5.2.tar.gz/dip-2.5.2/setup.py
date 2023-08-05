14df37682a15e221f6466ec2f9d6574f878e23d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:41 +0000

    Add package dip/2.5.2/dip-2.5.2.tar.gz

---
+++ packages/dip/dip-2.5.2.tar.gz/dip-2.5.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    author='amancevice',
+    author_email='smallweirdnum@gmail.com',
+    description='Install CLIs using docker-compose',
+    entry_points={
+        'console_scripts': [
+            'dip=dip.main:dip',
+        ],
+    },
+    install_requires=[
+        'click >= 6.7.0',
+        'colored >= 1.3.93',
+        'docker-compose >= 1.23.0',
+        'gitpython >= 2.1.3',
+    ],
+    name='dip',
+    packages=['dip'],
+    setup_requires=['setuptools_scm'],
+    url='https://github.com/amancevice/dip',
+    use_scm_version=True)
