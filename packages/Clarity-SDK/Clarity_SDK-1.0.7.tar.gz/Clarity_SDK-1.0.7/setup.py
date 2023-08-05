38e79c1e922d2683f1ef78ddb0359eaa4972c000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:10 +0000

    Add package Clarity-SDK/1.0.7/Clarity_SDK-1.0.7.tar.gz

---
+++ packages/Clarity-SDK/Clarity_SDK-1.0.7.tar.gz/Clarity_SDK-1.0.7/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Clarity_SDK",
+    version="1.0.7",
+    author="Intuli",
+    author_email="intuli.developer@gmail.com",
+    description="Python SDK for the Clarity API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://test.pypi.org/legacy/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
