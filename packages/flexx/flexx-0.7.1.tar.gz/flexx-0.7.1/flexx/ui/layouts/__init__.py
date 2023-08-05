bd76b5796bd53fa7ed3156ddebecddae7e854a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:21 +0000

    Add package flexx/0.7.1/flexx-0.7.1.tar.gz

---
+++ packages/flexx/flexx-0.7.1.tar.gz/flexx-0.7.1/flexx/ui/layouts/__init__.py
@@ -0,0 +1,13 @@
+""" Namespace for all layout widgets.
+"""
+
+# flake8: noqa
+
+from .._widget import Widget
+
+from ._layout import Layout
+from ._hv import HVLayout, HBox, VBox, HFix, VFix, HSplit, VSplit
+from ._stack import StackLayout
+from ._tabs import TabLayout
+from ._pinboard import PinboardLayout
+from ._form import FormLayout
