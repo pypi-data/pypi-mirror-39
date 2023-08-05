6464cb40bdfd46248dab1a8af11d480aaa1b4eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:39 +0000

    Add package happytrans/0.8/happytrans-0.8.tar.gz

---
+++ packages/happytrans/happytrans-0.8.tar.gz/happytrans-0.8/happytrans/vanish_model_dir/test.py
@@ -0,0 +1,14 @@
+import van_model
+from tqdm import tqdm
+import codecs
+test1 = '../data/chinese/xiaohuangji.conv'
+p2c,c2p = '../data/p2c_happy.py','../data/c2p_happy.py'
+model = van_model.van_m(p2c=p2c,c2p=c2p)
+def convert(filename):
+    outfile = codecs.open(filename+'.cy','w','utf8')
+    lines = codecs.open(filename,'r','utf8').readlines()
+    for line in tqdm(lines):
+        out = model.convert(line)
+        outfile.write(line)
+        outfile.write(out)
+convert(test1)
