ba8b305445253b7482e5629bd7885a664acd9132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:33 +0000

    Add package redis-helper/0.3.34/redis_helper-0.3.34-py3-none-any.whl

---
+++ packages/redis-helper/redis_helper-0.3.34-py3-none-any.whl/redis_helper/scripts/shell.py
@@ -0,0 +1,16 @@
+import click
+from pprint import pprint
+
+
+@click.command()
+def main():
+    """Interactively select a Collection model and start ipython shell"""
+    import redis_helper as rh
+    import input_helper as ih
+
+    selected = rh.Collection.select_models(named=True)
+    ih.start_ipython(warn=True, rh=rh, ih=ih, **selected)
+
+
+if __name__ == '__main__':
+    main()
