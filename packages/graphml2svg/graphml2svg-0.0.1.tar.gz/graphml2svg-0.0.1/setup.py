6a64e3778d1fcdf09113e7b6bf7a6d427a06df3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:26 +0000

    Add package graphml2svg/0.0.1/graphml2svg-0.0.1.tar.gz

---
+++ packages/graphml2svg/graphml2svg-0.0.1.tar.gz/graphml2svg-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='graphml2svg',
+    url='https://github.com/alex-fe/Graphml-to-SVG-converter',
+    author='Alex Feldman',
+    author_email='alexfeldman93@gmail.com',
+    packages=['converter'],
+    install_requires=['svgwriter'],
+    version='0.0.1',
+    license='MIT',
+    description='YFile .graphml converter to .svg',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
