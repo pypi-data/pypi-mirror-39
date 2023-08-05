44d2228a52519d5bb2334e905bceb5341b8e36a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:31 +0000

    Add package pagarme-surfmappers/1.4.1/pagarme_surfmappers-1.4.1-py3-none-any.whl

---
+++ packages/pagarme-surfmappers/pagarme_surfmappers-1.4.1-py3-none-any.whl/pagarme_surfmappers/transaction.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import pagarme_surfmappers.constants as constants
+import pagarme_surfmappers.utils as utils
+
+
+class Transaction(object):
+    def __init__(self):
+        super(Transaction, self).__init__()
+
+    def create(self, data):
+        return utils.request_post(constants.CREATE_TRANSACTION_URL, data)
+
+    def capture(self, token, data):
+        api_route = constants.CAPTURE_TRANSACTION_URL.format(token)
+        return utils.request_post(api_route, data)
+
+    def calculate_installments_amount(self, data):
+        return utils.request_get(constants.CALCULE_TRANSACTION_INSTALLMENTS)
