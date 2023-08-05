28d183c001d9c50074f1fdbe0ef7e7ed9c03a2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:22 +0000

    Add package cmdline-mlgridengine-lazy/0.0.1/cmdline-mlgridengine-lazy-0.0.1.tar.gz

---
+++ packages/cmdline-mlgridengine-lazy/cmdline-mlgridengine-lazy-0.0.1.tar.gz/cmdline-mlgridengine-lazy-0.0.1/README.rst
@@ -0,0 +1,33 @@
+Terminal-api
+============
+
+This is a terminal that implements MLGridEngine apis.
+
+Requirements
+------------
+
+-  Python3.6
+
+Commands
+--------
+
+-  pip3 install -i `https://pypi.tuna.tsinghua.edu.cn/simple/`_ -r
+   requirements.txt
+
+For unit test
+-------------
+
+-  sh unit.sh
+
+How to run this terminal app
+----------------------------
+
+-  cd Terminal-api
+-  source run.sh
+
+Have fun with 'lazy' command!
+-----------------------------
+
+-  lazy --help for Usage
+
+.. _`https://pypi.tuna.tsinghua.edu.cn/simple/`: https://pypi.tuna.tsinghua.edu.cn/simple/
---
+++ packages/cmdline-mlgridengine-lazy/cmdline-mlgridengine-lazy-0.0.1.tar.gz/cmdline-mlgridengine-lazy-0.0.1/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+"""setup.py: setuptools control."""
+
+
+import re
+from setuptools import setup
+
+
+with open("README.rst", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+
+setup(
+    name = "cmdline-mlgridengine-lazy",
+    packages = ["TerminalApi"],
+    entry_points = {
+        "console_scripts": ['lazy=TerminalApi:main']
+        },
+    version = "0.0.1",
+    description = "Python command line application for mlgridengine.",
+    long_description = long_descr,
+    author = "RootLee",
+    author_email = "ligen_thu@163.com",
+    url = "https://github.com/ligengen/lazy-cmdline-tool",
+    )
