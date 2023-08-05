07f9cd1079fb44f433e069dd39dfc763f176663b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:01 +0000

    Add package protosync/0.0.3.2/protosync-0.0.3.2-py3-none-any.whl

---
+++ packages/protosync/protosync-0.0.3.2-py3-none-any.whl/protosync/dest/dest.py
@@ -0,0 +1,65 @@
+import pyrsync2
+import os
+from datetime import datetime
+from termcolor import colored
+from protosync.common import *
+
+
+def compute_dest_hashes(dst_root, structure):
+    structured_hashes = {}
+    for path in structure:
+        file_path = os.path.join(dst_root, path)
+        dir_path = os.path.dirname(file_path)
+
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        if not os.path.exists(file_path):
+            with open(file_path, "w") as f:
+                f.write('')
+
+        unpatched = open(file_path, 'rb')
+        hashes = pyrsync2.blockchecksums(unpatched)
+
+        structured_hashes[path] = hashes
+    return structured_hashes
+
+
+def update_dst(dst_root, structured_deltas):
+    structured_deltas = gen_dict_to_list_dict(structured_deltas)
+    for path, delta in structured_deltas.items():
+
+        if delta != [0]:
+            file_path = os.path.join(dst_root, path)
+            time_str = datetime.now().strftime('%H:%M:%S')
+            print('{}  File changed: {}'.format(time_str, file_path))
+            unpatched = open(file_path, 'rb')
+            unpatched.seek(0)
+            save_to = open(file_path, 'wb')
+            pyrsync2.patchstream(unpatched, save_to, delta)
+
+
+def dest_fetch_structure(pin):
+    structure = fetch_temp_and_load('/dest/fetch/structure', pin)
+    return structure
+
+
+def dest_push_hashes(pin, hashes):
+    hashes = gen_dict_to_list_dict(hashes)
+    save_temp_and_push(hashes, '/dest/push/hashes', pin)
+
+
+def dest_fetch_deltas(pin):
+    deltas = fetch_temp_and_load('/dest/fetch/deltas', pin)
+    deltas = list_dict_to_gen_dict(deltas)
+    return deltas
+
+
+def start_dest_sync(dest_root, pin):
+    print(colored('Syncing directory to source', 'yellow'))
+    while True:
+        structure = dest_fetch_structure(pin)
+        hashes = compute_dest_hashes(dest_root, structure)
+        dest_push_hashes(pin, hashes)
+        deltas = dest_fetch_deltas(pin)
+        update_dst(dest_root, deltas)
+
---
+++ packages/protosync/protosync-0.0.3.2-py3-none-any.whl/protosync/source/source.py
@@ -0,0 +1,54 @@
+import pyrsync2
+import os
+from termcolor import colored
+import uuid
+from protosync.common import list_dict_to_gen_dict, save_temp_and_push, fetch_temp_and_load, gen_dict_to_list_dict
+
+
+def get_src_structure(src_root):
+    structure = {}
+    for path, subdirs, files in os.walk(src_root):
+        for name in files:
+            file_path = os.path.join(path, name)
+            sub_path = file_path[len(src_root) + 1:]
+
+            structure[sub_path] = 0
+    return structure
+
+
+def compute_source_deltas(src_root, structured_hashes):
+    structured_deltas = {}
+    for path, hashes in structured_hashes.items():
+        file_path = os.path.join(src_root, path)
+        patchedfile = open(file_path, 'rb')
+        delta = pyrsync2.rsyncdelta(patchedfile, hashes)
+        structured_deltas[path] = delta
+    return structured_deltas
+
+
+def source_push_structure(pin, structure):
+    save_temp_and_push(structure, '/source/push/structure', pin)
+
+
+def source_fetch_hashes(pin):
+    hashes = fetch_temp_and_load('/source/fetch/hashes', pin)
+    hashes = list_dict_to_gen_dict(hashes)
+    return hashes
+
+
+def source_push_deltas(pin, deltas):
+    deltas = gen_dict_to_list_dict(deltas)
+    save_temp_and_push(deltas, '/source/push/deltas', pin)
+
+
+def start_source_sync(src_root, pin):
+    if len(pin) == 0:
+        pin = uuid.uuid4().hex
+    print('\n\tRun in remote repository:')
+    print('\t' + colored('protosync dest {}'.format(pin, src_root), 'yellow'))
+    while True:
+        structure = get_src_structure(src_root)
+        source_push_structure(pin, structure)
+        hashes = source_fetch_hashes(pin)
+        deltas = compute_source_deltas(src_root, hashes)
+        source_push_deltas(pin, deltas)
