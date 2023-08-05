388dbbe7a4f519f5e68f6796835195d15a8e239e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:03 +0000

    Add package ambition-ae/0.1.27/ambition_ae-0.1.27-py3-none-any.whl

---
+++ packages/ambition-ae/ambition_ae-0.1.27-py3-none-any.whl/ambition_ae/models/list_models.py
@@ -0,0 +1,16 @@
+from edc_base.model_mixins import ListModelMixin, BaseUuidModel
+
+
+class AntibioticTreatment(ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class MeningitisSymptom(ListModelMixin, BaseUuidModel):
+
+    pass
+
+
+class Neurological(ListModelMixin, BaseUuidModel):
+
+    pass
