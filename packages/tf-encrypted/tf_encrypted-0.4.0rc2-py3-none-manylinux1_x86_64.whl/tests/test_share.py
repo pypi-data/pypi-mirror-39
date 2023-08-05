64e48f9f6165a30c54c8d5ea97a8b73e2b52266a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:33 +0000

    Add package tf-encrypted/0.4.0rc2/tf_encrypted-0.4.0rc2-py3-none-manylinux1_x86_64.whl

---
+++ packages/tf-encrypted/tf_encrypted-0.4.0rc2-py3-none-manylinux1_x86_64.whl/tests/test_share.py
@@ -0,0 +1,38 @@
+import unittest
+
+import numpy as np
+import tensorflow as tf
+import tf_encrypted as tfe
+from tf_encrypted.tensor.prime import PrimeFactory
+from tf_encrypted.tensor import int64factory, int100factory
+
+
+class TestShare(unittest.TestCase):
+
+    def setUp(self):
+        tf.reset_default_graph()
+
+    def _core_test_sharing(self, dtype):
+
+        expected = np.array([[1, 2, 3], [4, 5, 6]])
+
+        with tfe.protocol.Pond() as prot:
+
+            with tfe.Session() as sess:
+                shares = prot._share(dtype.tensor(expected))
+                actual = sess.run(prot._reconstruct(*shares).to_native())
+
+        np.testing.assert_array_equal(actual, expected)
+
+    def test_int64(self):
+        self._core_test_sharing(int64factory)
+
+    def test_int100(self):
+        self._core_test_sharing(int100factory)
+
+    def test_prime(self):
+        self._core_test_sharing(PrimeFactory(67))
+
+
+if __name__ == '__main__':
+    unittest.main()
