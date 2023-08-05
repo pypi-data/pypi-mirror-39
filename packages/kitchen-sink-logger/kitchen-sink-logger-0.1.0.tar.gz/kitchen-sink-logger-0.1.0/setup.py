4b22d16d1c11c12e31f88d6876e4f7b7efd22b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:48 +0000

    Add package kitchen-sink-logger/0.1.0/kitchen-sink-logger-0.1.0.tar.gz

---
+++ packages/kitchen-sink-logger/kitchen-sink-logger-0.1.0.tar.gz/kitchen-sink-logger-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+setup(
+    name="kitchen-sink-logger",
+    version='0.1.0',
+    author="Troy Larson",
+    author_email="troylar@gmail.com",
+    description=(
+        "Easily log everything, including the kitchen sink, if that's your thing"),  # noqa: E501
+    license="MIT",
+    keywords="logging",
+    url="http://packages.python.org/kitchen-sink-logger",
+    packages=find_packages(),
+    long_description='here',
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+    ],
+)
