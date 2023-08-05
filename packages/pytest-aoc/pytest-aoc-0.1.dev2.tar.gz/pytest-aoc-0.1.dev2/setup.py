843c2355062da8813c443265269f68ad9c3e320f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:55 +0000

    Add package pytest-aoc/0.1.dev2/pytest-aoc-0.1.dev2.tar.gz

---
+++ packages/pytest-aoc/pytest-aoc-0.1.dev2.tar.gz/pytest-aoc-0.1.dev2/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="pytest-aoc",
+    py_modules="pytest_aoc",
+    install_requires=['requests'],
+    setup_requires=['setuptools-version-command'],
+    version_command=('git describe', 'pep440-git'),
+    author="Joost Molenaar",
+    author_email="j.j.molenaar@gmail.com",
+
+    entry_points={
+        "pytest11": ["pytest-aoc=pytest_aoc"]
+    },
+    classifiers=["Framework :: Pytest"])
