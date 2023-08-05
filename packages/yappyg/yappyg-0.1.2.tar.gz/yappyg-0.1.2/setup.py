e8ee3ad3020f0ed093e25496d5a7d419d8c33b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:53 +0000

    Add package yappyg/0.1.2/yappyg-0.1.2.tar.gz

---
+++ packages/yappyg/yappyg-0.1.2.tar.gz/yappyg-0.1.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+yappyG is Yet Another Package for Py Git
+"""
+
+from setuptools import find_packages, setup
+
+VERSION = '0.1.2'
+
+
+options = dict(
+    name="yappyg",
+    version=VERSION,
+    description="Yet Another Package for Py Git, that handles user/pass prompts in native python",
+    long_description=__doc__,
+    author="Lucas Durand",
+    author_email="lucas@lucasdurand.xyz",
+    license="BSD",
+    platforms="Any",
+    install_requires=['log_colorizer'],
+    provides=['yappyg'],
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules"])
+
+setup(**options)
