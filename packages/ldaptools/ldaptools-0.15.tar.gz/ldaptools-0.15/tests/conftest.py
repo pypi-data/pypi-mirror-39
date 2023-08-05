5ae20c5914bc2281dbc67e3194e3085a1393b553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:47 +0000

    Add package ldaptools/0.15/ldaptools-0.15.tar.gz

---
+++ packages/ldaptools/ldaptools-0.15.tar.gz/ldaptools-0.15/tests/conftest.py
@@ -0,0 +1,106 @@
+import pytest
+import tempfile
+import os
+import random
+
+from ldaptools.slapd import Slapd
+
+base_path = os.path.dirname(__file__)
+key_file = os.path.join(base_path, 'ssl-cert-snakeoil.key')
+certificate_file = os.path.join(base_path, 'ssl-cert-snakeoil.pem')
+
+
+@pytest.fixture
+def slapd(request):
+    with Slapd(ldap_url=getattr(request, 'param', None)) as s:
+        yield s
+
+
+@pytest.fixture
+def slapd_tcp1(request):
+    port = 4389
+    with Slapd(ldap_url='ldap://localhost:%s' % port) as s:
+        yield s
+
+
+@pytest.fixture
+def slapd_tcp2(request):
+    port = 5389
+    with Slapd(ldap_url='ldap://localhost:%s' % port) as s:
+        yield s
+
+
+@pytest.fixture
+def slapd_ssl(request):
+    port = 6389
+    with Slapd(ldap_url='ldaps://localhost:%s' % port, tls=(key_file, certificate_file)) as s:
+        yield s
+
+
+@pytest.fixture
+def slapd_tls(request):
+    port = 7389
+    with Slapd(ldap_url='ldap://localhost:%s' % port, tls=(key_file, certificate_file)) as s:
+        yield s
+
+
+@pytest.fixture(params=['slapd_tcp1', 'slapd_ssl', 'slapd_tls'])
+def any_slapd(request, slapd_tcp1, slapd_ssl, slapd_tls):
+    return vars().get(request.param)
+
+
+@pytest.fixture
+def ldif():
+    return '''dn: dc=orga2
+o: orga
+dc: orga2
+objectClass: organization
+objectClass: dcObject
+
+dn: uid=admin,dc=orga2
+objectClass: inetOrgPerson
+cn: John Doe
+uid: admin
+sn: John
+givenName: Doe
+mail: john.doe@entrouvert.com
+
+'''
+
+
+@pytest.fixture
+def attributes():
+    return ['o', 'objectClass', 'uid', 'sn', 'givenName', 'mail', 'dc', 'cn']
+
+
+@pytest.fixture
+def pivot_attributes():
+    return (
+        ('organization', 'o'),
+        ('inetOrgPerson', 'uid'),
+    )
+
+
+@pytest.fixture
+def ldif_path(request, ldif):
+    handle, path = tempfile.mkstemp()
+    with open(path, 'w') as f:
+        f.write(ldif)
+        f.flush()
+    def finalize():
+        os.unlink(path)
+    request.addfinalizer(finalize)
+    return path
+
+
+@pytest.fixture
+def attributes_path(request, attributes):
+    handle, path = tempfile.mkstemp()
+    with open(path, 'w') as f:
+        for attribute in attributes:
+            print >>f, ' %s ' % attribute
+        f.flush()
+    def finalize():
+        os.unlink(path)
+    request.addfinalizer(finalize)
+    return path
---
+++ packages/ldaptools/ldaptools-0.15.tar.gz/ldaptools-0.15/tests/test_ldif_utils.py
@@ -0,0 +1,14 @@
+import StringIO
+
+from ldaptools.ldif_utils import ListLDIFParser
+
+
+def test_ldifparser():
+    parser = ListLDIFParser(StringIO.StringIO('''dn: o=orga
+objectClass: organization
+
+'''))
+    parser.parse()
+    assert len(list(parser)) == 1
+    assert list(parser)[0][0] == 'o=orga'
+    assert list(parser)[0][1] == {'objectClass': ['organization']}
