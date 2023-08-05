e524e5587db4ba8783a1e6e88346d048c72124e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:43 +0000

    Add package pipedrive-python-lib/0.1.3/pipedrive-python-lib-0.1.3.tar.gz

---
+++ packages/pipedrive-python-lib/pipedrive-python-lib-0.1.3.tar.gz/pipedrive-python-lib-0.1.3/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name='pipedrive-python-lib',
+      version='0.1.3',
+      description='API wrapper for Pipedrive written in Python',
+      long_description=read('README.md'),
+      url='https://github.com/GearPlug/pipedrive-python',
+      author='Yordy Gelvez',
+      author_email='yordy.gelvez@gmail.com',
+      license='GPL',
+      packages=['pipedrive'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
