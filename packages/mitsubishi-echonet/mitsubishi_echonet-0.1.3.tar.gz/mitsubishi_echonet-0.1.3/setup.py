f47d2b5eb7729d014f103e06079c86113f436d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:27 +0000

    Add package mitsubishi-echonet/0.1.3/mitsubishi_echonet-0.1.3.tar.gz

---
+++ packages/mitsubishi-echonet/mitsubishi_echonet-0.1.3.tar.gz/mitsubishi_echonet-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='mitsubishi_echonet',
+    version='0.1.3',
+    author='Scott Phillips',
+    author_email='jrh@example.com',
+    packages=['mitsubishi_echonet'],
+    url='http://pypi.python.org/pypi/mitsubishi_echonet/',
+    license='LICENSE.txt',
+    description='A library for interfacing with Mitsubishi HVAC via the Echonet lite protocol.',
+    long_description=open('README.md').read(),
+    # install_requires=[
+    #    "Django >= 1.1.1",
+    #    "caldav == 0.1.4",
+    #],
+)
