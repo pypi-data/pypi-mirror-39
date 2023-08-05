8290f6c0493eabfb7bca1d72f0534ef52c2a76e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:33 +0000

    Add package Tuo_nester/1.2.0/Tuo_nester-1.2.0.tar.gz

---
+++ packages/Tuo_nester/Tuo_nester-1.2.0.tar.gz/Tuo_nester-1.2.0/nester.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  1 00:19:24 2018
+
+@author: neptune.sun
+"""
+
+def print_lol(the_list,indent=False,level=0): #indent控制是否缩进，默认否；level控制缩进级数，默认0
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(each_item)
---
+++ packages/Tuo_nester/Tuo_nester-1.2.0.tar.gz/Tuo_nester-1.2.0/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  1 00:21:41 2018
+
+@author: neptune.sun
+"""
+
+from distutils.core import setup
+
+setup(
+      name  = 'Tuo_nester',                    #必填
+      version = '1.2.0',                    #必填
+      py_modules = ['nester'],        #必填
+      author = 'Tuo.Sun',
+      author_email = 'neptune_ps@126.com',
+      description = 'A simple printer of nested lists.',
+      )
