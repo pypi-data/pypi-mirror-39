06f5b211b833ea8aea06697ed0025457b3c79408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:12 +0000

    Add package openxdf/0.4.1/openxdf-0.4.1.tar.gz

---
+++ packages/openxdf/openxdf-0.4.1.tar.gz/openxdf-0.4.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name="openxdf",
+    version="0.4.1",
+    author="Ryan Opel",
+    author_email="ryan.a.opel@gmail.com",
+    description="OpenXDF is a Python module built for interacting with Open eXchange Data Format files.",
+    long_description=open("README.md").read(),
+    url="https://github.com/opelr/openxdf",
+    packages=setuptools.find_packages(),
+    license="LICENSE",   
+)
