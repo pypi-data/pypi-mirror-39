7df6066eb93f2cfb1d48ec78ad6ae702035c83d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:07 +0000

    Add package phpbridge/0.0.1/phpbridge-0.0.1.tar.gz

---
+++ packages/phpbridge/phpbridge-0.0.1.tar.gz/phpbridge-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="phpbridge",
+    version="0.0.1",
+    author="Jan Verbeek",
+    author_email="jan.verbeek@posteo.nl",
+    description="Import PHP code into Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blyxxyz/Python-PHP-Bridge",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    license="ISC",
+    classifiers=[
+        "Programming Language :: PHP",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: ISC License (ISCL)",
+    ]
+)
