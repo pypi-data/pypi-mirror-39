604384e8aa3aa91fc3b6d145305e83e620c6c97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:29 +0000

    Add package django-perf-rec/3.1.0/django-perf-rec-3.1.0.tar.gz

---
+++ packages/django-perf-rec/django-perf-rec-3.1.0.tar.gz/django-perf-rec-3.1.0/tests/testapp/models.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    age = models.IntegerField()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=128)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class Award(models.Model):
+    name = models.CharField(max_length=128)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class Contract(models.Model):
+    amount = models.IntegerField()
+    author = models.ManyToManyField(Author)
