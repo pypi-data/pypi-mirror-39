9a8953dade9dbf5d63e092b10f7f23009b22fdbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:55 +0000

    Add package count-api/3.1.5/count_api-3.1.5.tar.gz

---
+++ packages/count-api/count_api-3.1.5.tar.gz/count_api-3.1.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="count_api",
+    version="3.1.5",
+    author="Count Technologies LTD",
+    author_email="hello@count.co",
+    description="API client for count.co",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://count.co",
+    packages=find_packages(),
+    install_requires=[
+      'requests',
+      'protobuf==3.5.1',
+      'future',
+      'pyhamcrest'
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
