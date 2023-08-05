a6ca8d72f912a4d4b00ca33cd21e5c2fb044adce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:36 +0000

    Add package platypush/0.9.2/platypush-0.9.2.tar.gz

---
+++ packages/platypush/platypush-0.9.2.tar.gz/platypush-0.9.2/platypush/backend/redis.py
@@ -0,0 +1,77 @@
+import json
+
+from redis import Redis
+
+from platypush.backend import Backend
+from platypush.message import Message
+
+
+class RedisBackend(Backend):
+    """
+    Backend that reads messages from a configured Redis queue (default:
+    ``platypush_bus_mq``) and posts them to the application bus.  Very
+    useful when you have plugin whose code is executed in another process
+    and can't post events or requests to the application bus.
+
+    Requires:
+
+        * **redis** (``pip install redis``)
+    """
+
+    def __init__(self, queue='platypush_bus_mq', redis_args={}, *args, **kwargs):
+        """
+        :param queue: Queue name to listen on (default: ``platypush_bus_mq``)
+        :type queue: str
+
+        :param redis_args: Arguments that will be passed to the redis-py constructor (e.g. host, port, password), see http://redis-py.readthedocs.io/en/latest/
+        :type redis_args: dict
+        """
+
+        super().__init__(*args, **kwargs)
+
+        self.queue = queue
+        self.redis_args = redis_args
+        self.redis = Redis(**self.redis_args)
+
+
+    def send_message(self, msg, queue_name=None):
+        msg = str(msg)
+        if queue_name:
+            self.redis.rpush(queue_name, msg)
+        else:
+            self.redis.rpush(self.queue, msg)
+
+
+    def get_message(self, queue_name=None):
+        queue = queue_name or self.queue
+        msg = self.redis.blpop(queue)[1].decode('utf-8')
+
+        try:
+            msg = Message.build(json.loads(msg))
+        except:
+            try:
+                import ast
+                msg = Message.build(ast.literal_eval(msg))
+            except:
+                try:
+                    msg = json.loads(msg)
+                except Exception as e:
+                    self.logger.exception(e)
+
+        return msg
+
+
+    def run(self):
+        super().run()
+
+        self.logger.info('Initialized Redis backend on queue {} with arguments {}'.
+                     format(self.queue, self.redis_args))
+
+        while not self.should_stop():
+            msg = self.get_message()
+            self.logger.info('Received message on the Redis backend: {}'.format(msg))
+            self.on_message(msg)
+
+
+# vim:sw=4:ts=4:et:
+
---
+++ packages/platypush/platypush-0.9.2.tar.gz/platypush-0.9.2/platypush/plugins/serial/__init__.py
@@ -0,0 +1,156 @@
+import json
+import serial
+import threading
+import time
+
+from platypush.plugins import Plugin, action
+from platypush.plugins.gpio.sensor import GpioSensorPlugin
+
+
+class SerialPlugin(GpioSensorPlugin):
+    """
+    The serial plugin can read data from a serial device, as long as the serial
+    device returns a JSON. You can use this plugin to interact for example with
+    some sensors connected through an Arduino. Just make sure that the code on
+    your serial device returns JSON values. If you're using an Arduino or any
+    ATMega compatible device, take a look at
+    https://github.com/bblanchon/ArduinoJson.
+    """
+
+    def __init__(self, device, baud_rate=9600, *args, **kwargs):
+        """
+        :param device: Device path (e.g. ``/dev/ttyUSB0`` or ``/dev/ttyACM0``)
+        :type device: str
+
+        :param baud_rate: Serial baud rate (default: 9600)
+        :type baud_rate: int
+        """
+
+        super().__init__(*args, **kwargs)
+
+        self.device = device
+        self.baud_rate = baud_rate
+        self.serial = None
+        self.serial_lock = threading.Lock()
+        self.last_measurement = None
+
+
+    def _read_json(self, serial_port):
+        n_brackets = 0
+        is_escaped_ch = False
+        parse_start = False
+        output = bytes()
+
+        while True:
+            ch = serial_port.read()
+            if not ch:
+                break
+
+            if ch.decode() == '{' and not is_escaped_ch:
+                parse_start = True
+                n_brackets += 1
+
+            if not parse_start:
+                continue
+
+            output += ch
+
+            if ch.decode() == '}' and not is_escaped_ch:
+                n_brackets -= 1
+                if n_brackets == 0:
+                    break
+
+            is_escaped_ch = ch.decode() == '\\'
+
+        return output.decode().strip()
+
+    def _get_serial(self, reset=False):
+        if self.serial:
+            if not reset:
+                return self.serial
+
+            self._close_serial()
+
+        self.serial = serial.Serial(self.device, self.baud_rate)
+        return self.serial
+
+    def _close_serial(self):
+        if self.serial:
+            try:
+                self.serial.close()
+                self.serial = None
+            except Exception as e:
+                self.logger.warning('Error while closing serial communication: {}')
+                self.logger.exception(e)
+
+    @action
+    def get_measurement(self):
+        """
+        Reads JSON data from the serial device and returns it as a message
+        """
+
+        data = None
+
+        try:
+            serial_available = self.serial_lock.acquire(timeout=2)
+            if serial_available:
+                try:
+                    ser = self._get_serial()
+                except:
+                    time.sleep(1)
+                    ser = self._get_serial(reset=True)
+
+                data = self._read_json(ser)
+
+                try:
+                    data = json.loads(data)
+                except:
+                    self.logger.warning('Invalid JSON message from {}: {}'.
+                                        format(self.device, data))
+            else:
+                data = self.last_measurement
+        finally:
+            try:
+                self.serial_lock.release()
+            except:
+                pass
+
+        if data:
+            self.last_measurement = data
+
+        return data
+
+    @action
+    def write(self, data):
+        """
+        Writes data to the serial device.
+
+        :param data: Data to send to the serial device
+        :type data: str, bytes or dict. If dict, it will be serialized as JSON.
+        """
+
+        if isinstance(data, dict):
+            data = json.dumps(data)
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+
+        try:
+            serial_available = self.serial_lock.acquire(timeout=2)
+            if serial_available:
+                try:
+                    ser = self._get_serial()
+                except:
+                    time.sleep(1)
+                    ser = self._get_serial(reset=True)
+
+                self.logger.info('Writing {} to {}'.format(data, self.device))
+                ser.write(data)
+        finally:
+            try:
+                self.serial_lock.release()
+            except:
+                pass
+
+
+# vim:sw=4:ts=4:et:
+
