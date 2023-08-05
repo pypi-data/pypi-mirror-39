c42b7d5acab642c1beaf142e73370d3168806807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:28 +0000

    Add package mautrix/0.4.0.dev18/mautrix-0.4.0.dev18.tar.gz

---
+++ packages/mautrix/mautrix-0.4.0.dev18.tar.gz/mautrix-0.4.0.dev18/mautrix/client/api/base.py
@@ -0,0 +1,67 @@
+from typing import Pattern, Optional, Tuple
+import asyncio
+import logging
+import re
+
+from ...api import HTTPAPI
+from .types import UserID
+
+
+class BaseClientAPI:
+    """
+    BaseClientAPI is the base class for :class:`ClientAPI`. This is separate from the main
+    ClientAPI class so that the ClientAPI methods can be split into multiple classes (that
+    inherit this class).All those section-specific method classes are inherited by the main
+    ClientAPI class to create the full class.
+    """
+    mxid_regex: Pattern = re.compile("@(.+):(.+)")
+
+    localpart: str
+    domain: str
+    mxid: UserID
+    api: HTTPAPI
+    loop: asyncio.AbstractEventLoop
+    log: logging.Logger
+
+    def __init__(self, mxid: UserID, api: HTTPAPI = None,
+                 loop: Optional[asyncio.AbstractEventLoop] = None, *args, **kwargs) -> None:
+        """
+        Initialize a ClientAPI. You must either provide the
+
+        Args:
+            mxid: The Matrix ID of the user. This is used for things like setting profile metadata.
+                Additionally, the homeserver domain is extracted from this string and used for
+                setting aliases and such. This can be changed later using `set_mxid`.
+            api: The :class:`HTTPAPI` instance to use. You can also pass the ``args`` and ``kwargs``
+                to create a HTTPAPI instance rather than creating the instance yourself.``
+        """
+        self.set_mxid(mxid)
+        self.loop = loop or asyncio.get_event_loop()
+        kwargs["loop"] = self.loop
+        self.api = api or HTTPAPI(*args, **kwargs)
+        self.log = self.api.log
+
+    def parse_mxid(self, mxid: UserID) -> Tuple[str, str]:
+        """
+        Parse the localpart and server name from a Matrix user ID.
+
+        Args:
+            mxid: The Matrix user ID.
+
+        Returns:
+            A tuple of (localpart, server_name)
+        """
+        mxid_parts = self.mxid_regex.match(mxid)
+        if not mxid_parts:
+            raise ValueError("invalid MXID")
+        return mxid_parts.group(1), mxid_parts.group(2)
+
+    def set_mxid(self, mxid: UserID) -> None:
+        """
+        Update the Matrix user ID used by this ClientAPI.
+
+        Args:
+            mxid: The new Matrix user ID.
+        """
+        self.localpart, self.domain = self.parse_mxid(mxid)
+        self.mxid = mxid
