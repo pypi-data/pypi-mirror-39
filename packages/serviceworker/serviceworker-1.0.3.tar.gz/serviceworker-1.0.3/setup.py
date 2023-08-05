050c8109b76ef47566722133db2de1d9241c301a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:56 +0000

    Add package serviceworker/1.0.3/serviceworker-1.0.3.tar.gz

---
+++ packages/serviceworker/serviceworker-1.0.3.tar.gz/serviceworker-1.0.3/setup.py
@@ -0,0 +1,29 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+requires = []
+
+message_extractors = {},
+
+setup(name='serviceworker',
+      version='1.0.3',
+      description='serviceworker',
+      long_description=README,
+      classifiers=[],
+      author='hemanjosko',
+      author_email='',
+      url='',
+      keywords='serviceworker',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='serviceworker',
+      install_requires=requires,
+      entry_points={'console_scripts': [
+            #'encrypt=main:run'
+            ]},
+      )
