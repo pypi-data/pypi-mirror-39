c2c41302e8647386b091e03a0f9147b3e8a7b1d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:24 +0000

    Add package django-denormalized/0.3.0/django_denormalized-0.3.0.tar.gz

---
+++ packages/django-denormalized/django_denormalized-0.3.0.tar.gz/django_denormalized-0.3.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='django_denormalized',
+    version='0.3.0',
+    packages=['denormalized'],
+    url='https://github.com/tumb1er/django-denormalized',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='Beer License',
+    author='Sergey Tikhonov',
+    author_email='zimbler@gmail.com',
+    description='Utils for maintaining denormalized '
+                'aggregates for Django models',
+    install_requires=['Django']
+)
