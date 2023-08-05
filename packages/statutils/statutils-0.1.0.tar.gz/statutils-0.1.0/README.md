a4572ca32ebe89db4c05528b1659f39d2f50f5f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:17 +0000

    Add package statutils/0.1.0/statutils-0.1.0.tar.gz

---
+++ packages/statutils/statutils-0.1.0.tar.gz/statutils-0.1.0/README.md
@@ -0,0 +1,22 @@
+# statutils
+
+This package is aselections of various utils for statistics.
+It is inspired by R functions and packages.
+
+## Usage
+
+### Adjust p values for multiple comparisons
+```python
+p = [0.005, 0.04, 0.01, 0.5, 0.01, 0.7]
+p_adjusted = p_adjust(p, method='holm')
+```
+
+Supported methods for multiple comparison:
+- Bonferroni
+- Holm-Bonferroni
+- Hochberg
+- Hommel
+- BH
+- BY
+- none
+
---
+++ packages/statutils/statutils-0.1.0.tar.gz/statutils-0.1.0/setup.py
@@ -0,0 +1,49 @@
+import setuptools
+import os
+import logging
+
+PACKAGE_NAME = 'statutils'
+
+
+def read_package_variable(key, filename='__init__.py'):
+    """Read the value of a variable from the package without importing."""
+    module_path = os.path.join(PACKAGE_NAME, filename)
+    with open(module_path) as module:
+        for line in module:
+            parts = line.strip().split(' ', 2)
+            if parts[:-1] == [key, '=']:
+                return parts[-1].strip("'")
+    logging.warning("'%s' not found in '%s'", key, module_path)
+    return None
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name=read_package_variable('__project__'),
+    version=read_package_variable('__version__'),
+    author='Sašo Karakatič',
+    author_email='karakatic@gmail.com',
+    description='Utils for statistics, mainly inspired by R packages.',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/karakatic/stat-utils',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering',
+    ],
+    install_requires=[
+        'numpy'
+    ]
+)
