d0caa0782652d7c60f4551ff98d55dada0a3ab7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:09 +0000

    Add package PyShEx/0.5.12/PyShEx-0.5.12.tar.gz

---
+++ packages/PyShEx/PyShEx-0.5.12.tar.gz/PyShEx-0.5.12/README.md
@@ -0,0 +1,134 @@
+# Python implementation of ShEx 2.0
+[![Pyversions](https://img.shields.io/pypi/pyversions/PyShEx.svg)](https://pypi.python.org/pypi/PyShEx)
+
+[![PyPi](https://img.shields.io/pypi/v/PyShEx.svg)](https://pypi.python.org/pypi/PyShEx)
+
+
+[![DOI](https://zenodo.org/badge/116042298.svg)](https://zenodo.org/badge/latestdoi/116042298)
+
+
+
+This package is a reasonably literal implementation of the [Shape Expressions Language 2.0](http://shex.io/shex-semantics/).  It can parse and "execute" ShExC and ShExJ source.
+
+## Revisions
+* 0.2.dev3 -- added SchemaEvaluator and other tweaks.  There are still some unit tests that fail -- beware
+* 0.3.0 -- Fix several issues.  Still does not pass all unit tests -- see `test_manifest.py` for details
+* 0.4.0 -- Added sparql_slurper capabilities. 
+* 0.4.1 -- Resolves several issues with reactome and disease test cases
+* 0.4.2 -- Fix issues #13 (missing start) and #14 (Inconsistent shape causes loop)
+* 0.4.3 -- Fix issues #16 and #15 and some refactoring
+* 0.5.0 -- First cut at returning fail reasons... some work still needed
+* 0.5.1 -- Update shexc parser to include multi-line comments and bug fixes
+* 0.5.2 -- Issue with installer - missed the parse_tree package
+* 0.5.3 -- make sparql_slurper a dependency
+* 0.5.4 -- Fixed long recursion issue with blood pressure example
+* 0.5.5 -- Fixed zero cardinality issue (#20)
+* 0.5.6 -- Added CLI entry point and cleaned up error reporting
+* 0.5.7 -- Throw an error on an invalid focus node (#23)
+* 0.5.9 -- Candidate for ShEx 2.1
+* 0.5.10 -- Fixed evaluator to load files, strings, etc. as ShEx
+* 0.5.11 -- Added Collections Flattening graph option to evaluator.
+* 0.5.12 -- Added -A option, catch missing start node early 
+
+## Installation
+```bash
+pip install PyShEx
+```
+Note: If you need to escape single quotes in RDF literals, you will need to install the bleeding edge
+of rdflib:
+```bash
+pip uninstall rdflib
+pip install git+https://github.com/rdflib/rdflib
+```
+Unfortunately, however, `rdflib-jsonld` is NOT compatible with the bleeding edge rdflib, so you can't use a json-ld parser in this situation.
+
+## evalshex CLI
+```bash
+> shexeval -h
+usage: shexeval [-h] [-f FORMAT] [-s START] [-fn FOCUS] [-A] [-d] [-ss] [-cf]
+                rdf shex
+
+positional arguments:
+  rdf                   Input RDF file or SPARQL endpoint if slurper option
+                        set
+  shex                  ShEx specification
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FORMAT, --format FORMAT
+                        Input RDF Format
+  -s START, --start START
+                        Start shape
+  -fn FOCUS, --focus FOCUS
+                        RDF focus node
+  -A, --allsubjects     Evaluate all non-bnode subjects in the graph
+  -d, --debug           Add debug output
+  -ss, --slurper        Use SPARQL slurper graph
+  -cf, --flattener      Use RDF Collections flattener graph
+
+
+```
+
+
+## General Layout
+The root `pyshex` package is subdivided into:
+
+* [shape_expressions_language](pyshex/shape_expressions_language) - implementation of the various sections in  [Shape Expressions Language 2.0](http://shex.io/shex-semantics/).  As an example, [3. Terminology](http://shex.io/shex-semantics/#terminology) is implemented in [p3_terminology.py](pyshex/shape_expressions_language/p3_terminology.py), [5.2 Validation Definition](http://shex.io/shex-semantics/#validation) in [p5_2_validation_definition.py](pyshex/shape_expressions_language/p5_2_validation_definition.py), etc.
+* [shapemap_structure_and_language](pyshex/shapemap_structure_and_language) - implementation of [ShapeMap Structure and Language](http://shex.io/shape-map/) (as well as we can understand it)
+* [sparql11_query](pyshex/sparql11_query) - required sections from [SPARQL 1.1 Query Language section 17.2](https://www.w3.org/TR/sparql11-query/#operandDataTypes)
+* [utils](pyshex/utils) - supporting utilities
+
+The ShEx schema definitions for this package come from [ShExJSG](https://github.com/hsolbrig/ShExJSG)
+
+We are trying to keep the python as close as possible to the (semi-)formal specification.  As an example, the statement:
+```text
+Se is a ShapeAnd and for every shape expression se2 in shapeExprs, satisfies(n, se2, G, m)
+``` 
+is implemented in Python as:
+```python
+        ...
+if isinstance(se, ShExJ.ShapeAnd):
+    return satisfiesShapeAnd(cntxt, n, se)
+        ...
+def satisfiesShapeAnd(cntxt: Context, n: nodeSelector, se: ShExJ.ShapeAnd) -> bool:
+    return all(satisfies(cntxt, n, se2) for se2 in se.shapeExprs)
+```
+
+## Dependencies
+This package is built using:
+* [ShExJSG](https://github.com/hsolbrig/ShExJSG) -- an object representation of the ShEx AST as defined by [ShEx.jsg](https://github.com/shexSpec/shexTest/blob/master/doc/ShExJ.jsg) and compiled through the [PyJSG](https://github.com/hsolbrig/pyjsg) compiler.
+* The python [ShExC](https://github.com/shexSpec/grammar/tree/master/parsers/python) compiler -- which transforms the [Shape Expressions Language](http://shex.io/shex-semantics/index.html) into ShExJSG images.
+* [rdflib](https://rdflib.readthedocs.io/en/stable/) 
+
+## Current status
+Performance has been improved, but our current implementation of the `sparql_slurper` is entirely too fine-grained. Our
+next steps include:
+1) Get non-conformance reasons into the responses
+2) Improve diagnostic and debugging tools
+3) Add a time-out to catch really long evaluations
+4) Adjust the slurper to pull larget chunks as needed and then refine on the retrieval end
+
+This implementation passes all of the tests in the master branch of [validation/manifest.ttl](https://raw.githubusercontent.com/shexSpec/shexTest/master/validation/manifest.ttl) with the following exceptions:
+
+At the moment, there are 1077 tests, of which:
+
+* 970 pass
+* 107 are skipped - reasons:
+1) (52) sht:toldBNode, sht:LexicalBNode and sht:BNodeShapeLabel test non-blank blank nodes (`rdflib` does not preserve bnode "identity")
+2) (24) sht:OutsideBMP -- test uses multi byte unicode (two aren't tagged)
+3) (16) Uses ShEx 2.1 IMPORT feature -- not yet implemented (three aren't tagged)
+5) (3) Focus is a Literal  -- not yet implemented
+6) (5) Uses ShEx 2.1 INCLUDE feature -- not yet implemented
+7) (3) Uses manifest shapemap feature -- not yet implemented
+8) (2) sht:relativeIRI -- this isn't a real problem, but we havent taken time to deal with this in the test harness
+9) (2) `rdflib` has a parsing error when escaping single quotes. (Issue submitted, awaiting release)
+
+As mentioned above, at the moment this is as literal an implementation of the specification as was sensible.  This means, in particular, that we are less than clever when it comes to partition management.
+
+
+
+## Notes
+[manifest_tester.py](tests/utils/manifest_tester.py) is the current testing tool.  Once we get through the complete set of tests we'll create a command line tool and a UI
+
+Note: At the moment we're just returning pass/fail.  We need to find documentation about what the return document should look like before we start returning detailed reports.
+
---
+++ packages/PyShEx/PyShEx-0.5.12.tar.gz/PyShEx-0.5.12/pyshex/shex_evaluator.py
@@ -0,0 +1,245 @@
+import sys
+from argparse import ArgumentParser
+from typing import Optional, Union, List, NamedTuple, Iterable, Type
+
+from CFGraph import CFGraph
+from ShExJSG import ShExJ, ShExC
+from rdflib import Graph, URIRef, Namespace
+from rdflib.util import guess_format
+from sparql_slurper import SlurpyGraph
+
+from pyshex.shape_expressions_language.p5_2_validation_definition import isValid
+from pyshex.shape_expressions_language.p5_context import Context
+from pyshex.shapemap_structure_and_language.p3_shapemap_structure import FixedShapeMap, ShapeAssociation, START
+from pyshex.utils.schema_loader import SchemaLoader
+
+
+class EvaluationResult(NamedTuple):
+    result: bool
+    focus: URIRef
+    start: URIRef
+    reason: Optional[str]
+
+
+# Handy types
+URI = Union[str, URIRef]
+URILIST = List[URI]
+URIPARM = Union[str, URIRef, URILIST]
+
+
+def normalize_uriparm(p: Optional[URIPARM]) -> Optional[List[URIRef]]:
+    return [URIRef(p)] if isinstance(p, str) else [p] if isinstance(p, URIRef) else p
+
+
+class ShExEvaluator:
+    """ Shape Expressions Evaluator """
+
+    def __init__(self,
+                 rdf: Optional[Union[str, Graph]] = None,
+                 schema: Optional[Union[str, ShExJ.Schema]] = None,
+                 focus: Optional[URIPARM] = None,
+                 start: Optional[URIPARM] = None,
+                 rdf_format: str = "turtle",
+                 debug: bool = False,
+                 debug_slurps: bool = False,
+                 over_slurp: bool = None) -> None:
+        """ Evaluator constructor.  All of the parameters below can be set in the constructor or at runtime
+
+        :param rdf: RDF string, file name, URL or Graph for evaluation.
+        :param schema: ShEx Schema to evaluate. Can be ShExC, ShExJ or a pre-parsed schema
+        :param focus: focus node(s).  If absent, all non-BNode subjects in the graph are evaluated
+        :param start: start node(s). If absent, the START node in the schema is used
+        :param rdf_format: format for RDF. Default: "Turtle"
+        :param debug: emit semi-helpful debug information
+        :param debug: debug graph fetch calls
+        :param over_slurp: Controls whether SPARQL slurper does exact or over slurps
+        """
+        self.rdf_format = rdf_format
+        self.g = None
+        self.rdf = rdf
+        self._schema = None
+        self.schema = schema
+        self._focus = None
+        self.focus = normalize_uriparm(focus)
+        self._start = None
+        self.start = normalize_uriparm(start)
+        self.debug = debug
+        self.debug_slurps = debug_slurps
+        self.over_slurp = over_slurp
+
+    @property
+    def rdf(self) -> str:
+        """
+
+        :return: The rendering of whatever RDF is currently being evaluated
+        """
+        return self.g.serialize(format=self.rdf_format).decode()
+
+    @rdf.setter
+    def rdf(self, rdf: Optional[Union[str, Graph]]) -> None:
+        """ Set the RDF DataSet to be evaulated.  If ``rdf`` is a string, the presence of a return is the
+        indicator that it is text instead of a location.
+
+        :param rdf: File name, URL, representation of rdflib Graph
+        """
+        if isinstance(rdf, Graph):
+            self.g = rdf
+        else:
+            self.g = Graph()
+            if isinstance(rdf, str):
+                if '\n' in rdf or '\r' in rdf:
+                    self.g.parse(data=rdf, format=self.rdf_format)
+                elif ':' in rdf:
+                    self.g.parse(location=rdf, format=self.rdf_format)
+                else:
+                    self.g.parse(source=rdf, format=self.rdf_format)
+
+    @property
+    def schema(self) -> Optional[str]:
+        """
+
+        :return: The ShExC representation of the schema if one is supplied
+        """
+        return str(ShExC(self._schema)) if self._schema else None
+
+    @schema.setter
+    def schema(self, shex: Optional[Union[str, ShExJ.Schema]]) -> None:
+        """ Set the schema to be used.  Schema can either be a ShExC or ShExJ string or a pre-parsed schema.
+
+        :param shex:  Schema
+        """
+        if shex is not None:
+            shext = shex.strip()
+            if ('\n' in shex or '\r' in shex) or shext[0] in '#<_: ':
+                self._schema = SchemaLoader().loads(shex)
+            else:
+                self._schema = SchemaLoader().load(shex) if isinstance(shex, str) else shex
+            if self._schema is None:
+                raise ValueError("Unable to parse shex file")
+
+    @property
+    def focus(self) -> Optional[List[URIRef]]:
+        """
+        :return: The list of focus nodes (if any)
+        """
+        return self._focus
+
+    @property
+    def foci(self) -> List[URIRef]:
+        """
+
+        :return: The current set of focus nodes
+        """
+        return self._focus if self._focus else sorted([s for s in set(self.g.subjects()) if isinstance(s, URIRef)])
+
+    @focus.setter
+    def focus(self, focus: Optional[URIPARM]) -> None:
+        """ Set the focus node(s).  If no focus node is specified, the evaluation will occur for all non-BNode
+        graph subjects.  Otherwise it can be a string, a URIRef or a list of string/URIRef combinations
+
+        :param focus: None if focus should be all URIRefs in the graph otherwise a URI or list of URI's
+        """
+        if focus is None:
+            self._focus = None
+        else:
+            if not isinstance(focus, Iterable) or isinstance(focus, (Namespace, URIRef)):
+                focus = [focus]
+            self._focus = [f if isinstance(f, URIRef) else URIRef(str(f)) for f in focus]
+
+    @property
+    def start(self) -> Optional[Union[List[URIRef], Type[START]]]:
+        """
+
+        :return: The schema start node(s)
+        """
+        return self._start
+
+    @start.setter
+    def start(self, start: Optional[URIPARM]) -> None:
+        if start is None:
+            self._start = [START]
+        else:
+            if not isinstance(start, Iterable) or isinstance(start, (Namespace, URIRef)):
+                start = [start]
+            self._start = [s if isinstance(s, URIRef) else URIRef(str(s)) for s in start]
+
+    def evaluate(self,
+                 rdf: Optional[Union[str, Graph]] = None,
+                 shex: Optional[Union[str, ShExJ.Schema]] = None,
+                 focus: Optional[URIPARM] = None,
+                 start: Optional[URIPARM] = None,
+                 rdf_format: Optional[str] = None,
+                 debug: Optional[bool] = None,
+                 debug_slurps: Optional[bool] = None,
+                 over_slurp: Optional[bool] = None) -> List[EvaluationResult]:
+        if rdf or shex or focus or start:
+            if not rdf_format:
+                rdf_format = self.rdf_format
+            evaluator = ShExEvaluator(rdf, shex, focus, start, rdf_format)
+            if rdf is None:
+                evaluator.g = self.g
+            if not shex:
+                evaluator._schema = self._schema
+            evaluator._focus = normalize_uriparm(focus) if focus else self._focus
+            evaluator._start = normalize_uriparm(start) if start else self._start
+        else:
+            evaluator = self
+
+        if self.start == [START] and evaluator._schema.start is None:
+            return [EvaluationResult(False, None, None, 'Error: START node is not specified')]
+
+        cntxt = Context(evaluator.g, evaluator._schema)
+        # TODO: Clean this up
+        cntxt.debug_context.debug = debug if debug is not None else self.debug
+        cntxt.debug_context.trace_slurps = debug_slurps if debug_slurps is not None else self.debug_slurps
+        cntxt.over_slurp = self.over_slurp if over_slurp is not None else self.over_slurp
+
+        rval = []
+        for start in evaluator.start:
+            for focus in evaluator.foci:
+                map_ = FixedShapeMap()
+                map_.add(ShapeAssociation(focus, start))
+                success, fail_reasons = isValid(cntxt, map_)
+                rval.append(EvaluationResult(success, focus, start, '\n'.join(fail_reasons) if not success else ''))
+        return rval
+
+
+def genargs(prog: Optional[str]=None) -> ArgumentParser:
+    """
+    Create a command line parser
+    :return: parser
+    """
+    parser = ArgumentParser(prog)
+    parser.add_argument("rdf", help="Input RDF file or SPARQL endpoint if slurper option set")
+    parser.add_argument("shex", help="ShEx specification")
+    parser.add_argument("-f", "--format", help="Input RDF Format", default="turtle")
+    parser.add_argument("-s", "--start", help="Start shape")
+    parser.add_argument("-fn", "--focus", help="RDF focus node")
+    parser.add_argument("-A", "--allsubjects", help="Evaluate all non-bnode subjects in the graph", action="store_true")
+    parser.add_argument("-d", "--debug", action="store_true", help="Add debug output")
+    parser.add_argument("-ss", "--slurper", action="store_true", help="Use SPARQL slurper graph")
+    parser.add_argument("-cf", "--flattener", action="store_true", help="Use RDF Collections flattener graph")
+    return parser
+
+
+def evaluate_cli(argv: Optional[List[str]] = None, prog: Optional[str]=None) -> bool:
+    opts = genargs(prog).parse_args(argv if argv is not None else sys.argv[1:])
+    if opts.slurper and opts.flattener:
+        print("Error: Cannot combine slurper and flattener graphs")
+        return False
+    if not opts.format:
+        opts.format = guess_format(opts.rdf)
+    if not opts.format:
+        print('Error: Cannot determine RDF format from file name - use "--format" option')
+        return False
+    g = SlurpyGraph(opts.rdf) if opts.slurper else CFGraph() if opts.flattener else Graph()
+    if not opts.slurper:
+        g.load(opts.rdf, format=opts.format)
+    if not (opts.focus or opts.allsubjects):
+        print('Error: You must specify one or more graph focus nodes or use the "-A" option')
+        return False
+    result = ShExEvaluator(g, opts.shex, opts.focus, opts.start, rdf_format=opts.format, debug=opts.debug).evaluate()
+    for rslt in result:
+        if not rslt.result:
+            print(f"{rslt.reason}")
+    return all(r.result for r in result)
