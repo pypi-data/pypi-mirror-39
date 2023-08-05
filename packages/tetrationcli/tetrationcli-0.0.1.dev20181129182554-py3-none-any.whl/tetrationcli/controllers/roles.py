2dba8c8561f303f445fb7fe7fe98d398fab9619e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:02 +0000

    Add package tetrationcli/0.0.1.dev20181129182554/tetrationcli-0.0.1.dev20181129182554-py3-none-any.whl

---
+++ packages/tetrationcli/tetrationcli-0.0.1.dev20181129182554-py3-none-any.whl/tetrationcli/controllers/roles.py
@@ -0,0 +1,34 @@
+from cement import Controller, ex
+import json
+
+class Roles(Controller):
+
+    class Meta:
+        label = 'roles'
+        stacked_type = 'nested'
+        # stacked_on = 'base'
+
+    @ex(help='list roles')
+    def list(self):
+        restclient = self.app.tetpyclient
+        response = restclient.get('/roles')
+        self.app.log.debug('Roles Command: %s' % response.status_code)
+        data = {
+            'results': json.loads(response.content.decode("utf-8"))
+        }
+        
+        self.app.log.debug('Roles Data: %s' % data)
+        self.app.render(data, 'roles_list.jinja2')
+
+    @ex(help='delete')
+    def delete(self):
+        self.app.log.error('FEATURE NOT IMPLEMENTED YET, OPEN A ISSUE')
+
+    # @ex(help='create')
+    # def create(self):
+    #     self.app.log.error('FEATURE NOT IMPLEMENTED YET, OPEN A ISSUE')
+    
+    
+    # @ex(help='update')
+    # def update(self):
+    #     self.app.log.error('FEATURE NOT IMPLEMENTED YET, OPEN A ISSUE')
