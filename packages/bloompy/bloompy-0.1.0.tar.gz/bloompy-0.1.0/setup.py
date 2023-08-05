3f48aed6039c38a688434ecf60fccf577b7854dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:16 +0000

    Add package bloompy/0.1.0/bloompy-0.1.0.tar.gz

---
+++ packages/bloompy/bloompy-0.1.0.tar.gz/bloompy-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bloompy",
+    version="0.1.0",
+    author="linkin",
+    author_email="yooleak@outlook.com",
+    description="Implementation of Bloom Filter.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/01ly/bloompy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "bitarray >= 0.8.3",
+        'mmh3 >= 2.5.1',
+    ],
+)
