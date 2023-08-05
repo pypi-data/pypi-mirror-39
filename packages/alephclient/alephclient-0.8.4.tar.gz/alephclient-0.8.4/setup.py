2bfef5fcd3eaf39432a72580b7696ed924c784e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:44 +0000

    Add package alephclient/0.8.4/alephclient-0.8.4.tar.gz

---
+++ packages/alephclient/alephclient-0.8.4.tar.gz/alephclient-0.8.4/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='alephclient',
+    version='0.8.4',
+    description='Command-line client for Aleph API',
+    author='Journalism Development Network',
+    author_email='data@occrp.org',
+    url='http://github.com/alephdata/alephclient',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    packages=find_packages(exclude=['alephclient.tests']),
+    install_requires=[
+        'six >= 1.11.0',
+        'banal',
+        'pyyaml',
+        'requests >= 2.20.0',
+        'requests_toolbelt >= 0.8.0',
+        'click >= 6.7',
+        'normality >= 0.6.1',
+        'grpcio >= 1.11.0',
+        'pathlib2 >= 2.3.2'
+    ],
+    extras_require={
+        # ':python_version < "3.4"': [
+        #     'pathlib2>=2.3.2'
+        # ],
+        'dev': [
+            'pytest',
+            'pytest-mock>=1.10.0',
+            'grpcio-tools>=1.11.0'
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'alephclient = alephclient.cli:cli'
+        ]
+    },
+)
