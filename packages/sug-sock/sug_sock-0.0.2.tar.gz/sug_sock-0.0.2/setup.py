a78124b614583b23d6dc8e99c1622da7fbf7d207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:18 +0000

    Add package sug-sock/0.0.2/sug_sock-0.0.2.tar.gz

---
+++ packages/sug-sock/sug_sock-0.0.2.tar.gz/sug_sock-0.0.2/setup.py
@@ -0,0 +1,14 @@
+'''
+socket extention gui setup file
+'''
+from setuptools import setup
+
+setup(name='sug_sock',
+	  version='0.0.2',
+      description='suguv socket expantion pack',
+      url='https://bitbucket.org/SuguvHyde/sug_sock/src/master/',
+      author='Suguv',
+      author_email='orsegev7@gmail.com',
+      license='MIT',
+      packages=['sug_sock'],
+      zip_safe=False)
