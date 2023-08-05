0c180e28e3988ff37256a5f5cc00bba34900da1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:38 +0000

    Add package py-vkontakte/5.63.3/py-vkontakte-5.63.3.tar.gz

---
+++ packages/py-vkontakte/py-vkontakte-5.63.3.tar.gz/py-vkontakte-5.63.3/vk/error.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+class VKError(Exception):
+    def __init__(self, message, code):
+        super(VKError, self).__init__(message)
+        self.code = code
+
+
+class VKParseJsonError(Exception):
+    pass
