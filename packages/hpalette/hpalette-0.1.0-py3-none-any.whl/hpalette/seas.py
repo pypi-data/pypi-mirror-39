ca890902557c4c21b8bf62197b35a135c1743be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:13:15 +0000

    Add package hpalette/0.1.0/hpalette-0.1.0-py3-none-any.whl

---
+++ packages/hpalette/hpalette-0.1.0-py3-none-any.whl/hpalette/seas.py
@@ -0,0 +1,25 @@
+"""
+Harvard SEAS colors.
+
+From: https://www.seas.harvard.edu/communications/identity-guidelines/color-palettes
+(Accessed 2018-10-02.)
+"""
+
+CRIMSON = "#A51C30"
+
+# core palette: neutral field as backdrop for crimson 
+INK = "#1E1E1E"
+MORTAR = "#8C8179"
+PARCHMENT = "#F3F3F1"
+SLATE = "#8996A0"
+SHADE = "#BAC5C6"
+
+# accent color palette
+INDIGO = "#293352"
+BLUEBONNET = "#4E84C4"
+IVY = "#52854C"
+PEAR = "#C3D7A4"
+SAFFRON = "#D16103"
+CREME = "#F4EDCA"
+GOLD = "#C4961A"
+LEMON = "#FFDB6D"
