16b165097de68daf4fa15da328e152faeab79a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:11 +0000

    Add package pfla/0.1.9/pfla-0.1.9.tar.gz

---
+++ packages/pfla/pfla-0.1.9.tar.gz/pfla-0.1.9/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='pfla',
+      version='0.1.9',
+      description='Python facial landmarking and analysis',
+      long_description=readme(),
+      classifiers=[
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Scientific/Engineering :: Medical Science Apps.',
+          'Topic :: Scientific/Engineering :: Image Recognition'
+	],
+      url='https://github.com/maxrousseau/pfla',
+      author='Maxime Rousseau',
+      author_email='maximerousseau08@gmail.com',
+      scripts=['bin/pfla'],
+      include_package_data=True,
+      license='MIT',
+      packages=['pfla'],
+      zip_safe=False)
