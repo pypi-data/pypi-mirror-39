3a78766c1eb474e6498200919e4fbdc0a73f9c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:24 +0000

    Add package snw/0.5.3/snw-0.5.3.tar.gz

---
+++ packages/snw/snw-0.5.3.tar.gz/snw-0.5.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'snw',
+    version = '0.5.3',
+    description = 'snw client tool',
+    scripts = ['client/snw'],
+    package_dir = {'': 'nmt-wizard'},
+    packages = ['client'],
+    install_requires=[
+        'prettytable',
+        'regex',
+        'requests',
+        'six',
+        'setuptools'
+    ]
+)
