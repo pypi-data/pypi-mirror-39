79b50417c29beced47bdeb1f841fb4909e7ab954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:32 +0000

    Add package brewblox-devcon-spark/0.3.1.dev132/brewblox-devcon-spark-0.3.1.dev132.tar.gz

---
+++ packages/brewblox-devcon-spark/brewblox-devcon-spark-0.3.1.dev132.tar.gz/brewblox-devcon-spark-0.3.1.dev132/brewblox_devcon_spark/__main__.py
@@ -0,0 +1,121 @@
+"""
+Example of how to import and use the brewblox service
+"""
+
+import logging
+
+from brewblox_service import brewblox_logger, events, scheduler, service
+
+from brewblox_devcon_spark import (broadcaster, commander, commander_sim,
+                                   communication, datastore, device, seeder,
+                                   status)
+from brewblox_devcon_spark.api import (alias_api, codec_api, debug_api,
+                                       error_response, object_api, remote_api,
+                                       sse_api, system_api)
+from brewblox_devcon_spark.codec import codec
+
+LOGGER = brewblox_logger(__name__)
+
+
+def create_parser(default_name='spark'):
+    parser = service.create_parser(default_name=default_name)
+
+    # Local config
+    group = parser.add_argument_group('Local configuration')
+    group.add_argument('--database',
+                       help='Backing file for the object database. [%(default)s]',
+                       default='brewblox_db.json')
+    group.add_argument('--config',
+                       help='Backing file for service configuration. [%(default)s]',
+                       default='brewblox_cfg.json')
+    group.add_argument('--seed-objects',
+                       help='A file of objects that should be created on connection. [%(default)s]')
+    group.add_argument('--seed-profiles',
+                       nargs='+',
+                       type=int,
+                       help='Profiles that should be made active on connection. [%(default)s]')
+
+    # Device options
+    group = parser.add_argument_group('Device communication')
+    group.add_argument('--simulation',
+                       help='Start in simulation mode. Will not connect to a physical device. [%(default)s]',
+                       action='store_true')
+    group.add_argument('--device-host',
+                       help='Spark device URL host. '
+                       'Will connect to this URL regardless of advertised device ID. [%(default)s]')
+    group.add_argument('--device-port',
+                       help='Spark device URL port when accessing a device over WiFi. [%(default)s]',
+                       type=int,
+                       default=8332)
+    group.add_argument('--device-serial',
+                       help='Spark device serial port. Takes precedence over URL connections. '
+                       'Will only connect if device ID matches advertised ID, or is not set. [%(default)s]')
+    group.add_argument('--device-id',
+                       help='Spark serial number. Any spark is valid if not set. '
+                       'This will be ignored if --device-host is used. [%(default)s]')
+    group.add_argument('--list-devices',
+                       action='store_true',
+                       help='List connected devices and exit. [%(default)s]')
+
+    # Service network options
+    group = parser.add_argument_group('Service communication')
+    group.add_argument('--broadcast-exchange',
+                       help='Eventbus exchange to which controller state should be broadcasted. [%(default)s]',
+                       default='brewcast')
+    group.add_argument('--broadcast-interval',
+                       help='Interval (in seconds) between broadcasts of controller state.'
+                       'Set to a value <= 0 to disable broadcasting. [%(default)s]',
+                       type=float,
+                       default=5)
+    group.add_argument('--sync-exchange',
+                       help='Eventbus exchange used to synchronize remote blocks. [%(default)s]',
+                       default='syncast')
+
+    return parser
+
+
+def main():
+    app = service.create_app(parser=create_parser())
+    logging.captureWarnings(True)
+    config = app['config']
+
+    if config['list_devices']:
+        LOGGER.info('Listing connected devices: ')
+        for dev in [[v for v in p] for p in communication.all_ports()]:
+            LOGGER.info(f'>> {" | ".join(dev)}')
+        # Exit application
+        return
+
+    status.setup(app)
+
+    if config['simulation']:
+        commander_sim.setup(app)
+    else:
+        communication.setup(app)
+        commander.setup(app)
+
+    scheduler.setup(app)
+    events.setup(app)
+
+    codec.setup(app)
+    datastore.setup(app)
+    device.setup(app)
+    broadcaster.setup(app)
+
+    error_response.setup(app)
+    debug_api.setup(app)
+    alias_api.setup(app)
+    object_api.setup(app)
+    system_api.setup(app)
+    remote_api.setup(app)
+    codec_api.setup(app)
+    sse_api.setup(app)
+
+    seeder.setup(app)
+
+    service.furnish(app)
+    service.run(app)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/brewblox-devcon-spark/brewblox-devcon-spark-0.3.1.dev132.tar.gz/brewblox-devcon-spark-0.3.1.dev132/test/test_codec.py
@@ -0,0 +1,105 @@
+"""
+Tests brewblox codec
+"""
+
+from brewblox_devcon_spark.codec import _path_extension  # isort:skip
+
+import pytest
+from brewblox_service import scheduler
+
+from brewblox_devcon_spark import datastore, exceptions
+from brewblox_devcon_spark.codec import codec
+
+_path_extension.avoid_lint_errors()
+
+
+@pytest.fixture
+def app(app):
+    app['config']['unit_defaults'] = ['degC']
+    scheduler.setup(app)
+    datastore.setup(app)
+    codec.setup(app)
+    return app
+
+
+@pytest.fixture
+def cdc(app) -> codec.Codec:
+    return codec.get_codec(app)
+
+
+async def test_encode_system_objects(app, client, cdc):
+    objects = [
+        {
+            'type': 'SysInfo',
+            'data': {},
+        },
+        {
+            'type': 'Ticks',
+            'data': {},
+        },
+        {
+            'type': 'OneWireBus',
+            'data': {},
+        },
+        {
+            'type': 'Profiles',
+            'data': {
+                'active': [0]
+            },
+        }
+    ]
+
+    encoded = [await cdc.encode(o['type'], o['data']) for o in objects]
+    assert encoded
+
+
+async def test_encode_errors(app, client, cdc):
+    with pytest.raises(TypeError):
+        await cdc.encode('TempSensorOneWire', None)
+
+    with pytest.raises(exceptions.EncodeException):
+        await cdc.encode('MAGIC', {})
+
+
+async def test_decode_errors(app, client, cdc):
+    with pytest.raises(TypeError):
+        await cdc.decode('TempSensorOneWire', 'string')
+
+    with pytest.raises(exceptions.DecodeException):
+        await cdc.decode('MAGIC', b'\x00')
+
+
+async def test_invalid_object(app, client, cdc):
+    assert await cdc.encode('InvalidLink', {'args': True}) == (0, b'\x00')
+    assert await cdc.decode(0, b'\xAA') == ('InvalidLink', {})
+
+
+async def test_encode_constraint(app, client, cdc):
+    assert await cdc.decode('ActuatorPwm', b'\x00')
+    assert await cdc.encode('ActuatorPwm', {
+        'constrainedBy': {
+            'constraints': [
+                {'min': -100},
+                {'max': 100},
+            ],
+        },
+    })
+
+
+async def test_encode_delta_sec(app, client, cdc):
+    # Check whether [delta_temperature / time] can be converted
+    enc_id, enc_val = await cdc.encode('EdgeCase', {
+        'deltaV': 100,
+    })
+    dec_id, dec_val = await cdc.decode(enc_id, enc_val)
+    assert dec_val['deltaV[delta_degC / second]'] == pytest.approx(100, 0.1)
+
+
+async def test_transcode_interfaces(app, client, cdc):
+    types = [
+        'EdgeCase',
+        'BalancerLink',
+        'SetpointSensorPair',
+        'SetpointSensorPairLink'
+    ]
+    assert [await cdc.decode(await cdc.encode(t)) for t in types] == types
