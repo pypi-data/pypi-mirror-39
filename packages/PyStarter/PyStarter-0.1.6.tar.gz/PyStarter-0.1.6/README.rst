d00a35270dbb2e27776563f5ae9925a75007eda7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:55 +0000

    Add package PyStarter/0.1.6/PyStarter-0.1.6.tar.gz

---
+++ packages/PyStarter/PyStarter-0.1.6.tar.gz/PyStarter-0.1.6/README.rst
@@ -0,0 +1,38 @@
+=========
+PyStarter
+=========
+
+PyStarter starter provides an easy way to start a git project specifically python projects from the command line. You might find
+it most useful for creating all need files, venvs, and more. Typical usage
+often looks like this::
+
+    pystarter create
+
+This command will start creating your python and git ready project.
+
+
+Authors
+=======
+
+1. Rafael Cenzano
+
+
+Full Command List
+=================
+
+Always start commands with pystarter.
+
+pystarter::
+
+* --version or -V
+
+* --help -h
+
+
+
+
+
+Other helpful rst file stuff that will be removed
+
+Paragraphs are separated by blank lines. *Italics*, **bold**,
+and ``monospace`` look like this.
---
+++ packages/PyStarter/PyStarter-0.1.6.tar.gz/PyStarter-0.1.6/pystarter/command_line.py
@@ -0,0 +1,34 @@
+from .pystarter import *
+import sys
+import os
+
+def main():
+    args = sys.argv[1:]
+    first_arg = sys.argv[1].lower()
+
+    if first_arg == '--version' or first_arg == '-v':
+        print(pystarterVersion())
+
+    elif first_arg == '--help' or first_arg == '-h':
+        print(pystarterCommands())
+
+    elif first_arg == 'create':
+        try:
+            second_arg = sys.argv[2].lower()
+        except ValueError:
+            second_arg = None
+
+        if second_arg == 'python':
+            pass
+        elif second_arg == 'git':
+            pass
+        elif second_arg == None:
+            pass
+        else:
+            pass
+
+    elif first_arg == 'test':
+        print(os.getcwd())
+
+    else:
+        print(pystarterCommands())
