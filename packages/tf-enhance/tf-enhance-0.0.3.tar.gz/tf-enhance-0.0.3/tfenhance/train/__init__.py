fd7ad96195cf4463c501ff2df0f3da1bd43789a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:51 +0000

    Add package tf-enhance/0.0.3/tf-enhance-0.0.3.tar.gz

---
+++ packages/tf-enhance/tf-enhance-0.0.3.tar.gz/tf-enhance-0.0.3/tfenhance/train/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from train.basic import BasicTrain as BasicTrain
---
+++ packages/tf-enhance/tf-enhance-0.0.3.tar.gz/tf-enhance-0.0.3/train/basic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding=UTF-8 -*-
+from train.train import Train
+
+
+class BasicTrain(Train):
+
+    def hello(self):
+        print('hello')
