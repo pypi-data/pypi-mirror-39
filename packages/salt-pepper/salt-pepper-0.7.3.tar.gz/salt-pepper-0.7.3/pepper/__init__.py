04daa2c8ec330910442c0327abfd04677ebd4524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:20 +0000

    Add package salt-pepper/0.7.3/salt-pepper-0.7.3.tar.gz

---
+++ packages/salt-pepper/salt-pepper-0.7.3.tar.gz/salt-pepper-0.7.3/pepper/__init__.py
@@ -0,0 +1,34 @@
+'''
+Pepper is a CLI front-end to salt-api
+
+'''
+import json
+import os
+
+from pepper.libpepper import Pepper, PepperException
+
+__all__ = ('__version__', '__gitrev__', 'Pepper', 'PepperException')
+
+try:
+    # First try to grab the version from the version.json build file.
+    vfile = os.path.join(os.path.dirname(__file__), 'version.json')
+
+    with open(vfile, 'rb') as f:
+        data = f.read().decode("utf-8")
+        ret = json.loads(data)
+        version = ret.get('version')
+        sha = ret.get('sha')
+except IOError:
+    # Build version file doesn't exist; we may be running from a clone.
+    setup_file = os.path.join(os.path.dirname(__file__), os.pardir, 'setup.py')
+
+    if os.path.exists(setup_file):
+        import imp
+
+        setup = imp.load_source('pepper_setup', setup_file)
+        version, sha = setup.get_version()
+    else:
+        version, sha = None, None
+
+__version__ = version or 'Unknown'
+__gitrev__ = sha
---
+++ packages/salt-pepper/salt-pepper-0.7.3.tar.gz/salt-pepper-0.7.3/setup.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+'''
+A CLI front-end to a running salt-api system
+
+'''
+import json
+import os
+import re
+import sys
+
+from distutils.core import setup
+from distutils.command import sdist, install_data
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup_kwargs = {
+    'name': 'salt-pepper',
+    'description': __doc__.strip(),
+    'author': 'Seth House',
+    'author_email': 'shouse@saltstack.com',
+    'url': 'http://saltstack.com',
+    'long_description': long_description,
+    'long_description_content_type': "text/x-rst",
+    'classifiers': [
+        'Programming Language :: Python',
+        'Programming Language :: Cython',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: System :: Clustering',
+        'Topic :: System :: Distributed Computing',
+    ],
+    'packages': [
+        'pepper',
+    ],
+    'package_data': {
+        'pepper': ['version.json'],
+    },
+    'scripts': [
+        'scripts/pepper',
+    ]
+}
+
+
+def read_version_tag():
+    git_dir = os.path.join(os.path.dirname(__file__), '.git')
+
+    if os.path.isdir(git_dir):
+        import subprocess
+
+        try:
+            p = subprocess.Popen(['git', 'describe'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = p.communicate()
+        except Exception:
+            pass
+        else:
+            return out.strip() or None
+
+    else:
+        return read_version_from_json_file()
+
+    return None
+
+
+def read_version_from_json_file():
+    with open(os.path.join(os.path.dirname(__file__), "pepper", "version.json")) as f:
+        return json.load(f)['version']
+
+
+def parse_version_tag(tag):
+    '''
+    Parse the output from Git describe
+
+    Returns a tuple of the version number, number of commits (if any), and the
+    Git SHA (if available).
+    '''
+    if isinstance(tag, bytes):
+        tag = tag.decode()
+    if not tag or '-g' not in tag:
+        return tag, None, None
+
+    match = re.search('(?P<version>.*)-(?P<num_commits>[0-9]+)-g(?P<sha>[0-9a-fA-F]+)', tag)
+
+    if not match:
+        return tag, None, None
+
+    match_dict = match.groupdict()
+    return (
+        match_dict.get('version'),
+        match_dict.get('num_commits'),
+        match_dict.get('sha'))
+
+
+def get_version():
+    '''
+    Return a tuple of the version and Git SHA
+    '''
+    version, num_commits, sha = parse_version_tag(read_version_tag())
+    if sha:
+        version = '{0}.dev{1}'.format(version, num_commits)
+    return version, sha
+
+
+def write_version_file(base_dir):
+    ver_path = os.path.join(base_dir, 'pepper', 'version.json')
+    version, sha = get_version()
+
+    with open(ver_path, 'wb' if sys.version_info[0] == 2 else 'w') as f:
+        json.dump({'version': version, 'sha': sha}, f)
+
+
+class PepperSdist(sdist.sdist):
+    '''
+    Write the version.json file to the sdist tarball build directory
+    '''
+    def make_release_tree(self, base_dir, files):
+        sdist.sdist.make_release_tree(self, base_dir, files)
+        write_version_file(base_dir)
+
+
+class PepperInstallData(install_data.install_data):
+    '''
+    Write the version.json file to the installation directory
+    '''
+    def run(self):
+        install_cmd = self.get_finalized_command('install')
+        install_dir = getattr(install_cmd, 'install_lib')
+        write_version_file(install_dir)
+
+        return install_data.install_data.run(self)
+
+
+if __name__ == '__main__':
+    version, sha = get_version()
+
+    setup(cmdclass={
+        'sdist': PepperSdist,
+        'install_data': PepperInstallData,
+    }, version=version, git_sha=sha, **setup_kwargs)
