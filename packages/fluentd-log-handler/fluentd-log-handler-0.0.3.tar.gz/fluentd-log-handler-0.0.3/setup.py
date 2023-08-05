f76e0fe8d78c0e6977de459587ae789fbd239017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:13:05 +0000

    Add package fluentd-log-handler/0.0.3/fluentd-log-handler-0.0.3.tar.gz

---
+++ packages/fluentd-log-handler/fluentd-log-handler-0.0.3.tar.gz/fluentd-log-handler-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+def readme() -> str:
+    with open('./README.md') as f:
+        return f.read()
+
+
+setup(
+    name="fluentd-log-handler",
+    version='0.0.3',
+    author="Hyun-Tae Hwang",
+    author_email="hwanght1@gmail.com",
+    description="Python logging handler for Fluentd",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    license='GPL',
+    url="https://github.com/neillab/fluentdloghandler",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Topic :: System :: Logging",
+        "Intended Audience :: Developers",
+    ],
+)
