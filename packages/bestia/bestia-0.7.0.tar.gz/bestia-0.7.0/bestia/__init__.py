8b521d8370f78a19cdffac04a260a0fc42d0b142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:47 +0000

    Add package bestia/0.7.0/bestia-0.7.0.tar.gz

---
+++ packages/bestia/bestia-0.7.0.tar.gz/bestia-0.7.0/bestia/__init__.py
@@ -0,0 +1,4 @@
+from bestia.connect import *
+from bestia.misc import *
+from bestia.output import *
+from bestia.sort import *
---
+++ packages/bestia/bestia-0.7.0.tar.gz/bestia-0.7.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bestia",
+    version="0.7.0",
+    author="Federico Rizzo",
+    author_email="yo@gmail.com",
+    description="A collection of tools for building dynamic Command-Line applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/synestematic/bestia",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+)
