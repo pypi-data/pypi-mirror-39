928ff1e564ced6ac19344ca09d9e4eca0d6599a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:34 +0000

    Add package performance-features/0.1.1/performance_features-0.1.1.tar.gz

---
+++ packages/performance-features/performance_features-0.1.1.tar.gz/performance_features-0.1.1/profiler/workload.h
@@ -0,0 +1,27 @@
+
+#include <exception>
+#include <thread>
+#include <vector>
+#include <string>
+
+class Workload
+{
+    std::vector<int> fds;
+    std::vector<std::string> args;
+    std::thread* waiter;
+private:
+    int create_wrokload(const std::vector<std::string>& args);
+    void wait_finish();
+public:
+    int MAX_SIZE_GROUP= 512;
+    int pid, isAlive;
+
+    Workload(std::vector<std::string> args);
+
+    // start at background
+    void start();
+
+    // stat forground and sample on fds
+    void add_events(std::vector<int> fds_);
+    std::vector<std::vector<signed long int>> run(double sample_perid, bool reset);
+};
