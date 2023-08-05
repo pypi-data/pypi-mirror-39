07287fefbe714b703df8dc4e65e3e5ac322741af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:58 +0000

    Add package nester-dummyred/1.1.0/nester_dummyred-1.1.0.tar.gz

---
+++ packages/nester-dummyred/nester_dummyred-1.1.0.tar.gz/nester_dummyred-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name             ='nester_dummyred',
+      version          ='1.1.0',
+      py_modules       =['nester_dummyred'],
+      author           ='dummyred',
+      author_email     ='1742438223@qq.com',
+      url              ='http://www.headfirstlabs.com',
+      description      ='A simple printer of nested lists',
+      )
