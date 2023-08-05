aa7e1fc73aeb7ed20ec296db95f92ec532a8c265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:28 +0000

    Add package deepaffects/1.4.0/deepaffects-1.4.0.tar.gz

---
+++ packages/deepaffects/deepaffects-1.4.0.tar.gz/deepaffects-1.4.0/deepaffects/apis/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .denoise_api import DenoiseApi
+from .diarize_api import DiarizeApi
+from .emotion_api import EmotionApi
+from .featurize_api import FeaturizeApi
+from .ellipsis_api import EllipsisAPI
+from .diarize_api_v2 import DiarizeApiV2
