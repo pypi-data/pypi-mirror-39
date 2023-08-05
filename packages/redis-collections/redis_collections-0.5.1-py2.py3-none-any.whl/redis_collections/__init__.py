21aae08a94aacbbc7c661d1c1e3ec23aa59154fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:28 +0000

    Add package redis-collections/0.5.1/redis_collections-0.5.1-py2.py3-none-any.whl

---
+++ packages/redis-collections/redis_collections-0.5.1-py2.py3-none-any.whl/redis_collections/__init__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+
+__title__ = 'redis-collections'
+__version__ = '0.5.1'
+__author__ = 'Honza Javorek'
+__license__ = 'ISC'
+__copyright__ = 'Copyright 2013-? Honza Javorek'
+
+
+from .base import RedisCollection  # NOQA
+from .dicts import DefaultDict, Dict, Counter  # NOQA
+from .lists import Deque, List  # NOQA
+from .sets import Set  # NOQA
+from .sortedsets import SortedSetCounter  # NOQA
+from .syncable import (  # NOQA
+    LRUDict,
+    SyncableDict,
+    SyncableCounter,
+    SyncableDeque,
+    SyncableDefaultDict,
+    SyncableList,
+    SyncableSet,
+)
+
+__all__ = [
+    'Counter',
+    'DefaultDict',
+    'Deque',
+    'Dict',
+    'List',
+    'LRUDict',
+    'RedisCollection',
+    'Set',
+    'SortedSetCounter',
+    'SyncableDict',
+    'SyncableCounter',
+    'SyncableDefaultDict',
+    'SyncableDeque',
+    'SyncableList',
+    'SyncableSet',
+]
