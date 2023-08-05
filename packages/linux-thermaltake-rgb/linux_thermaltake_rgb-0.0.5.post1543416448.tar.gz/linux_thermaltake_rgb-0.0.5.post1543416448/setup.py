4f91bbe298d397ba7baadbdd4e4b0f2f7032ac72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:02 +0000

    Add package linux-thermaltake-rgb/0.0.5.post1543416448/linux_thermaltake_rgb-0.0.5.post1543416448.tar.gz

---
+++ packages/linux-thermaltake-rgb/linux_thermaltake_rgb-0.0.5.post1543416448.tar.gz/linux_thermaltake_rgb-0.0.5.post1543416448/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='linux_thermaltake_rgb',
+    version='0.0.5.post1543416448',
+    packages=find_packages(),
+    url='https://github.com/chestm007/linux_thermaltake_rgb',
+    license='GPL-2.0',
+    author='Max Chesterfield',
+    author_email='chestm007@hotmail.com',
+    maintainer='Max Chesterfield',
+    maintainer_email='chestm007@hotmail.com',
+    description='python driver and daemon for thermaltake hardware products',
+    long_description=readme,
+    install_requires=[
+        "pyyaml",
+        "PyGObject",
+        "dbus-python",
+        "psutil",
+        "pyusb"
+    ],
+    entry_points="""
+        [console_scripts]
+        linux-thermaltake-rgb=linux_thermaltake_rgb.daemon.main:main
+    """,
+    data_files=[('/etc/udev/rules.d', ['linux_thermaltake_rgb/assets/90-linux_thermaltake_rgb.rules']),
+                ('/usr/lib/systemd/user', ['linux_thermaltake_rgb/assets/linux-thermaltake-rgb.service']),
+                ('/etc/linux_thermaltake_rgb', ['linux_thermaltake_rgb/assets/config.yml'])]
+)
