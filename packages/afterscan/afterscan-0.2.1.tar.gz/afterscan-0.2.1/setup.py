c00b3b71ca8693ea3f44c05d2bfafbf931265cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:47 +0000

    Add package afterscan/0.2.1/afterscan-0.2.1.tar.gz

---
+++ packages/afterscan/afterscan-0.2.1.tar.gz/afterscan-0.2.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+
+setup(
+    name='afterscan',
+    description='Turn sloppy photoscans into crisp black and white images',
+    long_description = long_descr,
+    version='0.2.1',
+    url='https://github.com/larskarbo/afterscan',
+    author='Lars Karbo',
+    author_email='mail@larskarbo.no',
+    license='Apache2',
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ],
+    packages=['afterscan'],
+    install_requires=[
+        'emoji',
+        'Pillow',
+        'click',
+        'art',
+        'progress'
+    ],
+    entry_points={
+        'console_scripts': [
+            'afterscan=afterscan.afterscan:main'
+        ]
+    },
+)
