bd8a5c47bfe2602906eb17c3347f631be843c724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:26 +0000

    Add package smb-lib/0.0.9/smb-lib-0.0.9.tar.gz

---
+++ packages/smb-lib/smb-lib-0.0.9.tar.gz/smb-lib-0.0.9/smb_lib/smb_lib.py
@@ -0,0 +1,36 @@
+# -*- coding: utf8 -*-
+# from py4j.java_gateway import JavaGateway
+
+
+# gateway = JavaGateway()
+# smbionet = gateway.entry_point.getSmbionet();
+
+
+
+class SmbionetException(Exception):
+    def __init__(self, message=None, cause=None):
+        self.message = message
+        self.cause = cause
+    
+    def __str__(self):
+        return self.message or str(self.cause)
+
+
+
+def addGrapheInfluence(graphe):
+    #smbionet.generateInputFile(graphe)
+    return graphe
+
+def addCTL(ctl):
+    #smbionet.addCTL(ctl)
+    return ctl
+
+def runSMBionet():
+    #smbionet.run()
+    return "SMBionet is running"
+
+#def resultSMBionet():
+#    return smbionet.result()
+
+def helloword():
+    return "salut toi"
