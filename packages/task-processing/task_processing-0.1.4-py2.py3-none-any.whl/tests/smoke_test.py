c8b10d68cf618c24e8185fe3a860fe4402097b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:09 +0000

    Add package task-processing/0.1.4/task_processing-0.1.4-py2.py3-none-any.whl

---
+++ packages/task-processing/task_processing-0.1.4-py2.py3-none-any.whl/tests/smoke_test.py
@@ -0,0 +1,14 @@
+from task_processing.interfaces.task_executor import TaskExecutor
+
+
+def test_task_executor():
+    class TaskExecutorImpl(TaskExecutor):
+        def run(self, task_config):
+            return task_config
+
+        def kill(self, task_id):
+            return True
+
+    t = TaskExecutorImpl()
+    assert t.run('foo') == 'foo'
+    assert t.kill('mock')
