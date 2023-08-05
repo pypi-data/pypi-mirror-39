276eaa0f73d47d64e15acf7f2e39ac56c79dc64c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:58 +0000

    Add package sdss-marvin/2.3.0/sdss-marvin-2.3.0.tar.gz

---
+++ packages/sdss-marvin/sdss-marvin-2.3.0.tar.gz/sdss-marvin-2.3.0/python/marvin/utils/datamodel/dap/__init__.py
@@ -0,0 +1,11 @@
+
+from .base import *
+
+from .MPL4 import MPL4
+from .MPL5 import MPL5
+from .MPL6 import MPL6
+from .MPL7 import MPL7
+
+# Defines the list of datamodels.
+datamodel = DAPDataModelList([MPL4, MPL5, MPL6, MPL7])
+
