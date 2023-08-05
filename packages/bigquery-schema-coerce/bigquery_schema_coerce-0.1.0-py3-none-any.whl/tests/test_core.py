e5a24563f9325c39085bee53b7da86895cfd4c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:10 +0000

    Add package bigquery-schema-coerce/0.1.0/bigquery_schema_coerce-0.1.0-py3-none-any.whl

---
+++ packages/bigquery-schema-coerce/bigquery_schema_coerce-0.1.0-py3-none-any.whl/tests/test_core.py
@@ -0,0 +1,24 @@
+import unittest
+
+from bigquery_schema_coerce import core
+
+FIXTURE_PATH = "tests/fixtures/schema.json"
+
+
+class TestCore(unittest.TestCase):
+    def test_convert_fields(self):
+        candidate = {
+            "name": "name1",
+            "value": "123,120.02",
+            "number": "2",
+            "series": [{"float": "123.2", "type": "type1", "time": "20181129T171218Z"}],
+        }
+        schema = core.parse_schema(path=FIXTURE_PATH)
+        result = core.convert_fields(candidate, schema)
+        self.assertAlmostEqual(123120.02, result["value"], places=2)
+        self.assertAlmostEqual(123.2, result["series"][0]["float"], places=1)
+
+    def test_parse_schema(self):
+        schema = list(core.parse_schema(path=FIXTURE_PATH))
+        self.assertEqual(4, len(schema))
+        self.assertEqual(3, len(schema[3].fields))
