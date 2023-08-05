c929de0ba956b4960de6eaf925c64afd29735e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:28 +0000

    Add package hydraseq/0.0.10/hydraseq-0.0.10-py3-none-any.whl

---
+++ packages/hydraseq/hydraseq-0.0.10-py3-none-any.whl/hydraseq/__init__.py
@@ -0,0 +1,16 @@
+name = "hydraseq"
+
+from hydraseq.hydraseq import Hydraseq
+from hydraseq.hydraseq import Node
+from hydraseq.hydraseq import Convo
+from hydraseq.hydraseq import to_convo_node
+from hydraseq.hydraseq import link
+from hydraseq.hydraseq import to_tree_nodes
+from hydraseq.hydraseq import reconstruct
+from hydraseq.hydraseq import patterns_only
+from hydraseq.hydraseq import get_init_sentence_from_hydra
+from hydraseq.hydraseq import run_them_all
+from hydraseq.hydraseq import think
+from hydraseq.hydraseq import get_downwards
+from hydraseq.hydraseq import reverse_convo
+from hydraseq.hydraseq import run_convolutions
