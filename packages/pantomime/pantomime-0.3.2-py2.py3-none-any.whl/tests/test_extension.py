cb5ea11ddafac892a9e20b41d9f4825c5738ce59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:34 +0000

    Add package pantomime/0.3.2/pantomime-0.3.2-py2.py3-none-any.whl

---
+++ packages/pantomime/pantomime-0.3.2-py2.py3-none-any.whl/tests/test_extension.py
@@ -0,0 +1,21 @@
+import unittest
+
+from pantomime import normalize_extension
+
+
+class ExtensionTest(unittest.TestCase):
+
+    def test_normalize(self):
+        self.assertEqual(normalize_extension('.doc'), 'doc')
+        self.assertEqual(normalize_extension(None), None)
+        self.assertEqual(normalize_extension(''), None)
+        self.assertEqual(normalize_extension('bla.doc'), 'doc')
+        self.assertEqual(normalize_extension('bla.DOC'), 'doc')
+        self.assertEqual(normalize_extension('bla.DO C'), 'doc')
+        self.assertEqual(normalize_extension('bla.  DOC  '), 'doc')
+
+        self.assertEqual(normalize_extension('TXT'), 'txt')
+        self.assertEqual(normalize_extension('.TXT'), 'txt')
+        self.assertEqual(normalize_extension('foo.txt'), 'txt')
+        self.assertEqual(normalize_extension('foo..TXT'), 'txt')
+        self.assertEqual(normalize_extension('.HTM,L'), 'html')
