f13fade7240327571266c9cc42188435ed53b719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:19 +0000

    Add package appfly/0.1.3/appfly-0.1.3-py2.py3-none-any.whl

---
+++ packages/appfly/appfly-0.1.3-py2.py3-none-any.whl/appfly/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from appfly.app import app
+from appfly.app import response
+from appfly.app import routes
+
+"""Top-level package for appfly."""
+
+__author__ = """Italo José G. de Oliveira"""
+__email__ = 'italo.i@live.com'
+__version__ = '0.1.3'
+ 
