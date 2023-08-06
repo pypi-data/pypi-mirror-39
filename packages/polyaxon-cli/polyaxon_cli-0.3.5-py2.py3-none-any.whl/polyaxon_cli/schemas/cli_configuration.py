22c63a5b0df93a70de2aafcc65089e71200ee53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:59 +0000

    Add package polyaxon-cli/0.3.5/polyaxon_cli-0.3.5-py2.py3-none-any.whl

---
+++ packages/polyaxon-cli/polyaxon_cli-0.3.5-py2.py3-none-any.whl/polyaxon_cli/schemas/cli_configuration.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+from marshmallow import Schema, fields, post_dump, post_load
+
+from polyaxon_cli.schemas.base import BaseConfig
+from polyaxon_cli.schemas.log_handler import LogHandlerSchema
+
+
+class CliConfigurationSchema(Schema):
+    check_count = fields.Int(allow_none=True)
+    current_version = fields.Str(allow_none=True)
+    min_version = fields.Str(allow_none=True)
+    log_handler = fields.Nested(LogHandlerSchema, allow_none=True)
+
+    class Meta:
+        ordered = True
+
+    @post_load
+    def make(self, data):
+        return CliConfigurationConfig(**data)
+
+    @post_dump
+    def unmake(self, data):
+        return CliConfigurationConfig.remove_reduced_attrs(data)
+
+
+class CliConfigurationConfig(BaseConfig):
+    SCHEMA = CliConfigurationSchema
+    IDENTIFIER = 'cli'
+
+    def __init__(self, check_count=0, current_version=None, min_version=None, log_handler=None):
+        self.check_count = check_count
+        self.current_version = current_version
+        self.min_version = min_version
+        self.log_handler = log_handler
