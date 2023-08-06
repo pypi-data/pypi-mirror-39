c26272bdc70447f68fada59fc7efd1e8c88ef5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:27 +0000

    Add package jpyutils/0.1.0/jpyutils-0.1.0-py3-none-any.whl

---
+++ packages/jpyutils/jpyutils-0.1.0-py3-none-any.whl/jpyutils/__init__.py
@@ -0,0 +1,18 @@
+"""
+Personal code for daily use.
+============================
+"""
+__version__ = "0.1.0"
+
+from . import utils
+from . import runner
+
+#from . import datasets
+#from . import mltools
+#
+#try:
+#    from . import models
+#    __all__ = ["utils", "datasets", "runner", "mltools"]
+#
+#except Exception as e:
+#    __all__ = ["utils", "models", "datasets", "runner", "mltools"]
