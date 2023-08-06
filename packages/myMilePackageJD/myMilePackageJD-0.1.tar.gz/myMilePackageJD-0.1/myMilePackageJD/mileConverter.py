974e4d8262e6008aeca7d20a2b14fffbc1ca4fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:01 +0000

    Add package myMilePackageJD/0.1/myMilePackageJD-0.1.tar.gz

---
+++ packages/myMilePackageJD/myMilePackageJD-0.1.tar.gz/myMilePackageJD-0.1/myMilePackageJD/mileConverter.py
@@ -0,0 +1,8 @@
+MILE_TO_YARD = 1760
+MILE_TO_FEET = 5280
+
+def to_yard(val):
+    return val * MILE_TO_YARD
+
+def to_feet(val):
+    return val * MILE_TO_FEET
---
+++ packages/myMilePackageJD/myMilePackageJD-0.1.tar.gz/myMilePackageJD-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myMilePackageJD',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A test python package',
+	url='https://github.com/khalad-hasan/myMileConverter',
+    author='Zhiji',
+    author_email='jimmydzj2006@gmail.com'
+)
