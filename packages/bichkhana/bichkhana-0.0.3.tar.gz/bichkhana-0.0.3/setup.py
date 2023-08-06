27712213caa75aee87696a3f633836de7d8bde1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:21 +0000

    Add package bichkhana/0.0.3/bichkhana-0.0.3.tar.gz

---
+++ packages/bichkhana/bichkhana-0.0.3.tar.gz/bichkhana-0.0.3/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name="bichkhana",
+    version="0.0.3",
+    author="shankar",
+    author_email="ishankar.prasad@gmail.com",
+    description="A small example package",
+    url="https://github.com/pypa/example-project",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
