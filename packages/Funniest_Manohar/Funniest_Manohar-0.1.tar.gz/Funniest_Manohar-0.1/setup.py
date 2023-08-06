f25dbb723f9232b870a1d4c9a4c36844484be9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:40 +0000

    Add package Funniest_Manohar/0.1/Funniest_Manohar-0.1.tar.gz

---
+++ packages/Funniest_Manohar/Funniest_Manohar-0.1.tar.gz/Funniest_Manohar-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Funniest_Manohar',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='',
+      author='Manohar Kowthavarapu',
+      author_email='mailme.manu31@gmail.com',
+      license='MIT',
+      packages=['Funniest_Manohar'],
+      zip_safe=False)
