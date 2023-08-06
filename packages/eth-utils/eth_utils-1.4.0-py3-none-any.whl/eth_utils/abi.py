d34cf1dbde4612e9a321ac9f8a0ddb2c876920f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:06 +0000

    Add package eth-utils/1.4.0/eth_utils-1.4.0-py3-none-any.whl

---
+++ packages/eth-utils/eth_utils-1.4.0-py3-none-any.whl/eth_utils/abi.py
@@ -0,0 +1,60 @@
+from typing import Any, Dict
+
+from .crypto import keccak
+
+
+def collapse_if_tuple(abi):
+    """Converts a tuple from a dict to a parenthesized list of its types.
+
+    >>> from eth_utils.abi import collapse_if_tuple
+    >>> collapse_if_tuple(
+    ...     {
+    ...         'components': [
+    ...             {'name': 'anAddress', 'type': 'address'},
+    ...             {'name': 'anInt', 'type': 'uint256'},
+    ...             {'name': 'someBytes', 'type': 'bytes'},
+    ...         ],
+    ...         'type': 'tuple',
+    ...     }
+    ... )
+    '(address,uint256,bytes)'
+    """
+    if not abi["type"].startswith("tuple"):
+        return abi["type"]
+
+    component_types = [collapse_if_tuple(component) for component in abi["components"]]
+
+    collapsed = "(" + ",".join(component_types) + ")"
+
+    if abi["type"].endswith("[]"):
+        collapsed += "[]"
+
+    return collapsed
+
+
+def _abi_to_signature(abi: Dict[str, Any]) -> str:
+    function_signature = "{fn_name}({fn_input_types})".format(
+        fn_name=abi["name"],
+        fn_input_types=",".join(
+            [collapse_if_tuple(abi_input) for abi_input in abi.get("inputs", [])]
+        ),
+    )
+    return function_signature
+
+
+def function_signature_to_4byte_selector(event_signature: str) -> bytes:
+    return keccak(text=event_signature.replace(" ", ""))[:4]
+
+
+def function_abi_to_4byte_selector(function_abi: Dict[str, Any]) -> bytes:
+    function_signature = _abi_to_signature(function_abi)
+    return function_signature_to_4byte_selector(function_signature)
+
+
+def event_signature_to_log_topic(event_signature: str) -> bytes:
+    return keccak(text=event_signature.replace(" ", ""))
+
+
+def event_abi_to_log_topic(event_abi: Dict[str, Any]) -> bytes:
+    event_signature = _abi_to_signature(event_abi)
+    return event_signature_to_log_topic(event_signature)
