97c168d5f50874f143f776674003605db7bafcc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:18 +0000

    Add package backgroundchanger/0.1.4/backgroundchanger-0.1.4.tar.gz

---
+++ packages/backgroundchanger/backgroundchanger-0.1.4.tar.gz/backgroundchanger-0.1.4/backgroundchanger/config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from os import path
+
+AUTHOR = 'Connor Ruggles'
+EMAIL = 'conruggles@gmail.com'
+VERSION = '0.1.4'
+URL = 'https://github.com/rugglcon/python-backgroundchanger'
+CONFIG_FOLDER = path.join(Path.home(), '.config', '.python-backgroundchanger')
+CONFIG_FILE = path.join(CONFIG_FOLDER, 'unsplash_keys.json')
+CONFIG_DOWNLOAD_FOLDER = path.join(CONFIG_FOLDER, 'photos')
