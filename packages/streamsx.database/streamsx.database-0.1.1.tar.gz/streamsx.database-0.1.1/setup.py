b057fda72837e9c3c8b3a517715e48db434a9dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:07 +0000

    Add package streamsx.database/0.1.1/streamsx.database-0.1.1.tar.gz

---
+++ packages/streamsx.database/streamsx.database-0.1.1.tar.gz/streamsx.database-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(
+  name = 'streamsx.database',
+  packages = ['streamsx.database'],
+  include_package_data=True,
+  version = '0.1.1',
+  description = 'IBM Streams Database integration',
+  long_description = open('DESC.txt').read(),
+  author = 'IBM Streams @ github.com',
+  author_email = 'hegermar@de.ibm.com',
+  license='Apache License - Version 2.0',
+  url = 'https://github.com/IBMStreams/streamsx.database',
+  keywords = ['streams', 'ibmstreams', 'streaming', 'analytics', 'streaming-analytics', 'db2', 'database'],
+  classifiers = [
+    'Development Status :: 1 - Planning',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+  install_requires=['streamsx'],
+  
+  test_suite='nose.collector',
+  tests_require=['nose']
+)
