90d8fe76f244cc6bae0f7590f74d0d7afbb8a8be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:35 +0000

    Add package nestgo/1.4.0/nestgo-1.4.0-py3-none-any.whl

---
+++ packages/nestgo/nestgo-1.4.0-py3-none-any.whl/nestgo.py
@@ -0,0 +1,15 @@
+import sys
+def print_lol(the_list,indent=False,level=0,fh=sys.stdout):
+    """This function takes a positional argument called"the_list",which is any
+    Python list(of,possibly,nested lists).Each data item in the provided list
+    is(recursively) printed to the screenon its own line.
+    A second argument called "level" is used to insert tab_stops when a nested
+    list is encountered"""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1,fh)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end=' ',file=fh) 
+            print(each_item,file=fh)
