4739b904a0635526185557bc1add3f39d067297e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:37 +0000

    Add package py-cgrates/0.0.1/py-cgrates-0.0.1.tar.gz

---
+++ packages/py-cgrates/py-cgrates-0.0.1.tar.gz/py-cgrates-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='py-cgrates',
+      description='Py CGRrateS',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      version='0.0.1',
+      url='https://github.com/hampsterx/py-cgrates',
+      author='Tim van der Hulst',
+      author_email='tim.vdh@gmail.com',
+      license='GNU General Public License v3.0',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Programming Language :: Python :: 3'
+      ],
+      packages=['cgrates'],
+      install_requires=[
+
+      ]
+)
