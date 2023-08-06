a10b3d2577306341bac5384fb657a9d1d0058cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:05 +0000

    Add package sphinx-removed-in/0.1.1/sphinx-removed-in-0.1.1.tar.gz

---
+++ packages/sphinx-removed-in/sphinx-removed-in-0.1.1.tar.gz/sphinx-removed-in-0.1.1/sphinx_removed_in/__init__.py
@@ -0,0 +1,14 @@
+from sphinx.locale import versionlabels
+from sphinx.directives.other import VersionChange
+
+
+__version__ = '0.1.1'
+
+
+def setup(app):
+    for _directive in ['versionremoved', 'removed-in']:
+        if _directive not in versionlabels:
+            versionlabels[_directive] = 'Removed in version %s'
+            app.add_directive(_directive, VersionChange)
+
+    return {'version': __version__}
