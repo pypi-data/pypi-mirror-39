b04d2ff644a95e530c69150a33159b25e11ca7c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:04 +0000

    Add package electrical-formula/0.1/electrical_formula-0.1.tar.gz

---
+++ packages/electrical-formula/electrical_formula-0.1.tar.gz/electrical_formula-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(name='electrical_formula',
+        version='0.1',
+        description='The formula of electrical enginering ',
+        url='https://github.com/zizonk/electrical-formula.git',
+        author='M afif Nur Azizi',
+        author_email='afifnurazizi@gmail.com',
+        license='MIT',
+        packages=['electrical_formula'],
+        
+        dependency_links=[
+                'https://github.com/zizonk/electrical-formula'
+        ],
+        
+    long_description_content_type="text/markdown",
+        zip_safe=False)
