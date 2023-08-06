84db7bfe2662cc235797bab3544a202c691f0a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:09 +0000

    Add package redis-opentracing/1.0.0/redis_opentracing-1.0.0.tar.gz

---
+++ packages/redis-opentracing/redis_opentracing-1.0.0.tar.gz/redis_opentracing-1.0.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+version = open('VERSION').read()
+setup(
+    name='redis_opentracing',
+    version=version,
+    url='https://github.com/opentracing-contrib/python-redis/',
+    download_url='https://github.com/opentracing-contrib/python-redis/tarball/'+version,
+    license='Apache License 2.0',
+    author='Carlos Alberto Cortez',
+    author_email='calberto.cortez@gmail.com',
+    description='OpenTracing support for Elasticsearch',
+    long_description=open('README.rst').read(),
+    packages=['redis_opentracing'],
+    platforms='any',
+    install_requires=[
+        'redis',
+        'opentracing>=2.0,<2.1'
+    ],
+    extras_require={
+        'tests': [
+            'flake8<3',
+            'flake8-quotes',
+            'mock<1.1.0',
+            'pytest>=2.7,<3',
+            'pytest-cov',
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
