325dac89ae0f45ec1f4ecd0119c14090bd4af400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:36 +0000

    Add package pymadx/1.6.0/pymadx-1.6.0.tar.gz

---
+++ packages/pymadx/pymadx-1.6.0.tar.gz/pymadx-1.6.0/pymadx/Convert/__init__.py
@@ -0,0 +1,10 @@
+"""
+Conversion from and to various formats related to the MadX format
+
+See individual methods for documentation
+
+"""
+
+from _Mad8ToMadx import Mad8ToMadx
+from _TfsToPtc import TfsToPtc
+from _Transport2Madx import Transport2Madx
