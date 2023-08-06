dd8af5f5ef1d56faecb4d0eb41b6322200a8a0b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:36 +0000

    Add package mjlib/0.1.54/mjlib-0.1.54.tar.gz

---
+++ packages/mjlib/mjlib-0.1.54.tar.gz/mjlib-0.1.54/mjlib/gui/GridBuilder.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+
+from mjlib.gui.TkBuilder import TkBuilder
+
+
+class GridBuilder(TkBuilder):
+    def __init__(self, widget):
+        TkBuilder.__init__(self, widget)
+        self.row = 0
+        self.col = 0
+        self._maxRow = 0
+        self._maxCol = 0
+        self.orientation = tk.RIGHT
+        self.columnspan = 1  # default tkinter value
+
+    def layout(self, widget):
+        widget.grid(row=self.row, column=self.col)
+        widget.pack()
+        self._move()
+
+    def nextRow(self):
+        self.row += 1
+        self.col = 0
+
+    def evenOut(self):
+        for i in range(self._maxCol + 1):
+            tk.Grid.columnconfigure(self.widget, i, weight=1)
+        for i in range(self._maxRow + 1):
+            tk.Grid.rowconfigure(self.widget, i, weight=1)
+
+    def resetExtraOptions(self):
+        self.columnspan = 1
+
+    def _move(self):
+        if self.orientation is tk.RIGHT:
+            self.col += 1
+        else:
+            self.row += 1
+        self._maxCol = max(self.col, self._maxCol)
+        self._maxRow = max(self.row, self._maxRow)
