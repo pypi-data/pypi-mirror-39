45eabeb6817d236db3a6308d81b2da72d9e270a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:52 +0000

    Add package podiant-workflows-client/3.6.1/podiant-workflows-client-3.6.1.tar.gz

---
+++ packages/podiant-workflows-client/podiant-workflows-client-3.6.1.tar.gz/podiant-workflows-client-3.6.1/workflows/__init__.py
@@ -0,0 +1,9 @@
+from .workers import LoggedWorker as logged
+
+
+__version__ = '3.6.1'
+__all__ = [
+    'endpoint',
+    'operation',
+    'logged'
+]
---
+++ packages/podiant-workflows-client/podiant-workflows-client-3.6.1.tar.gz/podiant-workflows-client-3.6.1/workflows/exceptions.py
@@ -0,0 +1,10 @@
+class NamespaceError(Exception):
+    pass
+
+
+class OperationError(Exception):
+    pass
+
+
+class ProgrammingError(Exception):
+    pass
