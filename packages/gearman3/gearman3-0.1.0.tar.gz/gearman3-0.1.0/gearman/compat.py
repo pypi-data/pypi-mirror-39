177f1d809ac886be27af058c084fc4855a5f709f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:24 +0000

    Add package gearman3/0.1.0/gearman3-0.1.0.tar.gz

---
+++ packages/gearman3/gearman3-0.1.0.tar.gz/gearman3-0.1.0/gearman/compat.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8
+
+import sys
+
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+
+
+if PY3:
+    binary_type = bytes
+
+    def array_to_bytes(arr):
+        return arr.tobytes()
+
+    def itervalues(d):
+        return d.values()
+
+    def to_hex(binary_str):
+        return binary_str.hex()
+
+else:
+    binary_type = str
+
+    def array_to_bytes(arr):
+        return arr.tostring()
+
+    def itervalues(d):
+        return d.itervalues()
+
+    def to_hex(binary_str):
+        return binary_str.encode('hex')
