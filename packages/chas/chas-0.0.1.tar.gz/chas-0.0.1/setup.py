d2e56c3e147db8821deb507d26efeb843e90ae4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:31 +0000

    Add package chas/0.0.1/chas-0.0.1.tar.gz

---
+++ packages/chas/chas-0.0.1.tar.gz/chas-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chas",
+    version="0.0.1",
+    author="Lukas Cerny",
+    author_email="lukas.cerny@exponea.com",
+    scripts=["script.py"],
+    description="Framework for creating and running cron jobs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lukasotocerny/chas",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
