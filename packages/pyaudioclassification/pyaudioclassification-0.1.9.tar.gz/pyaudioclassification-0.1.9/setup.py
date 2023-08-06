09b05b2c1ec45d756c389715e2808a45278b1da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:45 +0000

    Add package pyaudioclassification/0.1.9/pyaudioclassification-0.1.9.tar.gz

---
+++ packages/pyaudioclassification/pyaudioclassification-0.1.9.tar.gz/pyaudioclassification-0.1.9/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='pyaudioclassification',
+      version='0.1.9',
+      description='Dead simple audio classification',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+      ],
+      keywords='simple audio classification',
+      url='https://github.com/98mprice/pyAudioClassification',
+      author='98mprice',
+      author_email='98mprice@gmail.com',
+      license='MIT',
+      packages=['pyaudioclassification'],
+      install_requires=[
+          'numpy',
+          'librosa',
+          'soundfile',
+          'tqdm',
+          'keras'
+      ],
+      scripts=['bin/feature_extraction'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      include_package_data=True,
+      zip_safe=False)
