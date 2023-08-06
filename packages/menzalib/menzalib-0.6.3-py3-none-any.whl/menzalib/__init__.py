c011470544fd6cb75d0c0e958a101c095a8f7119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:26 +0000

    Add package menzalib/0.6.3/menzalib-0.6.3-py3-none-any.whl

---
+++ packages/menzalib/menzalib-0.6.3-py3-none-any.whl/menzalib/__init__.py
@@ -0,0 +1,5 @@
+from .errori_strumenti import dVdig, dRdig, dVosc, dtosc
+from .errore_capacita import dCdig
+from .analisi_errori import drapp, dprod, dpoli, dlog, d_dB, int_rette
+from .funzioni_fit import curve_fitdx, chi2_pval
+from .latex import ns_tex, ne_tex, mat_tex
