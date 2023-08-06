aaa7cdd5ff846cb5f7f0dac1d1a1ebb7339ae6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:24 +0000

    Add package mkdocs-minify-plugin/0.1.0/mkdocs_minify_plugin-0.1.0-py2-none-any.whl

---
+++ packages/mkdocs-minify-plugin/mkdocs_minify_plugin-0.1.0-py2-none-any.whl/mkdocs_minify_plugin/plugin.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from timeit import default_timer as timer
+from datetime import datetime, timedelta
+
+from mkdocs import utils as mkdocs_utils
+from mkdocs.config import config_options, Config
+from mkdocs.plugins import BasePlugin
+
+from htmlmin import minify
+
+class MinifyPlugin(BasePlugin):
+
+    config_scheme = (
+        ('param', config_options.Type(mkdocs_utils.string_types, default='')),
+    )
+
+    def __init__(self):
+        self.enabled = True
+        self.total_time = 0
+
+    def on_post_page(self, output_content, page, config):
+        return minify(output_content)
+
