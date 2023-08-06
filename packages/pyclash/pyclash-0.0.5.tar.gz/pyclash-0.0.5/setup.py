d50e7cda9055bcf1fcd32775331b46829b96fe9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:26 +0000

    Add package pyclash/0.0.5/pyclash-0.0.5.tar.gz

---
+++ packages/pyclash/pyclash-0.0.5.tar.gz/pyclash-0.0.5/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+version = "0.0.5"
+
+install_requires = [
+    "jinja2",
+    "google-api-python-client>=1.7.4",
+    "pyyaml",
+    "click",
+    "google-cloud-pubsub>=0.38.0",
+    "google-cloud-logging>=1.7.0",
+    "halo",
+    "future-fstrings",
+]
+
+tests_require = ["pytest", "docker", "mock"]
+
+setup(
+    name="pyclash",
+    version=version,
+    description="Running bash scripts on the Google Compute Engine",
+    author="Otto Group",
+    license="Apache License, Version 2.0",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={"console_scripts": ["clash = pyclash.clash:cli"]},
+)
