7b0f9e35d1e84e24022b762e3d701cc290297edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:28 +0000

    Add package sleepy-install/0.1.1/sleepy_install-0.1.1.tar.gz

---
+++ packages/sleepy-install/sleepy_install-0.1.1.tar.gz/sleepy_install-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import time
+import setuptools
+
+t = 1
+
+setuptools.setup(
+    name="sleepy_install",
+    version="0.%d.1" % t,
+    author="Jon Peck",
+    author_email="peckjon@gmail.com",
+    description="Waits for [minor version number] minutes during package install. No other features; use for testing time-dependent automation.",
+    long_description="Wait for several minutes during package installation.\nNo other features; use for testing time-dependent automation.\nMinor version indicates wait time (e.g. for a 2 minute delay, use 0.2.*)",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+print("Begin %d minute wait..." % t)
+time.sleep(t*60)
+print("End wait.")
