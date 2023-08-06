b33071d296ddb143fd7a2f6cd865c35a052cbe2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:39 +0000

    Add package yoctopuce/1.10.33576/yoctopuce-1.10.33576.tar.gz

---
+++ packages/yoctopuce/yoctopuce-1.10.33576.tar.gz/yoctopuce-1.10.33576/setup.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from distutils.core import setup
+
+
+setup(name='yoctopuce',
+version='1.10.33576',
+packages=['yoctopuce'],
+description="Yoctopuce python API",
+author="Yoctopuce",
+author_email="support@yoctopuce.com",
+url="https://www.yoctopuce.com",
+include_package_data=True,
+keywords = 'Yoctopuce USB sensors actuators API',
+zip_safe=False,
+ classifiers=[
+'Development Status :: 5 - Production/Stable',
+'Intended Audience :: Customer Service',
+'Intended Audience :: Developers',
+'Intended Audience :: Science/Research',
+'Natural Language :: English',
+'Operating System :: MacOS :: MacOS X',
+'Operating System :: Microsoft :: Windows :: Windows XP',
+'Operating System :: Microsoft :: Windows :: Windows Server 2008',
+'Operating System :: Microsoft :: Windows :: Windows Vista',
+'Operating System :: Microsoft :: Windows :: Windows 7',
+'Operating System :: Microsoft :: Windows :: Windows 8',
+'Operating System :: Microsoft :: Windows :: Windows 8.1',
+'Operating System :: Microsoft :: Windows :: Windows 10',
+'Operating System :: Unix',
+'Operating System :: Unix',
+'Programming Language :: Python :: 2.6',
+'Programming Language :: Python :: 2.7',
+'Programming Language :: Python :: 3',
+'Programming Language :: Python :: 3.0',
+'Programming Language :: Python :: 3.1',
+'Programming Language :: Python :: 3.2',
+'Programming Language :: Python :: 3.3',
+'Programming Language :: Python :: 3.4',
+'Programming Language :: Python :: 3.5',
+'Programming Language :: Python :: 3.6',
+'Programming Language :: Python :: 3.7',
+]
+)
