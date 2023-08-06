03a36a8cd7f3b7597ddba919792694d36409051d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:35 +0000

    Add package gkec/0.1.1/gkec-0.1.1.tar.gz

---
+++ packages/gkec/gkec-0.1.1.tar.gz/gkec-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gkec",
+    version="0.1.1",
+    author="Sergio Perez",
+    author_email="sergiopr89@gmail.com",
+    description="A simple client to operate with GKE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/cattlelizers.io/gkec",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    install_requires=[
+        "google-api-python-client",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
