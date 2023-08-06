45f96739db69b1a3fd12aeb15c41aee6e1c5756b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:04 +0000

    Add package django-include-by-ajax/0.5.0/django-include-by-ajax-0.5.0.tar.gz

---
+++ packages/django-include-by-ajax/django-include-by-ajax-0.5.0.tar.gz/django-include-by-ajax-0.5.0/include_by_ajax/static/include_by_ajax/js/include_by_ajax.js
@@ -0,0 +1,24 @@
+jQuery(function($) {
+    // 1. Check if there are placeholders
+    let $placeholders = $('.js-ajax-placeholder');
+    let placeholder_count = $placeholders.length;
+    if (!placeholder_count) {
+        return;
+    }
+    // 2. If yes, then load the same page with full_render=1 query parameter again by Ajax
+    let url = location.href.replace(/#.*/, '');
+    if (url.indexOf('?') === -1) {
+        url += '?include_by_ajax_full_render=1';
+    } else {
+        url += '&include_by_ajax_full_render=1';
+    }
+    // 3. Load the page again by Ajax and with additional parameter
+    $.get(url, function(responseHTML) {
+        // 4. For each placeholder fill in the content
+        $('<div>').append($.parseHTML(responseHTML)).find('.js-ajax-placeholder>*').each(function(index, element) {
+            $placeholders[index].replaceWith(element);
+        });
+        // 5. Trigger a special event "include_by_ajax_all_loaded"
+        $(document).trigger('include_by_ajax_all_loaded');
+    }, 'html');
+});
