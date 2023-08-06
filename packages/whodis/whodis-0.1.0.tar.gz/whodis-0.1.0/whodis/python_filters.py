26688acaed47bda52fe49bc8f4b0ee1a8c5cd89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:08 +0000

    Add package whodis/0.1.0/whodis-0.1.0.tar.gz

---
+++ packages/whodis/whodis-0.1.0.tar.gz/whodis-0.1.0/whodis/python_filters.py
@@ -0,0 +1,12 @@
+PYTHON_FILTERS = [
+    "__pycache__",
+    "build",
+    "dist",
+    "egg-info",
+    "develop-eggs",
+    "sdist",
+    "wheels",
+    "pytest_cache",
+    "env",
+    "venv",
+]
