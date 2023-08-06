90123264f3a546bcd438ad6ac0f89db72c970b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:58 +0000

    Add package pybling/0.0.2/pybling-0.0.2.tar.gz

---
+++ packages/pybling/pybling-0.0.2.tar.gz/pybling-0.0.2/pybling/products.py
@@ -0,0 +1,24 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BLING_SECRET_KEY = os.getenv("BLING_API_KEY")
+
+
+def list_products():
+    url = 'https://bling.com.br/Api/v2/produtos/json/'
+    payload = {'apikey': BLING_SECRET_KEY}
+
+    produtos = requests.get(url, params=payload)
+    return produtos
+
+
+def get_product(codigo):
+    url = f'https://bling.com.br/Api/v2/produto/{codigo}/json/'
+    payload = {'apikey': BLING_SECRET_KEY}
+
+    produto = requests.get(url, params=payload)
+    return produto
