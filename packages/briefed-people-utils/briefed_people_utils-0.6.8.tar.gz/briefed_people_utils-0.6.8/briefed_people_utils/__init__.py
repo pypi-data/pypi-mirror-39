29abc3397cf9ab08b9cc964d6b23588cc7c4a73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:32 +0000

    Add package briefed-people-utils/0.6.8/briefed_people_utils-0.6.8.tar.gz

---
+++ packages/briefed-people-utils/briefed_people_utils-0.6.8.tar.gz/briefed_people_utils-0.6.8/briefed_people_utils/__init__.py
@@ -0,0 +1,1 @@
+name = "briefed_people_utils"
---
+++ packages/briefed-people-utils/briefed_people_utils-0.6.8.tar.gz/briefed_people_utils-0.6.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='briefed_people_utils',
+    version='0.6.8',
+    description='Tools to support collect and send publically available data to Briefed.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://Briefed.eu',
+    author='Marek Zaremba-Pike',
+    author_email='marekzp@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'bs4',
+        'lxml',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
