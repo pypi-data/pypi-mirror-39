75b389945591a030d9157c49b34df892502e7750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:14 +0000

    Add package ultron/0.0.1/ultron-0.0.1.tar.gz

---
+++ packages/ultron/ultron-0.0.1.tar.gz/ultron-0.0.1/README.md
@@ -0,0 +1,3 @@
+# ultron
+
+The chatbot library
---
+++ packages/ultron/ultron-0.0.1.tar.gz/ultron-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name='ultron',
+    version='0.0.1',
+    description='',
+    author='Maikel van den Boogerd',
+    author_email='maikelboogerd@gmail.com',
+    url='https://github.com/maikelboogerd/ultron',
+    keywords=[],
+    packages=['ultron'],
+    install_requires=[],
+    license='MIT',
+    zip_safe=False,
+)
