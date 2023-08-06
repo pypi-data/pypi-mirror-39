7da6303e344ac58da6a33d4431cadd4801862482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:20 +0000

    Add package ssmx/1.0.0/ssmx-1.0.0.tar.gz

---
+++ packages/ssmx/ssmx-1.0.0.tar.gz/ssmx-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='ssmx',
+    author="Johny Georges",
+    author_email="jgeorges371@gmail.com",
+    url="https://github.com/JetJet13/ssmx",
+    version='v1.0.0',
+    py_modules=['ssmx'],
+    download_url="https://github.com/JetJet13/ssmx/archive/v1.0.0.tar.gz",
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'boto3',
+        'tabulate',
+        'subprocess32'
+    ],
+    entry_points='''
+        [console_scripts]
+        ssmx=ssmx:cli
+    '''
+)
