ef7ff6f2e07b62138b44c415514e2367f9d64bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:51 +0000

    Add package instakit/0.4.2/instakit-0.4.2.tar.gz

---
+++ packages/instakit/instakit-0.4.2.tar.gz/instakit-0.4.2/instakit/processors/ext/halftone.pyx
@@ -0,0 +1,77 @@
+
+from __future__ import division
+
+import numpy
+cimport numpy
+cimport cython
+
+from instakit.utils.ndarrays import ndarray_fromimage, ndarray_toimage
+
+cdef extern from "halftone.h" nogil:
+    unsigned char adderror(int b, int e)
+    unsigned char* threshold_matrix
+
+INT = numpy.int
+FLOAT32 = numpy.float32
+
+ctypedef numpy.int_t int_t
+ctypedef numpy.float32_t float32_t
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+cdef void atkinson_dither(int_t[:, :] input_view, int_t w, int_t h) nogil:
+    
+    cdef int_t y, x, err, oldpx, newpx
+    
+    for y in range(h):
+        for x in range(w):
+            oldpx = input_view[y, x]
+            newpx = <int_t>threshold_matrix[oldpx]
+            err = (oldpx - newpx) >> 3
+            
+            input_view[y, x] = newpx
+            
+            if y + 1 < h:
+                input_view[y+1, x] = adderror(input_view[y+1, x], err)
+            
+            if y + 2 < h:
+                input_view[y+2, x] = adderror(input_view[y+2, x], err)
+            
+            if (y > 0) and (x + 1 < w):
+                input_view[y-1, x+1] = adderror(input_view[y-1, x+1], err)
+            
+            if x + 1 < w:
+                input_view[y, x+1] = adderror(input_view[y, x+1], err)
+            
+            if (y + 1 < h) and (x + 1 < w):
+                input_view[y+1, x+1] = adderror(input_view[y+1, x+1], err)
+            
+            if x + 2 < w:
+                input_view[y, x+2] = adderror(input_view[y, x+2], err)
+
+
+cdef class Atkinson:
+    
+    cdef:
+        bint threshold_matrix_allocated
+    
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    @cython.cdivision(True)
+    def __cinit__(self, float32_t threshold = 128.0):
+        cdef int_t i
+        if not self.threshold_matrix_allocated:
+            for i in range(255):
+                threshold_matrix[i] = <unsigned char>(<int_t>(<float32_t>i / threshold) * 255)
+            self.threshold_matrix_allocated = True
+    
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    @cython.cdivision(True)
+    def process(self, image not None):
+        input_array = ndarray_fromimage(image.convert('L')).astype(INT)
+        cdef int_t[:, :] input_view = input_array
+        atkinson_dither(input_view, image.size[0], image.size[1])
+        output_array = numpy.asarray(input_view.base)
+        return ndarray_toimage(output_array)
---
+++ packages/instakit/instakit-0.4.2.tar.gz/instakit-0.4.2/instakit/processors/halftone.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+halftone.py
+
+Created by FI$H 2000 on 2012-08-23.
+Copyright (c) 2012 Objects In Space And Time, LLC. All rights reserved.
+"""
+from __future__ import print_function
+
+from PIL import Image, ImageDraw, ImageStat
+
+from instakit.utils import pipeline
+from instakit.utils.gcr import gcr
+
+LO_TUP = (0,)
+HI_TUP = (255,)
+
+class SlowAtkinson(object):
+    
+    def __init__(self, threshold = 128.0):
+        self.threshold_matrix = int(threshold)  * LO_TUP + \
+                           (256-int(threshold)) * HI_TUP
+    
+    def process(self, image):
+        image = image.convert('L')
+        for y in range(image.size[1]):
+            for x in range(image.size[0]):
+                old = image.getpixel((x, y))
+                new = self.threshold_matrix[old]
+                err = (old - new) >> 3 # divide by 8.
+                image.putpixel((x, y), new)
+                for nxy in [(x+1, y), (x+2, y), (x-1, y+1), (x, y+1), (x+1, y+1), (x, y+2)]:
+                    try:
+                        image.putpixel(nxy, image.getpixel(nxy) + err)
+                    except IndexError:
+                        pass # it happens, evidently.
+        return image
+
+try:
+    from instakit.processors.ext.halftone import Atkinson as FastAtkinson
+except ImportError:
+    Atkinson = SlowAtkinson
+else:
+    Atkinson = FastAtkinson
+
+class DotScreen(object):
+    
+    def __init__(self, sample=1, scale=2, angle=0):
+        self.sample = sample
+        self.scale = scale
+        self.angle = angle
+    
+    def process(self, image):
+        origsize = image.size
+        image = image.convert('L').rotate(self.angle, expand=1)
+        size = image.size[0]*self.scale, image.size[1]*self.scale
+        halftone = Image.new('L', size)
+        dotscreen = ImageDraw.Draw(halftone)
+        
+        for x in range(0, image.size[0], self.sample):
+            for y in range(0, image.size[0], self.sample):
+                cropbox = image.crop((x,             y,
+                                      x+self.sample, y+self.sample))
+                stat = ImageStat.Stat(cropbox)
+                diameter = (stat.mean[0] / 255) ** 0.5
+                edge = 0.5 * (1-diameter)
+                xpos, ypos = (x+edge)*self.scale, (y+edge)*self.scale
+                boxedge = self.sample * diameter * self.scale
+                dotscreen.ellipse((xpos,         ypos,
+                                   xpos+boxedge, ypos+boxedge),
+                                   fill=255)
+        
+        halftone = halftone.rotate(-self.angle, expand=1)
+        halfwidth, halfheight = halftone.size
+        xx = (halfwidth - origsize[0]*self.scale) / 2
+        yy = (halfheight - origsize[1]*self.scale) / 2
+        return halftone.crop((xx, yy, xx+origsize[0]*self.scale,
+                                      yy+origsize[1]*self.scale))
+
+class CMYKDotScreen(object):
+    
+    def __init__(self,
+        gcr=20, sample=10, scale=10,
+        thetaC=0, thetaM=15, thetaY=30, thetaK=45):
+        
+        self.gcr = max(min(100, gcr), 0)
+        self.overprinter = pipeline.ChannelFork(DotScreen, mode='CMYK')
+        self.overprinter.update({
+            'C': DotScreen(angle=thetaC, sample=sample, scale=scale),
+            'M': DotScreen(angle=thetaM, sample=sample, scale=scale),
+            'Y': DotScreen(angle=thetaY, sample=sample, scale=scale),
+            'K': DotScreen(angle=thetaK, sample=sample, scale=scale), })
+    
+    def process(self, image):
+        return self.overprinter.process(
+            gcr(image, self.gcr))
+
+
+if __name__ == '__main__':
+    from instakit.utils import static
+    
+    image_paths = list(map(
+        lambda image_file: static.path('img', image_file),
+            static.listfiles('img')))
+    image_inputs = list(map(
+        lambda image_path: Image.open(image_path).convert('RGB'),
+            image_paths))
+    
+    for image_input in image_inputs:
+        #image_input.show()
+        Atkinson(threshold=128.0).process(image_input).show()
+        CMYKDotScreen(sample=2, scale=2).process(image_input).show()
+    
+    print(image_paths)
