7da1ecaeec33377c080677177c69bf5b72c4778b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:19 +0000

    Add package biztime/0.0.4/biztime-0.0.4.tar.gz

---
+++ packages/biztime/biztime-0.0.4.tar.gz/biztime-0.0.4/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="biztime",
+  version="0.0.4",
+  author="austen0",
+  author_email="austen@cas3y.com",
+  description=("Utility for calculating the total working time between given "
+               "dates/times."),
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/austen0/biztime",
+  packages=setuptools.find_packages(),
+)
