e2fd32d17f11de881b144249cd74d1306cbac857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:22 +0000

    Add package menzalib/0.5.20/menzalib-0.5.20.tar.gz

---
+++ packages/menzalib/menzalib-0.5.20.tar.gz/menzalib-0.5.20/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r", encoding="latin1") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name="menzalib",
+      version="0.5.20",
+      author="Lettera, Francesco215",
+      author_email="",
+      description="Funzioni utili per lab3",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://bitbucket.org/Lettera/menza-lib/src",
+      packages=setuptools.find_packages(),
+      )
