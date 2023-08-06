662a275fca9f53b7f05efb5263bd9692d73829cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:27 +0000

    Add package rolling-window/0.1.2/rolling_window-0.1.2.tar.gz

---
+++ packages/rolling-window/rolling_window-0.1.2.tar.gz/rolling_window-0.1.2/rolling_window/helpers.py
@@ -0,0 +1,18 @@
+import functools
+
+
+def make_async(fn):
+    """
+    turns a sync function to async function using threads
+    """
+    from concurrent.futures import ThreadPoolExecutor
+    import asyncio
+
+    pool = ThreadPoolExecutor()
+
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        future = pool.submit(fn, *args, **kwargs)
+        return asyncio.wrap_future(future)  # make it awaitable
+
+    return wrapper
---
+++ packages/rolling-window/rolling_window-0.1.2.tar.gz/rolling_window-0.1.2/rolling_window/peristance.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import os
+from typing import Dict
+import hickle
+import re
+from typing import Dict, Union
+
+import asyncio
+from concurrent.futures import ProcessPoolExecutor
+
+from rolling_window.helpers import make_async
+
+
+class Peristance:
+    @make_async
+    def save(self, data_dict: Dict, path: str, id: str) -> None:
+        pp = Path(path)
+        if pp.exists() is False or pp.is_dir() is False:
+            pp.mkdir(parents=True, exist_ok=True)
+
+        with open("%s/%s.hkl" % (path, id), "w") as fi:
+            hickle.dump(data_dict[id], fi)
+
+    def load_all(self, path: str) -> Dict:
+        data_dict = {}
+        for datafile in filter(lambda x: x.endswith(".hkl"), os.listdir(path)):
+            id = re.sub("\.hkl$", "", datafile)
+            data_dict[id] = hickle.load(datafile)
+
+        return data_dict
+
+    def load(self, id: str, data_dict: dict, path: str) -> Union[Dict, None]:
+        os.chdir(path)
+        try:
+            with open("%s/%s.hkl" % (path, id), "r") as fi:
+                data_dict[id] = hickle.load(datafile)
+            return data_dict[id]
+        except:
+            raise OSError("Missing data file for given id")
