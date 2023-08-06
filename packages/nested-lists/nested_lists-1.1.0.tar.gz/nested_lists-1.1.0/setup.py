ba251b2dd1da9b168d7453a75dc0e3d05b1beca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:47 +0000

    Add package nested-lists/1.1.0/nested_lists-1.1.0.tar.gz

---
+++ packages/nested-lists/nested_lists-1.1.0.tar.gz/nested_lists-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup (
+	name		= 'nested_lists',
+	version		= '1.1.0',
+	py_modules	= ['nested_lists'],
+	author		= 'Pedro Moura',
+	author_email	= 'pedro.zzz.moura@gmail.com',
+	url		= 'https://github.com/pedro612xyz',
+	description	= 'A simple printer of nested lists, based on "Head First Python" (Barry, Paul)',
+      )
+
+
