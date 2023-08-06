d25e37fc2afb6d51dc29bb92577988ec1ff8dc05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:33 +0000

    Add package ig-scraper/0.0.9/ig-scraper-0.0.9.tar.gz

---
+++ packages/ig-scraper/ig-scraper-0.0.9.tar.gz/ig-scraper-0.0.9/README.md
@@ -0,0 +1,10 @@
+IG Scraper
+==========
+
+```python
+from ig_scraper import IGScraper
+
+
+scraper = IGScraper()
+scraper.scrape_hashtag('YourHashtag', maximum=100)
+```
---
+++ packages/ig-scraper/ig-scraper-0.0.9.tar.gz/ig-scraper-0.0.9/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+__author__ = 'Sutrisno Efendi <kangfend@gmail.com>'
+__version__ = '0.0.9'
+
+
+setup(
+    name='ig-scraper',
+    packages=['ig_scraper'],
+    version=__version__,
+    description='Instagram hashtag scraper',
+    license='MIT',
+    author='Sutrisno Efendi',
+    author_email='kangfend@gmail.com',
+    url='https://github.com/kangfend/ig-scraper',
+    download_url='https://github.com/kangfend/ig-scraper/tarball/' + __version__,
+    keywords=['Instagram', 'Scraper'],
+    install_requires=['requests'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
