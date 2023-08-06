4e6f8abe637986e17eef15e20e124942c0609c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:22 +0000

    Add package volpy/18.12.0.dev0/volpy-18.12.0.dev0.tar.gz

---
+++ packages/volpy/volpy-18.12.0.dev0.tar.gz/volpy-18.12.0.dev0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='volpy',
+    version='18.12.0dev',
+    packages=find_packages(),
+    install_requires=['numpy',
+                      'scipy',
+                      'pandas',
+                      'utm',
+                      'sympy',
+                      'plotly',],
+
+    package_data={
+        'volpy': ['sample_data/survey_ibema_faxinal_Cartesian.csv', ],
+    },
+
+    author='Andre Guerra',
+    author_email='agu3rra@gmail.com',
+    description='volpy: Volume calculations in Python',
+    long_description='Calculate volumes out of a Digital Elevation Model (DEM) represented by Triangulated Irregular Network',
+    url='https://github.com/agu3rra/volpy/',
+    license='GPL',
+    keywords='gps volume terrain survey'
+)
