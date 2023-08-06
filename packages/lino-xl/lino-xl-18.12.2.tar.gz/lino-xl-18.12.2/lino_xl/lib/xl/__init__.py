d4d29731c153bb75f822f1c281baa1f39c49faef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:18 +0000

    Add package lino-xl/18.12.2/lino-xl-18.12.2.tar.gz

---
+++ packages/lino-xl/lino-xl-18.12.2.tar.gz/lino-xl-18.12.2/lino_xl/lib/xl/__init__.py
@@ -0,0 +1,15 @@
+# Copyright 2016-2017 Rumma & Ko Ltd
+#
+# License: BSD (see file COPYING for details)
+
+"""A "base" plugin which should be installed in every application
+which uses at least one Lino XL plugin. It does not define any models,
+but a :xfile:`locale` directory with translation messages for Django.
+
+.. autosummary::
+   :toctree:
+
+    user_types
+    mixins
+
+"""
