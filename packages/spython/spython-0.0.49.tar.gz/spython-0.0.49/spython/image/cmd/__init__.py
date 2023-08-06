c75026f600bb9bd67b4aa2ee28fa3829cb0b6387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:13 +0000

    Add package spython/0.0.49/spython-0.0.49.tar.gz

---
+++ packages/spython/spython-0.0.49.tar.gz/spython-0.0.49/spython/image/cmd/__init__.py
@@ -0,0 +1,56 @@
+
+# Copyright (C) 2017-2018 Vanessa Sochat.
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+# License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+def generate_image_commands():
+    ''' The Image client holds the Singularity image command group, mainly
+        deprecated commands (image.import) and additional command helpers
+        that are commonly use but not provided by Singularity
+
+        The levels of verbosity (debug and quiet) are passed from the main
+        client via the environment variable MESSAGELEVEL.
+
+        These commands are added to Client.image under main/__init__.py to 
+        expose subcommands:
+
+            Client.image.export
+            Client.image.imprt
+            Client.image.decompress
+            Client.image.create
+
+    '''
+
+    class ImageClient(object):
+        group = "image"
+
+    from spython.main.base.logger import println
+    from spython.main.base.command import ( init_command, run_command )
+    from .utils import ( compress, decompress )
+    from .create import create
+    from .importcmd import importcmd
+    from .export import export
+
+    ImageClient.create = create
+    ImageClient.imprt = importcmd
+    ImageClient.export = export
+    ImageClient.decompress = decompress
+    ImageClient.compress = compress
+    ImageClient.println = println
+    ImageClient.init_command = init_command
+    ImageClient.run_command = run_command
+ 
+    cli = ImageClient()
+    return cli
---
+++ packages/spython/spython-0.0.49.tar.gz/spython-0.0.49/spython/version.py
@@ -0,0 +1,29 @@
+
+# Copyright (C) 2017-2018 Vanessa Sochat.
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+# License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+__version__ = "0.0.49"
+AUTHOR = 'Vanessa Sochat'
+AUTHOR_EMAIL = 'vsochat@stanford.edu'
+NAME = 'spython'
+PACKAGE_URL = "http://www.github.com/singularityhub/singularity-cli"
+KEYWORDS = 'singularity python client (spython)'
+DESCRIPTION = "Command line python tool for working with singularity."
+LICENSE = "LICENSE"
+
+INSTALL_REQUIRES = (
+)
