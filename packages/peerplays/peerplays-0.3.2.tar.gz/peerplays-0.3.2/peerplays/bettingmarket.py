11ff8137fa521e4f3d17005404b7ca1dccb3c6a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:53 +0000

    Add package peerplays/0.3.2/peerplays-0.3.2.tar.gz

---
+++ packages/peerplays/peerplays-0.3.2.tar.gz/peerplays-0.3.2/peerplays/bettingmarket.py
@@ -0,0 +1,51 @@
+from peerplays.instance import BlockchainInstance
+from .exceptions import BettingMarketDoesNotExistException
+from .blockchainobject import BlockchainObject, ObjectCache
+
+
+class BettingMarket(BlockchainObject):
+    """ Read data about a Betting Market on the chain
+
+        :param str identifier: Identifier
+        :param peerplays blockchain_instance: PeerPlays() instance to use when
+            accesing a RPC
+
+    """
+    type_id = 25
+
+    def refresh(self):
+        assert self.identifier[:5] == "1.25.",\
+            "Identifier needs to be of form '1.25.xx'"
+        data = self.blockchain.rpc.get_object(self.identifier)
+        if not data:
+            raise BettingMarketDoesNotExistException(self.identifier)
+        super(BettingMarket, self).__init__(data)
+        self.cached = True
+
+    @property
+    def bettingmarketgroup(self):
+        from .bettingmarketgroup import BettingMarketGroup
+        return BettingMarketGroup(self["group_id"])
+
+
+class BettingMarkets(list, BlockchainInstance):
+    """ List of all available BettingMarkets
+
+        :param str betting_market_group_id: Market Group ID (``1.24.xxx``)
+    """
+    cache = ObjectCache()
+
+    def __init__(self, betting_market_group_id, *args, **kwargs):
+        BlockchainInstance.__init__(self, *args, **kwargs)
+
+        if betting_market_group_id in BettingMarkets.cache:
+            self.bettingmarkets = BettingMarkets.cache[betting_market_group_id]
+        else:
+            self.bettingmarkets = self.blockchain.rpc.list_betting_markets(
+                betting_market_group_id)
+            BettingMarkets.cache[betting_market_group_id] = self.bettingmarkets
+
+        super(BettingMarkets, self).__init__([
+            BettingMarket(x, lazy=False, blockchain_instance=self.blockchain)
+            for x in self.bettingmarkets
+        ])
---
+++ packages/peerplays/peerplays-0.3.2.tar.gz/peerplays-0.3.2/peerplays/sport.py
@@ -0,0 +1,50 @@
+from .instance import BlockchainInstance
+from .exceptions import SportDoesNotExistException
+from .blockchainobject import BlockchainObject, ObjectCache
+
+
+class Sport(BlockchainObject):
+    """ Read data about a sport on the chain
+
+        :param str identifier: Identifier
+        :param peerplays blockchain_instance: PeerPlays() instance to use when
+            accesing a RPC
+
+    """
+
+    type_id = 20
+
+    def refresh(self):
+        data = self.blockchain.rpc.get_object(self.identifier)
+        if not data:
+            raise SportDoesNotExistException(self.identifier)
+        super(Sport, self).__init__(data)
+
+    @property
+    def eventgroups(self):
+        from .eventgroup import EventGroups
+
+        return EventGroups(self["id"])
+
+
+class Sports(list, BlockchainInstance):
+    """ List of all available sports
+    """
+
+    cache = ObjectCache()
+
+    def __init__(self, **kwargs):
+        BlockchainInstance.__init__(self, **kwargs)
+
+        if "sports" in Sports.cache:
+            self.sports = Sports.cache["sports"]
+        else:
+            self.sports = self.blockchain.rpc.list_sports()
+            Sports.cache["sports"] = self.sports
+
+        super(Sports, self).__init__(
+            [
+                Sport(x, lazy=False, blockchain_instance=self.blockchain)
+                for x in self.sports
+            ]
+        )
