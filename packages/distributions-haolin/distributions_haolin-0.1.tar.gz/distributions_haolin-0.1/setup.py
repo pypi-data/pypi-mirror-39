b117a4c6010ca0d81dc56d860b03eb290ad9f32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:57 +0000

    Add package distributions-haolin/0.1/distributions_haolin-0.1.tar.gz

---
+++ packages/distributions-haolin/distributions_haolin-0.1.tar.gz/distributions_haolin-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_haolin',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_haolin'],
+      zip_safe=False)
