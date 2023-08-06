271f4d520f486c680b479e89a619a0e6f315b2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:17 +0000

    Add package submitter/0.2/submitter-0.2.tar.gz

---
+++ packages/submitter/submitter-0.2.tar.gz/submitter-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='submitter',
+      version='0.2',
+      description='Simple SGE job submitter.',
+      url='https://github.com/BillyLiggins/Submitter',
+      author='Billy Liggins',
+      author_email='billyliggins@gmail.com',
+      license='MIT',
+      packages=['submitter'],
+      package_dir={'submitter': 'submitter'},
+      package_data={'': ['config/config.ini']},
+      scripts=['bin/submitter'],
+      install_requires=[
+          'logzero',
+      ],
+      zip_safe=False
+      )
