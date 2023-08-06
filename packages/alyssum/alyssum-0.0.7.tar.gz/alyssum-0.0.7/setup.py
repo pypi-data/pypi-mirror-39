306433a0064d1b1101a32e9621986d7013942895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:17 +0000

    Add package alyssum/0.0.7/alyssum-0.0.7.tar.gz

---
+++ packages/alyssum/alyssum-0.0.7.tar.gz/alyssum-0.0.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="alyssum",
+    version="0.0.7",
+    author="Erdem Aybek",
+    author_email="eaybek@gmail.com",
+    description="A small helper package for personal needs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eaybek/alyssum",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'termcolor',
+    ],
+)
