9b84f60d54fb29eb8f516c6bf2203dec35d5482a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:30 +0000

    Add package cleanlab/0.0.6/cleanlab-0.0.6-py2.py3-none-any.whl

---
+++ packages/cleanlab/cleanlab-0.0.6-py2.py3-none-any.whl/cleanlab/version.py
@@ -0,0 +1,8 @@
+__version__ = '0.0.6'
+
+# 0.0.6 - NO API changes. README updates. Examples added. Tutorials added.
+# 0.0.5 - Numerous small bug fixes, but not major API changes. 100% testing code coverage.
+# 0.0.4 - FIRST CROSS-PLATFORM WORKING VERSION OF CLEANLAB. Adding test support.
+# 0.0.3 - Adding working logo to README, pypi working
+# 0.0.2 - Added logo to README, but link does not load on pypi
+# 0.0.1 - initial commit
