e89fe12ebfc92cee0b71a44e8784f8aaa54194d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:55 +0000

    Add package pymatex/0.0.14/pymatex-0.0.14.tar.gz

---
+++ packages/pymatex/pymatex-0.0.14.tar.gz/pymatex-0.0.14/pymatex/node/IterativeFunction.py
@@ -0,0 +1,25 @@
+from enum import Enum, auto
+from pymatex.node.Node import Node
+
+
+class IterativeType(Enum):
+    INTEGRAL = auto()
+    PRODUCT = auto()
+    SUM = auto()
+
+
+class IterativeFunction(Node):
+
+    def __init__(self, iterative_type: IterativeType, variable, start_range, end_range, expression):
+        super().__init__()
+
+        self.iterative_type = iterative_type
+        self.variable = variable
+        self.start_range = start_range
+        self.end_range = end_range
+        self.expression = expression
+
+    def __eq__(self, other):
+        return self.iterative_type == other.iterative_type and \
+               self.variable == other.variable and self.start_range == other.start_range and \
+               self.end_range == other.end_range and self.expression == other.expression
