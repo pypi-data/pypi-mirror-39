ef8a07b09b3e0113a693bc51bd543e2e52462491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:25 +0000

    Add package hdfscontents/0.7/hdfscontents-0.7.tar.gz

---
+++ packages/hdfscontents/hdfscontents-0.7.tar.gz/hdfscontents-0.7/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='hdfscontents',
+    version='0.7',
+    author='Ahmad Al-Shishtawy',
+    author_email='alshishtawy@gmail.com',
+    description='Jupyter content manager that uses the HDFS filesystem',
+    license='Apache License 2.0',
+    keywords='Jupyter, HDFS, HOPS, Hadoop',
+    url='https://github.com/hopshadoop/hdfscontents',
+    download_url = 'https://github.com/hopshadoop/hdfscontents/archive/0.6.tar.gz',
+    packages=['hdfscontents', 'tests'],
+    long_description=read('README.rst'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: IPython',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ], install_requires=['traitlets', 'notebook', 'pydoop', 'tornado', 'nbformat', 'ipython_genutils']
+)
