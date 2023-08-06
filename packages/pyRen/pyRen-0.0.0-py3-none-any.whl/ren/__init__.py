b563e414f4c0f480001b70a2183a193fec304751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:15 +0000

    Add package pyRen/0.0.0/pyRen-0.0.0-py3-none-any.whl

---
+++ packages/pyRen/pyRen-0.0.0-py3-none-any.whl/ren/__init__.py
@@ -0,0 +1,8 @@
+"""
+Ren <https://pointillistic.com/ren/> is a lightweight data-exchange text format. It is programming
+language independent with familiar conventions.
+
+:mod:`ren` exposes an API familiar to users of the standard library :mod:`json`, :mod:`marshal`
+and :mod:`pickle` modules. It is derived from the CPython ``json`` library.
+"""
+pass
