3ae74543ceba261b2882f2b377d7ea9eaac7e3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:49 +0000

    Add package get-wayback-machine/0.1.1/get_wayback_machine-0.1.1.tar.gz

---
+++ packages/get-wayback-machine/get_wayback_machine-0.1.1.tar.gz/get_wayback_machine-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+classifiers = [
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'License :: OSI Approved :: MIT License',
+]
+
+setup(name='get_wayback_machine',
+      version='0.1.1',
+      description="Fetch a URL via the latest Wayback Machine Snapshot",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/jfilter/get-wayback-machine',
+      author='Johannes Filter',
+      author_email='hi@jfilter.de',
+      license='MIT',
+      packages=['get_wayback_machine'],
+      install_requires=['get_retries'],
+      classifiers=classifiers)
