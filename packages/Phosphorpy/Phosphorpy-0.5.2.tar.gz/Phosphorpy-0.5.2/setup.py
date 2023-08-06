955988d49b1dc9e4330396631a3aab74d74c3620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:06 +0000

    Add package Phosphorpy/0.5.2/Phosphorpy-0.5.2.tar.gz

---
+++ packages/Phosphorpy/Phosphorpy-0.5.2.tar.gz/Phosphorpy-0.5.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='Phosphorpy',
+    version='0.5.2',
+    packages=['Phosphorpy', 'Phosphorpy.data',
+              'Phosphorpy.data.sub', 'Phosphorpy.data.sub.plots',
+              'Phosphorpy.test', 'Phosphorpy.config', 'Phosphorpy.report',
+              'Phosphorpy.fitting', 'Phosphorpy.external'],
+    # package_dir={'': 'Phosphorpy'},
+    include_package_data=True,
+    package_data={
+          '': ['local/survey.conf']
+      },
+    url='',
+    license='GPL',
+    author='Patrick Rauer',
+    author_email='j.p.rauer@sron.nl',
+    description='',
+    zip_safe=False,
+    install_requires=['seaborn', 'numpy', 'astropy', 'pandas', 'astroquery', 'numba', 'scikit-learn']
+)
