22629f68154a212cf7fb85498d3a5fa6c7132993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:41 +0000

    Add package sorting-asmaa/0.4.5/sorting_asmaa-0.4.5.tar.gz

---
+++ packages/sorting-asmaa/sorting_asmaa-0.4.5.tar.gz/sorting_asmaa-0.4.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name="sorting_asmaa",
+    version="0.4.5",
+    author="Asmaa Aly",
+    author_email="asmaa.ali@minerva.kgi.edu",
+    description="A sorting  package",
+    url="https://github.com/asmaaalaa99/sorting",
+    packages= ['sorting_asmaa'],
+    long_description=readme(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    
+
+)
