b40332f67a4201769cec0d7cc2e37dc83eadd5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:23 +0000

    Add package neuropot/0.1.5/neuropot-0.1.5.tar.gz

---
+++ packages/neuropot/neuropot-0.1.5.tar.gz/neuropot-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='neuropot',
+      version='0.1.5',
+      description='NeuroPot is a library which provides automated processing of neuroradiological images, data cleaning and manipulation functionality, machine learning algorithms and transfer learning methods.',
+      url='https://github.com/MEDCOMP/neuropot',
+      author='Abhinit Ambastha',
+      author_email='abhinit@comp.nus.edu.sg',
+      license='MIT',
+      packages=find_packages(),
+      package_data={
+      	'neuropot.preprocessing':['template/*.nii']
+      },
+      scripts=[
+      	'neuropot/preprocessing/bin/ACPCAlignment.sh'
+      ],
+      install_requires=[
+      	'SimpleITK'
+      ],
+      zip_safe=False)
