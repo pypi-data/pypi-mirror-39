c51b5a3e8ca09063736849641d1cace7ad817bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:11 +0000

    Add package pypistart/0.1.0/pypistart-0.1.0.tar.gz

---
+++ packages/pypistart/pypistart-0.1.0.tar.gz/pypistart-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pypistart',
+    version='0.1.0',
+    description='Starter kit for initializing a python package skeleton',
+    long_description=open('README').read(),
+    author='Jack Workman',
+    author_email='jworkman16@gmail.com',
+    url='http://pypi.python.org/pypi/pypistart/',
+    packages=['pypistart', 'pypistart.tests'],
+    #scripts=[],  # TODO -- make script for creating pypi package skeleton
+    license='LICENSE'
+)
