9a1d6ec93b01cb39452e5707a5846027b64cfc21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:56 +0000

    Add package Atest/0.0.0.7/Atest-0.0.0.7.tar.gz

---
+++ packages/Atest/Atest-0.0.0.7.tar.gz/Atest-0.0.0.7/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     setup
+   Description :
+   Author :        Asdil
+   date：          2018/10/26
+-------------------------------------------------
+   Change Activity:
+                   2018/10/26:
+    version = 1.7.1.7
+-------------------------------------------------
+"""
+__author__ = 'Asdil'
+
+from setuptools import setup
+
+setup(name='Atest',
+      version='0.0.0.7',
+      description='Tool of Asdil',
+      author='Asdil',
+      author_email='jpl4job@126.com',
+      maintainer='Asdil',
+      maintainer_email='jpl4job@126.com',
+      license="MIT",
+      packages=['Atest'],
+      platforms=["all"],
+      url='https://github.com/Asdil/Atest',
+      install_requires=[],
+      classifiers=[
+          "Environment :: Web Environment",
+          "Intended Audience :: Developers",
+          "Operating System :: OS Independent",
+          "Topic :: Text Processing :: Indexing",
+          "Topic :: Utilities",
+          "Topic :: Internet",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",])
+
+#  python setup.py check
+#  python setup.py sdist
+#  python setup.py register -r pypi
+#  python setup.py sdist upload -r pypi
