b6f9320e323d88a979ca080b29605f15f1825e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:02 +0000

    Add package politico-civic-campaign-tracker/0.1.7/politico-civic-campaign-tracker-0.1.7.tar.gz

---
+++ packages/politico-civic-campaign-tracker/politico-civic-campaign-tracker-0.1.7.tar.gz/politico-civic-campaign-tracker-0.1.7/tracker/serializers/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+from .person import (
+    PersonSerializer,
+    PersonListSerializer,
+    PersonHomeSerializer,
+)
+from .candidate import (
+    StorySerializer,
+    EndorsementSerializer,
+    QuoteSerializer,
+    TweetSerializer,
+)
