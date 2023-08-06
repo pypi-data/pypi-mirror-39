a9be2e9fd403170ead0a8f5bf54ecff58f6d7245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:16 +0000

    Add package pycfslib/0.6/pycfslib-0.6.tar.gz

---
+++ packages/pycfslib/pycfslib-0.6.tar.gz/pycfslib-0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycfslib',
+      version='0.6',
+      description='Library to read, write amd create CFS file and stream.',
+      url='https://github.com/neurobittechnologies/pycfslib',
+      author='Amiya Patanaik',
+      author_email='amiya@neurobit.io',
+      license='GPL',
+      packages=['pycfslib'],
+      zip_safe=False)
