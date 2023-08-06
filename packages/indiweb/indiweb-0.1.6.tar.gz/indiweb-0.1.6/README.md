9375bedd582c25775633e0026e314308186b7779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:42 +0000

    Add package indiweb/0.1.6/indiweb-0.1.6.tar.gz

---
+++ packages/indiweb/indiweb-0.1.6.tar.gz/indiweb-0.1.6/README.md
@@ -0,0 +1,181 @@
+# INDI Web Manager
+
+INDI Web Manager is a simple Web Application to manage
+[INDI](http://www.indilib.org) server. It supports multiple driver profiles
+along with optional custom remote drivers. It can be used to start INDI server
+locally, and also to connect or **chain** to remote INDI servers.
+
+![INDI Web Manager](http://indilib.org/images/indi/indiwebmanager.png)
+
+The Web Application is based on [Bottle Py](http://bottlepy.org)
+micro-framework. It has a built-in webserver and by default listens on port
+8624.
+
+# Installation
+
+Before installing the **indiweb** package, make sure the INDI library is
+installed on the target system.
+
+You can install the **indiweb** Python package using pip:
+
+```
+$ pip install indiweb
+```
+
+You may want to install it system-wide, only in your user account or even into
+a [virtual environment](https://virtualenv.pypa.io/en/stable/) (if you are a
+developer).
+
+If you want to install it system-wide, you will have to invoke pip with
+superuser rights:
+
+```
+$ sudo pip install indiweb
+```
+
+# Usage
+
+After installing the **indiweb** package, the command **indi-web** will be
+available in your sytem PATH.
+
+You can obtain help about the **indi-web** command by invoking:
+
+```
+$ indi-web -h
+```
+
+The INDI Web Manager runs as a standalone web server. It can be started
+manually by invoking:
+
+```
+$ indi-web -v
+```
+
+Then using your favorite web browser, go to
+[http://localhost:8624](http://localhost:8624) if the INDI Web Manager is
+running locally. If the INDI Web Manager is installed on a remote system,
+simply replace localhost with the hostname or IP address of the remote system.
+
+# Auto start
+
+If you selected any profile as **Auto Start** then the INDI server shall be
+automatically started when the service is executed at start up.
+
+# Systemd configuration
+
+The provided file `indiwebmanager.service` is an example *systemd service file*
+that can be used to run `indi-web` as *pi* user. If your username is different
+please edit the file and change the username first.
+
+Copy your preferred service file to `/etc/systemd/system`:
+
+```
+sudo cp indiwebmanager.service /etc/systemd/system/
+sudo chmod 644 /etc/systemd/system/indiwebmanager.service
+```
+
+Now configure systemd to load the service file during boot:
+
+```
+sudo systemctl daemon-reload
+sudo systemctl enable indiwebmanager.service
+```
+
+Finally, reboot the system for your changes to take effect:
+
+```
+sudo reboot
+```
+
+After startup, check the status of the INDI Web Manager service:
+
+```
+sudo systemctl status indiwebmanager.service
+```
+
+If all appears OK, you can start using the Web Application using any browser.
+
+# Profiles
+
+The Web Application provides a default profile to run simulator drivers. To use
+a new profile, add the profile name and then click  the plus button. Next,
+select which drivers to run under this particular profile. After selecting the
+drivers, click the **Save** icon to save all your changes. To enable automatic
+startup of INDI server for a particular profile when the device boots up or
+when invoked manually via the **systemctl** command, check the **Auto Start**
+checkbox.
+
+# API
+
+INDI Web Manager provides a RESTful API to control all aspects of the
+application. Data communication is via JSON messages. All URLs are appended to
+the hostname:port running the INDI Web Manager.
+
+## INDI Server Methods
+
+### Get Server Status
+
+ URL | Method | Return | Format
+--- | --- | --- | ---
+/api/server/status | GET | INDI server status (running or not) | {'server': bool, 'active_profile': profile_name}
+
+**Example:** curl http://localhost:8624/api/server/status
+**Reply:** [{"status": "False"}, {"active_profile": ""}]
+
+### Start Server
+
+ TODO
+
+### Stop Server
+URL | Method | Return | Format
+--- | --- | --- | ---
+/api/server/stop | POST | None | []
+
+### Get running drivers list
+URL | Method | Return | Format
+--- | --- | --- | ---
+/api/server/drivers | GET | Returns an array for all the locally running drivers | {'driver': driver_executable}
+
+**Example:** curl http://localhost:8624/api/server/drivers
+**Reply:** [{"driver": "indi_simulator_ccd"}, {"driver": "indi_simulator_telescope"}, {"driver": "indi_simulator_focus"}]
+
+## Profiles
+
+### Add new profile
+URL | Method | Return | Format
+--- | --- | --- | ---
+/api/profiles/<name> | POST | None | None
+
+To add a profile named **foo**:
+
+```
+curl -H "Content-Type: application/json" -X POST http://localhost:8624/api/profiles/foo
+```
+
+### Delete profile
+URL | Method | Return | Format
+--- | --- | --- | ---
+/api/profiles/<name> | DELETE | None | None
+
+To delete a profile named **foo**:
+
+```
+curl -X DELETE http://localhost:8624/api/profiles/foo
+```
+
+### Get All Profiles
+
+URL | Method | Return | Format
+--- | --- | --- | ---
+/api/profiles | GET | Returns all profiles | [{"port": number, "id": ID, "autostart": number, "name": profile_name}, ...]
+
+**Example:** curl http://localhost:8624/api/profiles
+**Reply:** [{"port": 7624, "id": 1, "autostart": 0, "autoconnect": 0, "name": "Simulators"}, {"port": 7624, "id": 2, "autostart": 0, "name": "EQ5"}]
+
+### TODO
+
+# Authors
+
+Jasem Mutlaq (mutlaqja@ikarustech.com)
+
+Juan Menendez (juanmb@gmail.com)
---
+++ packages/indiweb/indiweb-0.1.6.tar.gz/indiweb-0.1.6/indiweb/views/js/indi.js
@@ -0,0 +1,308 @@
+// Startup function
+$(function()
+{
+    $('[data-toggle="tooltip"]').tooltip();
+
+    loadCurrentProfileDrivers();
+    getStatus();
+
+    $("#drivers_list").change(function() {
+        var name = $("#profiles option:selected").text();
+        saveProfileDrivers(name, true);
+    });
+
+    $("#remote_drivers").change(function() {
+        var name = $("#profiles option:selected").text();
+        saveProfileDrivers(name, true);
+    });
+});
+
+function saveProfile() {
+    var options = profiles.options;
+    var name = options[options.selectedIndex].value;
+    // Remove any extra spaces
+    name = name.trim();
+
+    var url = "/api/profiles/" + name;
+
+    //console.log(url)
+
+    $.ajax({
+        type: 'POST',
+        url: encodeURI(url),
+        success: function() {
+            //console.log("add new a profile " + name);
+            saveProfileDrivers(name);
+        },
+        error: function() {
+            alert('error add new  profile failed');
+        }
+    });
+}
+
+function saveProfileInfo() {
+    var options = profiles.options;
+    var name = options[options.selectedIndex].value;
+    console.log(name);
+    var port = $("#profile_port").val();
+    console.log(port);
+    var autostart = ($('#profile_auto_start').is(':checked')) ? 1 : 0;
+    var autoconnect = ($('#profile_auto_connect').is(':checked')) ? 1 : 0;
+    //console.log(autostart);
+    //var url     =  "/api/profiles/" + name + "/" + port + "/" + autostart;
+    var url = "/api/profiles/" + name;
+
+    var profileInfo = {
+        "port": port,
+        "autostart": autostart,
+        "autoconnect": autoconnect,
+    };
+    profileInfo = JSON.stringify(profileInfo);
+    console.log("Profile info " + profileInfo);
+
+    console.log(url);
+
+    $.ajax({
+        type: 'PUT',
+        url: encodeURI(url),
+        data: profileInfo,
+        contentType: "application/json; charset=utf-8",
+        success: function() {
+            console.log("Profile " + name + " info is updated");
+        },
+        error: function() {
+            alert('error update profile info failed');
+        }
+    });
+}
+
+function saveProfileDrivers(profile, silent) {
+
+    if (typeof(silent) === 'undefined') silent = false;
+
+    var url = "/api/profiles/" + profile + "/drivers";
+    var drivers = [];
+
+    $("#drivers_list :selected").each(function(i, sel) {
+        drivers.push({
+            "label": $(sel).text()
+        });
+    });
+
+    // Check for remote drivers
+    var remote = $("#remote_drivers").val();
+    if (remote) {
+        drivers.push({
+            "remote": remote
+        });
+        console.log({
+            "remote": remote
+        });
+    }
+
+    drivers = JSON.stringify(drivers);
+
+    //console.log("my json string is " + drivers);
+
+    $.ajax({
+        type: 'POST',
+        url: encodeURI(url),
+        data: drivers,
+        contentType: "application/json; charset=utf-8",
+        success: function() {
+            //console.log("Drivers added successfully to profile");
+            if (silent === false)
+                $("#notify_message").html('<br/><div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Profile ' + profile + ' saved.</div>');
+        },
+        error: function() {
+            alert('error failed to add drivers to profile');
+        }
+    });
+}
+
+function loadCurrentProfileDrivers() {
+    clearDriverSelection();
+
+    var name = $("#profiles option:selected").text();
+    var url = "/api/profiles/" + name + "/labels";
+
+    $.getJSON(url, function(drivers) {
+        $.each(drivers, function(i, driver) {
+            var label = driver.label;
+            //console.log("Driver label is " + label);
+            var selector = "#drivers_list [value='" + label + "']";
+            $(selector).prop('selected', true);
+        });
+
+        $("#drivers_list").selectpicker('refresh');
+    });
+
+    url = encodeURI("/api/profiles/" + name + "/remote");
+
+    $.getJSON(url, function(data) {
+        if (data && data.drivers !== undefined) {
+            $("#remote_drivers").val(data.drivers);
+        }
+        else {
+            $("#remote_drivers").val("");
+        }
+    });
+
+    loadProfileData();
+
+}
+
+function loadProfileData() {
+    var name = $("#profiles option:selected").text();
+    var url = encodeURI("/api/profiles/" + name);
+
+    $.getJSON(url, function(info) {
+        if (info.autostart == 1)
+            $("#profile_auto_start").prop("checked", true);
+        else
+            $("#profile_auto_start").prop("checked", false);
+
+        if (info.autoconnect == 1)
+            $("#profile_auto_connect").prop("checked", true);
+        else
+            $("#profile_auto_connect").prop("checked", false);
+
+        $("#profile_port").val(info.port);
+
+    });
+}
+
+function clearDriverSelection() {
+    $("#drivers_list option").prop('selected', false);
+    $("#drivers_list").selectpicker('refresh');
+    // Uncheck Auto Start
+    $("#profile_auto").prop("checked", false);
+    $("#profile_port").val("7624");
+}
+
+function addNewProfile() {
+    var profile_name = $("#new_profile_name").val();
+    if (profile_name) {
+        //console.log("profile is " + profile_name);
+        $("#profiles").append("<option id='" + profile_name + "' selected>" + profile_name + "</option>");
+
+        clearDriverSelection();
+
+        $("#notify_message").html('<br/><div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Profile ' + profile_name + ' created. Select the profile drivers and then save the profile.</div>');
+    }
+}
+
+function removeProfile() {
+    //console.log("in delete profile");
+    var name = $("#profiles option:selected").text();
+    var url = "/api/profiles/" + name;
+
+    console.log(url);
+
+    if ($("#profiles option").size() == 1) {
+        $("#notify_message").html('<br/><div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Cannot delete default profile.</div>');
+        return;
+    }
+
+    $.ajax({
+        type: 'DELETE',
+        url: encodeURI(url),
+        success: function() {
+            //console.log("delete profile " + name);
+            $("#profiles option:selected").remove();
+            $("#profiles").selectpicker('refresh');
+            loadCurrentProfileDrivers();
+
+            $("#notify_message").html('<br/><div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Profile ' + name + ' deleted.</div>');
+        },
+        error: function() {
+            alert('error delete profile failed');
+        }
+    });
+}
+
+function toggleServer() {
+    var status = $.trim($("#server_command").text());
+
+    if (status == "Start") {
+        var profile = $("#profiles option:selected").text();
+        var url = "/api/server/start/" + profile;
+
+        $.ajax({
+            type: 'POST',
+            url: encodeURI(url),
+            success: function() {
+                //console.log("INDI Server started!");
+                getStatus();
+            },
+            error: function() {
+                alert('Failed to start INDI server.');
+            }
+        });
+    } else {
+        $.ajax({
+            type: 'POST',
+            url: "/api/server/stop",
+            success: function() {
+                //console.log("INDI Server stopped!");
+                getStatus();
+            },
+            error: function() {
+                alert('Failed to stop INDI server.');
+            }
+        });
+    }
+}
+
+function getStatus() {
+    $.getJSON("/api/server/status", function(data) {
+        if (data[0].status == "True")
+            getActiveDrivers();
+        else {
+            $("#server_command").html("<span class='glyphicon glyphicon-cog' aria-hidden='true'></span> Start");
+            $("#server_notify").html("<p class='alert alert-success'>Server is offline.</p>");
+        }
+
+    });
+}
+
+function getActiveDrivers() {
+    $.getJSON("/api/server/drivers", function(data) {
+        $("#server_command").html("<span class='glyphicon glyphicon-cog' aria-hidden='true'></span> Stop");
+        var msg = "<p class='alert alert-info'>Server is Online.<ul  class=\"list-unstyled\">";
+        var counter = 0;
+        $.each(data, function(i, field) {
+            msg += "<li>" + "<button class=\"btn btn-xs\" " +
+		"onCLick=\"restartDriver('" + field.label + "')\" data-toggle=\"tooltip\" " +
+		"title=\"Restart Driver\">" +
+		"<span class=\"glyphicon glyphicon-repeat\" aria-hidden=\"true\"></span></button> " +
+		field.label + "</li>";
+            counter++;
+        });
+
+        msg += "</ul></p>";
+
+        $("#server_notify").html(msg);
+
+        if (counter < $("#drivers_list :selected").size()) {
+            $("#notify_message").html('<br/><div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Not all profile drivers are running. Make sure all devices are powered and connected.</div>');
+            return;
+        }
+    });
+
+}
+
+
+function restartDriver(label) {
+        $.ajax({
+            type: 'POST',
+            url: "/api/drivers/restart/" + label,
+            success: function() {
+                getStatus();
+		$("#notify_message").html('<br/><div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Restarting driver "' + label + '" succeeded.</div>');
+            },
+            error: function() {
+                alert('Restarting driver "' + label + '" failed!');
+            }
+        });
+ }
