f2d621c0c8a343e0202cfca6f5e6a6053d7ec2f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:16 +0000

    Add package fatpack/0.5.8/fatpack-0.5.8-py2.py3-none-any.whl

---
+++ packages/fatpack/fatpack-0.5.8-py2.py3-none-any.whl/fatpack/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+from .rainflow import *
+from .endurance import *
+
+__version__ = "0.5.8"
