54e46f397da453d7721c06645a442b1fa033409b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:34 +0000

    Add package trask/0.1.3/trask-0.1.3.tar.gz

---
+++ packages/trask/trask-0.1.3.tar.gz/trask-0.1.3/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='trask',
+    version='0.1.3',
+    description='Declarative recipe-based task runner',
+    url='https://github.com/nicholasbishop/trask',
+    author='Nicholas Bishop',
+    author_email='nicholasbishop@gmail.com',
+    py_modules=['trask'],
+    install_requires=['attrs', 'tatsu'])
