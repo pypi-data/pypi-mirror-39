35440f25c19697a64a8bfe84254ba587c6fb059c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:05 +0000

    Add package ioppytest-agent/0.1.15/ioppytest-agent-0.1.15.tar.gz

---
+++ packages/ioppytest-agent/ioppytest-agent-0.1.15.tar.gz/ioppytest-agent-0.1.15/setup.py
@@ -0,0 +1,58 @@
+# Authors:
+#     Federico Sismondi
+#     Remy Leone
+
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+
+MAJOR = 0
+MINOR = 1
+PATCH = 15
+VERSION = '{}.{}.{}'.format(MAJOR, MINOR, PATCH)
+
+name = 'ioppytest-agent'
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Internet',
+    'Topic :: Software Development :: Testing',
+    'Topic :: Scientific/Engineering',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Operating System :: MacOS'
+]
+
+with open('version.py', 'w') as f:
+    f.write('__version__ = "{version}"\n'.format(version=VERSION))
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name=name,
+    author='Federico Sismondi',
+    author_email='federicosismondi@gmail.com',
+    maintainer='Federico Sismondi',
+    maintainer_email='federicosismondi@gmail.com',
+    url='https://gitlab.f-interop.eu/f-interop-contributors/agent',
+    description='Component for setting up users environment (mainly virtual network interfaces) for running tests',
+    version=VERSION,
+    license='GPLv3+',
+    classifiers=CLASSIFIERS,
+    packages=find_packages(exclude=['tests']),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'click',
+        'six',
+        'kombu',
+        'pika',
+        'pyserial',
+    ],
+    entry_points={'console_scripts': ['ioppytest-agent=agent.agent_cli:main']},
+)
