79575d8dd00a5a63e0739249733cdc1d33cf0278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:27 +0000

    Add package lino-noi/18.11.0/lino-noi-18.11.0.tar.gz

---
+++ packages/lino-noi/lino-noi-18.11.0.tar.gz/lino-noi-18.11.0/lino_noi/lib/cal/models.py
@@ -0,0 +1,45 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2017-2018 Rumma & Ko Ltd
+# License: BSD (see file COPYING for details)
+"""Database models for this plugin.
+
+"""
+
+from lino.api import dd, _
+
+from lino_xl.lib.cal.models import *
+
+class Event(Event, ContactRelated):
+    class Meta:
+        app_label = 'cal'
+        abstract = dd.is_abstract_model(__name__, 'Event')
+
+dd.update_field(Event, 'user', verbose_name=_("Author"))
+dd.update_field(Event, 'company', verbose_name=_("Organizer"))
+dd.update_field(Event, 'contact_person', verbose_name=_("Contact person"))
+
+
+class RoomDetail(dd.DetailLayout):
+    main = """
+    id name
+    company contact_person
+    description
+    cal.EntriesByRoom
+    """
+
+Rooms.set_detail_layout(RoomDetail())
+
+class EventDetail(EventDetail):
+    start = "start_date start_time"
+    end = "end_date end_time"
+    main = "general GuestsByEvent"
+    general = dd.Panel("""
+    event_type:20 summary:60 id
+    start end access_class #all_day #assigned_to #duration #state
+    user room company contact_person
+    project owner workflow_buttons 
+    # owner created:20 modified:20
+    description #blogs.EntriesByController #outbox.MailsByController
+    """, label=_("General"))
+    
+Events.set_detail_layout(EventDetail())
