66f1e1b9d40dac68ebc1650cbf8a4c6f71ce15cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:57 +0000

    Add package pdfreactor-api/1.0.dev4/pdfreactor-api-1.0.dev4.tar.gz

---
+++ packages/pdfreactor-api/pdfreactor-api-1.0.dev4.tar.gz/pdfreactor-api-1.0.dev4/setup.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*- vim: et ts=8 sw=4 sts=4 si tw=79 cc=+1
+"""Installer for the pdfreactor-api package."""
+
+from setuptools import find_packages
+from setuptools import setup
+from os.path import isfile
+
+package_name = 'pdfreactor-api'
+
+# -------------------------------------------- [ get the version ... [
+def read_version(fn, sfn):
+    main = open(fn).read().strip()
+    if sfn is not None and isfile(sfn):
+        suffix = valid_suffix(open(sfn).read().strip())
+    else:
+        suffix = ''
+    return main + suffix
+    # ... get the version ...
+def valid_suffix(suffix):
+    """
+    Enforce our suffix convention
+    """
+    suffix = suffix.strip()
+    if not suffix:
+        return suffix
+    allowed = set('.dev0123456789')
+    disallowed = set(suffix).difference(allowed)
+    if disallowed:
+        disallowed = ''.join(sorted(disallowed))
+        raise ValueError('Version suffix contains disallowed characters'
+                         ' (%(disallowed)s)'
+                         % locals())
+    chunks = suffix.split('.')
+    chunk = chunks.pop(0)
+    if chunk:
+        raise ValueError('Version suffix must start with "."'
+                         ' (%(suffix)r)'
+                         % locals())
+    if not chunks:
+        raise ValueError('Version suffix is too short'
+                         ' (%(suffix)r)'
+                         % locals())
+    for chunk in chunks:
+        if not chunk:
+            raise ValueError('Empty chunk %(chunk)r in '
+                             'version suffix %(suffix)r'
+                             % locals())
+        char = chunk[0]
+        if char in '0123456789':
+            raise ValueError('Chunk %(chunk)r of version suffix %(suffix)r'
+                             ' starts with a digit'
+                             % locals())
+        char = chunk[-1]
+        if char not in '0123456789':
+            raise ValueError('Chunk %(chunk)r of version suffix %(suffix)r'
+                             ' doesn\'t end with a digit'
+                             % locals())
+    return suffix  # ... valid_suffix
+    # ... get the version ...
+    # ... get the version ...
+VERSION = read_version('VERSION',
+                       'VERSION_SUFFIX')
+# -------------------------------------------- ] ... get the version ]
+
+
+# ------------------------------------------- [ for setup_kwargs ... [
+long_description = '\n\n'.join([
+    open('README.rst').read(),
+    open('CONTRIBUTORS.rst').read(),
+    open('CHANGES.rst').read(),
+])
+
+# see as well --> src/pdfreactor-api/configure.zcml:
+exclude_subpackages = (
+        )
+exclude_packages = []
+for subp in exclude_subpackages:
+    exclude_packages.extend([package_name + '.' + subp,
+                             package_name + '.' + subp + '.*',
+                             ])
+packages = find_packages(
+            'src',
+            exclude=exclude_packages)
+# ------------------------------------------- ] ... for setup_kwargs ]
+
+setup_kwargs = dict(
+    name=package_name,
+    version=VERSION,
+    description="Python API for PDFreactor",
+    long_description=long_description,
+    # Get more from https://pypi.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+    author='Tobias Herp',
+    author_email='tobias.herp@visaplan.com',
+    url='https://pypi.org/project/pdfreactor-api',
+    license='MIT License',
+    packages=packages,
+    namespace_packages=[
+        'pdfreactor',
+        ],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+        # ... further requirements removed
+    ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
+setup(**setup_kwargs)
