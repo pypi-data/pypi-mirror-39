8b4a16256d9253ae2f979d03dadd675faad430df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:38 +0000

    Add package feature-mining/0.0.6/feature_mining-0.0.6.tar.gz

---
+++ packages/feature-mining/feature_mining-0.0.6.tar.gz/feature_mining-0.0.6/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+#with open('requirements.txt', 'r') as fh:
+#    REQUIRES = fh.read().split("\n")
+REQUIRES = ['pandas']
+
+setuptools.setup(
+    name="feature_mining",
+    version="0.0.6",
+    author="H. Wilder, N. Freundlich, Santu Karmaker",
+    author_email="hwilder3@illinois.edu, norbert4@illinois.edu, karmake2@illinois.edu",
+    description="Mine implicit features using a generative feature language model.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(exclude=('tests',)),
+    install_requires=REQUIRES,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+print("Done installation...")
