9e9cd2903c4e1ab52838a2980f8ae9a53af1640f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:53 +0000

    Add package duckietown-world/1.0.18/duckietown-world-1.0.18.tar.gz

---
+++ packages/duckietown-world/duckietown-world-1.0.18.tar.gz/duckietown-world-1.0.18/src/duckietown_world/__init__.py
@@ -0,0 +1,15 @@
+# coding=utf-8 
+__version__ = '1.0.18' 
+
+import logging
+
+logging.basicConfig()
+logger = logging.getLogger('dt-world')
+logger.setLevel(logging.DEBUG)
+
+logger.info('duckietown-world %s' % __version__)
+
+from .geo import *
+from .seqs import *
+from .svg_drawing import *
+from .world_duckietown import *
