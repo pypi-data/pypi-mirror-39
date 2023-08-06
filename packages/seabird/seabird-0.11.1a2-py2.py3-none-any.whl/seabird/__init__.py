d1f0edd7cd916139a484122f77a622d05f0bf842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:46 +0000

    Add package seabird/0.11.1a2/seabird-0.11.1a2-py2.py3-none-any.whl

---
+++ packages/seabird/seabird-0.11.1a2-py2.py3-none-any.whl/seabird/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Guilherme Castelao'
+__email__ = 'guilherme@castelao.net'
+__version__ = '0.11.1a1'
+
+from .cnv import CNV, fCNV
+from .exceptions import CNVError
+
+# __all__ = ['CNV', 'fCNV']
