7836b9acbcceb037415f3202dc3495d1980a00eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:17 +0000

    Add package wc-csv/0.12/wc_csv-0.12-py3-none-any.whl

---
+++ packages/wc-csv/wc_csv-0.12-py3-none-any.whl/wc_csv/convert_wc_csv.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+def missing_value(csv, frequency):
+    df = pd.read_csv(csv, header=None)
+    df = df[df[1] <= frequency]
+    df.dropna(inplace=True)
+    df.reset_index(inplace=True)
+    return df
+
+def convert_string(df):
+    csv_str = "" 
+    words = []
+    for i in range(0, len(df[0])):
+        # 頻出回数分の単語を再生する
+        for j in range(0, df[1][i]):
+            words.append(df[0][i])
+    
+    np.random.shuffle(words)
+    for word in words:
+        csv_str += word + " "
+    return csv_str
