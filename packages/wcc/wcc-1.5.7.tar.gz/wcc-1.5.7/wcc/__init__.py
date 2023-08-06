9fc64ed5cd24bbd4709db05a9d546886ebe327bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:10 +0000

    Add package wcc/1.5.7/wcc-1.5.7.tar.gz

---
+++ packages/wcc/wcc-1.5.7.tar.gz/wcc-1.5.7/wcc/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '1.5.7'
+#从本目录下api.py里import Service和Bucket这个类
+#from .api import Service, Bucket
+from .urllibx import *
+from .utils import *
+from .oss   import *
+from .req   import getpage
+from .req   import getcookie
+from .req   import getua
