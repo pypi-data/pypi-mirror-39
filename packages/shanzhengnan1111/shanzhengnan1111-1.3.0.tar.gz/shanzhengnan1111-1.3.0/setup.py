c699434fd8822d4a9d6b67b5fca2032b4847e21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:17 +0000

    Add package shanzhengnan1111/1.3.0/shanzhengnan1111-1.3.0.tar.gz

---
+++ packages/shanzhengnan1111/shanzhengnan1111-1.3.0.tar.gz/shanzhengnan1111-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name="shanzhengnan1111",
+        version="1.3.0",
+        py_modules=["shanzhengnan1111"],
+        author="shanzhengnan",
+        author_email="28848861@qq.com",
+        url="http://www.headfirstlabs.com",
+        description="A S P OF T L",
+    )
