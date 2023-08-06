f5bf6ba4d0ec3fd71ec34058712cc5b1e79be36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:28 +0000

    Add package pygost/3.15/pygost-3.15.tar.gz

---
+++ packages/pygost/pygost-3.15.tar.gz/pygost-3.15/pygost/stubs/pygost/gost3410.pyi
@@ -0,0 +1,47 @@
+from typing import Dict
+from typing import Tuple
+
+
+DEFAULT_CURVE = ...  # type: str
+CURVE_PARAMS = ...  # type: Dict[str, Tuple[bytes, bytes, bytes, bytes, bytes, bytes]]
+MODE2SIZE = ...  # type: Dict[int, int]
+PublicKey = Tuple[int, int]
+
+
+class GOST3410Curve(object):
+    p = ...  # type: int
+    q = ...  # type: int
+    a = ...  # type: int
+    b = ...  # type: int
+    x = ...  # type: int
+    y = ...  # type: int
+
+    def __init__(
+        self, p: bytes, q: bytes, a: bytes, b: bytes, x: bytes, y: bytes
+    ) -> None: ...
+
+    def exp(self, degree: int, x: int=..., y: int=...) -> int: ...
+
+
+def public_key(curve: GOST3410Curve, prv: int) -> PublicKey: ...
+
+
+def sign(curve: GOST3410Curve, prv: int, digest: bytes, mode: int=...) -> bytes: ...
+
+
+def verify(
+    curve: GOST3410Curve,
+    pub: PublicKey,
+    digest: bytes,
+    signature: bytes,
+    mode: int=...,
+) -> bool: ...
+
+
+def prv_unmarshal(prv: bytes) -> int: ...
+
+
+def pub_marshal(pub: PublicKey, mode: int=...) -> bytes: ...
+
+
+def pub_unmarshal(pub: bytes, mode: int=...) -> PublicKey: ...
