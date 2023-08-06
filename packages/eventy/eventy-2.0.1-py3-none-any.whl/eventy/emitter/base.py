9bfed9a1e5f2abbef75fe7248a160d03b8bee299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:06 +0000

    Add package eventy/2.0.1/eventy-2.0.1-py3-none-any.whl

---
+++ packages/eventy/eventy-2.0.1-py3-none-any.whl/eventy/emitter/base.py
@@ -0,0 +1,23 @@
+from ..service.command import BaseCommand
+from ..event.base import BaseEvent
+
+__all__ = [
+    'BaseEventEmitter',
+    'BaseCommandEmitter'
+]
+
+
+class BaseEventEmitter:
+    def __init__(self, *args, **kw) -> None:
+        pass
+
+    async def send(self, event: BaseEvent, destination: str):
+        raise NotImplementedError
+
+
+class BaseCommandEmitter:
+    def __init__(self, *args, **kw) -> None:
+        pass
+
+    async def send(self, command: BaseCommand, destination: str):
+        raise NotImplementedError
