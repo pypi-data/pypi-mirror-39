ae9f87a93919cdd1e5cbe24ca63599a7b29393d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:37 +0000

    Add package pyvlx/0.2.6/pyvlx-0.2.6.tar.gz

---
+++ packages/pyvlx/pyvlx-0.2.6.tar.gz/pyvlx-0.2.6/pyvlx/node_updater.py
@@ -0,0 +1,24 @@
+"""Module for updating nodes via frames."""
+from .frames import FrameNodeStatePositionChangedNotification
+from .opening_device import OpeningDevice
+from .log import PYVLXLOG
+
+
+class NodeUpdater():
+    """Class for updating nodes via incoming frames,  usually received by house monitor."""
+
+    def __init__(self, pyvlx):
+        """Initialize NodeUpdater object."""
+        self.pyvlx = pyvlx
+
+    async def process_frame(self, frame):
+        """Update nodes via frame, usually received by house monitor."""
+        if isinstance(frame, FrameNodeStatePositionChangedNotification):
+            try:
+                node = self.pyvlx.nodes[frame.node_id]
+            except IndexError:
+                PYVLXLOG.warning("Could not access node with id %i", frame.node_id)
+            else:
+                if isinstance(node, OpeningDevice):
+                    node.position = frame.current_position
+                    await node.after_update()
