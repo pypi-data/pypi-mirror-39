44d6a5c47c347959fddb40e96849fec4a7485003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:19 +0000

    Add package infi.recipe.close_application/0.0.18/infi.recipe.close_application-0.0.18.tar.gz

---
+++ packages/infi.recipe.close_application/infi.recipe.close_application-0.0.18.tar.gz/infi.recipe.close_application-0.0.18/README.md
@@ -0,0 +1,17 @@
+Overview
+========
+This is an Infinidat project.
+
+Usage
+-----
+Nothing to use here.
+
+Checking out the code
+=====================
+
+Run the following:
+
+    easy_install -U infi.projector
+    projector devenv build
+
+Python 3 support is experimental and not fully tested.
---
+++ packages/infi.recipe.close_application/infi.recipe.close_application-0.0.18.tar.gz/infi.recipe.close_application-0.0.18/setup.py
@@ -0,0 +1,52 @@
+
+SETUP_INFO = dict(
+    name = 'infi.recipe.close_application',
+    version = '0.0.18',
+    author = 'Arnon Yaari',
+    author_email = 'arnony@infinidat.com',
+
+    url = 'https://github.com/Infinidat/infi.recipe.close_application',
+    license = 'BSD',
+    description = """buildout recipe for terminating all executables""",
+    long_description = """buildout recipe for terminating all executables""",
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    install_requires = ['psutil>=2.0',
+'setuptools',
+'zc.buildout'],
+    namespace_packages = ['infi', 'infi.recipe'],
+
+    package_dir = {'': 'src'},
+    package_data = {'': []},
+    include_package_data = True,
+    zip_safe = False,
+
+    entry_points = {
+        'console_scripts': [],
+        'gui_scripts': [],
+        'zc.buildout': ['default = infi.recipe.close_application:CloseApplication']
+        }
+)
+
+if SETUP_INFO['url'] is None:
+    _ = SETUP_INFO.pop('url')
+
+def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
+    _setup(**SETUP_INFO)
+
+if __name__ == '__main__':
+    setup()
+
