61ce7442d0e9b8b6353c5ee80c011b053e3ad18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:10 +0000

    Add package nifty-nesting/0.2.1/nifty-nesting-0.2.1.tar.gz

---
+++ packages/nifty-nesting/nifty-nesting-0.2.1.tar.gz/nifty-nesting-0.2.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='nifty-nesting',
+      version='0.2.1',
+      description='Python utilities for arbitrarily nested data structures.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Aetius Flavius',
+      author_email='aetius.flavius.390@gmail.com',
+      url='https://github.com/aetiusflavius/nifty-nesting/',
+      packages=['nifty_nesting'],
+      keywords=['nested', 'data', 'structure', 'arbitrary', 'utilities', 'manipulation'],
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+      ]
+     )
