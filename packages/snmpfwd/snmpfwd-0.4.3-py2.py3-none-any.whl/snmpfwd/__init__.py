7a6c1e33602844cfcb0055b68ef55144efea9750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:20 +0000

    Add package snmpfwd/0.4.3/snmpfwd-0.4.3-py2.py3-none-any.whl

---
+++ packages/snmpfwd/snmpfwd-0.4.3-py2.py3-none-any.whl/snmpfwd/__init__.py
@@ -0,0 +1,8 @@
+#
+# This file is part of snmpfwd software.
+#
+# Copyright (c) 2014-2018, Ilya Etingof <etingof@gmail.com>
+# License: http://snmplabs.com/snmpfwd/license.html
+#
+# http://www.python.org/dev/peps/pep-0396/
+__version__ = '0.4.3'
