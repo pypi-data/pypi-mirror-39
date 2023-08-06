440788e088cd729cafc220dfa2999dc69028970b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:03 +0000

    Add package xbrl_parser/0.1/xbrl_parser-0.1.tar.gz

---
+++ packages/xbrl_parser/xbrl_parser-0.1.tar.gz/xbrl_parser-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='xbrl_parser',
+    version='0.1',
+    packages=['xbrl_parser',],
+    license='',
+    long_description=open('README.md').read(),
+)
