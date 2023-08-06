1537def97281aea27660ad810b9ca2ebddaadfe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:06 +0000

    Add package vs-ip/0.2/vs-ip-0.2.tar.gz

---
+++ packages/vs-ip/vs-ip-0.2.tar.gz/vs-ip-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='vs-ip',
+    version='0.2',
+    url='http://github.com/peppelg/vs-ip',
+    author='peppelg',
+    author_email='me@peppelg.space',
+    license='The Unlicense',
+    scripts=['vs-ip.py'],
+    install_requires=['splinter'],
+    zip_safe=False
+)
