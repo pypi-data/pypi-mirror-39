81c418d21837daa0f421dc54ddbf31afe1c1b785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:35 +0000

    Add package DotMeme/0.1.1/DotMeme-0.1.1.tar.gz

---
+++ packages/DotMeme/DotMeme-0.1.1.tar.gz/DotMeme-0.1.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="DotMeme",
+	version="0.1.1",
+	author="Jake Leahy",
+	author_email="darhyaust@gmail.com",
+	description="A small example package to make memes from .meme files",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://gitlab.com/ire4ever1190/dotmeme",
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	install_requires=[
+		'Pillow',
+		'pyyaml',
+		'requests'
+	],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
