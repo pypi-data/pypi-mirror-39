04ed1f688d12239d05a81f17a5e6e72fbcbe3e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:59 +0000

    Add package kinto/11.2.1/kinto-11.2.1-cp3-none-any.whl

---
+++ packages/kinto/kinto-11.2.1-cp3-none-any.whl/kinto/plugins/quotas/listener.py
@@ -0,0 +1,223 @@
+import copy
+
+from pyramid.httpexceptions import HTTPInsufficientStorage
+from kinto.core.errors import http_error, ERRORS
+from kinto.core.storage.exceptions import RecordNotFoundError
+from kinto.core.utils import instance_uri
+
+from .utils import record_size
+
+
+QUOTA_RESOURCE_NAME = "quota"
+BUCKET_QUOTA_OBJECT_ID = "bucket_info"
+COLLECTION_QUOTA_OBJECT_ID = "collection_info"
+
+
+def raise_insufficient_storage(message):
+    raise http_error(HTTPInsufficientStorage(), errno=ERRORS.FORBIDDEN.value, message=message)
+
+
+def get_bucket_settings(settings, bucket_id, name):
+    return settings.get(
+        # Bucket specific
+        "quotas.bucket_{}_{}".format(bucket_id, name),
+        # Global to all buckets
+        settings.get("quotas.bucket_{}".format(name), None),
+    )
+
+
+def get_collection_settings(settings, bucket_id, collection_id, name):
+    return settings.get(
+        # Specific for a given bucket collection
+        "quotas.collection_{}_{}_{}".format(bucket_id, collection_id, name),
+        # Specific to given bucket collections
+        settings.get(
+            "quotas.collection_{}_{}".format(bucket_id, name),
+            # Global to all buckets collections
+            settings.get("quotas.collection_{}".format(name), None),
+        ),
+    )
+
+
+def on_resource_changed(event):
+    """
+    Everytime an object is created/changed/deleted, we update the
+    bucket counters.
+
+    If a new object exceeds the quotas, we reject the request.
+    """
+    payload = event.payload
+    action = payload["action"]
+    resource_name = payload["resource_name"]
+
+    if action == "delete" and resource_name == "bucket":
+        # Deleting a bucket already deletes everything underneath (including
+        # quotas info). See kinto/views/bucket.
+        return
+
+    settings = event.request.registry.settings
+
+    event_uri = payload["uri"]
+    bucket_id = payload["bucket_id"]
+    bucket_uri = instance_uri(event.request, "bucket", id=bucket_id)
+    collection_id = None
+    collection_uri = None
+    if "collection_id" in payload:
+        collection_id = payload["collection_id"]
+        collection_uri = instance_uri(
+            event.request, "collection", bucket_id=bucket_id, id=collection_id
+        )
+
+    bucket_max_bytes = get_bucket_settings(settings, bucket_id, "max_bytes")
+    bucket_max_items = get_bucket_settings(settings, bucket_id, "max_items")
+    bucket_max_bytes_per_item = get_bucket_settings(settings, bucket_id, "max_bytes_per_item")
+    collection_max_bytes = get_collection_settings(settings, bucket_id, collection_id, "max_bytes")
+    collection_max_items = get_collection_settings(settings, bucket_id, collection_id, "max_items")
+    collection_max_bytes_per_item = get_collection_settings(
+        settings, bucket_id, collection_id, "max_bytes_per_item"
+    )
+
+    max_bytes_per_item = collection_max_bytes_per_item or bucket_max_bytes_per_item
+
+    storage = event.request.registry.storage
+
+    targets = []
+    for impacted in event.impacted_records:
+        target = impacted["new" if action != "delete" else "old"]
+        # On POST .../records, the URI does not contain the newly created
+        # record id.
+        obj_id = target["id"]
+        parts = event_uri.split("/")
+        if resource_name in parts[-1]:
+            parts.append(obj_id)
+        else:
+            # Make sure the id is correct on grouped events.
+            parts[-1] = obj_id
+        uri = "/".join(parts)
+
+        old = impacted.get("old", {})
+        new = impacted.get("new", {})
+
+        targets.append((uri, obj_id, old, new))
+
+    try:
+        bucket_info = copy.deepcopy(
+            storage.get(
+                parent_id=bucket_uri,
+                collection_id=QUOTA_RESOURCE_NAME,
+                object_id=BUCKET_QUOTA_OBJECT_ID,
+            )
+        )
+    except RecordNotFoundError:
+        bucket_info = {"collection_count": 0, "record_count": 0, "storage_size": 0}
+
+    collection_info = {"record_count": 0, "storage_size": 0}
+    if collection_id:
+        try:
+            collection_info = copy.deepcopy(
+                storage.get(
+                    parent_id=collection_uri,
+                    collection_id=QUOTA_RESOURCE_NAME,
+                    object_id=COLLECTION_QUOTA_OBJECT_ID,
+                )
+            )
+        except RecordNotFoundError:
+            pass
+
+    # Update the bucket quotas values for each impacted record.
+    for (uri, obj_id, old, new) in targets:
+        old_size = record_size(old)
+        new_size = record_size(new)
+
+        if max_bytes_per_item is not None and action != "delete":
+            if new_size > max_bytes_per_item:
+                message = "Maximum bytes per object exceeded " "({} > {} Bytes.".format(
+                    new_size, max_bytes_per_item
+                )
+                raise_insufficient_storage(message)
+
+        if action == "create":
+            bucket_info["storage_size"] += new_size
+            if resource_name == "collection":
+                bucket_info["collection_count"] += 1
+                collection_info["storage_size"] += new_size
+            if resource_name == "record":
+                bucket_info["record_count"] += 1
+                collection_info["record_count"] += 1
+                collection_info["storage_size"] += new_size
+        elif action == "update":
+            bucket_info["storage_size"] -= old_size
+            bucket_info["storage_size"] += new_size
+            if resource_name in ("collection", "record"):
+                collection_info["storage_size"] -= old_size
+                collection_info["storage_size"] += new_size
+        else:  # action == 'delete':
+            bucket_info["storage_size"] -= old_size
+            if resource_name == "collection":
+                collection_uri = uri
+                bucket_info["collection_count"] -= 1
+                # When we delete the collection all the records in it
+                # are deleted without notification.
+                collection_records, _ = storage.get_all(
+                    collection_id="record", parent_id=collection_uri
+                )
+                for r in collection_records:
+                    old_record_size = record_size(r)
+                    bucket_info["record_count"] -= 1
+                    bucket_info["storage_size"] -= old_record_size
+                    collection_info["record_count"] -= 1
+                    collection_info["storage_size"] -= old_record_size
+                collection_info["storage_size"] -= old_size
+
+            if resource_name == "record":
+                bucket_info["record_count"] -= 1
+                collection_info["record_count"] -= 1
+                collection_info["storage_size"] -= old_size
+
+    if bucket_max_bytes is not None:
+        if bucket_info["storage_size"] > bucket_max_bytes:
+            message = "Bucket maximum total size exceeded " "({} > {} Bytes). ".format(
+                bucket_info["storage_size"], bucket_max_bytes
+            )
+            raise_insufficient_storage(message)
+
+    if bucket_max_items is not None:
+        if bucket_info["record_count"] > bucket_max_items:
+            message = "Bucket maximum number of objects exceeded " "({} > {} objects).".format(
+                bucket_info["record_count"], bucket_max_items
+            )
+            raise_insufficient_storage(message)
+
+    if collection_max_bytes is not None:
+        if collection_info["storage_size"] > collection_max_bytes:
+            message = "Collection maximum size exceeded " "({} > {} Bytes).".format(
+                collection_info["storage_size"], collection_max_bytes
+            )
+            raise_insufficient_storage(message)
+
+    if collection_max_items is not None:
+        if collection_info["record_count"] > collection_max_items:
+            message = "Collection maximum number of objects exceeded " "({} > {} objects).".format(
+                collection_info["record_count"], collection_max_items
+            )
+            raise_insufficient_storage(message)
+
+    storage.update(
+        parent_id=bucket_uri,
+        collection_id=QUOTA_RESOURCE_NAME,
+        object_id=BUCKET_QUOTA_OBJECT_ID,
+        record=bucket_info,
+    )
+
+    if collection_id:
+        if action == "delete" and resource_name == "collection":
+            # Deleting a collection already deletes everything underneath
+            # (including quotas info). See kinto/views/collection.
+            return
+        else:
+            storage.update(
+                parent_id=collection_uri,
+                collection_id=QUOTA_RESOURCE_NAME,
+                object_id=COLLECTION_QUOTA_OBJECT_ID,
+                record=collection_info,
+            )
---
+++ packages/kinto/kinto-11.2.1-cp3-none-any.whl/tests/core/resource/test_sync.py
@@ -0,0 +1,175 @@
+import json
+import time
+from unittest import mock
+
+from kinto.core.testing import ThreadMixin
+
+from . import BaseTest
+
+
+class SinceModifiedTest(ThreadMixin, BaseTest):
+    def setUp(self):
+        super().setUp()
+        self.validated["body"] = {"data": {}}
+
+        with mock.patch.object(self.model.storage, "bump_and_store_timestamp") as msec_mocked:
+            for i in range(6):
+                msec_mocked.return_value = i
+                self.resource.collection_post()
+
+    def test_filter_with_since_is_exclusive(self):
+        self.validated["querystring"] = {"_since": 3}
+        result = self.resource.collection_get()
+        self.assertEqual(len(result["data"]), 2)
+
+    def test_filter_with__to_is_exclusive(self):
+        self.validated["querystring"] = {"_to": 3}
+        result = self.resource.collection_get()
+        self.assertEqual(len(result["data"]), 3)
+
+    def test_filter_with__before_is_exclusive(self):
+        self.validated["querystring"] = {"_before": 3}
+        result = self.resource.collection_get()
+        self.assertEqual(len(result["data"]), 3)
+
+    def test_filter_with__to_return_an_alert_header(self):
+        self.validated["querystring"] = {"_to": 3}
+        self.resource.collection_get()
+        self.assertIn("Alert", self.resource.request.response.headers)
+        alert = self.resource.request.response.headers["Alert"]
+        self.assertDictEqual(
+            json.loads(alert),
+            {
+                "code": "soft-eol",
+                "message": ("_to is now deprecated, " "you should use _before instead"),
+                "url": (
+                    "https://kinto.readthedocs.io/en/2.4.0/api/resource"
+                    ".html#list-of-available-url-parameters"
+                ),
+            },
+        )
+
+    def test_get_timestamp_header_is_equal_to_last_modification(self):
+        result = self.resource.collection_post()["data"]
+        modification = result["last_modified"]
+        self.resource = self.resource_class(request=self.get_request(), context=self.get_context())
+        self.resource.request.validated = self.validated
+        self.resource.collection_get()
+        header = int(self.last_response.headers["ETag"][1:-1])
+        self.assertEqual(header, modification)
+
+    def test_delete_timestamp_header_is_equal_to_last_deleted(self):
+        self.resource.collection_post()["data"]
+        self.resource = self.resource_class(request=self.get_request(), context=self.get_context())
+        self.resource.request.validated = self.validated
+        result = self.resource.collection_delete()
+        modification = max([record["last_modified"] for record in result["data"]])
+        header = int(self.last_response.headers["ETag"][1:-1])
+        self.assertEqual(header, modification)
+
+    def test_post_timestamp_header_is_equal_to_creation(self):
+        result = self.resource.collection_post()["data"]
+        modification = result["last_modified"]
+        header = int(self.last_response.headers["ETag"][1:-1])
+        self.assertEqual(header, modification)
+
+    def test_filter_with_since_accepts_numeric_value(self):
+        self.validated["querystring"] = {"_since": 6}
+        self.resource.collection_post()
+        result = self.resource.collection_get()
+        self.assertEqual(len(result["data"]), 1)
+
+    def test_filter_from_last_modified_is_exclusive(self):
+        result = self.resource.collection_post()["data"]
+        current = result["last_modified"]
+
+        self.validated["querystring"] = {"_since": current}
+        result = self.resource.collection_get()
+        self.assertEqual(len(result["data"]), 0)
+
+    def test_filter_with_last_modified_includes_deleted_data(self):
+        self.resource.collection_post()
+        result = self.resource.collection_post()["data"]
+        current = result["last_modified"]
+
+        self.resource.record_id = result["id"]
+        self.resource.delete()
+
+        self.validated["querystring"] = {"_since": current}
+        result = self.resource.collection_get()
+        self.assertEqual(len(result["data"]), 1)
+        self.assertTrue(result["data"][0]["deleted"])
+
+    def test_filter_from_last_header_value_is_exclusive(self):
+        self.resource.collection_get()
+        current = int(self.last_response.headers["ETag"][1:-1])
+
+        self.validated["querystring"] = {"_since": current}
+        result = self.resource.collection_get()
+        self.assertEqual(len(result["data"]), 0)
+
+    def test_filter_works_with_empty_list(self):
+        self.resource.model.parent_id = "alice"
+        self.validated["querystring"] = {"_since": 3}
+        result = self.resource.collection_get()
+        self.assertEqual(len(result["data"]), 0)
+
+    def test_timestamp_are_always_identical_on_read(self):
+        def read_timestamp():
+            self.resource.collection_get()
+            return int(self.last_response.headers["ETag"][1:-1])
+
+        before = read_timestamp()
+        now = read_timestamp()
+        after = read_timestamp()
+        self.assertEqual(before, now)
+        self.assertEqual(now, after)
+
+    def test_timestamp_are_always_incremented_on_creation(self):
+        def read_timestamp():
+            record = self.resource.collection_post()["data"]
+            return record["last_modified"]
+
+        before = read_timestamp()
+        now = read_timestamp()
+        after = read_timestamp()
+        self.assertTrue(before < now < after)
+
+    def test_records_created_during_fetch_are_above_fetch_timestamp(self):
+
+        timestamps = {}
+
+        def long_fetch():
+            """Simulate a overhead while reading on storage."""
+
+            def delayed_get(*args, **kwargs):
+                time.sleep(0.100)  # 100 msec
+                return [], 0
+
+            with mock.patch.object(self.model.storage, "get_all", delayed_get):
+                self.resource.collection_get()
+                fetch_at = self.last_response.headers["ETag"][1:-1]
+                timestamps["fetch"] = int(fetch_at)
+
+        # Create a real record with no patched timestamp
+        self.resource.collection_post()
+
+        # Some client start fetching
+        thread = self._create_thread(target=long_fetch)
+        thread.start()
+
+        # Create record while other is fetching
+        time.sleep(0.020)  # 20 msec
+        # Instantiate a new resource/request to avoid shared references with
+        # the other one running in a thread:
+        resource = self.resource_class(request=self.get_request(), context=self.get_context())
+        resource.request.validated = self.validated
+        resource.request.validated["body"] = {"data": {}}
+        record = resource.collection_post()["data"]
+        timestamps["post"] = record["last_modified"]
+
+        # Wait for the fetch to finish
+        thread.join()
+
+        # Make sure fetch timestamp is below (for next fetch)
+        self.assertGreater(timestamps["post"], timestamps["fetch"])
