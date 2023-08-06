73af56c4f217f1a40188aafd76c7dee03f3e67c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:03 +0000

    Add package mlrank/0.0.1a2/mlrank-0.0.1a2.tar.gz

---
+++ packages/mlrank/mlrank-0.0.1a2.tar.gz/mlrank-0.0.1a2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mlrank",
+    version="0.0.1-alpha.2",
+    author="Prabakaran Kumaresshan",
+    author_email="k_prabakaran@hotmail.com",
+    description="Python client to submit output to evaluation server.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nixphix/mlrank",
+    packages=setuptools.find_packages(exclude=['docs', 'tests*']),
+    python_requires='>=3.5',
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning",
+    ],
+)
