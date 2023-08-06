dcdc2268b2f37112a726d6bedd0a0d2b1e56b4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:24 +0000

    Add package parsl/0.7.0a0/parsl-0.7.0a0-py3-none-any.whl

---
+++ packages/parsl/parsl-0.7.0a0-py3-none-any.whl/parsl/providers/torque/template.py
@@ -0,0 +1,19 @@
+template_string = '''#!/bin/bash
+
+#PBS -S /bin/bash
+#PBS -N ${jobname}
+#PBS -m n
+#PBS -k eo
+#PBS -l walltime=$walltime
+#PBS -l nodes=${nodes_per_block}:ppn=${tasks_per_node}
+#PBS -o ${submit_script_dir}/${jobname}.submit.stdout
+#PBS -e ${submit_script_dir}/${jobname}.submit.stderr
+${scheduler_options}
+
+${worker_init}
+
+export JOBNAME="${jobname}"
+
+${user_script}
+
+'''
