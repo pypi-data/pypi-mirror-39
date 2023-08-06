0b4ecd6d4f6cfc3678f6fb7f92c6b9b4ad3fc2a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:56 +0000

    Add package radinitio/0.0.1/radinitio-0.0.1.tar.gz

---
+++ packages/radinitio/radinitio-0.0.1.tar.gz/radinitio-0.0.1/radinitio/__init__.py
@@ -0,0 +1,12 @@
+def simulate(genome,
+    msprime_args,
+    indel_rate=0.01,
+    pcr_opts=None):
+
+    assert type(genome) == dict
+    assert type(msprime_args) == dict
+    pass
+
+# def sim_chroms(...)
+# def extract_loci(...)
+# def sim_reads(...)
