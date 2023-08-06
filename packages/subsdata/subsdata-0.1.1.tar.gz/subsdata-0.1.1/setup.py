85368db4c144c594143c848192be63345dc1f19c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:06 +0000

    Add package subsdata/0.1.1/subsdata-0.1.1.tar.gz

---
+++ packages/subsdata/subsdata-0.1.1.tar.gz/subsdata-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="subsdata",
+    version="0.1.1",
+    author="Miguel Ángel Alarcos Torrecillas",
+    author_email="miguel.alarcos@gmail.com",
+    description="SDP: Subscription Data Protocol",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/miguel.alarcos/subsdata",
+    packages=setuptools.find_packages(),
+    install_requires=['rethinkdb>=2.3.0.post6', 'PyJWT>=1.6.4'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest>=3.9.1', 'pytest-asyncio>=0.9.0'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
