7710fb27bae6afa766dc5b38f55a623bd4bd6e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:26 +0000

    Add package dummy-useragent/0.4/dummy_useragent-0.4.tar.gz

---
+++ packages/dummy-useragent/dummy_useragent-0.4.tar.gz/dummy_useragent-0.4/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="dummy_useragent",
+    version="0.4",
+    description="random useragent without network",
+    long_description="",
+    author="Hexmagic",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    author_email="191440042@qq.com",
+    url="https://github.com/Hexmagic/dummy_useragent.git",
+    license="GNU General Public License v3.0",
+    install_requires=["aiohttp"],
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Operating System :: OS Independent",
+    ],
+)
