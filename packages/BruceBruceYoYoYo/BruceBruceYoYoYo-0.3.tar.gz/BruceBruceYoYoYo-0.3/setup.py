bd12119c20420f3489c12f6cff39b711cc64041a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:01 +0000

    Add package BruceBruceYoYoYo/0.3/BruceBruceYoYoYo-0.3.tar.gz

---
+++ packages/BruceBruceYoYoYo/BruceBruceYoYoYo-0.3.tar.gz/BruceBruceYoYoYo-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BruceBruceYoYoYo',
+    version='0.3',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A test python package',
+	url='https://github.com/khalad-hasan/myMileConverter',
+    author='Bruce Pei',
+    author_email='brucepei1025@gmail.com'
+)
