34db79236257d8aaaf4ec360c587d52f30b9f28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:15 +0000

    Add package djangokarat/1.2.2/djangokarat-1.2.2.tar.gz

---
+++ packages/djangokarat/djangokarat-1.2.2.tar.gz/djangokarat-1.2.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="djangokarat",
+    version="1.2.2",
+    author="David Simonyi",
+    author_email="david@whys.eu",
+    description="ability to create models with send/accept fields to/from karat DB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    url="https://kiwi.whys.eu/karaterp/axima-agent",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
