a6bdb9f90406382a6508c5a18e88e6fc409c6dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:56 +0000

    Add package ds100nbconvert/0.3/ds100nbconvert-0.3.tar.gz

---
+++ packages/ds100nbconvert/ds100nbconvert-0.3.tar.gz/ds100nbconvert-0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='ds100nbconvert',
+      version='0.3',
+      description='dsin100days custom nbconvert, copied from nbextensions',
+      url='',
+      install_requires=['nbconvert', 'lxml'],
+      author='dinesh',
+      author_email='dinesh@micropyramid.com',
+      license='LICENCE',
+      packages=['ds100nbconvert'],
+      zip_safe=False,
+      entry_points = {
+          'nbconvert.exporters': [
+              'html_embed = ds100nbconvert:EmbedHTMLExporter',
+          ],
+      })
