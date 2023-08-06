3f5c2db9af417b8776b04769ed973992c977efaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:44 +0000

    Add package pydygp/0.2.2/pydygp-0.2.2.tar.gz

---
+++ packages/pydygp/pydygp-0.2.2.tar.gz/pydygp-0.2.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='pydygp',
+      version='0.2.2',
+      author='Daniel Tait',
+      author_email='tait.djk@gmail.com',
+      url='http://github.com/danieljtait/pydygp',
+      license='MIT',
+      include_package_data=True,
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+      install_requires=[
+          'numpy',
+          'scipy',
+          'matplotlib>=2.2.0',
+          'scikit-learn',
+      ],
+      zip_safe=False)      
