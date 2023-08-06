a45bd2453a0500772a53a41ee96193a2724e3b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:58 +0000

    Add package daymark/1.0.0/daymark-1.0.0.tar.gz

---
+++ packages/daymark/daymark-1.0.0.tar.gz/daymark-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name = 'daymark',
+	version = '1.0.0',
+	licence = 'MIT',
+	description = 'daymark is a helper package.',
+	author = 'Samarthya Choudhary',
+	author_email = 'samarthyachoudhary7@gmail.com',
+	url = 'https://gitlab.com/aman.indshine/lighthouse-function-helper.git',
+	install_requires =['psutil', 'requests', 'redis', 'boto'],
+	classifiers=['Development Status :: 3 - Alpha', 'Intended Audience :: Developers', 'Topic :: Software Development :: Build Tools', 'License :: OSI Approved :: MIT License'])
