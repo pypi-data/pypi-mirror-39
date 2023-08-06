f0b17d76fcad5518c5f0489d78767ae8f9694cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:16 +0000

    Add package KevinKarabinasPytrends/0.1/KevinKarabinasPytrends-0.1.tar.gz

---
+++ packages/KevinKarabinasPytrends/KevinKarabinasPytrends-0.1.tar.gz/KevinKarabinasPytrends-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='KevinKarabinasPytrends',
+      version='0.1',
+      description='Please Work',
+      author='Kevin Karabinas',
+      author_email='kevink@example.com',
+      license='MIT',
+      packages=['KevinKarabinasPytrends'],
+      install_requires=["requests", "pandas", 'lxml'],
+      zip_safe=False)
