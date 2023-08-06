bed16d90c381b814dd02bd5f0bc7bdb7e9221e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:10 +0000

    Add package pythumbnail/0.0.2/pythumbnail-0.0.2.tar.gz

---
+++ packages/pythumbnail/pythumbnail-0.0.2.tar.gz/pythumbnail-0.0.2/pythumbnail/rulegroup.py
@@ -0,0 +1,24 @@
+class RuleGroup:
+    def __init__(self, parent, name, group_level):
+        self.parent = parent
+        self.child = []
+        self.name = name
+        self.functionname = ''
+        self.params = ['']
+        self.logic = []
+        self.num_logic = -1
+        self.group_level = group_level
+    
+    def __str__(self, level=0):
+        if self.name == 'for':
+            ret = "\t"*level+repr(self.name + ' ' + self.functionname + ' ' + self.logic[0] + ','.join(self.params))+"\n"
+        elif self.name == 'if' or self.name == 'while':
+            ret = "\t"*level+repr(self.name + '[' + ','.join(self.params) + '] LOGIC: ['+ ','.join(self.logic) + ']') +"\n"
+        else:
+            ret = "\t"*level+repr(self.name + ' ' + self.functionname + '(' + ','.join(self.params) + ')')+"\n"
+        for c in self.child:
+            ret += c.__str__(level+1)
+        return ret
+
+    def __repr__(self):
+        return '<tree node representation>'
