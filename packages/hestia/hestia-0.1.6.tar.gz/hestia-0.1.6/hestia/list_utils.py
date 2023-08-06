02bbb6e01f10d3a522e186e8c45d289756766333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:18 +0000

    Add package hestia/0.1.6/hestia-0.1.6.tar.gz

---
+++ packages/hestia/hestia-0.1.6.tar.gz/hestia-0.1.6/hestia/list_utils.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function
+
+try:
+    import numpy as np
+except ImportError:
+    np = None
+
+
+def to_list(value):
+    if isinstance(value, (list, tuple)):
+        return list(value)
+    if np and isinstance(value, np.ndarray):
+        return list(value)
+    return [value]
