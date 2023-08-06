4dd817df3014308a2c6674537204b3234bd16b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:07 +0000

    Add package face-parser/0.0.1/face_parser-0.0.1.tar.gz

---
+++ packages/face-parser/face_parser-0.0.1.tar.gz/face_parser-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+        name="face_parser",
+        version="0.0.1",
+        author="Ross Leitch",
+        author_email="ross@end-game.com",
+        description="A small package to turn dlib's facial landmarks into more interesting values",
+        url="https://github.com/balbatross/dlib-face-parser",
+        packages=['parser'],
+        install_requires=[
+            'scipy',
+            'numpy',
+            'imutils'
+        ],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"
+        ],
+)
