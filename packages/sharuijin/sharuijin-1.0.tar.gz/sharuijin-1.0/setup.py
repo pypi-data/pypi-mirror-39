6562d5d6476f37894b1c65e1db7517719b6a6457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:01 +0000

    Add package sharuijin/1.0/sharuijin-1.0.tar.gz

---
+++ packages/sharuijin/sharuijin-1.0.tar.gz/sharuijin-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name = "sharuijin",
+   version = "1.0",
+   author="test",
+   author_email="test@qq.com",
+   url="https://github.com/sharuijin/config",
+   description="chisini",
+   packages=["pack"],
+   install_requires=[]
+   )
