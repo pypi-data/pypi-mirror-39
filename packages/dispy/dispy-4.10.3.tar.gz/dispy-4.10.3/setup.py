85ea80ac1c2557e79b96b173b39f1d37094125f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:39 +0000

    Add package dispy/4.10.3/dispy-4.10.3.tar.gz

---
+++ packages/dispy/dispy-4.10.3.tar.gz/dispy-4.10.3/setup.py
@@ -0,0 +1,53 @@
+import sys
+import os
+import re
+from setuptools import setup
+
+if sys.version_info.major == 3:
+    base_dir = 'py3'
+else:
+    assert sys.version_info.major == 2
+    assert sys.version_info.minor >= 7
+    base_dir = 'py2'
+
+with open(os.path.join(base_dir, 'dispy', '__init__.py')) as fd:
+    regex = re.compile(r'^__version__ = "([\d\.]+)"$')
+    for line in fd:
+        match = regex.match(line)
+        if match:
+            module_version = match.group(1)
+            break
+
+setup(
+    name='dispy',
+    version=module_version,
+    description='Distributed and Parallel Computing with/for Python.',
+    keywords='distributed computing, parallel processing, mapreduce, hadoop, job scheduler',
+    url='http://dispy.sourceforge.net',
+    author='Giridhar Pemmasani',
+    author_email='pgiri@yahoo.com',
+    package_dir={'':base_dir},
+    packages=['dispy'],
+    package_data = {
+        'dispy' : ['data/*', 'examples/*', 'doc/*'],
+    },
+    install_requires=['pycos >= 4.8.7'],
+    scripts=[os.path.join(base_dir, 'dispy', script) for script in \
+             ['dispynode.py', 'dispynetrelay.py', 'dispyscheduler.py']] + \
+            [os.path.join(base_dir, script) for script in ['dispy.py']],
+    license='Apache 2.0',
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development',
+        ]
+    )
