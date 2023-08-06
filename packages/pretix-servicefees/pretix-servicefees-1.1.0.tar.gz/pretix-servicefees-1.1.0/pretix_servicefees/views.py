c62b9ec2a8a3663b4cd79f2de3b82f02b12ad25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:12 +0000

    Add package pretix-servicefees/1.1.0/pretix-servicefees-1.1.0.tar.gz

---
+++ packages/pretix-servicefees/pretix-servicefees-1.1.0.tar.gz/pretix-servicefees-1.1.0/pretix_servicefees/views.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from pretix.base.forms import SettingsForm
+from pretix.base.models import Event
+from pretix.control.views.event import EventSettingsViewMixin, EventSettingsFormView
+
+
+class ServiceFeeSettingsForm(SettingsForm):
+    service_fee_abs = forms.DecimalField(label=_('Service fee'))
+    service_fee_abs_resellers = forms.DecimalField(label=_('Service fee with resellers'))
+
+
+class SettingsView(EventSettingsViewMixin, EventSettingsFormView):
+    model = Event
+    form_class = ServiceFeeSettingsForm
+    template_name = 'pretix_servicefees/settings.html'
+    permission = 'can_change_event_settings'
+
+    def get_success_url(self) -> str:
+        return reverse('plugins:pretix_servicefees:settings', kwargs={
+            'organizer': self.request.event.organizer.slug,
+            'event': self.request.event.slug
+        })
