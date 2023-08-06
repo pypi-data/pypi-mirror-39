7b5f6de006a4d696bde1a19785ce5f4fe9480a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:05 +0000

    Add package troncli/0.1.4/troncli-0.1.4-py3-none-any.whl

---
+++ packages/troncli/troncli-0.1.4-py3-none-any.whl/troncli/constants.py
@@ -0,0 +1,22 @@
+"""Constants"""
+
+JAVA_TRON_RELEASES_URL = 'https://github.com/tronprotocol/java-tron/releases/download/'
+JAVA_TRON_LASTEST_VERSION = '3.2.1'
+
+NODES_DIR = '/tron_nodes'
+FULL_NODE_DIR = '/fullnode'
+SOLIDITY_NODE_DIR = '/soliditynode'
+FULL_NODE_JAR = '/full.jar'
+SOLIDITY_NODE_JAR = '/solidity.jar'
+FULL_CONFIG = '/full.conf'
+SOL_CONFIG = '/sol.conf'
+
+LOCAL_HOST = '127.0.0.1:'
+
+MAINNET_P2P_VERSION = 11111
+PRIVATENET_P2P_VERSION = 1
+
+MAINNET_ALLOW_CREATION_OF_CONTRACTS = 0
+PRIVATENET_ALLOW_CREATION_OF_CONTRACTS = 1
+
+RUNNING_NODE_LIST_FILE = 'running_nodes.json'
