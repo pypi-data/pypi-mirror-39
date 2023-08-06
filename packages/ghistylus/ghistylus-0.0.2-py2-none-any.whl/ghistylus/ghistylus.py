4b55064b69f860abe287f07aab3c1a6b4b763f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:54 +0000

    Add package ghistylus/0.0.2/ghistylus-0.0.2-py2-none-any.whl

---
+++ packages/ghistylus/ghistylus-0.0.2-py2-none-any.whl/ghistylus/ghistylus.py
@@ -0,0 +1,12 @@
+class ghistylus(object):
+    def __init__(self, color, phrase):
+        colors = {
+        "HEADER" : '\033[95m',
+        "BLUE" : '\033[94m',
+        "GREEN" : '\033[92m',
+        "WARNING" : '\033[93m',
+        "FAIL" : '\033[91m',
+        "ENDC" : '\033[0m',
+        "BOLD" : '\033[1m',
+        "UNDERLINE" : '\033[4m'}
+        print (colors[color]+phrase+colors["ENDC"])
