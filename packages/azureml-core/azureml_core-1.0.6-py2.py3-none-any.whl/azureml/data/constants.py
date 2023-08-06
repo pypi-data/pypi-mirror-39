30cf6c94373880050e0e5d85ba79bfe135c94896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:13 +0000

    Add package azureml-core/1.0.6/azureml_core-1.0.6-py2.py3-none-any.whl

---
+++ packages/azureml-core/azureml_core-1.0.6-py2.py3-none-any.whl/azureml/data/constants.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+"""constants in azureml.data package"""
+
+AZURE_DATA_LAKE = "AzureDataLake"
+AZURE_FILE = "AzureFile"
+AZURE_BLOB = "AzureBlob"
+AZURE_SQL_DATABASE = "AzureSqlDatabase"
+
+ACCOUNT_KEY = "AccountKey"
+SAS = "Sas"
+NONE = "None"
