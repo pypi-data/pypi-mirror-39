dd2d72ec9c8ecb998097dfccfaed2ab748026c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:14 +0000

    Add package apiaccessor/0.0.1.2/apiaccessor-0.0.1.2.tar.gz

---
+++ packages/apiaccessor/apiaccessor-0.0.1.2.tar.gz/apiaccessor-0.0.1.2/README.md
@@ -0,0 +1,9 @@
+# API Accessor
+A module to access API's using OAuth2 or Header-Key authentication.
+
+## TODO
+* More request types
+
+# Funtions
+* GET data from an API using X-Auth-Key and OAuth2 given credienials
+
---
+++ packages/apiaccessor/apiaccessor-0.0.1.2.tar.gz/apiaccessor-0.0.1.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='apiaccessor',
+    version='0.0.1.2',
+    author='Daniel Cai',
+    author_email='',
+    description='A module to access API\'s using OAuth2 or Header-Key authentication.',
+    long_description=long_description,
+    # long_description_content_type='text/markdown',
+    url='https://github.com/MrTrainCow/API-Accessor',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
