915c982f35edf2f8f27ae380b8f84283a8947829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:30 +0000

    Add package faaspact-verifier/0.0.7/faaspact-verifier-0.0.7.tar.gz

---
+++ packages/faaspact-verifier/faaspact-verifier-0.0.7.tar.gz/faaspact-verifier-0.0.7/faaspact_verifier/__init__.py
@@ -0,0 +1,2 @@
+from .use_verifier import use_verifier
+from .user_defined import always, faasport, provider_state
---
+++ packages/faaspact-verifier/faaspact-verifier-0.0.7.tar.gz/faaspact-verifier-0.0.7/faaspact_verifier/user_defined/__init__.py
@@ -0,0 +1,3 @@
+from .always import always
+from .faasport import faasport
+from .provider_state import provider_state
