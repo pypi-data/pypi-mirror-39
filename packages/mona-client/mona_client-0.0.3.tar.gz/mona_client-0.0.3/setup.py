1c9319cf51f0dbe956eebda3636b83181386a341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:26 +0000

    Add package mona-client/0.0.3/mona_client-0.0.3.tar.gz

---
+++ packages/mona-client/mona_client-0.0.3.tar.gz/mona_client-0.0.3/setup.py
@@ -0,0 +1,24 @@
+# TODO(itai): Separate to different packages to prevent dependency on pubsub
+# client etc...
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mona_client",
+    version="0.0.3",
+    author="MonaLabs",
+    author_email="itai@monalabs.io",
+    description="Client code for python Mona instrumentation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/itai4/mona-python-client",
+    packages=setuptools.find_packages(),
+    install_requires=['fluent-logger', 'watchdog'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
