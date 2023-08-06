017c39c8c1a27b8dd21626cedf701cd0e48cfe6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:45 +0000

    Add package ScoutSuite/4.0.5/ScoutSuite-4.0.5.tar.gz

---
+++ packages/ScoutSuite/ScoutSuite-4.0.5.tar.gz/ScoutSuite-4.0.5/ScoutSuite/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+__author__  = 'NCC Group'
+__version__ = '4.0.5'
+
+AWSCONFIG  = 42
+EXCEPTIONS = 4242
+HTMLREPORT = 424242
+AWSRULESET = 42424242
+
+DEFAULT_REPORT_DIR = 'scoutsuite-report'
+AWSCONFIG_FILE  = 'inc-scoutsuite-run/scoutsuite_config.js'
+AWSRULESET_FILE = 'inc-scoutsuite-run/scoutsuite_ruleset.js'
+EXCEPTIONS_FILE = 'inc-scoutsuite-run/scoutsuite_exceptions.js'
+HTMLREPORT_FILE = 'report.html'
+GENERATOR_FILE  = 'ruleset-generator.html'
+
+REPORT_TITLE  = 'Scout Suite Report'
