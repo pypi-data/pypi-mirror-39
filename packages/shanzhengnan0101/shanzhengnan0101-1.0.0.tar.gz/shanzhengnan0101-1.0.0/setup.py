6c1e503c16d7e0049bf21e93a675ab980540e13b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:19 +0000

    Add package shanzhengnan0101/1.0.0/shanzhengnan0101-1.0.0.tar.gz

---
+++ packages/shanzhengnan0101/shanzhengnan0101-1.0.0.tar.gz/shanzhengnan0101-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name                  ="shanzhengnan0101",
+	version               ="1.0.0",
+	py_modules       =["shanzhengnan0101"],
+	author                ="hfpython",
+	author_email      ="hfpython@headfirstlabs.com",
+	url                       ="http://www.headfirstlabs.com",
+	description         ="A simple printer of hello",
+	)
---
+++ packages/shanzhengnan0101/shanzhengnan0101-1.0.0.tar.gz/shanzhengnan0101-1.0.0/shanzhengnan0101.py
@@ -0,0 +1,2 @@
+def printeveything():
+    print("hello")
