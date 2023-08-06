4ea6e430e2f479b1864ea35cb7f099bb2b9b7c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:15 +0000

    Add package choochoo/0.7.1/choochoo-0.7.1-py3-none-any.whl

---
+++ packages/choochoo/choochoo-0.7.1-py3-none-any.whl/ch2/data/__init__.py
@@ -0,0 +1,4 @@
+
+from .database import data
+from .plot import col_to_boxstats, bokeh_boxplot, statistic_gui
+from .heart_rate import *
---
+++ packages/choochoo/choochoo-0.7.1-py3-none-any.whl/ch2/lib/data.py
@@ -0,0 +1,59 @@
+
+from binascii import hexlify
+
+
+class WarnDict(dict):
+
+    def __init__(self, log, msg):
+        self.__log = log
+        self.__msg = msg
+        super().__init__()
+
+    def __getitem__(self, item):
+        try:
+            return super().__getitem__(item)
+        except KeyError:
+            msg = self.__msg % (item,)
+            self.__log.warn(msg)
+            raise KeyError(msg)
+
+
+class WarnList(list):
+
+    def __init__(self, log, msg):
+        self.__log = log
+        self.__msg = msg
+        super().__init__()
+
+    def __getitem__(self, item):
+        try:
+            return super().__getitem__(item)
+        except IndexError:
+            msg = self.__msg % item
+            self.__log.warn(msg)
+            raise IndexError(msg)
+
+
+def tohex(data):
+    return hexlify(data).decode('ascii')
+
+
+def assert_attr(instance, *attrs):
+    for attr in attrs:
+        if getattr(instance, attr) is None:
+            raise Exception('No %s defined' % attr)
+
+
+class AttrDict(dict):
+
+    def __getattr__(self, name):
+        if name.startswith('_'):
+            return super().__getattr__(name)
+        else:
+            return self[name]
+
+    def __setattr__(self, name, value):
+        if name.startswith('_'):
+            super().__setattr__(name, value)
+        else:
+            self[name] = value
