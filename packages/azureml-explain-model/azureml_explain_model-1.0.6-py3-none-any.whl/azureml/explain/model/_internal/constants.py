416066f2b108582d981b1f5b99aa5330f5b2ffd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:13 +0000

    Add package azureml-explain-model/1.0.6/azureml_explain_model-1.0.6-py3-none-any.whl

---
+++ packages/azureml-explain-model/azureml_explain_model-1.0.6-py3-none-any.whl/azureml/explain/model/_internal/constants.py
@@ -0,0 +1,26 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Defines constants for explain model."""
+
+CLASSES = 'classes'
+EXPECTED_VALUES = 'expected_values'
+EXPLANATION = 'explanation'
+EXPLANATION_ASSET = 'explanation_asset'
+EXPLANATION_ASSET_TYPE = 'azureml.v1.model.explanation'
+FEATURE_NAMES = 'feature_names'
+OVERALL_FEATURE_ORDER = 'overall_feature_order'
+OVERALL_IMPORTANCE_ORDER = 'overall_importance_order'
+OVERALL_SUMMARY = 'overall_summary'
+PER_CLASS_FEATURE_ORDER = 'per_class_feature_order'
+PER_CLASS_IMPORTANCE_ORDER = 'per_class_importance_order'
+PER_CLASS_SUMMARY = 'per_class_summary'
+SHAP_VALUES = 'shap_values'
+
+MODEL_TYPE = 'model_type'
+EXPLAINER_TYPE = 'explainer'
+CLASSIFICATION_TYPE = 'classification'
+REGRESSION_TYPE = 'regression'
+TABULAR = 'tabular'
+TEXT = 'text'
