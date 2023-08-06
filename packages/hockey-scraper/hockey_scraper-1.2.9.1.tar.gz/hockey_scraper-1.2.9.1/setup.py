9c590db886d17a5f7522a088a8894b003b13cc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:54 +0000

    Add package hockey-scraper/1.2.9.1/hockey_scraper-1.2.9.1.tar.gz

---
+++ packages/hockey-scraper/hockey_scraper-1.2.9.1.tar.gz/hockey_scraper-1.2.9.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+
+def read():
+    return open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setup(
+    name='hockey_scraper',
+    version='1.2.9.1',
+    description="""This package is designed to allow people to scrape Play by Play and Shift data off of the National
+                Hockey League (NHL) API and website for all preseason, regular season and playoff games since the 
+                2007-2008 season""",
+    long_description=read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Intended Audience :: Science/Research',
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 2",
+    ],
+    keywords='NHL',
+    url='https://github.com/HarryShomer/Hockey-Scraper',
+    author='Harry Shomer',
+    author_email='Harryshomer@gmail.com',
+    license='MIT',
+    packages=['hockey_scraper'],
+    install_requires=['BeautifulSoup4', 'requests', 'lxml', 'html5lib', 'pandas', 'sphinx', 'pytest'],
+    include_package_data=True,
+    zip_safe=False
+)
