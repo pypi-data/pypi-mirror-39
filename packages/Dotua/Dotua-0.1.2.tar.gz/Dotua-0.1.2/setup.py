a0e42a587842206168ca4875c98765ade276ca87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:27 +0000

    Add package Dotua/0.1.2/Dotua-0.1.2.tar.gz

---
+++ packages/Dotua/Dotua-0.1.2.tar.gz/Dotua-0.1.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("docs/documentation.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='Dotua',
+	version='0.1.2',
+	description='Package for Forward/Reverse Autodifferentiation',
+	author='Nick Stern, Vincent Viego, Summer Yuan, Zach Wehrwein',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=['Dotua'],
+	install_requires=[
+	  'numpy',
+	  'scipy'
+	],
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	url="https://github.com/VV-NS-CY-ZW-CS-207-Organization/cs207-FinalProject",
+	license="MIT",
+)
