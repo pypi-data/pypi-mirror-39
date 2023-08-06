2aeda2498b34f7d9027213ad9c3a80e10762ce9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:52 +0000

    Add package py2gc/1.0.6/py2gc-1.0.6.tar.gz

---
+++ packages/py2gc/py2gc-1.0.6.tar.gz/py2gc-1.0.6/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='py2gc',
+    version='1.0.6',
+    description='Add events to a Google Calendar from the command line.',
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Stefan Pfeuffer',
+    author_email='mail@stefanpfeuffer.com',
+    url="https://github.com/stefanpf/py2gc",
+    packages=find_packages(),
+    entry_points={'console_scripts': ['py2gc = py2gc.py2gc:main']},
+    install_requires=['httplib2', 'argparse', 'oauth2client', 'google-api-python-client', 'pathlib'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
