bbd8c6e2eaaed6b1aebb5a7c8585360753eddc26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:11 +0000

    Add package edgepy/0.1.14.dev0/edgepy-0.1.14.dev0.tar.gz

---
+++ packages/edgepy/edgepy-0.1.14.dev0.tar.gz/edgepy-0.1.14.dev0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+REQUIRES = [
+	'biopython>=1.71',
+	'xmltodict',
+	'pandas',
+	'bcbio-gff',
+]
+
+
+setup(
+	name = 'edgepy',
+	version = '0.1.14dev',
+	author = 'Alex Summers',
+	author_email='asummers.edgepy@gmail.com',
+	packages = find_packages(),
+	url = 'http://pypi.python.org/pypi/edgepy/',
+	license = 'LICENSE.txt',
+	long_description = open('README.txt').read(),
+	install_requires = REQUIRES,
+	include_package_data = True,
+)
+
