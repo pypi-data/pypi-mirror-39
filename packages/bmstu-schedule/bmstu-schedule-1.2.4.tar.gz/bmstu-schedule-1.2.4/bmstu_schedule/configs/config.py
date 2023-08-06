de4fd9a0bd4e7cdc04cfeec7714cc860130a1d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:58 +0000

    Add package bmstu-schedule/1.2.4/bmstu-schedule-1.2.4.tar.gz

---
+++ packages/bmstu-schedule/bmstu-schedule-1.2.4.tar.gz/bmstu-schedule-1.2.4/bmstu_schedule/configs/config.py
@@ -0,0 +1,28 @@
+# time-section
+DATE_FORMAT = '%d-%m-%Y'
+API_URL = 'http://142.93.174.191/bmstu_schedule/'
+
+# iCal section
+STABLE = 17
+PERIODIC = 8
+ICAL_HEADER = 'BEGIN:VCALENDAR\nVERSION:2.0\nCALSCALE:GREGORIAN'
+ICAL_BODY = \
+    '''
+    BEGIN:VEVENT
+    SUMMARY:{summary}
+    DTSTART:{startDate}T{startTime}
+    DTEND:{endDate}T{endTime}
+    RRULE:FREQ=WEEKLY;INTERVAL={interval};COUNT={count};
+    LOCATION:{auditorium}
+    DESCRIPTION:{professor}
+    END:VEVENT'''
+ICAL_BOTTOM = '\nEND:VCALENDAR'
+
+# bmstu
+MAIN_URL = 'https://students.bmstu.ru'
+GROUPS_LIST_URL = '/schedule/list'
+
+
+# logic
+ALL_GROUPS_PARAM = 'all'
+
