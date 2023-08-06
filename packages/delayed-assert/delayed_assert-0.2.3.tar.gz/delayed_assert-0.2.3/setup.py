c67ca7eb05ec1367ee02f529aca0e0d282cd445f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:33 +0000

    Add package delayed-assert/0.2.3/delayed_assert-0.2.3.tar.gz

---
+++ packages/delayed-assert/delayed_assert-0.2.3.tar.gz/delayed_assert-0.2.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='delayed_assert',
+    version='0.2.3',
+    description='Delayed/soft assertions for python',
+    long_description = 'Project description and details at https://github.com/pr4bh4sh/python-delayed-assert',
+    author='pr4bh4sh',
+    url='https://github.com/pr4bh4sh/python-delayed-assert',
+    packages=['delayed_assert'],
+)
