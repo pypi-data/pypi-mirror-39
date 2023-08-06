125accf60fede0f0cab21d2aadd583f42ee9cb05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:55 +0000

    Add package infi.instruct/0.6.33/infi.instruct-0.6.33-py2-none-any.whl

---
+++ packages/infi.instruct/infi.instruct-0.6.33-py2-none-any.whl/infi/instruct/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.6.33"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.6.33)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '93b1ab725cfd8d13227960dbf9e3ca1e7f9eebe8'
+__git_head_subject__ = u'Finished Release v0.6.33'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-12-20 16:49:31'
