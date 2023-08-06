f2e3727e5faebb5d14f782f13138cda70170dfa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:57 +0000

    Add package mo-installer/0.3.0/mo_installer-0.3.0.tar.gz

---
+++ packages/mo-installer/mo_installer-0.3.0.tar.gz/mo_installer-0.3.0/mo_installer/builder.py
@@ -0,0 +1,102 @@
+from setuptools.command.build_py import build_py as _build_py
+from distutils.command.build import build as _build
+from distutils.cmd import Command
+from distutils.errors import DistutilsSetupError
+from .vendor import msgfmt
+import os.path
+import re
+
+class build(_build):
+    parent = _build
+    def run(self):
+        if "build_py" in self.distribution.cmdclass:
+            p = self.distribution.cmdclass["build_py"]
+            if p != build_py:
+                build_py.parent = p
+        self.distribution.cmdclass["build_py"] = build_py
+        self.parent.run(self)
+        
+class build_py(_build_py):
+    parent = _build_py
+    def run(self):
+        self.run_command("build_mo")
+        self.parent.run(self)
+        
+    def get_outputs(self):
+        build_mo = self.get_finalized_command("build_mo")
+        return _build_py.get_outputs(self) + build_mo.get_outputs()
+
+
+class build_mo(Command):
+    description = "\"build\" mo gettext files form po ones"
+
+    user_options = [
+        ('locale-src', 's', "directory for po files"),
+        ('locale-dir', 'd', "top level locale directory inside package"),
+        ]
+
+    def initialize_options(self):
+        self.outputs = []
+        self.build_lib = None
+
+    def finalize_options(self):
+        self.set_undefined_options('build',
+                                   ('build_lib', 'build_lib')
+                                   )
+        name = self.distribution.get_name()
+        self.locale_src = self.distribution.locale_src
+        if self.locale_src is None:
+            self.locale_src = os.path.join(name, "locale")
+        self.locale_dir = self.distribution.locale_dir
+        if self.locale_dir is None:
+            self.locale_dir = os.path.join(name, "locale")
+
+    def run(self):
+        if not os.path.isdir(self.locale_src): return
+        self.announce("Compiling from {} to {}".format(self.locale_src,
+                                                       self.locale_dir))
+        po_loc = re.compile(r"(.*)-(.*)\.po$")
+        po = re.compile(r"(.*)\.po$")
+        for locale in os.listdir(self.locale_src):
+            if locale[0] == '.': continue
+            path = os.path.join(self.locale_src, locale)
+            if os.path.isdir(path):
+                for file in os.listdir(path):
+                    if file == "LC_MESSAGES" and os.path.isdir(
+                        os.path.join(path, file)):
+                        lcm = os.path.join(path, file)
+                        for file in os.listdir(lcm):
+                            m = po.match(file)
+                            if m:
+                                self.process(os.path.join(lcm, file),
+                                             m.group(1), locale)
+                    else:        
+                        m = po.match(file)
+                        if m:
+                            self.process(os.path.join(path, file),
+                                         m.group(1), locale)
+            else:
+                m = po_loc.match(locale)
+                if m:
+                    self.process(path, m.group(1), m.group(2))
+
+    def process(self, path, domain, locale):
+        dest = os.path.join(self.build_lib, self.locale_dir, locale,
+                            "LC_MESSAGES")
+        self.mkpath(dest)
+        file = os.path.join(dest, domain + ".mo")
+        self.outputs.append(file)
+        if not self.dry_run:
+            self.make_file(path, file, msgfmt.make, (path, file))
+            
+    def get_outputs(self):
+        return self.outputs
+
+def validate_src(dist, attr, value):
+    if not os.path.isdir(value):
+        raise DistutilsSetupError(
+            "%r must be a directory (got %r)" % (attr,value)
+            )
+
+def validate_dir(dist, attr, value):
+    pass
---
+++ packages/mo-installer/mo_installer-0.3.0.tar.gz/mo_installer-0.3.0/tests/test_builder.py
@@ -0,0 +1,60 @@
+from unittest import TestCase
+from unittest.mock import Mock, patch
+from mo_installer import builder
+from mo_installer.builder import build, build_mo, build_py
+from mo_installer.builder import validate_src, validate_dir
+from distutils.dist import Distribution
+from distutils.cmd import Command
+
+
+class BuildTest(TestCase):
+    def setUp(self):
+        self.dist = Mock(Distribution)
+        self.dist.verbose = 1
+        self.dist.dry_run = 0
+        self.dist.cmdclass = {}
+        self.obj = build(self.dist)
+
+    def test_no_previous(self):
+        with patch("mo_installer.builder._build.run") as run:
+            self.obj.run()
+            run.assert_called_once_with(self.obj)
+        self.assertEqual(builder._build_py, build_py.parent)
+        self.assertEqual(build_py, self.dist.cmdclass["build_py"])
+
+    def test_previous(self):
+        self.dist.cmdclass["build_py"] = Command
+        with patch("mo_installer.builder._build.run") as run:
+            self.obj.run()
+            run.assert_called_once_with(self.obj)
+        self.assertEqual(Command, build_py.parent)
+        self.assertEqual(build_py, self.dist.cmdclass["build_py"])
+
+class BuildPyTest(TestCase):
+    def setUp(self):
+        self.dist = Mock(Distribution)
+        self.dist.verbose = 1
+        self.dist.dry_run = 0
+        self.dist.cmdclass = {}
+        self.obj = build_py(self.dist)
+
+    def test_run(self):
+        with patch("mo_installer.builder._build_py.run") as run, \
+             patch("mo_installer.builder._build_py.run_command") as run_cmd:
+            self.obj.run()
+            run.assert_called_once_with(self.obj)
+            run_cmd.assert_called_once_with("build_mo")
+
+    def test_get_outputs(self):
+        with patch("distutils.command.build_py.build_py.get_outputs") \
+                 as out, \
+             patch("mo_installer.builder._build_py.get_finalized_command") \
+                 as cmd:
+            out.return_value = ["foo"]
+            mo = Mock("mo_install.builder.build_mo")
+            mo.get_outputs = Mock(return_value = ["bar"])
+            cmd.return_value = mo
+            l = self.obj.get_outputs()
+            self.assertEqual(2, len(l))
+            self.assertTrue("foo" in l)
+            self.assertTrue("bar" in l)
