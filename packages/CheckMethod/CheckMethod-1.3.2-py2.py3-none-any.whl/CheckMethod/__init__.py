816a86400014e4b5252974310c56dd69a09ab434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:13 +0000

    Add package CheckMethod/1.3.2/CheckMethod-1.3.2-py2.py3-none-any.whl

---
+++ packages/CheckMethod/CheckMethod-1.3.2-py2.py3-none-any.whl/CheckMethod/__init__.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+#
+#  Copyright © 2010—2014 Andrey Mikhaylenko and contributors
+#
+#  This file is part of Argh.
+#
+#  Argh is free software under terms of the GNU Lesser
+#  General Public License version 3 (LGPLv3) as published by the Free
+#  Software Foundation. See the file README.rst for copying conditions.
+#
+from CheckMethod.Check import *
+from CheckMethod.ReadXml import *
+
+__version__ = '0.0.1'
