6b871ab591631f4e1673c45a3d2cf806585268d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:36 +0000

    Add package side-kick/0.0.1/side-kick-0.0.1.tar.gz

---
+++ packages/side-kick/side-kick-0.0.1.tar.gz/side-kick-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="side-kick",
+    version="0.0.1",
+    author="Ben Currie",
+    author_email="benjamin.w.currie@gmail.com",
+    description="A lightweight task runner",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CurrieBen/sidekick",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
