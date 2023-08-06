0a1a5bb55a445cec57ca0c8a42589b88e52e3ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:21 +0000

    Add package pycodedock/0.0.3/pycodedock-0.0.3.tar.gz

---
+++ packages/pycodedock/pycodedock-0.0.3.tar.gz/pycodedock-0.0.3/README.md
@@ -0,0 +1,4 @@
+# pycodedock
+A python module that enables to run python scripts safely inside docker containers  
+
+[![PyPI version](https://badge.fury.io/py/pycodedock.svg)](https://badge.fury.io/py/pycodedock)  
---
+++ packages/pycodedock/pycodedock-0.0.3.tar.gz/pycodedock-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycodedock",
+    version="0.0.3",
+    author="Adwait Thattey",
+    author_email="adwaitthattey@gmail.com",
+    description="A module that allows you to programatically run python scripts safely inside docker containers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adwait-thattey/pycodedock",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 2 - Pre-Alpha",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
