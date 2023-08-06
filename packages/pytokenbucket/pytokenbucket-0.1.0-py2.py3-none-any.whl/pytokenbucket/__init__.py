6726d8a186365ea1ea6077471a095b2e075ed122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:10 +0000

    Add package pytokenbucket/0.1.0/pytokenbucket-0.1.0-py2.py3-none-any.whl

---
+++ packages/pytokenbucket/pytokenbucket-0.1.0-py2.py3-none-any.whl/pytokenbucket/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Python Token Bucket."""
+
+__author__ = """Max Dymond"""
+__email__ = 'cmeister2@gmail.com'
+__version__ = '0.1.0'
+
+from .tokenbucket import TokenBucket
+
+__all__ = ['TokenBucket']
