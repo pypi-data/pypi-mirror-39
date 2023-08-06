64be801bc7de65a64a56b13eb7f06caa312da186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:49 +0000

    Add package bulkergr_api/0.1.4/bulkergr_api-0.1.4-py2.py3-none-any.whl

---
+++ packages/bulkergr_api/bulkergr_api-0.1.4-py2.py3-none-any.whl/bulkergr_api/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Bulker.gr Python API Client."""
+
+__author__ = """Yiannis Inglessis """
+__email__ = 'negtheone@gmail.com'
+__version__ = '0.1.4'
+
+from .bulkergr_api import Bulkergr
+from .exceptions import AuthKeyMissingError
