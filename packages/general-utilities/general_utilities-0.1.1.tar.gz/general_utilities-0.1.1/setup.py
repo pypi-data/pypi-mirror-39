99354f1611aef316863d6188eecf7ab2b2556b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:06 +0000

    Add package general-utilities/0.1.1/general_utilities-0.1.1.tar.gz

---
+++ packages/general-utilities/general_utilities-0.1.1.tar.gz/general_utilities-0.1.1/setup.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+# -*- coding: utf-8 -*-
+
+__author__ = "Jalpesh Borad"
+__copyright__ = "Copyright 2018"
+
+__version__ = "0.0.1"
+__maintainer__ = "Jalpesh Borad"
+__email__ = "jalpeshborad@gmail.com"
+__status__ = "Development"
+
+import setuptools
+
+
+setuptools.setup(
+    name="general_utilities",
+    version="0.1.1",
+    author="Jalpesh Borad",
+    author_email="jalpeshborad@gmail.com",
+    description="A general utilities for day-to-day python programs",
+    packages=setuptools.find_packages(),
+    url="https://github.com/jalpeshborad/utilities",
+)
