7c5969c2b00db5668b2844647227f8bcb63bc5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:04 +0000

    Add package hashstate/0.1.2/hashstate-0.1.2.tar.gz

---
+++ packages/hashstate/hashstate-0.1.2.tar.gz/hashstate-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, Extension
+
+c_ext = Extension(
+    'hashstate._hashstate',
+    sources=['hashstate/_hashstate.c'],
+    libraries=['ssl']
+)
+
+setup(
+    name='hashstate',
+    version='0.1.2',
+    description='Serializable hash objects',
+    packages=['hashstate'],
+    ext_modules=[c_ext]
+)
