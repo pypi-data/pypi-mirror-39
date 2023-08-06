2d19ea59550f8e5908a2abdb1dcf67992c667312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:47 +0000

    Add package dragongrad/1.2/dragongrad-1.2.tar.gz

---
+++ packages/dragongrad/dragongrad-1.2.tar.gz/dragongrad-1.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dragongrad",
+    version="1.2",
+    author="cs207-project-group4",
+    author_email="dylanrandle@gmail.com",
+    description="Automatic Differentiation Library",
+    long_description=long_description,
+    url="https://github.com/cs207-project-group4/cs207-FinalProject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
