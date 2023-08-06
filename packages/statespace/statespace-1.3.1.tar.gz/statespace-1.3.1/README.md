cee95a21123c20e13b85af3ebae0b84dd53c1852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:33 +0000

    Add package statespace/1.3.1/statespace-1.3.1.tar.gz

---
+++ packages/statespace/statespace-1.3.1.tar.gz/statespace-1.3.1/README.md
@@ -0,0 +1,18 @@
+uncertainty, confidence, knowledge
+
+their evolution with time and various processes, including noise and observations
+
+risk and the cost of errors
+
+[references](http://statespace.icu/docs/references), [help](https://github.com/noahhsmith/statespace/tree/master/statespace)
+
+181216
+
+testing gitlab
+
+180910
+
+demonstration of covariance numerical instability using just the jazwinski tracking problem with two additional states for model-parameter estimation - first plot is direct ekf, second plot is ud factorized ekf
+
+![direct](docs/images/direct.png) 
+![ud](docs/images/udfactoring.png)
---
+++ packages/statespace/statespace-1.3.1.tar.gz/statespace-1.3.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools, os
+
+version = None
+if os.environ.get('CI_COMMIT_TAG'):
+    version = os.environ['CI_COMMIT_TAG']
+elif os.environ.get('CI_JOB_ID'):
+    version = os.environ['CI_JOB_ID']
+if version:
+    with open('version', 'wt') as fout:
+        fout.write(version)
+if os.path.exists('version'):
+    with open('version', 'rt') as fin:
+        for line in fin:
+            version = line.strip()
+
+with open('requirements.txt') as fin:
+    required = fin.read().splitlines()
+
+setuptools.setup(
+    name='statespace',
+    version=version,
+    author='noah smith',
+    author_email='noahhsmith@gmail.com',
+    description='state-space distributions and decisions',
+    url='https://gitlab.com/noahhsmith/statespace',
+    packages=setuptools.find_packages(),
+    entry_points={'console_scripts': ['statespace = statespace.__main__:main']},
+    install_requires=required,
+)
