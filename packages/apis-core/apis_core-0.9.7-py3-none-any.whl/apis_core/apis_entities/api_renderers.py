2ad36c3b06762fefaeb97066ffbfcfa4dc8ac85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:05 +0000

    Add package apis-core/0.9.7/apis_core-0.9.7-py3-none-any.whl

---
+++ packages/apis-core/apis_core-0.9.7-py3-none-any.whl/apis_core/apis_entities/api_renderers.py
@@ -0,0 +1,20 @@
+from rest_framework import renderers
+from apis_core.apis_tei.tei import TeiEntCreator
+
+
+class EntityToTEI(renderers.BaseRenderer):
+
+    media_type = "text/xml"
+    format = 'tei'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        tei_doc = TeiEntCreator(data)
+        return tei_doc.serialize_full_doc()
+
+
+class EntityToCIDOC(renderers.BaseRenderer):
+
+    media_type = "application/rdf+xml"
+    
+    def render(self, data, media_type=None, renderer_context=None):
+        pass
