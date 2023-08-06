4cb2746d79ec1334ef5b39433fcc37786df1fa26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:25 +0000

    Add package twitter-santa/2.0/twitter_santa-2.0-py2.py3-none-any.whl

---
+++ packages/twitter-santa/twitter_santa-2.0-py2.py3-none-any.whl/tweebot/__init__.py
@@ -0,0 +1,19 @@
+
+
+
+
+import logging
+import coloredlogs
+
+from tweebot.agents import UserAgentProvider
+
+__author__ = "Vasyl Paliy"
+__version__ = '2.0'
+__license___ = "MIT"
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='INFO', logger=logger)
+coloredlogs.install(fmt='%(asctime)s | %(message)s')
+
+ua_provider = UserAgentProvider()
