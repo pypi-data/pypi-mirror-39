8ed93da855d2e79ebc08bb1d89013a1d5798215d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:15 +0000

    Add package squid-py/0.2.16/squid_py-0.2.16-py2.py3-none-any.whl

---
+++ packages/squid-py/squid_py-0.2.16-py2.py3-none-any.whl/squid_py/keeper/network_names.py
@@ -0,0 +1,14 @@
+
+DEFAULT_NETWORK_NAME = 'development'
+
+NETWORK_NAME_MAP = {
+    1: 'Main',
+    2: 'Morden',
+    3: 'Ropsten',
+    4: 'Rinkeby',
+    42: 'Kovan',
+    77: 'POA_Sokol',
+    99: 'POA_Core',
+    8995: 'nile',
+    8996: 'spree',
+}
