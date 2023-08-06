d99b87356d2374539c866e85d2d08f42e34f81b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:39 +0000

    Add package CertGenerator/2.1.10/CertGenerator-2.1.10.tar.gz

---
+++ packages/CertGenerator/CertGenerator-2.1.10.tar.gz/CertGenerator-2.1.10/certgenerator/__version__.py
@@ -0,0 +1,10 @@
+__title__ = 'CertGenerator'
+__description__ = 'A simple command line tools to create and read CSR and P12'
+__long_description__ = 'README.rst'
+__url__ = 'https://github.com/johndoe-dev/CertGenerator.git'
+__version__ = '2.1.10'
+__build__ = 0x020000
+__author__ = 'Jonathan ULLINDAH'
+__author_email__ = 'jonathan.ullindah@gmail.com'
+__license__ = 'LICENSE.txt'
+__copyright__ = 'Copyright 2018 Jonathan ULLINDAH'
