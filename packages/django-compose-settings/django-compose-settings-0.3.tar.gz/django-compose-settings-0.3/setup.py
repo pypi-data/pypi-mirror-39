9b5de20e1e88405e45d0314f16e2a41300be71d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:19 +0000

    Add package django-compose-settings/0.3/django-compose-settings-0.3.tar.gz

---
+++ packages/django-compose-settings/django-compose-settings-0.3.tar.gz/django-compose-settings-0.3/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import os
+import codecs
+
+from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+setup(
+    name='django-compose-settings',
+    version=codecs.open(os.path.join(here, 'VERSION'), encoding='utf-8').read().strip(),
+    description='Django composable settings loader.',
+    long_description=codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+    keywords=[
+        'django',
+        'settings',
+    ],
+    author='PeopleDoc',
+    author_email='rd@novapost.fr',
+    url='https://github.com/novafloss/django-compose-settings',
+    license='MIT',
+    extras_require={
+        'release': [
+            'wheel',
+            'zest.releaser'
+        ],
+    },
+    packages=[
+        'django_compose_settings'
+    ],
+)
