598c080b80a06bcbdccf73a1bc3a49aa964fd3e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:11 +0000

    Add package xbase64/1.0.0/xbase64-1.0.0.tar.gz

---
+++ packages/xbase64/xbase64-1.0.0.tar.gz/xbase64-1.0.0/README.md
@@ -0,0 +1,4 @@
+# xbase64
+
+Base64 encode and decode, from type `bytes` or `str`, to `str`.
+
---
+++ packages/xbase64/xbase64-1.0.0.tar.gz/xbase64-1.0.0/setup.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xbase64",
+    version="1.0.0",
+    author="@neoctobers",
+    author_email="neoctobers@gmail.com",
+    description="Base64 encode and decode, from type `bytes` or `str`, to `str`.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/neoctobers/xbase64",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+)
