ffc3ac835fef99bb8d29ce6889b4a7cdcb369307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:46 +0000

    Add package mds-mdswordwrap/0.1.0/mds-mdswordwrap-0.1.0.tar.gz

---
+++ packages/mds-mdswordwrap/mds-mdswordwrap-0.1.0.tar.gz/mds-mdswordwrap-0.1.0/setup.py
@@ -0,0 +1,50 @@
+""" Performs a word wrap on a long line of words. 
+""" 
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+import re
+
+here = path.abspath(path.dirname(__file__))
+MODULE = 'mdswordwrap'
+PREFIX = 'mds'
+
+def read(fname):
+    return open(path.join(here, fname), 'r', 'utf-8').read()
+
+def get_version():
+    init = read(path.join(MODULE, '__init__.py'))
+    return re.search("__version__ = '([0-9.]*)'", init).group(1)
+
+# Get the long description from the README file
+long_description = read('README.rst')
+    
+requires = []
+
+setup(name='mds-mdswordwrap',
+    version=get_version(),
+    description='Performs a word wrap on a long line of words. ',
+    long_description=long_description,
+    author='martin-data services',
+    author_email='service@m-ds.de',
+    url='https://www.m-ds.de/',
+    install_requires=requires,
+    zip_safe=False,
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    license='GPL-3',
+    keywords='word wrap text block',
+)
