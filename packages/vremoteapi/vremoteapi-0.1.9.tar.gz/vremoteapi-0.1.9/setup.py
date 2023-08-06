785cb54639d644f718483e7f60765d42141f4a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:28 +0000

    Add package vremoteapi/0.1.9/vremoteapi-0.1.9.tar.gz

---
+++ packages/vremoteapi/vremoteapi-0.1.9.tar.gz/vremoteapi-0.1.9/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['vremoteapi', 'vremoteapi.OLD_']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'vremoteapi',
+    'version': '0.1.9',
+    'description': 'Mecanismo para permitir chamada de funcao do backend remotamente',
+    'long_description': None,
+    'author': 'Flavio',
+    'author_email': 'fvilante1@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
