0d1768e616f2812c6b1f39359bb96d5e4ddef82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:17 +0000

    Add package marsutils/0.1.0/marsutils-0.1.0.tar.gz

---
+++ packages/marsutils/marsutils-0.1.0.tar.gz/marsutils-0.1.0/marsutils/__init__.py
@@ -0,0 +1,7 @@
+"""A collection of utilities for building robots"""
+
+__author__ = """Noskcaj19"""
+__version__ = "0.1.0"
+
+
+from .controlmanager import ControlManager, ControlInterface, with_ctrl_manager
