5661d999509d7929756f0a60a95e03c2e3c23283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:25 +0000

    Add package py-kaomoji/0.1.3/py_kaomoji-0.1.3.tar.gz

---
+++ packages/py-kaomoji/py_kaomoji-0.1.3.tar.gz/py_kaomoji-0.1.3/kaomoji/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for kaomoji."""
+
+__author__ = """Shibui Yusuke"""
+__email__ = 'shibuiyusuke@gmail.com'
+__version__ = '0.1'
---
+++ packages/py-kaomoji/py_kaomoji-0.1.3.tar.gz/py_kaomoji-0.1.3/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+import sys
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+
+info = sys.version_info
+
+setup(
+    name='py_kaomoji',
+    version='0.1.3',
+    description='Python Boilerplate contains all the boilerplate you need to create a Python package.',
+    long_description=readme,
+    author='Shibui Yusuke',
+    author_email='shibuiyusuke@gmail.com',
+    url='https://github.com/shibuiwilliam/py-kaomoji',
+    packages=find_packages(),
+    include_package_data=True,
+    keywords='kaomoji',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3.6',
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['kao=kaomoji.kaomoji:main'],
+    },
+    test_suite="test",
+)
