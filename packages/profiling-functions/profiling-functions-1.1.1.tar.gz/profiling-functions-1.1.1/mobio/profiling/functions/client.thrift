c5260120e6d496b58615477bad6c37981075e970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:17 +0000

    Add package profiling-functions/1.1.1/profiling-functions-1.1.1.tar.gz

---
+++ packages/profiling-functions/profiling-functions-1.1.1.tar.gz/profiling-functions-1.1.1/mobio/profiling/functions/client.thrift
@@ -0,0 +1,7 @@
+
+service ProfilingService {
+
+    string build_query(1: string merchant_id, 2: string business_case_id, 3: string json_pf),
+    string add_user(1: string merchant_id, 2: i32 source, 3: string json_str_data, 4: string business_case_id),
+    string add_user_b2b(1: string merchant_id, 2: i32 source, 3: string json_str_data, 4: string business_case_id)
+}
