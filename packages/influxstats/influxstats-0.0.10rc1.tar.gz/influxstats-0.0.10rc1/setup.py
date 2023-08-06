876474e71b3142524c8333f381609255db2a4a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:40 +0000

    Add package influxstats/0.0.10rc1/influxstats-0.0.10rc1.tar.gz

---
+++ packages/influxstats/influxstats-0.0.10rc1.tar.gz/influxstats-0.0.10rc1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import find_packages, setup
+
+packages = find_packages('src')
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='influxstats',
+    url='https://github.com/openslate/influxstats',
+    author='Openslate',
+    author_email='code@openslate.com',
+    description='Create consistent stats across your entire codebase',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    version='0.0.10-rc1',
+    packages=packages,  # include all packages under src
+    package_dir={'':'src'},   # tell distutils packages are under src
+    install_requires=[
+        'statsd',
+    ]
+)
