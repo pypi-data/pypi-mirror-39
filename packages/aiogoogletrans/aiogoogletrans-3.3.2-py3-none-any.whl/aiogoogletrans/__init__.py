db081457ba096eefa56e28752869f8761de5b84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:02 +0000

    Add package aiogoogletrans/3.3.2/aiogoogletrans-3.3.2-py3-none-any.whl

---
+++ packages/aiogoogletrans/aiogoogletrans-3.3.2-py3-none-any.whl/aiogoogletrans/__init__.py
@@ -0,0 +1,7 @@
+"""Free Google Translate API for Python. Translates totally free of charge."""
+__all__ = 'Translator',
+__version__ = '3.3.2'
+
+
+from aiogoogletrans.client import Translator
+from aiogoogletrans.constants import LANGCODES, LANGUAGES
