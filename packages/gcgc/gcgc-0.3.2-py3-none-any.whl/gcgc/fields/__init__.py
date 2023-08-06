79377e9acc8b8aed149081664a38b559423ba81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:36 +0000

    Add package gcgc/0.3.2/gcgc-0.3.2-py3-none-any.whl

---
+++ packages/gcgc/gcgc-0.3.2-py3-none-any.whl/gcgc/fields/__init__.py
@@ -0,0 +1,6 @@
+# (c) Copyright 2018 Trent Hauck
+# All Rights Reserved
+
+from gcgc.fields.categorical_field import LabelField, FileMetaDataField, AnnotationField
+
+__all__ = ["LabelField", "FileMetaDataField", "AnnotationField"]
