a4f158f57d00d7d13500b610ab9043ab1a4761c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:53 +0000

    Add package politico-civic-campaign-tracker/0.1.2/politico_civic_campaign_tracker-0.1.2-py3-none-any.whl

---
+++ packages/politico-civic-campaign-tracker/politico_civic_campaign_tracker-0.1.2-py3-none-any.whl/tracker/models/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+from .campaign_content import CampaignContent
+from .candidate_rating_category import CandidateRatingCategory
+from .candidate_run_rating import CandidateRunRating
+from .candidate_win_rating import CandidateWinRating
+from .quote import Quote
+from .story import Story
+from .tweet import Tweet
