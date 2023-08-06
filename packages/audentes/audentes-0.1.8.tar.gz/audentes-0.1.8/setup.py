068428f39927a4c4ba42c2eb7a768dc2fe1b0344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:26 +0000

    Add package audentes/0.1.8/audentes-0.1.8.tar.gz

---
+++ packages/audentes/audentes-0.1.8.tar.gz/audentes-0.1.8/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="audentes",
+    version="0.1.8",
+    author="Magnus Odman",
+    author_email="magnus.odman@gmail.com",
+    description="A package for writing component tests with docker compose",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/magnus.odman/audentes",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'requests', 'ruamel.yaml'
+      ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
