d392720686dcc88adf04f7e0f47226d395a426f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:38 +0000

    Add package strict_functions/2018.12.16.1/strict_functions-2018.12.16.1.tar.gz

---
+++ packages/strict_functions/strict_functions-2018.12.16.1.tar.gz/strict_functions-2018.12.16.1/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Author: Cody Kochmann
+# @Date:   2017-11-09 11:50:41
+# @Last Modified 2018-03-15
+# @Last Modified time: 2018-05-07 11:04:29
+
+from distutils.core import setup
+
+version = '2018.12.16.1'
+
+setup(
+  name = 'strict_functions',
+  packages = ['strict_functions'], # this must be the same as the name above
+  version = version,
+  description = 'helpful decorators that change how python functions work such as scope control or overloading.',
+  author = 'Cody Kochmann',
+  author_email = 'kochmanncody@gmail.com',
+  url = 'https://github.com/CodyKochmann/strict_functions',
+  download_url = 'https://github.com/CodyKochmann/strict_functions/tarball/{}'.format(version),
+  keywords = ['strict_functions', 'scope', 'overload', 'overloading', 'type', 'enforce', 'strict', 'restrict', 'strict defaults', 'defaults'],
+  classifiers = [],
+)
