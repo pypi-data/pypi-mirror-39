399336cb09c7bd1ac5fde843575f6795fb0cba98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:10 +0000

    Add package perspective-python/0.1.3/perspective-python-0.1.3.tar.gz

---
+++ packages/perspective-python/perspective-python-0.1.3.tar.gz/perspective-python-0.1.3/perspective/data/__init__.py
@@ -0,0 +1,14 @@
+from .base import _is_dict, _is_list, Data
+from .pd import _is_pandas
+from .pa import _is_pyarrow
+
+EXPORTERS = [_is_dict, _is_list, _is_pandas, _is_pyarrow]
+
+
+def type_detect(data):
+    for foo in EXPORTERS:
+        data_object = foo(data)
+        if data_object.type:
+            return data_object
+    # throw error?
+    return Data.Empty()
