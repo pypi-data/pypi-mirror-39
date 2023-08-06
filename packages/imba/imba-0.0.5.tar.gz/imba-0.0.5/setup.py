d88792d91094d7ecfa6b54143d23088a7bfe943f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:51 +0000

    Add package imba/0.0.5/imba-0.0.5.tar.gz

---
+++ packages/imba/imba-0.0.5.tar.gz/imba-0.0.5/setup.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+try:
+    import setuptools
+    from setuptools import setup, find_packages
+except ImportError:
+    print("Please install setuptools.")
+
+setup(
+    name  = 'imba',
+    version = '0.0.5',
+    license = 'MIT',
+    author = 'hiroyam',
+    author_email = 'hogehoge@gmail.com',
+    url = 'https://github.com/hiroyam/imba',
+    packages = find_packages(),
+    install_requires = ['sklearn', 'pandas'],
+)
