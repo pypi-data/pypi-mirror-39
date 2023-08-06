3fa2c3488549da22ab0b7fd5790b8c9698b55b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:51 +0000

    Add package django-contact-form-recaptcha/1.6.1/django_contact_form_recaptcha-1.6.1-py2.py3-none-any.whl

---
+++ packages/django-contact-form-recaptcha/django_contact_form_recaptcha-1.6.1-py2.py3-none-any.whl/contact_form/tests/test_urls.py
@@ -0,0 +1,36 @@
+"""
+URLConf for testing django-contact-form.
+
+"""
+
+from django.conf.urls import url
+from django.views.generic import TemplateView
+
+from ..forms import AkismetContactForm, ReCaptchaContactForm
+from ..views import ContactFormView
+
+
+urlpatterns = [
+    url(r'^$',
+        ContactFormView.as_view(),
+        name='contact_form'),
+    url(r'^sent/$',
+        TemplateView.as_view(
+            template_name='contact_form/contact_form_sent.html'
+        ),
+        name='contact_form_sent'),
+    url(r'^test_recipient_list/$',
+        ContactFormView.as_view(
+            recipient_list=['recipient_list@example.com']),
+        name='test_recipient_list'),
+    url(r'^test_akismet_form/$',
+        ContactFormView.as_view(
+            form_class=AkismetContactForm
+        ),
+        name='test_akismet_form'),
+    url(r'^test_captcha_form/$',
+        ContactFormView.as_view(
+            form_class=ReCaptchaContactForm
+        ),
+        name='test_captcha_form'),
+]
---
+++ packages/django-contact-form-recaptcha/django_contact_form_recaptcha-1.6.1-py2.py3-none-any.whl/contact_form/views.py
@@ -0,0 +1,44 @@
+"""
+View which can render and send email from a contact form.
+
+"""
+
+from django.views.generic.edit import FormView
+
+from .forms import ContactForm
+
+try:
+    from django.urls import reverse
+except ImportError:  # pragma: no cover
+    from django.core.urlresolvers import reverse  # pragma: no cover
+
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    recipient_list = None
+    template_name = 'contact_form/contact_form.html'
+
+    def form_valid(self, form):
+        form.save()
+        return super(ContactFormView, self).form_valid(form)
+
+    def get_form_kwargs(self):
+        # ContactForm instances require instantiation with an
+        # HttpRequest.
+        kwargs = super(ContactFormView, self).get_form_kwargs()
+        kwargs.update({'request': self.request})
+
+        # We may also have been given a recipient list when
+        # instantiated.
+        if self.recipient_list is not None:
+            kwargs.update({'recipient_list': self.recipient_list})
+        return kwargs
+
+    def get_success_url(self):
+        # This is in a method instead of the success_url attribute
+        # because doing it as an attribute would involve a
+        # module-level call to reverse(), creating a circular
+        # dependency between the URLConf (which imports this module)
+        # and this module (which would need to access the URLConf to
+        # make the reverse() call).
+        return reverse('contact_form_sent')
