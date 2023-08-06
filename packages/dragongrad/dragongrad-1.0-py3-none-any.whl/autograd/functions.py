f625202c06b1a3bdab5e59edf5267ca0c6c8f639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:30 +0000

    Add package dragongrad/1.0/dragongrad-1.0-py3-none-any.whl

---
+++ packages/dragongrad/dragongrad-1.0-py3-none-any.whl/autograd/functions.py
@@ -0,0 +1,21 @@
+from autograd.blocks.hyperbolic import sinh, cosh, tanh
+from autograd.blocks.operations import add, subtract, multiply, divide, power
+from autograd.blocks.trigo import sin, cos, tan
+# from autograd.blocks.expo import exp, log
+
+sinh_=sinh()
+cosh=cosh()
+tanh=tanh()
+add=add()
+subtract=subtract()
+multiply=multiply()
+divide=divide()
+power=power()
+sin=sin()
+cos=cos()
+tan=tan()
+# exp=exp()
+# log=log()
+
+def sinh():
+	return sinh_
