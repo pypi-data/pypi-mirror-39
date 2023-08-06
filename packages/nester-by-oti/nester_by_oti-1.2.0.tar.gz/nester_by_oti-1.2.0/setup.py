2a847917393ee3c6a65847adea45603cf12e71c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:24 +0000

    Add package nester-by-oti/1.2.0/nester_by_oti-1.2.0.tar.gz

---
+++ packages/nester-by-oti/nester_by_oti-1.2.0.tar.gz/nester_by_oti-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_by_oti',
+    version = '1.2.0',
+    py_modules = ['nester'],
+    author = 'chukwuemeka',
+    author_email = 'donprexido@yahoo.com',
+    url = 'http://www.linkedin.com/chukwuemeka-nnanna-abb573176',
+    description = 'A printer for all items in a nested list',
+    )
