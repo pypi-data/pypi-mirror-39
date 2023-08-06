3daaac5df9387263a4664132efa0b72a0ac6474f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:03 +0000

    Add package singlet/0.4/singlet-0.4.tar.gz

---
+++ packages/singlet/singlet-0.4.tar.gz/singlet-0.4/singlet/dataset/cluster.py
@@ -0,0 +1,175 @@
+# vim: fdm=indent
+# author:     Fabio Zanini
+# date:       16/08/17
+# content:    Dataset functions to cluster samples, features, and phenotypes.
+# Modules
+import numpy as np
+import pandas as pd
+
+from ..utils.cache import method_caches
+
+
+# Classes / functions
+class Cluster():
+    '''Cluster samples, features, and phenotypes'''
+    def __init__(self, dataset):
+        '''Cluster samples, features, and phenotypes
+
+        Args:
+            dataset (Dataset): the dataset to analyze.
+        '''
+        self.dataset = dataset
+
+    def kmeans(
+            self,
+            n_clusters,
+            axis,
+            phenotypes=(),
+            random_state=0):
+        '''K-Means clustering.
+
+        Args:
+            n_clusters (int): The number of clusters you want.
+            axis (string): It must be 'samples' or 'features'.
+                The Dataset.counts matrix is used and
+                either samples or features are clustered.
+            phenotypes (iterable of strings): Phenotypes to add to the
+                features for joint clustering.
+            log_features (bool): Whether to add pseudocounts and take a log
+                of the feature counts before calculating distances.
+            random_state (int): Set to the same int for deterministic results.
+
+        Returns:
+            pd.Series with the labels of the clusters.
+        '''
+        from sklearn.cluster import KMeans
+
+        data = self.dataset.counts
+
+        if phenotypes is not None:
+            data = data.copy()
+            for pheno in phenotypes:
+                data.loc[pheno] = self.dataset.samplesheet.loc[:, pheno]
+
+        if axis == 'samples':
+            data = data.T
+        elif axis == 'features':
+            pass
+        else:
+            raise ValueError('axis must be "samples" or "features"')
+
+        kmeans = (KMeans(n_clusters=n_clusters, random_state=random_state)
+                  .fit(data.values))
+        labels = pd.Series(kmeans.labels_, index=data.index, dtype='category')
+        return labels
+
+    def dbscan(
+            self,
+            axis,
+            phenotypes=(),
+            **kwargs):
+        '''Density-Based Spatial Clustering of Applications with Noise.
+
+        Args:
+            axis (string): It must be 'samples' or 'features'.
+                The Dataset.counts matrix is used and
+                either samples or features are clustered.
+            phenotypes (iterable of strings): Phenotypes to add to the
+                features for joint clustering.
+            log_features (bool): Whether to add pseudocounts and take a log
+                of the feature counts before calculating distances.
+            **kwargs: arguments passed to sklearn.cluster.DBSCAN.
+
+        Returns:
+            pd.Series with the labels of the clusters.
+        '''
+        from sklearn.cluster import DBSCAN
+
+        data = self.dataset.counts
+
+        if phenotypes is not None:
+            data = data.copy()
+            for pheno in phenotypes:
+                data.loc[pheno] = self.dataset.samplesheet.loc[:, pheno]
+
+        if axis == 'samples':
+            data = data.T
+        elif axis == 'features':
+            pass
+        else:
+            raise ValueError('axis must be "samples" or "features"')
+
+        kmeans = (DBSCAN(**kwargs)
+                  .fit(data.values))
+        labels = pd.Series(kmeans.labels_, index=data.index, dtype='category')
+        return labels
+
+    # NOTE: caching this one is tricky because it has non-kwargs AND it would
+    # need a double cache, one for cells and one for features/phenotypes
+    def hierarchical(
+            self,
+            axis,
+            phenotypes=(),
+            metric='correlation',
+            method='average',
+            log_features=False,
+            optimal_ordering=False):
+        '''Hierarchical clustering.
+
+        Args:
+            axis (string): It must be 'samples' or 'features'. The
+                Dataset.counts matrix is used and either samples or features
+                are clustered.
+            phenotypes (iterable of strings): Phenotypes to add to the
+                features for joint clustering.
+            metric (string): Metric to calculate the distance matrix. Should
+                be a string accepted by scipy.spatial.distance.pdist.
+            method (string): Clustering method. Must be a string accepted by
+                scipy.cluster.hierarchy.linkage.
+            log_features (bool): Whether to add pseudocounts and take a log
+                of the feature counts before calculating distances.
+            optimal_ordering (bool): Whether to resort the linkage so that
+                nearest neighbours have shortest distance. This may take
+                longer than the clustering itself.
+        Returns:
+            dict with the linkage, distance matrix, and ordering.
+        '''
+        from scipy.spatial.distance import pdist
+        from scipy.cluster.hierarchy import linkage, leaves_list, optimal_leaf_ordering
+
+        data = self.dataset.counts
+
+        if log_features:
+            data = np.log10(self.dataset.counts.pseudocount + data)
+
+        if phenotypes is not None:
+            data = data.copy()
+            for pheno in phenotypes:
+                data.loc[pheno] = self.dataset.samplesheet.loc[:, pheno]
+
+        if axis == 'samples':
+            data = data.T
+        elif axis == 'features':
+            pass
+        else:
+            raise ValueError('axis must be "samples" or "features"')
+
+        Y = pdist(data.values, metric=metric)
+
+        # Some metrics (e.g. correlation) give nan whenever the matrix has no
+        # variation, default this to zero distance (e.g. two features that are
+        # both total dropouts.
+        Y = np.nan_to_num(Y)
+
+        Z = linkage(Y, method=method)
+
+        if optimal_ordering:
+            Z = optimal_leaf_ordering(Z, Y)
+
+        ids = data.index[leaves_list(Z)]
+
+        return {
+                'distance': Y,
+                'linkage': Z,
+                'leaves': ids,
+                }
---
+++ packages/singlet/singlet-0.4.tar.gz/singlet-0.4/singlet/io/googleapi/samplesheet.py
@@ -0,0 +1,105 @@
+# vim: fdm=indent
+# author:     Fabio Zanini
+# date:       16/01/17
+# content:    Google Sheets API for the sample sheet.
+# Modules
+import os
+import numpy as np
+import pandas as pd
+
+from .googleapi import GoogleIOError, GoogleAPI
+
+
+# Globals
+
+
+# Classes / functions
+class SampleSheet(GoogleAPI):
+    def __init__(self, sheet):
+        self.sheetname = sheet['sheet']
+
+        sheetid = sheet['google_id']
+        client_id_filename = sheet['client_id_filename']
+        client_secret_filename = sheet['client_secret_filename']
+        super().__init__(
+                sheetid,
+                self.sheetname,
+                client_id_filename,
+                client_secret_filename)
+
+    def get_number_virus_reads(self, virus, icols=None):
+        '''Get the number of virus reads from the spreadsheet'''
+        sheetname = 'sequenced'
+        vircolname = 'number'+virus.capitalize()+'Reads'
+
+        if icols is None:
+            icols = self.get_header_columns_indices(
+                    ['name', 'experiment', vircolname],
+                    sheetname)
+
+        if 'name' not in icols:
+            raise ValueError('name must be part of the icols dict')
+        if vircolname not in icols:
+            raise ValueError(vircolname+' must be part of the icols dict')
+
+        # Ask for the name first, that determines the range
+        colnames = ['name'] + [cn for cn in icols if cn !='name']
+
+        # Get the values
+        data = {}
+        for colname in colnames:
+            icol = icols[colname]
+
+            # Google figures out the max row number
+            if colname == 'name':
+                rangeName = sheetname+'!'+icol+'2:'+icol+'100000'
+            else:
+                nNames = len(data['name'])
+                rangeName = sheetname+'!'+icol+'2:'+icol+str(1+nNames)
+
+            result = self.service.spreadsheets().values().get(
+                spreadsheetId=self.spreadsheetId, range=rangeName).execute()
+            values = result.get('values', [])
+
+            # Google cuts trailing None
+            if (colname != 'name') and (len(values) < nNames):
+                values.extend([['']] * (nNames - len(values)))
+
+            # format
+            if colname == vircolname:
+                values = np.array([int(v[0]) if (len(v) and (v[0] != '')) else -1 for v in values], int)
+            else:
+                values = np.array(values)[:, 0]
+
+            data[colname] = values
+
+        # Check consistency
+        l = len(data['name'])
+        for colname, datum in data.items():
+            if len(datum) != l:
+                raise ValueError('Not all columns have the same length')
+
+        data = pd.DataFrame(data)
+        return data
+
+    def get_table(self, fmt='pandas'):
+        values = super().get_data(self.sheetname)
+        if fmt == 'pandas':
+            return pd.DataFrame(values[1:], columns=values[0])
+        elif fmt == 'numpy':
+            return np.array(values)
+        elif fmt == 'raw':
+            return values
+        else:
+            raise ValueError('Format not understood')
+
+    def update_tsv_table(self, sheetname, sandbox=True):
+        '''Update TSV table from the Google Sheet'''
+        from ..filenames import get_sample_table_filename
+        fn = get_sample_table_filename(kind=sheetname, sandbox=sandbox)
+
+        table = self.get_table(sheetname=sheetname, fmt='raw')
+
+        table_tsv = '\n'.join(map('\t'.join, table))+'\n'
+        with open(fn, 'w') as f:
+            f.write(table_tsv)
