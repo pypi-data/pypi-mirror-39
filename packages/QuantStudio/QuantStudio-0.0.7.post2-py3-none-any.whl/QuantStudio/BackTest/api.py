0e5d313bedbb2030c8ec784f401c7c79d428f611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:07 +0000

    Add package QuantStudio/0.0.7.post2/QuantStudio-0.0.7.post2-py3-none-any.whl

---
+++ packages/QuantStudio/QuantStudio-0.0.7.post2-py3-none-any.whl/QuantStudio/BackTest/api.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from .BackTestModel import BackTestModel
+from .SectionFactor import api as SectionFactor
+from .Strategy import api as Strategy
+from .PerformanceAnalysis import api as PerformanceAnalysis
