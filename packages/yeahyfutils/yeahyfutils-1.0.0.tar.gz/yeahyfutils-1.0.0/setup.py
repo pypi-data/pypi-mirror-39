7566a546c966be65d2bf79ac3957359564a0a430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:33 +0000

    Add package yeahyfutils/1.0.0/yeahyfutils-1.0.0.tar.gz

---
+++ packages/yeahyfutils/yeahyfutils-1.0.0.tar.gz/yeahyfutils-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'yeahyfutils',
+    version = '1.0.0',
+    py_modules = ['yeahyfutils'],
+    author='yeahyf',
+    author_email='yeahyf@gmail.com',
+    url = 'http://www.yeahyf.com',
+    description='a simple utils'
+)
---
+++ packages/yeahyfutils/yeahyfutils-1.0.0.tar.gz/yeahyfutils-1.0.0/yeahyfutils.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*
+
+'''这是一个共享测试模块而编写的模块'''
+
+def sum(max_num):
+    '''
+    :param max_num: 累加的最大值
+    :return:
+    计算从1 到 max_num的和
+    '''
+    return (1+max_num)*max_num/2
+
+
