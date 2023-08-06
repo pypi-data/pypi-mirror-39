fe698cb276777e48fd9bfda036c3fbe4c142f547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:11 +0000

    Add package alignak-checks-elasticsearch/1.1.0/alignak_checks_elasticsearch-1.1.0-py2-none-any.whl

---
+++ packages/alignak-checks-elasticsearch/alignak_checks_elasticsearch-1.1.0-py2-none-any.whl/alignak_checks_mongodb/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015-2018:
+#   Frederic Mohier, frederic.mohier@alignak.net
+#
+
+"""
+    Alignak - Checks pack for NRPE monitored Linux hosts/services
+"""
