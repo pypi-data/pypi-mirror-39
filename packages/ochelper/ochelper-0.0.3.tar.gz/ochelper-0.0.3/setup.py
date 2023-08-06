20fc6a89ca72623e4cd6e743ec1196ed183ef21f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:23 +0000

    Add package ochelper/0.0.3/ochelper-0.0.3.tar.gz

---
+++ packages/ochelper/ochelper-0.0.3.tar.gz/ochelper-0.0.3/setup.py
@@ -0,0 +1,81 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+
+    name='ochelper',  # Required
+
+    version='0.0.3',  
+
+    description='ochelper for objective-c',
+
+    long_description=long_description, 
+
+    long_description_content_type='text/markdown', 
+
+    url='https://github.com',  # Optional
+
+    author='weishqdev',  
+
+    author_email="weishqdev@gmail.com",
+
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+
+    keywords='objective-c code proc',  # Optional
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+
+    install_requires=[], 
+
+    # extras_require={  # Optional
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # data_files=[('my_data', ['data/data_file'])],  # Optional
+
+    entry_points = {
+        'console_scripts': [
+              'ochelper = ochelper.ochelper:main'
+        ]
+    }
+
+)
