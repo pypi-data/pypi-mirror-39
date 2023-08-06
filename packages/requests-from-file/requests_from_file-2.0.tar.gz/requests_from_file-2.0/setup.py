78c4a6e94f8eef0a51d95b5a2b92375b57d52943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:25 +0000

    Add package requests-from-file/2.0/requests_from_file-2.0.tar.gz

---
+++ packages/requests-from-file/requests_from_file-2.0.tar.gz/requests_from_file-2.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open ( "README.md" , "r",encoding="utf-8" ) as fh :
+    long_description = fh . read ()
+
+setuptools . setup (
+    name = "requests_from_file" ,
+    version = "2.0" ,
+    author = "zhangtao103239" ,
+    author_email = "zhangtao103239@163.com" ,
+    description = "create requests params from file,file is created by Firefox Header" ,
+    long_description = long_description ,
+    long_description_content_type = "text/markdown" ,
+    url = "https://github.com/zhangtao103239/requests_from_file" ,
+    packages = setuptools . find_packages (),
+    classifiers = (
+        "Programming Language :: Python :: 3" ,
+        "License :: OSI Approved :: MIT License" ,
+        "Operating System :: OS Independent" ,
+    ),
+    install_requires=['requests']
+)
