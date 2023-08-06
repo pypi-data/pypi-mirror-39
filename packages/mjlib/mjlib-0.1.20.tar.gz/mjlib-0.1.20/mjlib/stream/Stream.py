f8813ee8d4c648fd4252316b4f548824ecf18e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:25 +0000

    Add package mjlib/0.1.20/mjlib-0.1.20.tar.gz

---
+++ packages/mjlib/mjlib-0.1.20.tar.gz/mjlib-0.1.20/mjlib/stream/Stream.py
@@ -0,0 +1,34 @@
+from abc import abstractmethod, ABC
+
+
+class Stream(ABC):
+    def __init__(self):
+        self._listeners = []
+
+    def run(self):
+        while True:
+            sample = self._get_sample()
+            self._send(sample)
+
+    @abstractmethod
+    def _get_sample(self):
+        pass
+
+    def connect(self, output):
+        if output not in self._listeners:
+            self._listeners += output
+
+    def disconnect(self, output):
+        self._listeners -= output
+
+    def __iadd__(self, other):
+        self.connect(other)
+        return self
+
+    def __isub__(self, other):
+        self.disconnect(other)
+        return self
+
+    def _send(self, sample):
+        for l in self._listeners:
+            l(sample)
