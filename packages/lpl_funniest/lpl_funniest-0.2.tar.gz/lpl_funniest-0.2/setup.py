12c076774727e841479a9bcf44273a0ac95fbb94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:53 +0000

    Add package lpl_funniest/0.2/lpl_funniest-0.2.tar.gz

---
+++ packages/lpl_funniest/lpl_funniest-0.2.tar.gz/lpl_funniest-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lpl_funniest',
+      version='0.2',
+      description='The funniest joke in the world',
+      url='http://github.com/xxxxx',
+      author='Peilun',
+      author_email='xxxxx@example.com',
+      license='MIT',
+      packages=['lpl_funniest'],
+      zip_safe=False)
