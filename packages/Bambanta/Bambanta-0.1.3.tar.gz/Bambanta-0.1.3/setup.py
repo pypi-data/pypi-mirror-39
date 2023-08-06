7a3a26c531ca3ee36ea6f592a1f06a36bda3e8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:19 +0000

    Add package Bambanta/0.1.3/Bambanta-0.1.3.tar.gz

---
+++ packages/Bambanta/Bambanta-0.1.3.tar.gz/Bambanta-0.1.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='Bambanta',
+	version='0.1.3',
+	description='An automatic differentiation package',
+	author='Karina Huang, Rong Liu, Rory Maizels',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=['Bambanta'],
+	install_requires=[
+	  'numpy'
+	],
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	url="https://github.com/CS207-Project-Group-9/cs207-FinalProject",
+	license="MIT",
+)
