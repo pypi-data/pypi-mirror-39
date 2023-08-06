7a8ac64ce307d2418a3ef5b7db3b242b0b479faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:59 +0000

    Add package astronomy/0.0.1/astronomy-0.0.1.tar.gz

---
+++ packages/astronomy/astronomy-0.0.1.tar.gz/astronomy-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "astronomy",
+    version = "0.0.1",
+    author = "Rodrigo Luger",
+    author_email = "rodluger@gmail.com",
+    description = ("Astronomy!"),
+    license = "MIT",
+    url = "https://github.com/rodluger/astronomy",
+    packages=['astronomy']
+)
