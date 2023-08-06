85ff5da7d7a0a3a1fdd840b444117409ca8d175a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:55 +0000

    Add package pygenetics/0.5.2/pygenetics-0.5.2.tar.gz

---
+++ packages/pygenetics/pygenetics-0.5.2.tar.gz/pygenetics-0.5.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pygenetics',
+    version='0.5.2',
+    description='Genetic algorithm framework for arbitrary cost functions',
+    url='http://github.com/tjkessler/pygenetics',
+    author='Travis Kessler',
+    author_email='travis.j.kessler@gmail.com',
+    license='MIT',
+    packages=['pygenetics'],
+    install_requires=['numpy'],
+    zip_safe=False
+)
