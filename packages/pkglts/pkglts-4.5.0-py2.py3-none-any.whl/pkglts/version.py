22ad44b9e588e902c9f8fe85b282dea438afd7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:44 +0000

    Add package pkglts/4.5.0/pkglts-4.5.0-py2.py3-none-any.whl

---
+++ packages/pkglts/pkglts-4.5.0-py2.py3-none-any.whl/pkglts/version.py
@@ -0,0 +1,17 @@
+"""
+Maintain version for this package.
+"""
+# {# pkglts, version
+#  -*- coding: utf-8 -*-
+
+MAJOR = 4
+"""(int) Version major component."""
+
+MINOR = 5
+"""(int) Version minor component."""
+
+POST = 0
+"""(int) Version post or bugfix component."""
+
+__version__ = ".".join([str(s) for s in (MAJOR, MINOR, POST)])
+# #}
