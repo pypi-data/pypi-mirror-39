5230f02dfc2fa1eb921b19936e148d99e1aa3c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:24 +0000

    Add package django-request-logger/0.2.0/django_request_logger-0.2.0-py3-none-any.whl

---
+++ packages/django-request-logger/django_request_logger-0.2.0-py3-none-any.whl/request_logger/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# vi: et sw=2 fileencoding=utf-8
+
+#============================================================================
+# Request logger
+# Copyright (c) 2017 Pispalan Insinööritoimisto Oy (http://www.pispalanit.fi)
+#
+# All rights reserved.
+# Redistributions of files must retain the above copyright notice.
+#
+# @description [File description]
+# @created     24.03.2017
+# @author      Joni Saarinen <joni.saarinen@pispalanit.fi>
+# @copyright   Copyright (c) Pispalan Insinööritoimisto Oy
+# @license     All rights reserved
+#============================================================================
+
+
