4c9ef459f402159505cc6e6c75b96c1c8f5a7913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:19 +0000

    Add package print1991/1.3.0/print1991-1.3.0.tar.gz

---
+++ packages/print1991/print1991-1.3.0.tar.gz/print1991-1.3.0/print1991.py
@@ -0,0 +1,11 @@
+"""这个函数去一个位置参数，为the_list,可以是任何列表，指定的列表中的每个数据项会递归地
+输出到屏幕上，个数据项占一行"""
+def print1991(the_list,indent=False,level=0):
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item,indent,level+1)
+		else:
+                        if indent:
+                            for num in level:
+                                 print('\t',end='')
+			print(each_item)
---
+++ packages/print1991/print1991-1.3.0.tar.gz/print1991-1.3.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+         name         = 'print1991',
+         version      = '1.3.0',
+         py_modules   = ['print1991'],
+         author       = 'hfpython',
+         author_email = 'hfpython@headfirstlabs.com',
+         url          = 'http://www.headfirstlabs.com',
+         description  = 'A simple printer of nested lists',
+
+ )
