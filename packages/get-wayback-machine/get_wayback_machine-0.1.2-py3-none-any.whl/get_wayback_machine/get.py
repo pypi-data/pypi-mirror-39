7fcc48e5e9f2e1c53d937f0026747ecb0c5f1abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:37 +0000

    Add package get-wayback-machine/0.1.2/get_wayback_machine-0.1.2-py3-none-any.whl

---
+++ packages/get-wayback-machine/get_wayback_machine-0.1.2-py3-none-any.whl/get_wayback_machine/get.py
@@ -0,0 +1,25 @@
+import get_retries
+
+
+def get(url, **kwargs):
+    response = get_retries.get(
+        f"http://archive.org/wayback/available?url={url.split('?')[0]}", **{'max_backoff': 128, **kwargs})
+
+    if not response or response.status_code != 200:
+        return None
+
+    r_json = response.json()
+
+    if not 'closest' in r_json['archived_snapshots']:
+        return None
+
+    clo = r_json['archived_snapshots']['closest']
+    if clo['status'] != '200':
+        return None
+
+    response_final = get_retries.get(
+        clo['url'], **{'max_backoff': 128, **kwargs})
+    if not response_final or response_final.status_code != 200:
+        return None
+
+    return response_final
