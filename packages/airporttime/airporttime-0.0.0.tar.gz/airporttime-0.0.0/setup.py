66e7a7eb6b66161eb0bc52d2c56ce7e2ae7d0651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:22 +0000

    Add package airporttime/0.0.0/airporttime-0.0.0.tar.gz

---
+++ packages/airporttime/airporttime-0.0.0.tar.gz/airporttime-0.0.0/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+from io import open
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+install_requires = ['pytz', 'requests']
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+
+setup(name='airporttime',
+      version='0.0.0',
+      description='convert local time to utc time by airport or vise-versa.',
+      long_description=readme,
+      author='Liam Cryan',
+      author_email='cryan.liam@gmail.com',
+      packages=find_packages(),
+      install_requires=install_requires,
+      include_package_data=True,
+      url='https://github.com/liamcryan/airporttime',
+      classifiers=['Programming Language :: Python :: 3']
+      )
