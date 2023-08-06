36a19b20f3cc554add394f216714da243ae75ce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:19 +0000

    Add package pycfslib/0.8/pycfslib-0.8.tar.gz

---
+++ packages/pycfslib/pycfslib-0.8.tar.gz/pycfslib-0.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycfslib',
+      version='0.8',
+      description='Library to read, write amd create CFS file and stream, now supports NEO sleep staging system.',
+      url='https://github.com/neurobittechnologies/pycfslib',
+      author='Amiya Patanaik',
+      author_email='amiya@neurobit.io',
+      license='GPL',
+      packages=['pycfslib'],
+      zip_safe=False)
