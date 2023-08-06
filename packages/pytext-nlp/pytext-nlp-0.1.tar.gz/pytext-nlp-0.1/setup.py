4c6476a5f7acc583d749a94147603cefc694c8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:46 +0000

    Add package pytext-nlp/0.1/pytext-nlp-0.1.tar.gz

---
+++ packages/pytext-nlp/pytext-nlp-0.1.tar.gz/pytext-nlp-0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+import os
+
+from setuptools import setup
+
+
+setup(
+    name="pytext-nlp",
+    version="0.1",
+    description="pytorch modeling framework and model zoo for text models",
+    author="Facebook",
+    license="BSD",
+    packages=["pytext"],
+)
