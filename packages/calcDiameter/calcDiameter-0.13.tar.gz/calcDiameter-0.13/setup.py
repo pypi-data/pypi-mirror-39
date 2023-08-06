820f94df5797ec7e3a5a871fa99c9effa0bdf538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:12 +0000

    Add package calcDiameter/0.13/calcDiameter-0.13.tar.gz

---
+++ packages/calcDiameter/calcDiameter-0.13.tar.gz/calcDiameter-0.13/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='calcDiameter',
+      version='0.13',
+      description='The program takes in a Molecular Assembly PDB file as arument, finds the right orientation such that the ring sits on the XY plane, projects all atoms onto the XY plane and calculates the max diameter between two atoms that lie furthest apart on the XY plane',
+      long_description=readme(),
+      long_description_content_type='text/x-rst',
+      author='Jan Zaucha',
+      author_email='j.zaucha@tum.de',
+      license='MIT',
+      packages=['calcDiameter'],
+      install_requires=['numpy', 'scipy', 'biopython', 'matplotlib'],
+      dependency_links=['http://biopython.org/DIST/'],
+      zip_safe=False)
