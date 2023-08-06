7afcf589d7aeaee81b63de59df8503f0ae5ad996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:43 +0000

    Add package django-auth-adfs/1.1.2/django_auth_adfs-1.1.2-py2.py3-none-any.whl

---
+++ packages/django-auth-adfs/django_auth_adfs-1.1.2-py2.py3-none-any.whl/django_auth_adfs/middleware.py
@@ -0,0 +1,58 @@
+"""
+Based on https://djangosnippets.org/snippets/1179/
+"""
+from re import compile
+
+import django
+from django.conf import settings as django_settings
+from django.contrib.auth.views import redirect_to_login
+
+from django_auth_adfs.config import settings
+
+try:
+    from django.urls import reverse
+except ImportError:  # Django < 1.10
+    from django.core.urlresolvers import reverse
+
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:  # Django < 1.10
+    MiddlewareMixin = object
+
+LOGIN_EXEMPT_URLS = [
+    compile(django_settings.LOGIN_URL.lstrip('/')),
+    compile(reverse("django_auth_adfs:login").lstrip('/')),
+    compile(reverse("django_auth_adfs:logout").lstrip('/')),
+    compile(reverse("django_auth_adfs:callback").lstrip('/')),
+]
+if hasattr(settings, 'LOGIN_EXEMPT_URLS'):
+    LOGIN_EXEMPT_URLS += [compile(expr) for expr in settings.LOGIN_EXEMPT_URLS]
+
+
+class LoginRequiredMiddleware(MiddlewareMixin):
+    """
+    Middleware that requires a user to be authenticated to view any page other
+    than LOGIN_URL. Exemptions to this requirement can optionally be specified
+    in settings via a list of regular expressions in LOGIN_EXEMPT_URLS (which
+    you can copy from your urls.py).
+
+    Requires authentication middleware and template context processors to be
+    loaded. You'll get an error if they aren't.
+    """
+    def process_request(self, request):
+        assert hasattr(request, 'user'), "The Login Required middleware requires " \
+                                         "authentication middleware to be installed. " \
+                                         "Edit your MIDDLEWARE setting to insert " \
+                                         "'django.contrib.auth.middleware.AuthenticationMiddleware'. " \
+                                         "If that doesn't work, ensure your TEMPLATE_CONTEXT_PROCESSORS " \
+                                         "setting includes 'django.core.context_processors.auth'."
+
+        if django.VERSION[:2] < (1, 10):
+            user_authenticated = request.user.is_authenticated()
+        else:
+            user_authenticated = request.user.is_authenticated
+
+        if not user_authenticated:
+            path = request.path_info.lstrip('/')
+            if not any(m.match(path) for m in LOGIN_EXEMPT_URLS):
+                return redirect_to_login(request.get_full_path())
