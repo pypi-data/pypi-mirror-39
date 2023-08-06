8b6169342a1b4c1948b19e3cbb48ce70914e8bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:03 +0000

    Add package yo-ds/0.1.2/yo_ds-0.1.2.tar.gz

---
+++ packages/yo-ds/yo_ds-0.1.2.tar.gz/yo_ds-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+def readme():
+      with open('README.md') as file:
+            return file.read()
+
+setup(name='yo_ds',
+      version='0.1.2',
+
+      description='The toolkit for data science projects with a focus on functional programming',
+      long_description=readme(),
+      classifiers = [
+            'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      url='http://comingsoon.com',
+      author='Yuri Okulovsky',
+      author_email='yuri.okulovsky@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+            'pandas'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      include_package_data = True,
+      zip_safe=False)
