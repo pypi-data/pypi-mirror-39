8507a58bd39a2da1dc81fda78364e6a800e9ede4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:26 +0000

    Add package mjlib/0.1.30/mjlib-0.1.30.tar.gz

---
+++ packages/mjlib/mjlib-0.1.30.tar.gz/mjlib-0.1.30/mjlib/gui/binding.py
@@ -0,0 +1,18 @@
+@property
+def FREQ1(self):
+    return self._FREQ1
+
+
+@FREQ1.setter
+def FREQ1(self, value):
+    self._FREQ1 = value
+    for callback in self._FREQ1_observers:
+        callback(self._FREQ1)
+
+
+def bind_to_FREQ1(self, callback):
+    self._FREQ1_observers.append(callback)
+
+
+# self._FREQ1 = 28.0  # 31.243
+# self._FREQ1_observers = []
