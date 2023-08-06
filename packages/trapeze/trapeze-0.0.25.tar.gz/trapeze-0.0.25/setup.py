91f8399de7bbd5f0239adf72c9beaeee6a036aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:24 +0000

    Add package trapeze/0.0.25/trapeze-0.0.25.tar.gz

---
+++ packages/trapeze/trapeze-0.0.25.tar.gz/trapeze-0.0.25/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh :
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "trapeze",
+    version="0.0.25",
+    author="Eric Gee",
+    author_email="eric@gee.farm",
+    description="Store/restore configuration and credentials for applications with AWS S3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/ericgee/trapeze",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = ['boto3','click','colorama'],
+    entry_points = {
+        'console_scripts': [
+            'trapeze=trapeze.app:main'
+        ]
+    }
+)
