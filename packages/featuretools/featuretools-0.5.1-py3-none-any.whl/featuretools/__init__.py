01b7ae863869454ec60dd36e058d8ac1217b21cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:57 +0000

    Add package featuretools/0.5.1/featuretools-0.5.1-py3-none-any.whl

---
+++ packages/featuretools/featuretools-0.5.1-py3-none-any.whl/featuretools/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+# flake8: noqa
+from . import config
+from . import variable_types
+from .entityset.api import *
+from . import primitives
+from .synthesis.api import *
+from .primitives import  list_primitives, install_primitives, Feature
+from .computational_backends.api import *
+from . import tests
+from .utils.pickle_utils import *
+from .utils.time_utils import *
+import featuretools.demo
+import featuretools.wrappers
+
+__version__ = '0.5.1'
