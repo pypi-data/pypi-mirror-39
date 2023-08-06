0b0a2d35d8919f01e12f48ef3e7c8615df6f8173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:25 +0000

    Add package rotest/5.3.1/rotest-5.3.1.tar.gz

---
+++ packages/rotest/rotest-5.3.1.tar.gz/rotest-5.3.1/setup.py
@@ -0,0 +1,85 @@
+"""Setup file for handling packaging and distribution."""
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+
+
+__version__ = "5.3.1"
+
+result_handlers = [
+    "db = rotest.core.result.handlers.db_handler:DBHandler",
+    "excel = rotest.core.result.handlers.excel_handler:ExcelHandler",
+    "dots = rotest.core.result.handlers.stream.dots_handler:DotsHandler",
+    "tree = rotest.core.result.handlers.stream.tree_handler:TreeHandler",
+    "remote = rotest.core.result.handlers.remote_db_handler:RemoteDBHandler",
+    "loginfo = rotest.core.result.handlers.stream.log_handler:LogInfoHandler",
+    "artifact = rotest.core.result.handlers.artifact_handler:ArtifactHandler",
+    "logdebug = "
+    "rotest.core.result.handlers.stream.log_handler:LogDebugHandler",
+    "pretty = "
+    "rotest.core.result.handlers.stream.log_handler:PrettyHandler",
+    "signature = "
+    "rotest.core.result.handlers.signature_handler:SignatureHandler",
+    "full = "
+    "rotest.core.result.handlers.stream.stream_handler:EventStreamHandler",
+]
+
+requirements = [
+    'django>=1.8,<1.9',
+    'ipdb',
+    'py',
+    'isort',
+    'ipdbugger>=2',
+    'docopt',
+    'xlwt',
+    'attrdict',
+    'pyyaml',
+    'twisted',
+    'psutil',
+    'colorama',
+    'termcolor',
+    'jsonschema',
+    'basicstruct',
+    'future',
+    'swaggapi>=0.6.5',
+    'python-daemon;platform_system!="Windows"'
+]
+
+setup(
+    name='rotest',
+    version=__version__,
+    description="Resource oriented testing framework",
+    long_description=open("README.rst").read(),
+    license="MIT",
+    author="gregoil",
+    author_email="gregoil@walla.co.il",
+    url="https://github.com/gregoil/rotest",
+    keywords="testing system django unittest",
+    install_requires=requirements,
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
+    entry_points={
+        "console_scripts": [
+            "rotest = rotest.cli.main:main"
+        ],
+        "rotest.result_handlers": result_handlers,
+        "rotest.cli_client_parsers": [],
+        "rotest.cli_client_actions": [],
+    },
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    package_data={'': ['*.xls', '*.xsd', '*.json', '*.css', '*.xml', '*.rst']},
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Testing',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+    ],
+)
