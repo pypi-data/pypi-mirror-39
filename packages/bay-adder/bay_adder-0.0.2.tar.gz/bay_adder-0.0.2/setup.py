c49d69af370d3d7a693aee7df10a73fe338784f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:24 +0000

    Add package bay-adder/0.0.2/bay_adder-0.0.2.tar.gz

---
+++ packages/bay-adder/bay_adder-0.0.2.tar.gz/bay_adder-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="bay_adder",
+    version="0.0.2",
+    author="Alex Bers",
+    author_email="bay@hackerdom.ru",
+    description="Adds two nums",
+    long_description="Long description",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    py_modules = ['bay_adder'],
+    scripts=["helloworld"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
