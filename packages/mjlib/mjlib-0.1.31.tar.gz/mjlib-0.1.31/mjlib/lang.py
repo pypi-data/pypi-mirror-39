b9ed9c751a7955a28752818d1df6f63e7b2aad35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:26 +0000

    Add package mjlib/0.1.31/mjlib-0.1.31.tar.gz

---
+++ packages/mjlib/mjlib-0.1.31.tar.gz/mjlib-0.1.31/mjlib/lang.py
@@ -0,0 +1,13 @@
+# Signal Sources
+RAW = 1
+TRANSFORMED = 2
+CYTON = 1
+GANGLION = 2
+ENOBIO = 3
+
+# Frequency Bands
+DELTA = (0, 4)
+THETA = (4, 7.5)
+ALPHA_MU = (7.5, 12)
+BETA = (16, 31)
+GAMMA = (31, 100)
