d1ced4b5c7a253bb80ed2f59eaae111cb6648823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:15 +0000

    Add package proxychains-conf-generator/2.0.1/proxychains-conf-generator-2.0.1.tar.gz

---
+++ packages/proxychains-conf-generator/proxychains-conf-generator-2.0.1.tar.gz/proxychains-conf-generator-2.0.1/proxychains_conf_generator/proxychains_conf_generator.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import xprint as xp
+
+
+class Generator:
+    def __init__(self,
+                 proxy: str or list = 'socks5 127.0.0.0 1080',
+                 chain_mode: str = 'strict_chain',
+                 quiet_mode: bool = False,
+                 ):
+        self._proxy = list()
+        self._chain_mode = chain_mode
+        self._quite_mode = quiet_mode
+
+        if isinstance(proxy, str):
+            self._proxy.append(proxy)
+        else:
+            self._proxy = proxy
+
+    def write(self,
+              path_to_conf: str = '/etc/proxychains.conf'):
+
+        xp.about_t('Generate', path_to_conf, 'for proxychains')
+
+        configs = list()
+
+        configs.append('{chain_mode}'.format(chain_mode=self._chain_mode))
+        if self._quite_mode:
+            configs.append('quiet_mode')
+        configs.append('proxy_dns')
+        configs.append('remote_dns_subnet 224')
+        configs.append('tcp_read_time_out 15000')
+        configs.append('tcp_connect_time_out 8000')
+        configs.append('')
+        configs.append('[ProxyList]')
+        configs.append('{proxy}'.format(proxy='\n'.join(self._proxy)))
+        configs = '\n'.join(configs)
+
+        # write
+        with open(path_to_conf, 'wb') as f:
+            f.write(configs.encode('utf-8'))
+        xp.success()
+        xp.plain_text(configs)
+
+        return path_to_conf
