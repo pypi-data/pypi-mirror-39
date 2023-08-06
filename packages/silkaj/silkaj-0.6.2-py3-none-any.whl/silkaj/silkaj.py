65a3587138115a0b136c2a5f378c14a1df4e914d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:19 +0000

    Add package silkaj/0.6.1/silkaj-0.6.2-py3-none-any.whl

---
+++ packages/silkaj/silkaj-0.6.2-py3-none-any.whl/silkaj/silkaj.py
@@ -0,0 +1,162 @@
+from sys import stderr
+from commandlines import Command
+from silkaj.tx import send_transaction
+from silkaj.money import cmd_amount
+from silkaj.cert import send_certification
+from silkaj.commands import currency_info, difficulties, set_network_sort_keys,\
+        network_info, argos_info, list_issuers
+from silkaj.tools import message_exit
+from silkaj.wot import received_sent_certifications, id_pubkey_correspondence
+from silkaj.auth import generate_auth_file
+from silkaj.license import display_license
+from silkaj.constants import SILKAJ_VERSION, G1_SYMBOL, GTEST_SYMBOL, G1_DEFAULT_ENDPOINT, G1_TEST_DEFAULT_ENDPOINT
+
+
+def usage():
+    message_exit("Silkaj: command line client for Duniter currencies\
+    \n\nhelp: -h, --help, --usage \
+    \nnersion: -v, --version \
+    \nabout: display informations about the programm\
+    \n \
+    \nEndpoint:\
+    \nDefault endpoint will reach " + G1_SYMBOL + " currency with `https://" + G1_DEFAULT_ENDPOINT[0] + "` endpoint\
+    \n - `--gtest` to reach " + GTEST_SYMBOL + " currency with `https://" + G1_TEST_DEFAULT_ENDPOINT[0] + "` endpoint\
+    \n - custom endpoint can be specified with `-p` option followed by <domain>:<port>\
+    \n \
+    \nCommands: \
+    \n - info: Display information about currency \
+    \n \
+    \n - amount: Get amount of accounts \
+    \n      pubkeys and/or ids separated with colon: <pubkey:id:pubkey>\
+    \n      - authentication: see below section\
+    \n \
+    \n - tx/transaction: Send transaction\
+    \n     - authentication: see below section\
+    \n     - amount:\
+    \n         --amountUD=<relative value> | --amount=<quantitative value>\
+    \n         [--allSources] \
+    \n     --output=<public key>[:checksum] \
+    \n     [--comment=<comment>] \
+    \n     [--outputBackChange=<public key[:checksum]>] \
+    \n     [-y | --yes], don't ask for prompt confirmation \
+    \n \
+    \n - cert: Send certification\
+    \n     - e.g.: silkaj cert <id> <auth>\
+    \n     - authentication: see below section\
+    \n \
+    \n - net/network: Display current network with many information \
+    \n      [--discover]     Discover all network (could take a while), optional \
+    \n      [-s | --sort]     Sort column names comma-separated (for example \"-s block,diffi\"), optional \
+    \n                       Default sort is block,member,diffi,uid \
+    \n \
+    \n - diffi: list proof-of-work difficulty to generate next block \
+    \n \
+    \n - issuers n: display last n issuers (`0` for current window size) \
+    \n      last issuers are displayed under n <= 30.\
+    \n      To force display last ones, use `--last` option\
+    \n \
+    \n - argos: display currency information formated for Argos or BitBar\
+    \n \
+    \n - generate_auth_file: Generate file to store the seed of the account\
+    \n     - authentication: see below section\
+    \n \
+    \n - id/identities <pubkey> or <identity>: get corresponding identity or pubkey from pubkey or identity.\
+    \n      it could autocomplete the pubkey corresponding to an identity with three or four following characters.\
+    \n \
+    \n - wot <pubkey> or <identity>: display received and sent certifications for an account.\
+    \n \
+    \n - license: display Ğ1 currency license.\
+    \n \
+    \nAuthentication:\
+    \n for amount, transaction, certification, and generate_auth_file commands\
+    \n - Scrypt is the default authentication method with 4096,16,1 as default values\
+    \n    you can specify others values specifying following parameters: -n <N> -r <r> -p <p>\
+    \n - Seed: --auth-seed\
+    \n - File: --auth-file [--file=<path file>]\
+    \n - WIF: --auth-wif")
+
+
+def cli():
+    # ep: endpoint, node's network interface
+    ep, cli_args = dict(), Command()
+    subcmd = ["license", "about", "info", "diffi", "net", "network", "issuers", "argos", "amount", "tx", "transaction", "cert", "generate_auth_file", "id", "identities", "wot"]
+    if cli_args.is_version_request():
+        message_exit(SILKAJ_VERSION)
+    if cli_args.is_help_request() or cli_args.is_usage_request() or cli_args.subcmd not in subcmd:
+        usage()
+    ep["domain"], ep["port"] = G1_TEST_DEFAULT_ENDPOINT if cli_args.contains_switches("gtest") else G1_DEFAULT_ENDPOINT
+    try:
+        ep["domain"], ep["port"] = cli_args.get_definition('p').rsplit(':', 1)
+    except:
+        print("Requested default node: <{}:{}>".format(ep["domain"], ep["port"]), file=stderr)
+    if ep["domain"].startswith('[') and ep["domain"].endswith(']'):
+        ep["domain"] = ep["domain"][1:-1]
+    return ep, cli_args
+
+
+def manage_cmd(ep, cli_args):
+
+    if cli_args.subcmd == "about":
+        about()
+    elif cli_args.subcmd == "info":
+        currency_info(ep)
+
+    elif cli_args.subcmd == "diffi":
+        difficulties(ep)
+
+    elif cli_args.subcmd == "net" or cli_args.subcmd == "network":
+        if cli_args.contains_switches("sort"):
+            set_network_sort_keys(cli_args.get_definition("sort"))
+        if cli_args.contains_switches("s"):
+            set_network_sort_keys(cli_args.get_definition("s"))
+        network_info(ep, cli_args.contains_switches("discover"))
+
+    elif cli_args.subcmd == "issuers" and cli_args.subsubcmd and int(cli_args.subsubcmd) >= 0:
+        list_issuers(ep, int(cli_args.subsubcmd), cli_args.contains_switches('last'))
+
+    elif cli_args.subcmd == "argos":
+        argos_info(ep)
+
+    elif cli_args.subcmd == "amount" and cli_args.subsubcmd:
+        cmd_amount(ep, cli_args)
+
+    elif cli_args.subcmd == "tx" or cli_args.subcmd == "transaction":
+        send_transaction(ep, cli_args)
+
+    elif cli_args.subcmd == "cert":
+        send_certification(ep, cli_args)
+
+    elif cli_args.subcmd == "generate_auth_file":
+        generate_auth_file(cli_args)
+
+    elif cli_args.subcmd == "id" or cli_args.subcmd == "identities":
+        id_pubkey_correspondence(ep, cli_args.subsubcmd)
+
+    elif cli_args.subcmd == "wot":
+        received_sent_certifications(ep, cli_args.subsubcmd)
+
+    elif cli_args.subcmd == "license":
+        display_license()
+
+
+def about():
+    print("\
+\n             @@@@@@@@@@@@@\
+\n         @@@     @         @@@\
+\n      @@@   @@       @@@@@@   @@.           ", SILKAJ_VERSION, "\
+\n     @@  @@@       @@@@@@@@@@@  @@,\
+\n   @@  @@@       &@@@@@@@@@@@@@  @@@         Powerfull and lightweight command line client\
+\n  @@  @@@       @@@@@@@@@#   @@@@ @@(\
+\n  @@ @@@@      @@@@@@@@@      @@@  @@        Built in Python for Duniter’s currencies: Ğ1 and Ğ1-Test\
+\n @@  @@@      @@@@@@@@ @       @@@  @@\
+\n @@  @@@      @@@@@@ @@@@       @@  @@       Authors: Moul, Tortue, Jytou\
+\n @@  @@@@      @@@ @@@@@@@      @@  @@\
+\n  @@ @@@@*       @@@@@@@@@      @# @@        Website: https://silkaj.duniter.org\
+\n  @@  @@@@@    @@@@@@@@@@       @ ,@@\
+\n   @@  @@@@@ @@@@@@@@@@        @ ,@@         Repository: https://git.duniter.org/clients/python/silkaj\
+\n    @@@  @@@@@@@@@@@@        @  @@*\
+\n      @@@  @@@@@@@@        @  @@@            License: GNU AGPLv3\
+\n        @@@@   @@          @@@,\
+\n            @@@@@@@@@@@@@@@\n")
+
+
---
+++ packages/silkaj/silkaj-0.6.2-py3-none-any.whl/silkaj/tx.py
@@ -0,0 +1,342 @@
+from re import compile, search
+import math
+from time import sleep
+import urllib
+
+from tabulate import tabulate
+from silkaj.network_tools import get_request, post_request, HeadBlock
+from silkaj.crypto_tools import (
+    get_publickey_from_seed,
+    sign_document_from_seed,
+    check_public_key,
+)
+from silkaj.tools import message_exit, CurrencySymbol
+from silkaj.auth import auth_method
+from silkaj.wot import get_uid_from_pubkey
+from silkaj.money import get_sources, get_amount_from_pubkey, UDValue
+from silkaj.constants import NO_MATCHING_ID
+
+
+def send_transaction(cli_args):
+    """
+    Main function
+    """
+    tx_amount, output, comment, allSources, outputBackChange = cmd_transaction(cli_args)
+    seed = auth_method(cli_args)
+    issuer_pubkey = get_publickey_from_seed(seed)
+
+    pubkey_amount = get_amount_from_pubkey(issuer_pubkey)[0]
+    outputAddresses = output.split(":")
+    check_transaction_values(
+        comment,
+        outputAddresses,
+        outputBackChange,
+        pubkey_amount < tx_amount * len(outputAddresses),
+        issuer_pubkey,
+    )
+
+    if (
+        cli_args.contains_switches("yes")
+        or cli_args.contains_switches("y")
+        or input(
+            tabulate(
+                transaction_confirmation(
+                    issuer_pubkey, pubkey_amount, tx_amount, outputAddresses, comment
+                ),
+                tablefmt="fancy_grid",
+            )
+            + "\nDo you confirm sending this transaction? [yes/no]: "
+        )
+        == "yes"
+    ):
+        generate_and_send_transaction(
+            seed,
+            issuer_pubkey,
+            tx_amount,
+            outputAddresses,
+            comment,
+            allSources,
+            outputBackChange,
+        )
+
+
+def cmd_transaction(cli_args):
+    """
+    Retrieve values from command line interface
+    """
+    if not (
+        cli_args.contains_definitions("amount")
+        or cli_args.contains_definitions("amountUD")
+    ):
+        message_exit("--amount or --amountUD is not set")
+    if not cli_args.contains_definitions("output"):
+        message_exit("--output is not set")
+
+    if cli_args.contains_definitions("amount"):
+        tx_amount = float(cli_args.get_definition("amount")) * 100
+    if cli_args.contains_definitions("amountUD"):
+        tx_amount = float(cli_args.get_definition("amountUD")) * UDValue().ud_value
+
+    output = cli_args.get_definition("output")
+    comment = (
+        cli_args.get_definition("comment")
+        if cli_args.contains_definitions("comment")
+        else ""
+    )
+    allSources = cli_args.contains_switches("allSources")
+
+    if cli_args.contains_definitions("outputBackChange"):
+        outputBackChange = cli_args.get_definition("outputBackChange")
+    else:
+        outputBackChange = None
+    return tx_amount, output, comment, allSources, outputBackChange
+
+
+def check_transaction_values(
+    comment, outputAddresses, outputBackChange, enough_source, issuer_pubkey
+):
+    checkComment(comment)
+    for outputAddress in outputAddresses:
+        if check_public_key(outputAddress, True) is False:
+            message_exit(outputAddress)
+    if outputBackChange:
+        outputBackChange = check_public_key(outputBackChange, True)
+        if check_public_key(outputBackChange, True) is False:
+            message_exit(outputBackChange)
+    if enough_source:
+        message_exit(
+            issuer_pubkey + " pubkey doesn’t have enough money for this transaction."
+        )
+
+
+def transaction_confirmation(
+    issuer_pubkey, pubkey_amount, tx_amount, outputAddresses, comment
+):
+    """
+    Generate transaction confirmation
+    """
+
+    currency_symbol = CurrencySymbol().symbol
+    tx = list()
+    tx.append(
+        ["pubkey’s amount before tx", str(pubkey_amount / 100) + " " + currency_symbol]
+    )
+    tx.append(
+        [
+            "tx amount (unit)",
+            str(tx_amount / 100 * len(outputAddresses)) + " " + currency_symbol,
+        ]
+    )
+    tx.append(
+        [
+            "tx amount (relative)",
+            str(round(tx_amount / UDValue().ud_value, 4)) + " UD " + currency_symbol,
+        ]
+    )
+    tx.append(
+        [
+            "pubkey’s amount after tx",
+            str(((pubkey_amount - tx_amount * len(outputAddresses)) / 100))
+            + " "
+            + currency_symbol,
+        ]
+    )
+    tx.append(["from (pubkey)", issuer_pubkey])
+    id_from = get_uid_from_pubkey(issuer_pubkey)
+    if id_from is not NO_MATCHING_ID:
+        tx.append(["from (id)", id_from])
+    for outputAddress in outputAddresses:
+        tx.append(["to (pubkey)", outputAddress])
+        id_to = get_uid_from_pubkey(outputAddress)
+        if id_to is not NO_MATCHING_ID:
+            tx.append(["to (id)", id_to])
+    tx.append(["comment", comment])
+    return tx
+
+
+def generate_and_send_transaction(
+    seed,
+    issuers,
+    AmountTransfered,
+    outputAddresses,
+    Comment="",
+    all_input=False,
+    OutputbackChange=None,
+):
+
+    while True:
+        listinput_and_amount = get_list_input_for_transaction(
+            issuers, AmountTransfered * len(outputAddresses), all_input
+        )
+        intermediatetransaction = listinput_and_amount[2]
+
+        if intermediatetransaction:
+            totalAmountInput = listinput_and_amount[1]
+            print("Generate Change Transaction")
+            print("   - From:    " + issuers)
+            print("   - To:      " + issuers)
+            print("   - Amount:  " + str(totalAmountInput / 100))
+            transaction = generate_transaction_document(
+                issuers,
+                totalAmountInput,
+                listinput_and_amount,
+                issuers,
+                "Change operation",
+            )
+            transaction += sign_document_from_seed(transaction, seed) + "\n"
+            post_request(
+                "tx/process", "transaction=" + urllib.parse.quote_plus(transaction)
+            )
+            print("Change Transaction successfully sent.")
+            sleep(1)  # wait 1 second before sending a new transaction
+
+        else:
+            print("Generate Transaction:")
+            print("   - From:    " + issuers)
+            for outputAddress in outputAddresses:
+                print("   - To:      " + outputAddress)
+            if all_input:
+                print("   - Amount:  " + str(listinput_and_amount[1] / 100))
+            else:
+                print(
+                    "   - Amount:  "
+                    + str(AmountTransfered / 100 * len(outputAddresses))
+                )
+            transaction = generate_transaction_document(
+                issuers,
+                AmountTransfered,
+                listinput_and_amount,
+                outputAddresses,
+                Comment,
+                OutputbackChange,
+            )
+            transaction += sign_document_from_seed(transaction, seed) + "\n"
+
+            post_request(
+                "tx/process", "transaction=" + urllib.parse.quote_plus(transaction)
+            )
+            print("Transaction successfully sent.")
+            break
+
+
+def generate_transaction_document(
+    issuers,
+    AmountTransfered,
+    listinput_and_amount,
+    outputAddresses,
+    Comment="",
+    OutputbackChange=None,
+):
+
+    totalAmountTransfered = AmountTransfered * len(outputAddresses)
+
+    listinput = listinput_and_amount[0]
+    totalAmountInput = listinput_and_amount[1]
+
+    head_block = HeadBlock().head_block
+    currency_name = head_block["currency"]
+    blockstamp_current = str(head_block["number"]) + "-" + str(head_block["hash"])
+    curentUnitBase = head_block["unitbase"]
+
+    if not OutputbackChange:
+        OutputbackChange = issuers
+
+    # if it's not a foreign exchange transaction, we remove units after 2 digits after the decimal point.
+    if issuers not in outputAddresses:
+        totalAmountTransfered = (
+            totalAmountTransfered // 10 ** curentUnitBase
+        ) * 10 ** curentUnitBase
+
+    # Generate output
+    ################
+    listoutput = []
+    # Outputs to receiver (if not himself)
+    if isinstance(outputAddresses, str):
+        generate_output(listoutput, curentUnitBase, AmountTransfered, outputAddresses)
+    else:
+        for outputAddress in outputAddresses:
+            generate_output(listoutput, curentUnitBase, AmountTransfered, outputAddress)
+
+    # Outputs to himself
+    rest = totalAmountInput - totalAmountTransfered
+    generate_output(listoutput, curentUnitBase, rest, OutputbackChange)
+
+    # Generate transaction document
+    ##############################
+
+    transaction_document = "Version: 10\n"
+    transaction_document += "Type: Transaction\n"
+    transaction_document += "Currency: " + currency_name + "\n"
+    transaction_document += "Blockstamp: " + blockstamp_current + "\n"
+    transaction_document += "Locktime: 0\n"
+    transaction_document += "Issuers:\n"
+    transaction_document += issuers + "\n"
+    transaction_document += "Inputs:\n"
+    for input in listinput:
+        transaction_document += input + "\n"
+    transaction_document += "Unlocks:\n"
+    for i in range(0, len(listinput)):
+        transaction_document += str(i) + ":SIG(0)\n"
+    transaction_document += "Outputs:\n"
+    for output in listoutput:
+        transaction_document += output + "\n"
+    transaction_document += "Comment: " + Comment + "\n"
+
+    return transaction_document
+
+
+def generate_output(listoutput, unitbase, rest, recipient_address):
+    while rest > 0:
+        outputAmount = truncBase(rest, unitbase)
+        rest -= outputAmount
+        if outputAmount > 0:
+            outputAmount = int(outputAmount / math.pow(10, unitbase))
+            listoutput.append(
+                str(outputAmount)
+                + ":"
+                + str(unitbase)
+                + ":SIG("
+                + recipient_address
+                + ")"
+            )
+        unitbase = unitbase - 1
+
+
+def get_list_input_for_transaction(pubkey, TXamount, allinput=False):
+    listinput, amount = get_sources(pubkey)
+
+    # generate final list source
+    listinputfinal = []
+    totalAmountInput = 0
+    intermediatetransaction = False
+    for input in listinput:
+        listinputfinal.append(input)
+        inputsplit = input.split(":")
+        totalAmountInput += int(inputsplit[0]) * 10 ** int(inputsplit[1])
+        TXamount -= int(inputsplit[0]) * 10 ** int(inputsplit[1])
+        # if more 40 sources, it's an intermediate transaction
+        if len(listinputfinal) >= 40:
+            intermediatetransaction = True
+            break
+        if TXamount <= 0 and not allinput:
+            break
+    if TXamount > 0 and not intermediatetransaction:
+        message_exit("Error: you don't have enough money")
+    return listinputfinal, totalAmountInput, intermediatetransaction
+
+
+def checkComment(Comment):
+    if len(Comment) > 255:
+        message_exit("Error: Comment is too long")
+    regex = compile(
+        "^[0-9a-zA-Z\ \-\_\:\/\;\*\[\]\(\)\?\!\^\+\=\@\&\~\#\{\}\|\\\<\>\%\.]*$"
+    )
+    if not search(regex, Comment):
+        message_exit("Error: the format of the comment is invalid")
+
+
+def truncBase(amount, base):
+    pow = math.pow(10, base)
+    if amount < pow:
+        return 0
+    return math.trunc(amount / pow) * pow
