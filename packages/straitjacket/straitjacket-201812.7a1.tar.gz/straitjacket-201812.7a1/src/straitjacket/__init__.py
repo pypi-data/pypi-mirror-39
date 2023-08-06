bfafde94e5b27119a168f848d72586d3b68839a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:27 +0000

    Add package straitjacket/201812.7a1/straitjacket-201812.7a1.tar.gz

---
+++ packages/straitjacket/straitjacket-201812.7a1.tar.gz/straitjacket-201812.7a1/src/straitjacket/__init__.py
@@ -0,0 +1,9 @@
+# This file is part of the straitjacket project
+# https://gitlab.com/mbarkhau/straitjacket
+#
+# Copyright (c) 2018 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# SPDX-License-Identifier: MIT
+
+__version__ = "v201812.0007-alpha"
+
+from . import sjfmt  # noqa
