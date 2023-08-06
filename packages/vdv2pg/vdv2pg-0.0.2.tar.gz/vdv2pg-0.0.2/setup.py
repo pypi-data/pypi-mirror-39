b1290eb65acd58de6af2c4ae4c9cd91eda730df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:15 +0000

    Add package vdv2pg/0.0.2/vdv2pg-0.0.2.tar.gz

---
+++ packages/vdv2pg/vdv2pg-0.0.2.tar.gz/vdv2pg-0.0.2/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'requirements.txt')) as f:
+    requires = [line.strip() for line in f
+                if len(line) > 1 and not line.startswith('#')]
+
+setup(
+    name='vdv2pg',
+    version='0.0.2',
+    description='Import VDV 451 files into a database',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url="https://github.com/geops/vdv2pg",
+    author='Milan Oberkirch | geOps',
+    author_email='milan.oberkirch@geops.de',
+    keywords='vdv2pg sql database',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+    ],
+    packages=['vdv2pg'],
+    install_requires=requires,
+    include_package_data=True,
+    entry_points="""\
+        [console_scripts]
+        vdv2pg = vdv2pg:main
+    """,
+  )
