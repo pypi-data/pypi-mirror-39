6fb86f839ae86264c61f617880db2c52b347b190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:59 +0000

    Add package x-plan/0.0.1/x_plan-0.0.1-py3-none-any.whl

---
+++ packages/x-plan/x_plan-0.0.1-py3-none-any.whl/xplan/string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = 'string'
+__author__ = 'JieYuan'
+__mtime__ = '18-12-17'
+"""
+
+from xplan import X
+
+xjoin = X(lambda iterable, sep=' ': sep.join(iterable))
