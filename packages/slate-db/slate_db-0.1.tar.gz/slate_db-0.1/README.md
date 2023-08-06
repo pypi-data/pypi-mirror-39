7432aa9f1285466e3ab9029887aab5a5de8a292c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:56 +0000

    Add package slate-db/0.1/slate_db-0.1.tar.gz

---
+++ packages/slate-db/slate_db-0.1.tar.gz/slate_db-0.1/README.md
@@ -0,0 +1,46 @@
+# Slate DB
+A Python wrapper for executing Slate Custom SQL in Python.
+
+## Installation
+You must first install the [ODBC Driver for SQL Server](https://docs.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-2017).
+
+Then install via pip:
+```
+pip install git+https://github.com/jamie-r-davis/slate_db
+```
+
+## Usage
+The `SlateDB.select` method returns a generator which will yield any records that match your `sql` query. By default, each record is returned as a native `pyodbc` row, so columns can be accessed as attributes:
+
+```python
+>>> from slate_db import SlateDB
+>>> db = SlateDB(server='your.server.com',
+                 port=1441,
+                 db='dbname-test',
+                 username='user',
+                 password='secret-password')
+             
+# select all records with last name "Smith"
+>>> sql = """select * from person where last = 'Smith'"""
+>>> for record in db.select(sql):
+>>>     print("{} <{}>".format(record.name, record.email))
+# Smith, John <john.smith@example.com>
+# Smith, Granny <granny.smith@example.com> 
+```
+
+Rows can also be returned as dicts with the `as_dict` parameter:
+
+```python
+>>> list(db.select(sql, as_dict=True))
+# [
+#   {
+#     "name": "Smith, Granny",
+#     "email": "granny.smith@example.com
+#   },
+#   {
+#     "name": "Smith, John",
+#     "email": "john.smith@example.com
+#   },
+#   ...
+# ]
+```
---
+++ packages/slate-db/slate_db-0.1.tar.gz/slate_db-0.1/slate_db/slate_db.py
@@ -0,0 +1,96 @@
+from attrdict import AttrDict
+import pyodbc
+
+
+class SlateDB:
+    """
+    Wrapper for a Slate ODBC connection.
+    """
+
+    def __init__(self, server, port, db, username, password, driver=None):
+        self._driver = driver or pyodbc.drivers()[-1]
+        self._server = server
+        self._port = port
+        self._host = "{}, {}".format(self._server, self._port)
+        self._db = db
+        self._username = username
+        self._password = password
+        self._conn_parms = parms = {'driver': self._driver,
+                                    'server': self._host,
+                                    'uid': self._username,
+                                    'pwd': self._password,
+                                    'database': self._db}
+
+    def select(self,
+               sql: str,
+               binds: tuple = None,
+               max_rows: int or None = None):
+        """
+        Execute the given select sql.
+
+        Parameters
+        ----------
+        sql : str
+            A valid sql statement.
+        binds : tuple
+            A tuple of any bind variables to use in the prepared sql.
+        max_rows : int or None
+            The maximum number of rows to retrieve. If none, all rows
+            will be returned.
+        as_dict : bool
+            If True, results will be returned as dicts. Otherwise they
+            will be returned as pyodbc rows.
+
+        Yields
+        -------
+        results : generator
+            A generator of results as either a native pyodbc Row or as a dict.
+
+        Examples
+        --------
+        >>> db = SlateDB("server", "port", "db", "username", "password")
+        >>> for row in db.select("select * from people"):
+        >>>     print(row.id, row.name)
+        "1234 Kent, Clark"
+        "1544 Wayne, Bruce"
+        >>> db.select("select * where name = ?", binds=("Your Name",))
+        # yield all rows matching "Your Name"
+        >>> db.select("select * where name = ?", binds=("Your Name",), max_rows=10)
+        # yield first 10 rows matching "Your Name"
+        >>> list(db.select("select * from person where last = ?", ("Smith",), as_dict=True))
+        [
+            {"id": "123456", "last": "Smith", "first": "John", ...},
+            {"id": "15484", "last": "Smith", "first": "Bob", ...},
+            ...
+        ]
+        """
+        with pyodbc.connect(**self._conn_parms) as db:
+            with db.cursor() as c:
+                if binds:
+                    c.execute(sql, binds)
+                else:
+                    c.execute(sql)
+                if max_rows:
+                    i = 0
+                    while i <= max_rows:
+                        results = c.fetchmany(1)
+                        if not results:
+                            break
+                        else:
+                            i += 1
+                            yield self._todict(results[0])
+                else:
+                    while True:
+                        results = c.fetchmany(1)
+                        if not results:
+                            break
+                        else:
+                            yield self._todict(results[0])
+
+    @staticmethod
+    def _todict(row):
+        return AttrDict(dict(zip([x[0] for x in row.cursor_description], row)))
+
+    def cursor(self):
+        with pyodbc.connect(**self._conn_parms) as db:
+            return db.cursor()
