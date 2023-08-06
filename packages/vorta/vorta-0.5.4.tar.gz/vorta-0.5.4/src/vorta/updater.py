1367ac0c45a0b2b1a746f63684502bebaf43f96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:41 +0000

    Add package vorta/0.5.4/vorta-0.5.4.tar.gz

---
+++ packages/vorta/vorta-0.5.4.tar.gz/vorta-0.5.4/src/vorta/updater.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from vorta.models import SettingsModel
+
+
+def get_updater():
+    if sys.platform == 'darwin' and getattr(sys, 'frozen', False):
+        # Use sparkle framework on macOS.
+        # Examples: https://programtalk.com/python-examples/objc.loadBundle/
+        import objc
+        bundle_path = os.path.join(os.path.dirname(sys.executable), os.pardir, 'Frameworks', 'Sparkle.framework')
+        objc.loadBundle('Sparkle', globals(), bundle_path)
+        sparkle = SUUpdater.sharedUpdater()  # noqa: F821
+        if SettingsModel.get(key='updates_include_beta').value:
+            sparkle.SharedUpdater.FeedURL = 'https://borgbase.github.io/vorta/appcast-pre.xml'
+
+        if SettingsModel.get(key='check_for_updates').value:
+            sparkle.setAutomaticallyChecksForUpdates_(True)
+            sparkle.checkForUpdatesInBackground()
+
+        sparkle.setAutomaticallyDownloadsUpdates_(False)
+        return sparkle
+
+    else:  # TODO: implement for Linux
+        return None
