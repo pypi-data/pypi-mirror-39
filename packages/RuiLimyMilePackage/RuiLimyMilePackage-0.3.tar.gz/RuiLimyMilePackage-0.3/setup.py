0b8ef2386b8628a37f268eefa6c6ba95302a0e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:00 +0000

    Add package RuiLimyMilePackage/0.3/RuiLimyMilePackage-0.3.tar.gz

---
+++ packages/RuiLimyMilePackage/RuiLimyMilePackage-0.3.tar.gz/RuiLimyMilePackage-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RuiLimyMilePackage',
+    version='0.3',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A test python package',
+	url='https://github.com/lirui315/RuiLimyMileConverter.git',
+    author='Rui li',
+    author_email='ruiliii0315@gmail.com'
+)
