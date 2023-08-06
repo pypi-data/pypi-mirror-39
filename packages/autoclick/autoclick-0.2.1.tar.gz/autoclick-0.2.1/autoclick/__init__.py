72e663a2a7bb8f1f8167c485301ae4a9ba251a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:20 +0000

    Add package autoclick/0.2.1/autoclick-0.2.1.tar.gz

---
+++ packages/autoclick/autoclick-0.2.1.tar.gz/autoclick-0.2.1/autoclick/__init__.py
@@ -0,0 +1,5 @@
+from autoclick.core import (
+    SignatureError, ValidationError, ParameterCollisionError, TypeCollisionError,
+    conversion, create_composite, composite_type, composite_factory, validation,
+    command, group
+)
