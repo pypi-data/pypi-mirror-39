6f82fcf752b3bd93d3d420fe298ee8c921fec41b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:52 +0000

    Add package python-ripple-lib/1.0.5/python-ripple-lib-1.0.5.tar.gz

---
+++ packages/python-ripple-lib/python-ripple-lib-1.0.5.tar.gz/python-ripple-lib-1.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+version = '1.0.5'
+
+setup(
+    name='python-ripple-lib',
+    version=version,
+    packages=['ripple_api'],
+    url='https://github.com/arsenlosenko/python-ripple-rpc',
+    license='MIT',
+    author='Arsen Losenko',
+    author_email='arsenlosenko@gmail.com',
+    description='Ripple JSON-RPC API wrapper for python',
+    long_description=open('README.rst').read(),
+    keywords='ripple rippled json-rpc',
+    classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Utilities',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.5'
+
+    ]
+)
