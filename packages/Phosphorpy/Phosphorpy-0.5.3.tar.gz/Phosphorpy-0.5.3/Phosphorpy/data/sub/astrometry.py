ee6c11b78a8238c35d4dd39e08212d64284987db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:12 +0000

    Add package Phosphorpy/0.5.3/Phosphorpy-0.5.3.tar.gz

---
+++ packages/Phosphorpy/Phosphorpy-0.5.3.tar.gz/Phosphorpy-0.5.3/Phosphorpy/data/sub/astrometry.py
@@ -0,0 +1,17 @@
+from Phosphorpy.external.vizier import Gaia
+from .table import DataTable
+
+
+class AstrometryTable(DataTable):
+
+    def __init__(self, mask):
+        DataTable.__init__(self, mask)
+
+    @staticmethod
+    def load_to_dataset(ds):
+        g = Gaia()
+        gaia = g.query(ds.coordinates, 'ra', 'dec', use_xmatch=True, blank=True)
+        astronomy = AstrometryTable(ds.mask)
+        astronomy._data = gaia[['ra', 'ra_error', 'dec', 'dec_error',
+                                'parallax', 'parallax_error']]
+        return astronomy
