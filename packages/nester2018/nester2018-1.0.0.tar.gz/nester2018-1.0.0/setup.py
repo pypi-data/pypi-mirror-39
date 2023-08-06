bc85af02e641ac5154e73c039089e9f03d27abd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:25 +0000

    Add package nester2018/1.0.0/nester2018-1.0.0.tar.gz

---
+++ packages/nester2018/nester2018-1.0.0.tar.gz/nester2018-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'nester2018',
+    version         = '1.0.0',
+    py_modules      = ['nester2018'],
+    author          = 'josery',
+    author_email    = 'admin@chenxu.info',
+    url             = 'http://chenxu.info',
+    description     = 'A simple printer of nested lists', 
+)
