1f13171375497676f69e6bee9f355ed42aa0a7ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:04 +0000

    Add package djangorestframework-include-mixin/0.2/djangorestframework-include-mixin-0.2.tar.gz

---
+++ packages/djangorestframework-include-mixin/djangorestframework-include-mixin-0.2.tar.gz/djangorestframework-include-mixin-0.2/rest_framework_include_mixin/__init__.py
@@ -0,0 +1,15 @@
+from .mixins import SerializerIncludeMixin, QueryOptimizerMixin
+
+
+__title__ = 'Optimized includable serializer fields'
+__version__ = '0.2'
+__author__ = 'Artem Fedotov'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 Artem Fedotov'
+__all__ = [
+    'SerializerIncludeMixin',
+    'QueryOptimizerMixin',
+]
+
+
+VERSION = __version__
---
+++ packages/djangorestframework-include-mixin/djangorestframework-include-mixin-0.2.tar.gz/djangorestframework-include-mixin-0.2/rest_framework_include_mixin/mixins.py
@@ -0,0 +1,84 @@
+# Rest Framework
+from rest_framework.exceptions import ValidationError
+
+
+class IncludeMixin:
+    @staticmethod
+    def include_fields(request):
+        if request is None:
+            return []
+        return request.GET.getlist('include[]')
+
+
+class SerializerIncludeMixin(IncludeMixin):
+    @property
+    def fields(self):
+        """Add requested extra fields."""
+        fields = super().fields
+
+        is_view_serializer = (
+            self.root == self
+            or (
+                    self.parent == self.root
+                    and getattr(self.parent, 'many', False)
+            )
+        )
+        if not is_view_serializer:
+            return fields
+
+        try:
+            request = self.context['request']
+            extra_objects = self.extra_objects
+        except (AttributeError, LookupError):
+            return fields
+
+        include_fields = self.include_fields(request)
+        if not include_fields:
+            return fields
+
+        existing_fields = list(fields.keys())
+
+        for included_field in existing_fields:
+            real_field = included_field
+            included_field = real_field
+
+            if included_field.endswith('_id'):
+                included_field = included_field[:-3]
+
+            if included_field in include_fields:
+                del fields[real_field]
+
+        for included_field in include_fields:
+            try:
+                fields[included_field] = extra_objects[included_field]
+            except KeyError:
+                raise ValidationError('Wrong include: "%s".' % included_field)
+
+        return fields
+
+
+class QueryOptimizerMixin(IncludeMixin):
+    def get_queryset(self):
+        """Optimized DB query."""
+        queryset = super().get_queryset()
+        include = self.include_fields(self.request)
+
+        try:
+            select_related = self.select_related
+        except AttributeError:
+            pass
+        else:
+            for key, select in select_related.items():
+                if key in include:
+                    queryset = queryset.select_related(select)
+
+        try:
+            prefetch_related = self.prefetch_related
+        except AttributeError:
+            pass
+        else:
+            for key, prefetch in prefetch_related.items():
+                if key in include:
+                    queryset = queryset.prefetch_related(prefetch)
+
+        return queryset
