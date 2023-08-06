c3cce80f2d2fd1525dde63367a6f8d1a979f524b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:24 +0000

    Add package pisten/0.2.0/pisten-0.2.0.tar.gz

---
+++ packages/pisten/pisten-0.2.0.tar.gz/pisten-0.2.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setuptools.setup(
+        name='pisten',
+        version='0.2.0',
+        author='David Pratt',
+        author_email='davidpratt512@gmail.com',
+        description='A simple magic packet forwarder',
+        py_modules=['pisten'],
+        license='MIT',
+        long_description=readme,
+        long_description_content_type='text/markdown',
+        keywords='wakeonlan wake-on-lan port-forwarding magic-packet',
+        python_requires='>=3',
+        url='https://github.com/davidpratt512/pisten',
+        packages=setuptools.find_packages(),
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: System Administrators',
+            'Topic :: System :: Networking',
+            'Programming Language :: Python :: 3 :: Only',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent'
+            ],
+        entry_points={
+            'console_scripts': ['pisten = pisten.pisten:main']
+        }
+)
+
