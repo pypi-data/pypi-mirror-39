59780ab7827b6b16b54f04cef7a7da0449c69122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:22 +0000

    Add package DockerBuildManagement/0.0.4/DockerBuildManagement-0.0.4-py2.py3-none-any.whl

---
+++ packages/DockerBuildManagement/DockerBuildManagement-0.0.4-py2.py3-none-any.whl/DockerBuildManagement/BuildSelections.py
@@ -0,0 +1,56 @@
+from DockerBuildSystem import DockerComposeTools
+from SwarmManagement import SwarmTools
+from DockerBuildManagement import BuildTools
+import sys
+import os
+
+BUILD_KEY = 'build'
+
+
+def GetInfoMsg():
+    infoMsg = "Build selections is configured by adding a 'build' property to the .yaml file.\r\n"
+    infoMsg += "The 'build' property is a dictionary of build selections.\r\n"
+    return infoMsg
+
+
+def GetBuildSelections(arguments):
+    return BuildTools.GetProperties(arguments, BUILD_KEY, GetInfoMsg())
+
+
+def BuildSelections(selectionsToBuild, buildSelections):
+    if len(selectionsToBuild) == 0:
+        for buildSelection in buildSelections:
+            BuildSelection(buildSelections[buildSelection])
+    else:
+        for selectionToBuild in selectionsToBuild:
+            if selectionToBuild in buildSelections:
+                BuildSelection(buildSelections[selectionToBuild])
+
+
+def BuildSelection(buildSelection):
+    cwd = BuildTools.TryChangeToDirectoryAndGetCwd(buildSelection)
+    DockerComposeTools.DockerComposeBuild(
+        buildSelection[BuildTools.COMPOSE_FILES_KEY])
+    os.chdir(cwd)
+
+
+def HandleBuildSelections(arguments):
+    if len(arguments) == 0:
+        return
+    if not('-build' in arguments):
+        return
+
+    if '-help' in arguments:
+        print(GetInfoMsg())
+        return
+
+    selectionsToBuild = SwarmTools.GetArgumentValues(arguments, '-build')
+    selectionsToBuild += SwarmTools.GetArgumentValues(arguments, '-b')
+
+    buildSelections = GetBuildSelections(arguments)
+    BuildSelections(selectionsToBuild, buildSelections)
+
+
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    HandleBuildSelections(arguments)
