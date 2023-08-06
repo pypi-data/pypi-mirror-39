ba32da9a4707a98699c6e51483b947c28b6894a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:23 +0000

    Add package artifacts/20181213/artifacts-20181213.tar.gz

---
+++ packages/artifacts/artifacts-20181213.tar.gz/artifacts-20181213/tests/style_test.py
@@ -0,0 +1,28 @@
+"""Enforce code style."""
+
+import subprocess
+import unittest
+
+from artifacts import errors
+
+from tests import test_lib
+
+
+class StyleTest(test_lib.BaseTestCase):
+  """Enforce code style requirements."""
+
+  def testCodeStyle(self):
+    """Check yapf style enforcement runs cleanly."""
+    try:
+      subprocess.check_output(
+          ['yapf', '--diff', '-r', 'artifacts tools', 'artifacts', 'tests'])
+    except subprocess.CalledProcessError as e:
+      if hasattr(e, 'output'):
+        raise errors.CodeStyleError(
+            'Run "yapf -i -r artifacts tools/ artifacts/ tests/" to correct '
+            'these problems: {0}'.format(e.output))
+      raise
+
+
+if __name__ == '__main__':
+  unittest.main()
