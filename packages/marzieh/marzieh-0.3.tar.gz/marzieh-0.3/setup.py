d2c23a1cdd1875764348e9bc508680c8bdc9db90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:59 +0000

    Add package marzieh/0.3/marzieh-0.3.tar.gz

---
+++ packages/marzieh/marzieh-0.3.tar.gz/marzieh-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='marzieh',
+    version='0.3',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A test python package',
+	url='https://github.com/khalad-hasan/myMileConverter',
+    author='Marzi',
+    author_email='rafieenia@gmail.com'
+)
