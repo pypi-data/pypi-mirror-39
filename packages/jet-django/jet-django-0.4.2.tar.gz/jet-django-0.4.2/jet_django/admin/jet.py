9ff9a3e88fe85bc9283e406a0b51eb5dc575ac5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:13 +0000

    Add package jet-django/0.4.2/jet-django-0.4.2.tar.gz

---
+++ packages/jet-django/jet-django-0.4.2.tar.gz/jet-django-0.4.2/jet_django/admin/jet.py
@@ -0,0 +1,28 @@
+from jet_django.admin.model_description import JetAdminModelDescription
+
+
+class JetAdmin(object):
+    models = []
+
+    def register(self, Model, fields=None, actions=list(), hidden=False):
+        self.models.append(JetAdminModelDescription(Model, fields, actions, hidden))
+
+    def register_related_models(self):
+        def model_key(x):
+            return '{}_{}'.format(
+                x['app_label'],
+                x['model']
+            )
+        registered = set(map(lambda x: model_key(x.get_model()), self.models))
+
+        for models_description in self.models:
+            for item in models_description.get_related_models():
+                key = model_key(item['model_info'])
+
+                if key in registered:
+                    continue
+
+                self.register(item['model'], hidden=True)
+                registered.add(key)
+
+jet = JetAdmin()
