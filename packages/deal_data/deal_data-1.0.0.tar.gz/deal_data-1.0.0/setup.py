d5f86017d6825c042b21492ca2b14ff163660737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:02 +0000

    Add package deal_data/1.0.0/deal_data-1.0.0.tar.gz

---
+++ packages/deal_data/deal_data-1.0.0.tar.gz/deal_data-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name="deal_data",
+    version="1.0.0",
+    py_modules=["deal_deta"],
+    author="chejdj",
+    author_email ="yangyang.zhu96@gmail.com",
+    url="https://chejdj.github.io",
+    description="the data deal")
