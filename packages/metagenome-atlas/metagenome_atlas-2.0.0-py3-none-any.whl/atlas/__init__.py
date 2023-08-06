aa3053e0784a32bb5eb511638bd0bbf5af9445dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:15 +0000

    Add package metagenome-atlas/2.0.0/metagenome_atlas-2.0.0-py3-none-any.whl

---
+++ packages/metagenome-atlas/metagenome_atlas-2.0.0-py3-none-any.whl/atlas/__init__.py
@@ -0,0 +1,31 @@
+__version__ = "2.0.0"
+TAX_LEVELS = ["superkingdom", "phylum", "class", "order", "family", "genus", "species"]
+BLAST6 = [
+    "qseqid",
+    "sseqid",
+    "pident",
+    "length",
+    "mismatch",
+    "gapopen",
+    "qstart",
+    "qend",
+    "sstart",
+    "send",
+    "evalue",
+    "bitscore",
+]
+
+EGGNOG_HEADERS= [
+"query_name",
+"seed_eggNOG_ortholog",
+"seed_ortholog_evalue",
+"seed_ortholog_score",
+"predicted_gene_name",
+"GO_terms",
+"KEGG_KO",
+"BiGG_Reactions",
+"Annotation_tax_scope",
+"Matching_OGs",
+"best_OG|evalue|score",
+"categories",
+"eggNOG_HMM_model_annotation"]
