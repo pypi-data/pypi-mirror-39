f56e3ac2869265cab9afd3236a4b5107c50ac312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:43 +0000

    Add package pymatex/0.0.9/pymatex-0.0.9.tar.gz

---
+++ packages/pymatex/pymatex-0.0.9.tar.gz/pymatex-0.0.9/pymatex/node/Division.py
@@ -0,0 +1,14 @@
+from pymatex.listener import MatexASTVisitor
+from pymatex.node.BinaryOperator import BinaryOperator
+
+
+class Division(BinaryOperator):
+
+    def __init__(self, lhs, rhs):
+        super().__init__(lhs, rhs, '/', None)
+
+    def __str__(self):
+        return '\\frac{{{}}}{{{}}}'.format(self.lhs, self.rhs)
+
+    def accept(self, visitor: MatexASTVisitor):
+        return visitor.visit_division(self)
