e4c71f1cd98af60e562c67e1f62effb6ed2f9e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:22 +0000

    Add package prakriya/0.1.0/prakriya-0.1.0.tar.gz

---
+++ packages/prakriya/prakriya-0.1.0.tar.gz/prakriya-0.1.0/prakriya/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for prakriya."""
+
+__author__ = """Dr. Dhaval Patel"""
+__email__ = 'drdhaval2785@gmail.com'
+__version__ = '0.0.7'
+__all__ = ['Prakriya', 'VerbFormGenerator']
+
+
+from .verbforms import Prakriya
+from .generate import VerbFormGenerator
