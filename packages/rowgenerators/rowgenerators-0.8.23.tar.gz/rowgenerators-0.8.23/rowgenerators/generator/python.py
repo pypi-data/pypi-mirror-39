845fd8af2fcbefe928ac55be2d8e79ce1e65a2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:04 +0000

    Add package rowgenerators/0.8.23/rowgenerators-0.8.23.tar.gz

---
+++ packages/rowgenerators/rowgenerators-0.8.23.tar.gz/rowgenerators-0.8.23/rowgenerators/generator/python.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2017 Civic Knowledge. This file is licensed under the terms of the
+# MIT License, included in this distribution as LICENSE.txt
+
+""" """
+
+import sys
+from rowgenerators.exceptions import SourceError, RowGeneratorError
+from rowgenerators.source import Source
+
+class PythonSource(Source):
+    """Generate rows from a callable object. Takes kwargs from the spec to pass into the program. """
+
+    def __init__(self, ref, cache=None, working_dir=None, env=None, **kwargs):
+
+        super().__init__(ref, cache, working_dir, **kwargs)
+
+        if not working_dir in sys.path:
+
+            sys.path.append(working_dir)
+
+        self.env = env or {}
+
+        self.kwargs = kwargs
+
+
+    def __iter__(self):
+        try:
+            yield from self.ref(env=self.env, cache=self.cache, **self.kwargs)
+        except TypeError as e:
+            # call to Python Url has wrong signature
+
+            raise RowGeneratorError(str(e))
