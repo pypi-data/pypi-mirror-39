1773d92e616e4d71655a4f6d647cd7c9b407b034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:22 +0000

    Add package easyTCP/0.5.5/easyTCP-0.5.5.tar.gz

---
+++ packages/easyTCP/easyTCP-0.5.5.tar.gz/easyTCP-0.5.5/setup.py
@@ -0,0 +1,34 @@
+import re, io
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+	name="easyTCP",
+	version='0.5.5',
+	url='https://github.com/dsal3389/easyTCP',
+	download_url='https://github.com/dsal3389/easyTCP.git',
+	license='Apache 2.0',
+	author="Daniel Sonbolian",
+	author_email='dsal3389@gmail.com',
+	description="easy&fast way to create asyncronus server&clients",
+	python_requires='>=3.4.*',
+	install_requires=[
+		'cryptography==2.4.2'
+	],
+	packages=[
+                'easyTCP',
+                'easyTCP.SERVER',
+                'easyTCP.CLIENT'
+            ],
+	long_description=long_description,
+	classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+	]
+)
