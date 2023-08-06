33e4a55d0569ad58ecf0e93dfe6a738ce75c7e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:25 +0000

    Add package infi.logging/0.4.9/infi.logging-0.4.9-py2-none-any.whl

---
+++ packages/infi.logging/infi.logging-0.4.9-py2-none-any.whl/infi/logging/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.4.9"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.4.9)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '401a5497ca6fadf8873c3884ee9d20f3f61199b7'
+__git_head_subject__ = u'Finished Release v0.4.9'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-12-20 18:04:17'
