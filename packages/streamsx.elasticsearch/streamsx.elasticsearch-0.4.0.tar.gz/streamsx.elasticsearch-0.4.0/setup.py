eca6be09f5c10e876f73fd757e4eeded72b413b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:36 +0000

    Add package streamsx.elasticsearch/0.4.0/streamsx.elasticsearch-0.4.0.tar.gz

---
+++ packages/streamsx.elasticsearch/streamsx.elasticsearch-0.4.0.tar.gz/streamsx.elasticsearch-0.4.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(
+  name = 'streamsx.elasticsearch',
+  packages = ['streamsx.elasticsearch'],
+  include_package_data=True,
+  version = '0.4.0',
+  description = 'IBM Streams Elasticsearch integration',
+  long_description = open('DESC.txt').read(),
+  author = 'IBM Streams @ github.com',
+  author_email = 'hegermar@de.ibm.com',
+  license='Apache License - Version 2.0',
+  url = 'https://github.com/IBMStreams/streamsx.elasticsearch',
+  keywords = ['streams', 'ibmstreams', 'streaming', 'analytics', 'streaming-analytics', 'elasticsearch'],
+  classifiers = [
+    'Development Status :: 1 - Planning',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+  install_requires=['streamsx'],
+  
+  test_suite='nose.collector',
+  tests_require=['nose']
+)
