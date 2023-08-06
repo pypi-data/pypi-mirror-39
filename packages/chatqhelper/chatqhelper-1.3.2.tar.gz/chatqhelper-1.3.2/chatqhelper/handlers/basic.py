e8250eb426b84f6c2d564ca842b189cd951a2c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:13 +0000

    Add package chatqhelper/1.3.2/chatqhelper-1.3.2.tar.gz

---
+++ packages/chatqhelper/chatqhelper-1.3.2.tar.gz/chatqhelper-1.3.2/chatqhelper/handlers/basic.py
@@ -0,0 +1,61 @@
+from chatqhelper.mqtt import MqttClient
+from chatqhelper import scheduler
+from chatqhelper import debug
+import time
+
+
+logger = debug.logger(__name__)
+
+
+class MqttHandler:
+    def __init__(self, interval=1):
+        self._mqtt_client = None
+        self._interval = interval
+
+    @property
+    def mqtt(self):
+        return self._mqtt_client
+
+    def start(self):
+        self._mqtt_client = MqttClient.create(self._on_mqtt_connect, handle_in_thread=True)
+        self._mqtt_client.loop_start()
+        self.loop()
+
+    def loop(self):
+        while True:
+            time.sleep(self._interval)
+            self._loop_imp()
+
+    def _loop_imp(self):
+        pass
+
+    def stop(self):
+        self._mqtt_client.disconnect()
+        self._mqtt_client = None
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def _on_mqtt_connect(self, client, userdata, flags, rc):
+        logger.info('connected to solace with code ' + str(rc))
+
+
+class MqttAndScheduleHandler(MqttHandler):
+    def __init__(self):
+        super().__init__()
+        self._scheduler = None
+
+    @property
+    def scheduler(self):
+        return self._scheduler
+
+    def start(self):
+        self._scheduler = scheduler.UTCScheduler()
+        self._scheduler.loop_start()
+        super().start()
+
+    def stop(self):
+        self._scheduler.loop_stop()
+        self._scheduler = None
+        super().stop()
