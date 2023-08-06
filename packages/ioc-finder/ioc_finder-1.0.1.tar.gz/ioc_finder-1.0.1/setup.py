af8c0d7e9cce12e9c1ba14ed090c7d1f610517a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:56 +0000

    Add package ioc-finder/1.0.1/ioc_finder-1.0.1.tar.gz

---
+++ packages/ioc-finder/ioc_finder-1.0.1.tar.gz/ioc_finder-1.0.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('LICENSE') as license_file:
+    license = license_file.read()
+
+requirements = [
+    'pyparsing'
+]
+
+setup(
+    name='ioc_finder',
+    version='1.0.1',
+    description="Python package for finding and parsing indicators of compromise from text.",
+    long_description=readme,
+    author="Floyd Hightower",
+    author_email='',
+    url='https://github.com/fhightower/ioc-finder',
+    packages=find_packages(exclude=('tests', 'docs')),
+    include_package_data=True,
+    install_requires=requirements,
+    license=license,
+    zip_safe=True,
+    keywords='iocs indicators of compromise parsing finding searching threat intelligence',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests'
+)
