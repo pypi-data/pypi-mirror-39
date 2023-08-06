0aa14eed3adbbdea446ecfd26aa4d0d86dfb2ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:31 +0000

    Add package a2p2/0.1.6/a2p2-0.1.6-py2-none-any.whl

---
+++ packages/a2p2/a2p2-0.1.6-py2-none-any.whl/a2p2/version.py
@@ -0,0 +1,61 @@
+__version__ = "0.1.6"
+
+__release_notes__ = {
+    #"0.1.6": {
+    #    "A2P2": [
+    #        ""
+    #        ],
+    #    "VLTI": [
+    #        ""
+    #        ],
+    #    "CHARA": [
+    #        ""
+    #        ]
+    #    },
+    "0.1.6": {
+        "A2P2": [
+            "Major code reformating - pep8 compliant"
+        ],
+        "VLTI": [
+            "Add PIONIER"
+        ],
+        "CHARA": [
+            ""
+        ]
+    },
+    "0.1.5": {
+        "A2P2": [
+            ""
+        ],
+        "VLTI": [
+            "bugfix for dualfield cases"
+        ],
+        "CHARA": [
+            ""
+        ]
+    },
+    "0.1.4": {
+        "A2P2": [
+            "fix order of returned fluxes in OB.getFluxes()"
+        ]
+    },
+    "0.1.3": {
+        "VLTI": [
+            "fix telescope mode computation"
+        ]
+    },
+    "0.1.2": {
+        "A2P2": [
+            "bugfix for SPLIT polarisation mode detection on GRAVITY",
+            "bugfix that displays warning message during DIT calculation in GRAVITY LR mode",
+            "enhancement of out of bound exception message of DIT calculation method"
+        ]
+    },
+    "0.1.1": {
+        "A2P2": [
+            "muti-faciliy",
+            "multi-VltiInstruments",
+            "json VltiConfig (templates+dit tables)"
+        ]
+    }
+}
