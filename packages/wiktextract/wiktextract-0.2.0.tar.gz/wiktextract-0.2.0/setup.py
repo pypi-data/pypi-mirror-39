4042e533651283f3af8376c57f53287eaf986643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:08 +0000

    Add package wiktextract/0.2.0/wiktextract-0.2.0.tar.gz

---
+++ packages/wiktextract/wiktextract-0.2.0.tar.gz/wiktextract-0.2.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2018 Tatu Ylonen.  See LICENSE and https://ylonen.org
+
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name="wiktextract",
+      version="0.2.0",
+      description="Wiktionary dump file parser and multilingual data extractor",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="Tatu Ylonen",
+      author_email="ylo@clausal.com",
+      url="https://ylonen.org",
+      license="MIT",
+      download_url="https://github.com/tatuylonen/wiktextract",
+      scripts=["wiktwords"],
+      packages=["wiktextract"],
+      install_requires=["lxml", "wikitextparser"],
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: MIT License",
+          "Natural Language :: English",
+          "Natural Language :: Finnish",
+          "Operating System :: OS Independent",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3 :: Only",
+          "Topic :: Text Processing",
+          "Topic :: Text Processing :: Linguistic",
+          ])
