616ff03f742ee1ce171a757fdfaf9750df1264e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:51 +0000

    Add package intelecom/0.0.7/intelecom-0.0.7.tar.gz

---
+++ packages/intelecom/intelecom-0.0.7.tar.gz/intelecom-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='intelecom',
+    version='v0.0.7',
+    author='Joseph G. Serunjogi',
+    author_email='joeseggie@gmail.com',
+    description='Huawei IN interface library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/joeseggie/intelecom',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ),
+)
