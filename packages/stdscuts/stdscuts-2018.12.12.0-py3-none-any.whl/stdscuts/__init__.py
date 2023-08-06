eb896d0a218bd9fc0e73f4f78871d95adc6b3fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:00 +0000

    Add package stdscuts/2018.12.12.0/stdscuts-2018.12.12.0-py3-none-any.whl

---
+++ packages/stdscuts/stdscuts-2018.12.12.0-py3-none-any.whl/stdscuts/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This repo intends to holds lots of "pythonic short cuts", based on self experience, that combine "heuristics" and "tricks" in order to make the Pythonista's life easier. True Pythonists will see lots of dangerous "thin wrappers" of existing functions, let them live.
+"""
+
+__version__ = "2018.12.12.0"
+from .functions import hash_file
