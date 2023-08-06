54faa5b91a86e6964a062f13df85f8c46566b0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:05 +0000

    Add package syncloud-lib/58/syncloud-lib-58.tar.gz

---
+++ packages/syncloud-lib/syncloud-lib-58.tar.gz/syncloud-lib-58/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from os.path import join, dirname
+
+requirements = [
+    'jsonpickle==0.7.1',
+    'requests==2.11.1',
+    'Jinja2==2.9.6',
+    'requests-unixsocket==0.1.5'
+]
+
+version = open(join(dirname(__file__), 'version')).read().strip()
+
+setup(
+    name='syncloud-lib',
+    version=version,
+    install_requires = requirements,
+    description='Syncloud common library',
+    packages=['syncloud_app', 'syncloudlib', 'syncloudlib.integration', 'syncloudlib.application'],
+    py_modules = ['convertible'],
+    scripts = ['bin/syncloud-upload-artifact.sh'],
+    license='GPLv3',
+    author='Syncloud',
+    author_email='syncloud@syncloud.it',
+    url='https://github.com/syncloud/lib')
