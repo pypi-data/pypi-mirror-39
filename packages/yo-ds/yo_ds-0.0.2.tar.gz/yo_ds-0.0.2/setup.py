968cd2735ac5dbde5e1be8ace5a413cef7441113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:25 +0000

    Add package yo-ds/0.0.2/yo_ds-0.0.2.tar.gz

---
+++ packages/yo-ds/yo_ds-0.0.2.tar.gz/yo_ds-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='yo_ds',
+      version='0.0.2',
+      description='Functional-style tools, mostly used for data science',
+      url='http://github.com/okulovsky/yo/',
+      author='Yuri Okulovsky',
+      author_email='yuri.okulovsky@gmail.com',
+      license='GPL v3',
+      packages=['yo'],
+      zip_safe=False)
---
+++ packages/yo-ds/yo_ds-0.0.2.tar.gz/yo_ds-0.0.2/yo/__init__.py
@@ -0,0 +1,11 @@
+from .common import *
+from .fluq import *
+from .fluq_extensions import *
+from .pandas_monkey_patching import *
+from .pandas_extensions import *
+
+class YO:
+    Output = YoOutput()
+    With = YoWith()
+    Proc = YoProc()
+    Plot = YoPlot()
