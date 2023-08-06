43e3092b302194cc8fc5e6cc085617183fb65396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:30 +0000

    Add package py3helpers/0.0.2/py3helpers-0.0.2.tar.gz

---
+++ packages/py3helpers/py3helpers-0.0.2.tar.gz/py3helpers-0.0.2/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Create setup script for pip installation of python_utils"""
+########################################################################
+# File: setup.py
+#  executable: setup.py
+#
+# Author: Andrew Bailey
+# History: 12/09/17 Created
+########################################################################
+
+import sys
+from timeit import default_timer as timer
+from setuptools import setup
+
+
+def main():
+    """Main docstring"""
+    start = timer()
+    setup(
+        name="py3helpers",
+        version='0.0.2',
+        description='Python utility functions',
+        url='https://github.com/adbailey4/python_utils',
+        author='Andrew Bailey',
+        author_email='andbaile@ucsc.com',
+        packages=['py3helpers'],
+        install_requires=['mappy>=2.14',
+                          'biopython>=1.68',
+                          'pysam>=0.15',
+                          'numpy>=1.14.2'],
+        zip_safe=True
+    )
+
+    stop = timer()
+    print("Running Time = {} seconds".format(stop-start), file=sys.stderr)
+
+
+if __name__ == "__main__":
+    main()
+    raise SystemExit
