0896877442c2afe2acb5e938cc4318211aad1f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:47 +0000

    Add package reorientexpress/0.1/reorientexpress-0.1.tar.gz

---
+++ packages/reorientexpress/reorientexpress-0.1.tar.gz/reorientexpress-0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name = 'reorientexpress',
+    version = '0.1',
+    scripts = ['reorientexpress.py'],
+    author = 'Angel Ruiz Reche',
+    author_email = 'angelrure@gmail.com',
+    description = 'Script used to build, test and use models that predict the orientation of cDNA reads',
+    url = 'https://github.com/angelrure/reorientexpress',
+    packages=setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3"
+    ]
+)
