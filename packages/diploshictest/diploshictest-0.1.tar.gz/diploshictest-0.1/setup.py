6b542f609641f1e77504d726edc86254648bd917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:48 +0000

    Add package diploshictest/0.1/diploshictest-0.1.tar.gz

---
+++ packages/diploshictest/diploshictest-0.1.tar.gz/diploshictest-0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='diploshictest',
+      version='0.1',
+      description='DiploS/HIC',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/jgallowa07/diploSHIC',
+      author='Andrew Kern',
+      author_email='',
+      license='MIT',      
+      packages=find_packages(exclude=[]),
+      install_requires=['numpy',
+                        'scipy',
+                        'pandas',
+                        'scikit-allel',
+                        'scikit-learn',
+                        'tensorflow',
+                        'keras'],
+      scripts=['diploshic/diploSHIC'],
+      zip_safe=False,
+      extras_require={
+          'dev': [],
+      },
+      setup_requires=[],
+)
+
