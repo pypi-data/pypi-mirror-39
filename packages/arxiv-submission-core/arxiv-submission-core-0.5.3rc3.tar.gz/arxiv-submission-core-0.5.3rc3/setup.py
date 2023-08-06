a6e982e6e32de2b13a00a905c17ab63f36481e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:28 +0000

    Add package arxiv-submission-core/0.5.3rc3/arxiv-submission-core-0.5.3rc3.tar.gz

---
+++ packages/arxiv-submission-core/arxiv-submission-core-0.5.3rc3.tar.gz/arxiv-submission-core-0.5.3rc3/setup.py
@@ -0,0 +1,11 @@
+"""Install arXiv submission core package."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='arxiv-submission-core',
+    version='0.5.3rc3',
+    packages=[f'arxiv.{package}' for package
+              in find_packages('arxiv', exclude=['*test*'])],
+    zip_safe=False
+)
