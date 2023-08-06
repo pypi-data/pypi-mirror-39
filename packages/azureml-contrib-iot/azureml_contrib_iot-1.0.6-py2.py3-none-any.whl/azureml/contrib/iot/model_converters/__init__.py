3d05e7b799173f3f225f21c6ebc3c9252a19edda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:13 +0000

    Add package azureml-contrib-iot/1.0.6/azureml_contrib_iot-1.0.6-py2.py3-none-any.whl

---
+++ packages/azureml-contrib-iot/azureml_contrib_iot-1.0.6-py2.py3-none-any.whl/azureml/contrib/iot/model_converters/__init__.py
@@ -0,0 +1,11 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+"""
+Model flavor converters
+"""
+from .snpe_converter import SnpeConverter
+
+__all__ = [
+    "SnpeConverter",
+]
