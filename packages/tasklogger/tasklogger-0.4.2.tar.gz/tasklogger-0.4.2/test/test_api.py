73f4c4adb35a89ad930a0107b3a213b10ab4ded6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:22 +0000

    Add package tasklogger/0.4.2/tasklogger-0.4.2.tar.gz

---
+++ packages/tasklogger/tasklogger-0.4.2.tar.gz/tasklogger-0.4.2/test/test_api.py
@@ -0,0 +1,55 @@
+import tasklogger
+import time
+import logging
+import numpy as np
+import sys
+import platform
+
+
+def test_get_logger():
+    logger = tasklogger.get_tasklogger()
+    logger2 = tasklogger.get_tasklogger()
+    assert logger is logger2
+    logger2 = tasklogger.get_tasklogger("test")
+    assert logger is not logger2
+
+
+def test_tasks():
+    logger = tasklogger.log_start("test")
+    assert time.time() - logger.tasks['test'] < 0.01
+    time.sleep(logger.min_runtime)
+    tasklogger.log_complete("test")
+    assert 'test' not in logger.tasks
+
+
+def test_log():
+    tasklogger.log_debug('debug')
+    tasklogger.log_info('info')
+    tasklogger.log_warning('warning')
+    tasklogger.log_error('error')
+    tasklogger.log_critical('critical')
+
+
+def test_level():
+    logger = tasklogger.set_level(2)
+    assert logger.level == logging.DEBUG
+    assert logger.logger.level == logging.DEBUG
+
+
+def test_indent():
+    logger = tasklogger.set_indent(0)
+    assert logger.indent == 0
+
+
+def test_timer():
+    logger = tasklogger.set_timer("wall")
+    assert logger.timer == time.time
+    timer = lambda: 10
+    logger = tasklogger.set_timer(timer)
+    assert logger.timer == timer
+    if sys.version[0] == '2' and platform.system() == 'Windows':
+        np.testing.assert_raises(RuntimeError, logger.set_timer,
+                                 "cpu")
+    else:
+        logger = tasklogger.set_timer("cpu")
+        assert logger.timer == time.process_time
