013c7bf6cccb9008f10a630c6b49e01daac6d8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:11 +0000

    Add package regli/0.0.3/regli-0.0.3.tar.gz

---
+++ packages/regli/regli-0.0.3.tar.gz/regli-0.0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='regli',
+    version='0.0.3',
+    author='Bo Zhang',
+    author_email='bozhang@nao.cas.cn',
+    description='REgular Grid Linear Interpolator.',  # short description
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/hypergravity/regli',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Topic :: Scientific/Engineering :: Physics"],
+    package_dir={'regli': 'regli'},
+    include_package_data=True,
+    requires=['numpy', 'scipy']
+)
