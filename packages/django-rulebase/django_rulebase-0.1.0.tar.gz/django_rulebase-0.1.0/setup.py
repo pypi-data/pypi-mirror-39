d62c088b5e06c6d5eae873a3bafc91ead34f380d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:53 +0000

    Add package django-rulebase/0.1.0/django_rulebase-0.1.0.tar.gz

---
+++ packages/django-rulebase/django_rulebase-0.1.0.tar.gz/django_rulebase-0.1.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django_rulebase",
+    version="0.1.0",
+    author="Mojtaba Asadi",
+    author_email="m.asadi.al@outlook.com",
+    description="Django rule base validation , inspired by laravel Request",
+    long_description=long_description,
+    install_requires=[
+        "dateparser",
+        "dnspython",
+        "pytz","django"
+    ],
+    long_description_content_type="text/markdown",
+    url="https://github.com/mojtabaasadi/django-rulebase",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Framework :: Django",
+        "Operating System :: OS Independent",
+    ],
+)
