12e8379bbfdc32bd436a542d6d0a1c5953d87fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:02 +0000

    Add package kalman_detector/0.0.2/kalman_detector-0.0.2.tar.gz

---
+++ packages/kalman_detector/kalman_detector-0.0.2.tar.gz/kalman_detector-0.0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='kalman_detector',
+      version='0.0.2',
+      description='Detect radio bursts with variying intensity',
+      author='Barak Zackay',
+      author_email='bzackay@gmail.com',
+      url='https://bitbucket.org/bzackay/kalman_detector',
+      packages=['kalman_detector'],
+      requires=['numpy', 'scipy', 'numba', 'matplotlib'])
