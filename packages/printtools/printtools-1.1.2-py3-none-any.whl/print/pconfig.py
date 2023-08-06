cc4dab9c1ef463041b5ac27c50f94134f1b834f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:02 +0000

    Add package printtools/1.1.2/printtools-1.1.2-py3-none-any.whl

---
+++ packages/printtools/printtools-1.1.2-py3-none-any.whl/print/pconfig.py
@@ -0,0 +1,11 @@
+
+"""Print Configuration
+
+Attributes
+----------
+LOG_FILE : str
+    If not None, then all print output is saved to this file. Colors are not
+    rendered.
+"""
+
+LOG_FILE = None
