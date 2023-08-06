6f26ee981000e4694653d6f170096e9171ac7bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:15 +0000

    Add package rest-framework-apicontrol/0.7.6/rest_framework_apicontrol-0.7.6.tar.gz

---
+++ packages/rest-framework-apicontrol/rest_framework_apicontrol-0.7.6.tar.gz/rest_framework_apicontrol-0.7.6/setup.py
@@ -0,0 +1,32 @@
+# from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name='rest_framework_apicontrol',
+    packages=['rest_framework_apicontrol'],
+    include_package_data=True,
+    version='0.7.6',
+    description='Django Rest Framework Library to control Client Apps over APIs',
+    author='Marcelo Cueto',
+    author_email='cueto@live.cl',
+    url='https://github.com/mcueto/djangorestframework-apicontrol',
+    download_url='https://github.com/mcueto/djangorestframework-auth0/tarball/0.7.6',
+    keywords=['api', 'control', 'rest framework', 'django'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    install_requires=[
+        'django>=2.0.0',
+        'djangorestframework>=3.7.0'
+        'psycopg2>=2.7.3'
+    ],
+)
