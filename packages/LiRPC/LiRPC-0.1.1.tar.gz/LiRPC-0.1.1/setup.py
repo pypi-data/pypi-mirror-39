8caaef3f9fae7ac6e4a491307feaf5d981ccc76d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:12 +0000

    Add package LiRPC/0.1.1/LiRPC-0.1.1.tar.gz

---
+++ packages/LiRPC/LiRPC-0.1.1.tar.gz/LiRPC-0.1.1/setup.py
@@ -0,0 +1,14 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+        name= 'LiRPC',
+        version= '0.1.1',
+        description=('liziyi0914\'s RPC module'),
+        author= 'liziyi0914',
+        author_email= 'liziyi0914@gmail.com',
+        license= 'GPL License',
+        packages= find_packages(),
+        platforms= ["all"],
+        url= 'https://github.com/liziyi0914/LiRPC')
