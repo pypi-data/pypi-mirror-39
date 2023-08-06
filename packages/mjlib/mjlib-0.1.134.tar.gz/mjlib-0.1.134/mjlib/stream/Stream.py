6e0ac9adf27b22930295815ac66925737c96d91d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:58 +0000

    Add package mjlib/0.1.134/mjlib-0.1.134.tar.gz

---
+++ packages/mjlib/mjlib-0.1.134.tar.gz/mjlib-0.1.134/mjlib/stream/Stream.py
@@ -0,0 +1,54 @@
+from abc import abstractmethod, ABC
+
+streams = []
+
+
+class Stream(ABC):
+    def __init__(self):
+        self._listeners = []
+        self.open = True
+        global streams
+        streams.append(self)
+
+    def run(self):
+        print('running ' + str(self))
+        while self.open:
+            sample = self._get_sample()
+            if sample is None:
+                break
+            self._send(sample)
+
+    def close(self):
+        self.open = False
+
+    @abstractmethod
+    def _get_sample(self):
+        pass
+
+    def connect(self, output):
+        if output not in self._listeners:
+            self._listeners.append(output)
+
+    def disconnect(self, output):
+        self._listeners -= output
+
+    def __iadd__(self, other):
+        self.connect(other)
+        return self
+
+    def __isub__(self, other):
+        self.disconnect(other)
+        return self
+
+    def _send(self, sample):
+        for l in self._listeners:
+            l(sample)
+
+    @staticmethod
+    def shutdown():
+        _close_all_streams()
+
+
+def _close_all_streams():
+    for s in streams:
+        s.close()
