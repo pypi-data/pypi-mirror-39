3c96b31fe6fd0d2a4b9023190a345cc434bcd486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:14 +0000

    Add package fmga/2.8.0/fmga-2.8.0-py3-none-any.whl

---
+++ packages/fmga/fmga-2.8.0-py3-none-any.whl/fmga/__init__.py
@@ -0,0 +1,6 @@
+from fmga.function_maximize import Population
+from fmga.function_maximize import Point
+from fmga.function_maximize import maximize
+from fmga.function_maximize import unpack
+
+name = "fmga"
