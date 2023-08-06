b3f83848863e52505e110043fe7b07a0dcff1701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:15 +0000

    Add package pydygp/0.2.2.1/pydygp-0.2.2.1-py3-none-any.whl

---
+++ packages/pydygp/pydygp-0.2.2.1-py3-none-any.whl/pydygp/linlatentforcemodels/__init__.py
@@ -0,0 +1,29 @@
+"""
+===============================================================
+Linear Latent Force Models (:mod:`pydygp.linlatentforcemodels`)
+===============================================================
+
+.. currentmodule:: pydygp.linlatentforcemodels
+
+Linear ordinary differential equations driven by smooth Gaussian processes.
+
+
+Multiplicative Forces
+=====================
+
+.. autosummary::
+   :toctree:
+
+   BaseMLFM     -- Base class for the MLFM.
+   MLFMAdapGrad -- MLFM using adaptive gradient matching.
+   GibbsMLFMAdapGrad -- Gibbs sampling for MLFM using adaptive gradient matching.
+   MLFMSuccApprox -- MLFM using successive approximations.
+
+"""
+from .mlfm import BaseMLFM
+from .lfmorder2 import LFMorder2, LFMorder2Kernel
+from .mlfmadapgrad import MLFMAdapGrad, GibbsMLFMAdapGrad, VarMLFMAdapGrad
+from .mlfmsuccapprox import MLFMSuccApprox, VarMLFMSuccApprox
+from .mlfmsamoe import MLFMSA
+from .mlfmsamoe2 import MLFMSAMix
+from .mlfmmixsa import MLFMMixSA
