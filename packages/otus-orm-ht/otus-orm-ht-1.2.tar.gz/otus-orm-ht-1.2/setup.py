6855b47b1e2010f9df858b16db8f9dd8f6e518a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:30 +0000

    Add package otus-orm-ht/1.2/otus-orm-ht-1.2.tar.gz

---
+++ packages/otus-orm-ht/otus-orm-ht-1.2.tar.gz/otus-orm-ht-1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='otus-orm-ht',
+      version='1.2',
+      description='ORM with basic functions',
+      long_description='ORM with basic functions like INSERT, UPDATE, etc.',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.6'
+      ],
+      keywords='ORM OTUS',
+      url='https://github.com/MyHardWay/Otus_Homework/tree/master/homework_5',
+      author='MyHardWay',
+      author_email='',
+      packages=find_packages(),
+      install_requires=[
+          'markdown',
+          'pysqlite3 '
+      ],
+      include_package_data=True,
+      zip_safe=False)
---
+++ packages/otus-orm-ht/otus-orm-ht-1.2.tar.gz/otus-orm-ht-1.2/src/example.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+from models import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = ('INTEGER', 'not null')
+    username = ('INTEGER', '')
+
+
+class Posts(Base):
+    __tablename__ = 'posts'
+
+    id = ('INTEGER', 'not null')
+    postname = ('INTEGER', '')
+    user_id = ('INTEGER', 'not null')
+
+    __relationships__ = (('user_id', 'users', 'id'),)
+
+
+if __name__ == '__main__':
+    conn = sqlite3.connect('db')
+
+
+    print(Base(conn=conn).create_table(User))
+    print(Base(conn=conn).create_table(Posts))
+
+    print(Posts(conn=conn, lazy=True, id=1).select())
+    print((User(username=1, conn=conn).update(
+        id=123, username=1)))
+    #print(Base(conn=conn).drop_table())
+    print(User(conn=conn).update(id=123, username=123))
+    print(User(conn=conn).insert(id=1, username=123))
+    print(User(conn=conn).select('id'))
