6388dbafa19c4a67745c51334242309c4b0a6a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:13 +0000

    Add package CheckMethod/1.3.2/CheckMethod-1.3.2.tar.gz

---
+++ packages/CheckMethod/CheckMethod-1.3.2.tar.gz/CheckMethod-1.3.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+setup(
+    name='CheckMethod',
+    version='1.03.02',
+    description=(
+        '飞马主柜台上场文件合规检测'
+    ),
+    long_description=open('README.rst').read(),
+    author='WangZhp',
+    author_email='huoyingkk@163.com',
+    maintainer='WangZhp',
+    maintainer_email='huoyingkk@163.com',
+    license='BSD License',
+    packages=['CheckMethod', 'Files'],
+    platforms=["all"],
+    url='http://www.mirrorcffex.cn',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        're>=2.2.1',
+        'tarfile>=0.9.0',
+        'pandas>=0.22.0',
+        'paramiko>=2.4.2',
+    ],
+)
