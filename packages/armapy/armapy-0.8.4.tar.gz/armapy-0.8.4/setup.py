501832c4992971d9c5d6082d6975215bb9a2a62a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:06 +0000

    Add package armapy/0.8.4/armapy-0.8.4.tar.gz

---
+++ packages/armapy/armapy-0.8.4.tar.gz/armapy-0.8.4/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='armapy',
+    version='0.8.4',
+    packages=['armapy', 'armapy.plot'],
+    url='',
+    license='GPL',
+    author='Patrick Rauer',
+    author_email='j.p.rauer@sron.nl',
+    description='Package to calculate magnitudes from a spectra',
+    requires=['astropy', 'numpy', 'scipy'],
+    package_data={
+          '': ['./alpha_lyr_stis_006.fits',
+               './local/shortcuts.ini']
+      },
+)
