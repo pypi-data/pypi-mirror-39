09e751f6cbaf38b4542d500ba8f82614402ee684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:11 +0000

    Add package infi.iscsiapi/0.2.11/infi.iscsiapi-0.2.11.tar.gz

---
+++ packages/infi.iscsiapi/infi.iscsiapi-0.2.11.tar.gz/infi.iscsiapi-0.2.11/src/infi/iscsiapi/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.11"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.2.11)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '4e82d9181caa65e7e59245a07611f0044ec116dd'
+__git_head_subject__ = u'Finished Release v0.2.11'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-12-20 17:06:11'
