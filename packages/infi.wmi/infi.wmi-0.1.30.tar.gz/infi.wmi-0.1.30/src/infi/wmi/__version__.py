c0ec09966de482e29ebccbf802295c32560a224b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:52 +0000

    Add package infi.wmi/0.1.30/infi.wmi-0.1.30.tar.gz

---
+++ packages/infi.wmi/infi.wmi-0.1.30.tar.gz/infi.wmi-0.1.30/src/infi/wmi/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.30"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.1.30)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'f484a4db075508007937b0f8034add15cafb0fa3'
+__git_head_subject__ = u'Finished Release v0.1.30'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-12-20 16:41:04'
