b85d5cd34d5f3c080fd2079494937fc6fc2e4be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:38 +0000

    Add package finetune/0.5.13/finetune-0.5.13.tar.gz

---
+++ packages/finetune/finetune-0.5.13.tar.gz/finetune-0.5.13/finetune/target_encoders.py
@@ -0,0 +1,99 @@
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, MultiLabelBinarizer
+from abc import ABCMeta
+
+from finetune.utils import flatten
+
+
+class BaseEncoder(metaclass=ABCMeta):
+    @property
+    def target_labels(self):
+        return getattr(self, 'classes_', None)
+
+    @property
+    def target_dim(self):
+        return len(self.target_labels) if self.target_labels is not None else None
+
+
+class RegressionEncoder(BaseEncoder):
+    def __init__(self):
+        self.num_outputs = None
+
+    def fit(self, x):
+        self.fit_transform(x)
+        return self
+
+    def transform(self, x):
+        output = np.array(x)
+        rank = len(output.shape)
+        if rank == 1:
+            return np.expand_dims(output, 1)  # for single output value regression.
+        if rank == 2:
+            return output
+        raise ValueError("Unresolvable shape: {}. Must be able to fit a format [batch, n_outputs]".format(output.shape))
+
+    def fit_transform(self, x):
+        output = self.transform(x)
+        self.num_outputs = output.shape[1]
+        return output
+
+    def inverse_transform(self, y):
+        y = np.array(y)
+        if y.shape[1] == 1:
+            return np.squeeze(y, 1)
+        else:
+            return y
+
+    @property
+    def target_dim(self):
+        return self.num_outputs
+
+    @property
+    def target_labels(self):
+        raise ValueError
+
+
+class OneHotLabelEncoder(LabelEncoder, BaseEncoder):
+
+    def _make_one_hot(self, labels):
+        output = np.zeros([len(labels), len(self.classes_)], dtype=np.float)
+        output[np.arange(len(labels)), labels] = 1
+        return output
+
+    def fit_transform(self, y):
+        labels = super().fit_transform(y)
+        return self._make_one_hot(labels)
+
+    def transform(self, y):
+        labels = super().transform(y)
+        return self._make_one_hot(labels)
+
+
+class SequenceLabelingEncoder(LabelEncoder, BaseEncoder):
+    pass
+
+
+class SequenceMultiLabelingEncoder(MultiLabelBinarizer, BaseEncoder):
+    pass
+
+
+class MultilabelClassificationEncoder(MultiLabelBinarizer, BaseEncoder):
+    pass
+
+
+class IDEncoder(BaseEncoder):
+
+    def __init__(self):
+        self.classes_ = [0]
+
+    def transform(self, x):
+        return x
+
+    def fit(self, x):
+        return x
+
+    def fit_transform(self, x):
+        return x
+
+    def inverse_transform(self, x):
+        return x
