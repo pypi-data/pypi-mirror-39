a09bd6f5f8e7cf626da7e4cb7e6eedd0c730c392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:47 +0000

    Add package prezzibenzina-py/1.1.3/prezzibenzina_py-1.1.3.tar.gz

---
+++ packages/prezzibenzina-py/prezzibenzina_py-1.1.3.tar.gz/prezzibenzina_py-1.1.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="prezzibenzina_py",
+    version="1.1.3",
+    author="Eliseo Martelli",
+    author_email="me@eliseomartelli.it",
+    description="A python package to get Prezzibenzina info",
+    long_description=long_description,
+    url="https://github.com/eliseomartelli/prezzibenzina_py",
+    packages=setuptools.find_packages(),
+    install_requires=list(val.strip() for val in open('requirements.txt')),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/prezzibenzina-py/prezzibenzina_py-1.1.3.tar.gz/prezzibenzina_py-1.1.3/src/prezzibenzina/__init__.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://www.prezzibenzina.it/distributori/{}"
+
+class PrezziBenzinaPy():
+
+    def get_by_id(self, id):
+        gas_list = []
+        r = requests.get(BASE_URL.format(id))
+        soup = BeautifulSoup(r.text, 'html.parser')
+        table = soup.findAll('div', {"class": "st_reports_row"})
+        for row in table[1:]:
+            gas = {
+                'date': row.find('div', {"class": "st_reports_data"}).text,
+                'fuel': row.find('div', {"class": "st_reports_fuel"}).text,
+                'service': row.find('div', {"class": "st_reports_service"}).text,
+                'price': row.find('div', {"class": "st_reports_price"}).text
+            }
+            gas_list.append(gas)
+        return gas_list
