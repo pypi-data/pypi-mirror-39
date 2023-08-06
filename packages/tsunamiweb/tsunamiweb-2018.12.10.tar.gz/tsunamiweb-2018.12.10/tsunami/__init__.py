5edfbb14280e822cf9b9d403eebca0aa0d99927b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:44 +0000

    Add package tsunamiweb/2018.12.10/tsunamiweb-2018.12.10.tar.gz

---
+++ packages/tsunamiweb/tsunamiweb-2018.12.10.tar.gz/tsunamiweb-2018.12.10/tsunami/__init__.py
@@ -0,0 +1,11 @@
+try:
+    from aiohttp import web
+    from aiohttp import test_utils
+except:
+    pass
+
+VERSION = '2018.12.10'
+
+__ALL__ = [
+    '*'
+]
---
+++ packages/tsunamiweb/tsunamiweb-2018.12.10.tar.gz/tsunamiweb-2018.12.10/tsunami/core/management/commands/runtest.py
@@ -0,0 +1,56 @@
+from tsunami.core.management.base import BaseCommand, CommandError
+from tsunami.utils import list_appnames
+from tsunami.conf import settings
+import os
+import sys
+import pkgutil
+import unittest
+import logging
+
+
+class Command(BaseCommand):
+
+    def _get_tests(self, *appnames):
+        if 'all' in appnames:
+            return unittest.defaultTestLoader.discover(
+                'apps/', top_level_dir=settings.ROOT_DIR)
+        else:
+            suite = unittest.suite.TestSuite()
+            for app in appnames:
+                suite.addTests(
+                    unittest.defaultTestLoader.discover(
+                        f'apps/{app}',
+                        top_level_dir=settings.ROOT_DIR))
+            return suite
+
+    def check(self):
+        if not os.path.exists('.tsunami'):
+            raise CommandError('Invalid tsunami project')
+
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument(
+            'modules', metavar='name',
+            nargs='+',
+            help='Name of app',
+            default=['all']
+        )
+        parser.add_argument(
+            '--logging', '-L', metavar='port',
+            default='warning',
+            choices=['debug', 'info', 'warning', 'error'],
+            type=str.lower,
+            help='Logging Level'
+        )
+
+    def execute(self, **options):
+        _logging = options.get('logging')
+
+        logging.basicConfig(
+            level=getattr(logging, _logging.upper()),
+            format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s',
+            datefmt='%y%m%d %H:%M:%S'
+        )
+
+        unittest.TextTestRunner().run(
+            self._get_tests(*options.get('modules')))
