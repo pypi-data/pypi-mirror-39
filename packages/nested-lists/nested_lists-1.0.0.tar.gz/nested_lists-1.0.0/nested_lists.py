1a88d52a9ce7000f724e38d2c6617dfa5682fd8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:32 +0000

    Add package nested-lists/1.0.0/nested_lists-1.0.0.tar.gz

---
+++ packages/nested-lists/nested_lists-1.0.0.tar.gz/nested_lists-1.0.0/nested_lists.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dez 18 10:37 2018
+
+@author: Pedro Moura
+"""
+"""
+This is "nester.py" module, which provides print_lists fuction, which
+prints lists with or without nested lists
+"""
+
+
+
+def print_lists(some_list):
+    """This function requires a positional argument called "some_list",
+    which is a Python list, even with nested lists.
+    As result each item inside the list, and its nested lists, is printed
+    recursively."""
+    for each_item in some_list:
+        if isinstance(each_item,list):
+            print_lists(each_item)
+        else:
+            print(each_item)
+
