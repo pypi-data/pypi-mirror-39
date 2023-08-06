5246100fc50074b84ad0d847a394a7d8a754ef92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:00 +0000

    Add package modelforge/0.9.2/modelforge-0.9.2.tar.gz

---
+++ packages/modelforge/modelforge-0.9.2.tar.gz/modelforge-0.9.2/setup.py
@@ -0,0 +1,59 @@
+from importlib.machinery import SourceFileLoader
+import os
+
+from setuptools import find_packages, setup
+
+basedir = os.path.dirname(__file__)
+modelforge = SourceFileLoader(
+    "modelforge", os.path.join(basedir, "modelforge", "version.py")).load_module()
+with open(os.path.join(basedir, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="modelforge",
+    description="APIs and tools to work with abstract \"models\" - files "
+                "with numpy arrays and metadata. It is possible to publish "
+                "models, list them. There is a built-in cache. Storage has backends.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=".".join(str(x) for x in modelforge.__version__),
+    license="Apache 2.0",
+    author="source{d}",
+    author_email="machine-learning@sourced.tech",
+    url="https://github.com/src-d/modelforge",
+    download_url="https://github.com/src-d/modelforge",
+    packages=find_packages(exclude=("modelforge.tests",)),
+    keywords=["model", "git", "asdf", "gcs", "google cloud storage",
+              "machine learning", "registry"],
+    install_requires=["asdf>=2.3.0,<2.4",
+                      "lz4>=1.0,<3.0",
+                      "numpy>=1.12,<2.0",
+                      "scipy>=1.0,<2.0",
+                      "clint>=0.5.0,<0.6",
+                      "google-cloud-storage>=1.2,<2.0",
+                      "dulwich>=0.19,<1.0",
+                      "jinja2 >=2.0,<3.0",
+                      "humanize>=0.5.0,<0.6",
+                      "python-dateutil>=2.0,<3.0",
+                      "requests >=2.0,<3.0",
+                      "typing;python_version<'3.5'",
+                      "pygtrie>=1.0,<3.0",
+                      "xxhash>=1.0,<2.0"],
+    entry_points={
+        "console_scripts": ["modelforge=modelforge.__main__:main"],
+    },
+    package_data={"": ["LICENSE", "README.md"],
+                  "modelforge": ["templates/*"], },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries"
+    ]
+)
