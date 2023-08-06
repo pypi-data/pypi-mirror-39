ee58d660e23ea5bcb0aafc77896726e848d090fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:05 +0000

    Add package tinycal/0.1.8/tinycal-0.1.8.tar.gz

---
+++ packages/tinycal/tinycal-0.1.8.tar.gz/tinycal-0.1.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='tinycal',
+    version='0.1.8',
+    description='A Python implementation of cal utility',
+    author='Chang-Yen Chih',
+    author_email='michael66230@gmail.com',
+    packages=['tinycal'],
+    entry_points = {
+        'console_scripts': ['tcal=tinycal.tcal:main'],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ],
+)
