bb6545a32aa793364e761b810fcf78fee8e354b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:53 +0000

    Add package vagquery/1.1/vagquery-1.1.tar.gz

---
+++ packages/vagquery/vagquery-1.1.tar.gz/vagquery-1.1/vagquery/__init__.py
@@ -0,0 +1,6 @@
+from .station import Station
+from .station import StationQuery
+
+from .departure import Departure
+from .departure import DepartureQuery
+__all__ = ["Station", "StationQuery", "Departure", "DepartureQuery"]
