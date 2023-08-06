6a9fdba6dfe17b3b4af1d13792b0de5686b8546d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:37 +0000

    Add package awe/0.1/awe-0.1.tar.gz

---
+++ packages/awe/awe-0.1.tar.gz/awe-0.1/pages/resources/client/pages/build/precache-manifest.8934c26f0ce4134737f4515c093c2453.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22d3d44b7c02275b5a5f",
+    "url": "/static/static/js/runtime~main.22d3d44b.js"
+  },
+  {
+    "revision": "1a565b65ff4661a3aa83",
+    "url": "/static/static/js/main.1a565b65.chunk.js"
+  },
+  {
+    "revision": "1def8dbe1fc588b01568",
+    "url": "/static/static/js/1.1def8dbe.chunk.js"
+  },
+  {
+    "revision": "1a565b65ff4661a3aa83",
+    "url": "/static/static/css/main.cbf41ae9.chunk.css"
+  },
+  {
+    "revision": "1def8dbe1fc588b01568",
+    "url": "/static/static/css/1.bee4d011.chunk.css"
+  },
+  {
+    "revision": "3c1a0b93c9bbff16fc6e3a4666f640d3",
+    "url": "/static/index.html"
+  }
+];
