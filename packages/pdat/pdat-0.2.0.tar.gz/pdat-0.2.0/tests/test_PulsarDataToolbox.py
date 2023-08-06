d70f285416a5d8ce434774d240a36f7f13043212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:53 +0000

    Add package pdat/0.2.0/pdat-0.2.0.tar.gz

---
+++ packages/pdat/pdat-0.2.0.tar.gz/pdat-0.2.0/tests/test_PulsarDataToolbox.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `PulsarDataToolbox` package."""
+
+
+import unittest
+
+
+from pdat import pdat
+
+
+
+class TestPulsarDataToolbox(unittest.TestCase):
+    """Tests for `PulsarDataToolbox` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
