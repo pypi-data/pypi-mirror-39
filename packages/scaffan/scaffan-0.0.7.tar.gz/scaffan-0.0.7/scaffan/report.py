18f0c4307a7d1ecc8c71edc8b3ebc143fad44621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:35 +0000

    Add package scaffan/0.0.7/scaffan-0.0.7.tar.gz

---
+++ packages/scaffan/scaffan-0.0.7.tar.gz/scaffan-0.0.7/scaffan/report.py
@@ -0,0 +1,29 @@
+# /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Process lobulus analysis.
+"""
+import logging
+logger = logging.getLogger(__name__)
+import pandas as pd
+import os.path as op
+import sys
+import os
+
+
+class Report():
+    def __init__(self, outputdir):
+        self.outputdir = op.expanduser(outputdir)
+        if not op.exists(self.outputdir):
+            os.makedirs(self.outputdir)
+
+        self.df = pd.DataFrame()
+
+    def add_row(self, data):
+        self.df.append(pd.DataFrame(data), ignore_index=True)
+
+    # def write_table(self, filename):
+
+
+    def add_table(self):
+        pass
