4331877f62f87bee6905108eb168954121323dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:03 +0000

    Add package feature-mining/0.0.7/feature_mining-0.0.7.tar.gz

---
+++ packages/feature-mining/feature_mining-0.0.7.tar.gz/feature_mining-0.0.7/feature_mining/__init__.py
@@ -0,0 +1,5 @@
+name = "feature_mining"
+from .em_base import ExpectationMaximization
+from .em_original import ExpectationMaximizationOriginal
+from .em_vector import ExpectationMaximizationVector
+from .parse_and_model import ParseAndModel
