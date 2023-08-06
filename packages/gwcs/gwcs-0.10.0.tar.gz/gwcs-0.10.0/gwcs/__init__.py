54906633b56d4fcc9c394da590e7695137b2375e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:11 +0000

    Add package gwcs/0.10.0/gwcs-0.10.0.tar.gz

---
+++ packages/gwcs/gwcs-0.10.0.tar.gz/gwcs-0.10.0/gwcs/__init__.py
@@ -0,0 +1,25 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+This is an Astropy affiliated package.
+"""
+
+import sys
+if sys.version_info < (3, 5):
+    raise ImportError("GWCS does not support Python 2.x, 3.0, 3.1, 3.2, 3.3 or 3.4."
+                      "Beginning with GWCS 0.9, Python 3.5 and above is required.")
+
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import * # noqa
+# ----------------------------------------------------------------------------
+
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    from .wcs import *   # noqa
+    from .wcstools import *   # noqa
+    from .coordinate_frames import *  # noqa
+    from .selector import *   # noqa
