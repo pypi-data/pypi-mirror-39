71462671ab288c9e743c81e12bd28846a4ae2ac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:10 +0000

    Add package article-recommender/1.0.4/article_recommender-1.0.4.tar.gz

---
+++ packages/article-recommender/article_recommender-1.0.4.tar.gz/article_recommender-1.0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='article_recommender',
+      version='1.0.4',
+      description='Create a recommendation engine for articles',
+      packages=['article_recommender'],
+      author='Celestin Hermez',
+      author_email='celestinhermez@gmail.com',
+      zip_safe=False)
