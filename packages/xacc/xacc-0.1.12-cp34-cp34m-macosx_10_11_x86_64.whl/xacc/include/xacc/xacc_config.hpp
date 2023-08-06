4f1778da940964e541ff0dbbcc8339bfbb6a31a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:27 +0000

    Add package xacc/0.1.12/xacc-0.1.12-cp34-cp34m-macosx_10_11_x86_64.whl

---
+++ packages/xacc/xacc-0.1.12-cp34-cp34m-macosx_10_11_x86_64.whl/xacc/include/xacc/xacc_config.hpp
@@ -0,0 +1,7 @@
+#ifndef XACC_CONFIG_HPP
+#define XACC_CONFIG_HPP
+
+#define XACC_INSTALL_DIR "/Users/aqw/.jenkins/workspace/xacc-macosx-wheels/xacc/build/lib.macosx-10.11-x86_64-3.4/xacc"
+#define XACC_BUILD_DIR "/Users/aqw/.jenkins/workspace/xacc-macosx-wheels/xacc/tmp_build"
+
+#endif
