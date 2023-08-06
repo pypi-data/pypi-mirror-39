b694c2917e052f9aa339ce91315c493bc7c22369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:29 +0000

    Add package zyzFlask/0.2.0/zyzFlask-0.2.0.tar.gz

---
+++ packages/zyzFlask/zyzFlask-0.2.0.tar.gz/zyzFlask-0.2.0/core/global_settings.py
@@ -0,0 +1,15 @@
+
+
+REQUEST_SUCCESS = 1
+REQUEST_FAIL = -1
+LOGIN_FAIL = -401
+OTHER_LOGIN_FAIL = -402
+
+SYSTEM_CODE_404 = 404
+SYSTEM_CODE_503 = 503
+SYSTEM_ERROR = '很抱歉服务器异常，请您稍后再试'
+
+METHODS = ["GET", "POST", "DELETE", "PUT"]
+DEFAULT_METHODS = ["GET", "POST"]
+
+MOBILE_ORIGIN_URL = ['https://chart.mofanghr.com', 'https://i.mofanghr.com', 'https://m.mofanghr.com', 'https://ad.mofanghr.com', 'http://crm.mofanghr.com', 'http://crm1.mofanghr.com', 'https://vip.mofanghr.com']
---
+++ packages/zyzFlask/zyzFlask-0.2.0.tar.gz/zyzFlask-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+print("===========================================================")
+
+setup(
+    name = 'zyzFlask',
+    version = '0.2.0',
+    packages = ['configs', 'core'],
+    author = 'Asian.zhang',
+    author_email = 'Asian_zhang@163.com',
+    url='',
+    license = "MIT License",
+    description = '基于flask封装的web开发包'
+)
