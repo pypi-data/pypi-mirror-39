ce832943c414196d82d7b0814b1bd5b9b59c7266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:14 +0000

    Add package maximus48/0.3.2/maximus48-0.3.2.tar.gz

---
+++ packages/maximus48/maximus48-0.3.2.tar.gz/maximus48-0.3.2/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+long_description = u"""
+Here will be longer description\
+By now there is a SSIM division for flatfield substraction and some various functions that might be useful
+"""
+
+
+setup(
+    name='maximus48',
+    version='0.3.2',
+    description='Basic package with my tools',
+    long_description=long_description,
+    author='Maxim Polikarpov',
+    author_email='polikarpov.maxim@mail.ru',
+    url='https://github.com/maximka48/maximus48',
+    platforms='OS Independent',
+    license='MIT License',
+    packages=['maximus48'])
