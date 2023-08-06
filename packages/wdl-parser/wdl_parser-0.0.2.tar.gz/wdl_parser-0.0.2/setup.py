feae1eb272e4f1a722990fead1e0914524162a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:03 +0000

    Add package wdl-parser/0.0.2/wdl_parser-0.0.2.tar.gz

---
+++ packages/wdl-parser/wdl_parser-0.0.2.tar.gz/wdl_parser-0.0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    maintainer="TMiguelT",
+    maintainer_email="ttmigueltt@gmail.com",
+    description="Python package, containing Python parsers for the Broad's WDL language",
+    license="GPL",
+    test_suite="test",
+    url="https://github.com/TMiguelT/WdlParserPackaging",
+    name='wdl_parser',
+    packages=['wdl_parser'],
+    version='0.0.2',
+    install_requires=[],
+)
