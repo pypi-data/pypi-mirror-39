72389b42dc44c5236eb42e18ce3c250024577fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:31 +0000

    Add package doepipeline/1.0/doepipeline-1.0.zip

---
+++ packages/doepipeline/doepipeline-1.0.zip/doepipeline-1.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+import os
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='doepipeline',
+    version='1.0',
+    description='Package for optimizing pipelines using DoE.',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering'
+    ],
+    keywords='pipeline doe optimization',
+    url='https://github.com/clicumu/doepipeline',
+    author='Rickard Sjogren',
+    author_email='rickard.sjogren@umu.se',
+    license='MIT',
+    packages=['doepipeline', 'doepipeline.executor'],
+    install_requires=[
+        'pyyaml',
+        'pandas',
+        'pyDOE2'
+    ],
+    include_package_data=True,
+    tests_require=[
+        'mock',
+        'nose'
+    ],
+    scripts=[
+        os.path.join('bin', 'doepipeline')
+    ],
+    zip_safe=False
+)
