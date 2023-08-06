d98f4216b39ce3ec2801eb8912f9fd1f68139574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:41 +0000

    Add package pyout/0.3.0/pyout-0.3.0-py3-none-any.whl

---
+++ packages/pyout/pyout-0.3.0-py3-none-any.whl/pyout/__init__.py
@@ -0,0 +1,11 @@
+"""Terminal styling for tabular data.
+
+Exposes a single entry point, the Tabular class.
+"""
+
+from __future__ import unicode_literals
+
+__version__ = "0.1.0"
+
+from pyout.elements import schema
+from pyout.tabular import Tabular
