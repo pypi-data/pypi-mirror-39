206afb0e0f02224fadc6bd7b6c898eda15e80da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:00 +0000

    Add package ybc-switch/1.0.5/ybc_switch-1.0.5.tar.gz

---
+++ packages/ybc-switch/ybc_switch-1.0.5.tar.gz/ybc_switch-1.0.5/ybc_switch/ybc_switch.py
@@ -0,0 +1,32 @@
+import platform
+import os
+if platform.system() == 'Linux':
+    os.environ['PATH'] = '/usr/bin'
+import ybc_speech as speech
+import sys
+from pydub import AudioSegment
+from ybc_exception import *
+
+
+def analysis(filename):
+    """
+    功能：分析微信语音内容转换成文字。
+    参数 filename: 微信语音文件名。
+    返回：转换成的文字
+    """
+    if not isinstance(filename, str):
+        raise ParameterTypeError(sys._getframe().f_code.co_name, "'filename'")
+    if not filename:
+        raise ParameterValueError(sys._getframe().f_code.co_name, "'filename'")
+
+    try:
+        sound = AudioSegment.from_mp3(filename).set_frame_rate(16000)
+        sound.export('tmp.wav', format = "wav")
+        text = speech.voice2text('tmp.wav')
+        return text
+    except Exception as e:
+        raise InternalError(e, 'ybc_switch')
+
+
+if __name__ == '__main__':
+    print(analysis('1.mp3'))
