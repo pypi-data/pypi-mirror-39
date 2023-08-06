76178d339a163758cf776a066b904faee952b8f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:12 +0000

    Add package reg-bench/0.0.2/reg_bench-0.0.2.tar.gz

---
+++ packages/reg-bench/reg_bench-0.0.2.tar.gz/reg_bench-0.0.2/reg_bench/ode/__init__.py
@@ -0,0 +1,5 @@
+from .not_so_simple_ode import *
+from .simple_ode import all_loaders as simple_ode_loaders
+from .simple_ode import all_ode
+
+all_loaders = {**simple_ode_loaders}
---
+++ packages/reg-bench/reg_bench-0.0.2.tar.gz/reg_bench-0.0.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['reg_bench',
+ 'reg_bench.maps',
+ 'reg_bench.ode',
+ 'reg_bench.symbolic_regression']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.15,<2.0',
+ 'pyodesys>=0.12.4,<0.13.0',
+ 'scikit-learn[alldeps]>=0.20.1,<0.21.0',
+ 'toolz>=0.9.0,<0.10.0']
+
+setup_kwargs = {
+    'name': 'reg-bench',
+    'version': '0.0.2',
+    'description': '',
+    'long_description': 'Regression benchmarks\n---\n\n[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/ambv/black)\n',
+    'author': 'Markus Quade',
+    'author_email': 'info@markusqua.de',
+    'url': 'https://github.com/Ohjeah/regression-benchmarks',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
