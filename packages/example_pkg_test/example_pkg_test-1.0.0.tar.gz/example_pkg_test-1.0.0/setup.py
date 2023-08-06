0ae8d41575fba6ef6f2e580fd1862e5fae85dcbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:43 +0000

    Add package example_pkg_test/1.0.0/example_pkg_test-1.0.0.tar.gz

---
+++ packages/example_pkg_test/example_pkg_test-1.0.0.tar.gz/example_pkg_test-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'example_pkg_test',
+		version = '1.0.0',
+		py_modules = ['test'],
+		author = 'ljc',
+		author_email = '527449244@qq.com',
+		url = 'http://www.baidu.com',
+		description = 'test',
+	)
