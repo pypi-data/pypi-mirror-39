a688bfc6512d3cf7a4f1e7a210b195b73ead8887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:31 +0000

    Add package esst/2018.12.7.1/esst-2018.12.7.1-py36-none-any.whl

---
+++ packages/esst/esst-2018.12.7.1-py36-none-any.whl/esst/discord_bot/catch_exc.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+"""
+Catches connection error in Discord bot
+"""
+
+import asyncio
+
+import aiohttp
+import websockets.exceptions
+from discord import HTTPException
+
+from esst import LOGGER
+from esst.core import CTX
+
+
+def _pass_exception(exc: Exception):
+    LOGGER.error(exc)
+    # LOGGER.exception('Discord bot error')
+    # if CTX.sentry:
+    #     CTX.sentry.captureException(exc)
+
+
+def catch_exc(func):
+    """
+    Decorator to protect discord.client methods
+    """
+
+    async def _wrapper(*args, **kwargs):
+
+        try:
+            return await func(*args, **kwargs)
+        except (websockets.exceptions.InvalidHandshake,
+                websockets.exceptions.InvalidState,
+                websockets.exceptions.PayloadTooBig,
+                websockets.exceptions.WebSocketProtocolError,
+                websockets.exceptions.InvalidURI,
+                HTTPException) as exc:
+            _pass_exception(exc)
+        except (aiohttp.ClientError,
+                ConnectionError,
+                OSError,
+                aiohttp.ClientOSError,
+                aiohttp.ClientResponseError) as exc:
+            await asyncio.sleep(10)
+            while not CTX.wan:
+                await asyncio.sleep(2)
+            _pass_exception(exc)
+
+    return _wrapper
