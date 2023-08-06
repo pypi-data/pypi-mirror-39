c296ffa985a5e33739254390a701230aa014a608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:23 +0000

    Add package infi.app-repo/1.2.1/infi.app_repo-1.2.1.tar.gz

---
+++ packages/infi.app-repo/infi.app_repo-1.2.1.tar.gz/infi.app_repo-1.2.1/src/infi/app_repo/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "1.2.1"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v1.2.1)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '621cc23f207148e93c606b8d0f3d50d9f11b0ebf'
+__git_head_subject__ = u'Finished Release v1.2.1'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-12-20 18:02:17'
