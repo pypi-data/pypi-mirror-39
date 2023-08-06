a683c03bb84e824a9a285fbdb05fb77c9a171f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:17 +0000

    Add package usig-normalizador-amba-P32/1.2.5/usig-normalizador-amba-P32-1.2.5.tar.gz

---
+++ packages/usig-normalizador-amba-P32/usig-normalizador-amba-P32-1.2.5.tar.gz/usig-normalizador-amba-P32-1.2.5/usig_normalizador_amba/__init__.py
@@ -0,0 +1,20 @@
+# coding: UTF-8
+"""
+Normalizador de Direcciones AMBA
+
+El procedimiento de normalización de direcciones tiene por objetivo unificar la escritura de direcciones con respecto a un callejero de referencia.
+El normalizador de Direcciones AMBA toma como referencia los callejeros del Área Metropolitana de Buenos Aires.
+"""
+
+from __future__ import absolute_import
+
+from usig_normalizador_amba.NormalizadorDireccionesAMBA import NormalizadorDireccionesAMBA as ND
+NormalizadorAMBA = ND
+
+__title__ = 'usig-normalizador-amba-P32'
+__description__ = 'Normalizador de Direcciones AMBA'
+__author__ = 'USIG - GCBA'
+__author_email__ = 'Hernán Ogasawara <hogasawara@buenosaires.gob.ar>'
+__version__ = '1.2.5'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright (c) 2016 USIG - GCBA'
