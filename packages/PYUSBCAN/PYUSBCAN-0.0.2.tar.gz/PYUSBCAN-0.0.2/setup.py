6ddc5eabe0895f99ff2b8fe10a3486b5579726bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:38 +0000

    Add package PYUSBCAN/0.0.2/PYUSBCAN-0.0.2.tar.gz

---
+++ packages/PYUSBCAN/PYUSBCAN-0.0.2.tar.gz/PYUSBCAN-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "PYUSBCAN",      #这里是pip项目发布的名称
+    version = "0.0.2",  #版本号，数值大的会优先被pip
+    keywords = ("pip", "USBCAN","ZLG"),
+    description = "A Python tool to control zlg-usbcan",
+
+    url = "https://github.com/fa1247/PYCAN",     #项目相关文件地址，一般是github
+    author = "XY Fan",
+
+    packages=find_packages(),
+    python_requires='>=3',
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.dll', '*.h', '*.lib']
+    },
+    platforms = "win"
+)
