f6117f9b091d0e27c170f766f929966f0728bb69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:07 +0000

    Add package fibheap/0.0.8/fibheap-0.0.8.tar.gz

---
+++ packages/fibheap/fibheap-0.0.8.tar.gz/fibheap-0.0.8/setup.py
@@ -0,0 +1,22 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fibheap",
+    version="0.0.8",
+    author="Quang Tran",
+    author_email="quang.tran@minerva.kgi.edu",
+    description="An implementation of Fibonacci heap",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/quangntran/fibheap.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
