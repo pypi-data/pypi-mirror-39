500341ddfe643fe696bbbbc7fcd53a583c30fa68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:19 +0000

    Add package collective.elasticsearch/2.0.5/collective.elasticsearch-2.0.5.tar.gz

---
+++ packages/collective.elasticsearch/collective.elasticsearch-2.0.5.tar.gz/collective.elasticsearch-2.0.5/README.rst
@@ -0,0 +1,91 @@
+Introduction
+============
+
+See the full documentation on `readthedocs <http://collectiveelasticsearch.readthedocs.io/>`_.
+
+Install elasticsearch
+---------------------
+
+Less than 5 minutes:
+    - Download & install Java
+    - Download & install Elastic Search
+    - bin/elasticsearch
+
+Step by Step for Ubuntu:
+    - add-apt-repository ppa:webupd8team/java
+    - apt-get update
+    - apt-get install git curl oracle-java7-installer
+    - curl -O https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-2.4.3.tar.gz
+    - tar xfvz elasticsearch-2.4.3.tar.gz
+    - cd elasticsearch
+    - bin/elasticsearch &
+
+Does it work?
+    - curl http://localhost:9200/
+    - Do you see the Hudsucker Proxy reference? "You Know, for Search"
+
+Use Elastic Search in Plone:
+    - Add collective.elasticsearch to eggs & re-run buildout
+    - Restart Plone
+    - Goto Control Panel
+    - Add "Eleastic Search" in Add-on Products
+    - Click "Elastic Search" in "Add-on Configuration"
+    - Enable
+    - Click "Convert Catalog"
+    - Click "Rebuild Catalog"
+
+You now have a insanely scalable modern search engine. Now live the life of the Mind!
+
+Overview
+--------
+
+This package aims to index all fields the portal_catalog indexes
+and allows you to delete the `Title`, `Description` and `SearchableText`
+indexes which can provide significant improvement to performance and RAM usage.
+
+Then, ElasticSearch queries are ONLY used when Title, Description and SearchableText
+text are in the query. Otherwise, the plone's default catalog will be used.
+This is because Plone's default catalog is faster on normal queries than using
+ElasticSearch.
+
+
+Compatibility
+-------------
+
+Only tested with Plone 5 with Dexterity types.
+
+It should also work with Plone 4.3 and archetypes.
+
+Deployed with Elasticsearch 2.4.3
+
+State
+-----
+
+Support for all index column types is done EXCEPT for the DateRecurringIndex
+index column type. If you are doing a full text search along with a query that
+contains a DateRecurringIndex column, it will not work.
+
+
+Celery support
+--------------
+
+This package comes with Celery support where all indexing operations will be pushed
+into celery to be run asynchronously.
+
+Please see instructions for collective.celery to see how this works.
+
+
+TODO
+----
+
+- Spellcheck
+- Custom Similarity
+- Faceting
+
+
+Travis
+------
+
+.. image:: https://travis-ci.org/collective/collective.elasticsearch.png
+   :target: https://travis-ci.org/collective/collective.elasticsearch
+   :alt: Travis CI
---
+++ packages/collective.elasticsearch/collective.elasticsearch-2.0.5.tar.gz/collective.elasticsearch-2.0.5/collective/elasticsearch/patches.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+from collective.elasticsearch.interfaces import IReindexActive
+from zope.globalrequest import getRequest
+from zope.interface import alsoProvides
+from collective.elasticsearch import hook
+from collective.elasticsearch.es import ElasticSearchCatalog
+from plone import api
+
+
+def _apply_indexing_active():
+    req = getRequest()
+    if req is not None:
+        alsoProvides(req, IReindexActive)
+
+
+def catalog_object(self, object, uid=None, idxs=[],
+                   update_metadata=1, pghandler=None):
+    es = ElasticSearchCatalog(self)
+    return es.catalog_object(object, uid, idxs, update_metadata, pghandler)
+
+
+def uncatalog_object(self, uid, obj=None, *args, **kwargs):
+    es = ElasticSearchCatalog(self)
+    return es.uncatalog_object(uid, obj, *args, **kwargs)
+
+
+def unrestrictedSearchResults(self, REQUEST=None, **kw):
+    es = ElasticSearchCatalog(self)
+    return es.searchResults(REQUEST, check_perms=False, **kw)
+
+
+def safeSearchResults(self, REQUEST=None, **kw):
+    es = ElasticSearchCatalog(self)
+    return es.searchResults(REQUEST, check_perms=True, **kw)
+
+
+def manage_catalogRebuild(self, *args, **kwargs):
+    """ need to be publishable """
+    _apply_indexing_active()
+    es = ElasticSearchCatalog(self)
+    return es.manage_catalogRebuild(**kwargs)
+
+
+def manage_catalogReindex(self, REQUEST, RESPONSE, URL1):
+    """ need to be publishable """
+    _apply_indexing_active()
+    return self._old_manage_catalogReindex(REQUEST, RESPONSE, URL1)
+
+
+def manage_catalogClear(self, *args, **kwargs):
+    """ need to be publishable """
+    es = ElasticSearchCatalog(self)
+    return es.manage_catalogClear(*args, **kwargs)
+
+
+def unindexObject(self, ob):
+    # same reason as the patch above, we need the actual object passed along
+    # this handle dexterity types
+    path = '/'.join(ob.getPhysicalPath())
+    return self.uncatalog_object(path, obj=ob)
+
+
+def moveObjectsByDelta(self, ids, delta, subset_ids=None,
+                       suppress_events=False):
+    res = self._old_moveObjectsByDelta(ids, delta, subset_ids=subset_ids,
+                                       suppress_events=suppress_events)
+    es = ElasticSearchCatalog(api.portal.get_tool('portal_catalog'))
+    if es.enabled:
+        if subset_ids is None:
+            subset_ids = self.idsInOrder()
+        hook.index_positions(self.context, subset_ids)
+    return res
+
+
+def PloneSite_moveObjectsByDelta(self, ids, delta, subset_ids=None,
+                                 suppress_events=False):
+    res = self._old_moveObjectsByDelta(ids, delta, subset_ids=subset_ids,
+                                       suppress_events=suppress_events)
+    es = ElasticSearchCatalog(api.portal.get_tool('portal_catalog'))
+    if es.enabled:
+        if subset_ids is None:
+            objects = list(self._objects)
+            subset_ids = self.getIdsSubset(objects)
+        hook.index_positions(self, subset_ids)
+    return res
