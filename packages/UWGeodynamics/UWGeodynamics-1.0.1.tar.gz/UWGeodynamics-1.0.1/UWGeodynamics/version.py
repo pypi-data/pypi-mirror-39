a0bbd7dc0acca516ebadaea9b097407467e777b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:49 +0000

    Add package UWGeodynamics/1.0.1/UWGeodynamics-1.0.1.tar.gz

---
+++ packages/UWGeodynamics/UWGeodynamics-1.0.1.tar.gz/UWGeodynamics-1.0.1/UWGeodynamics/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM UWGeodynamics SETUP.PY
+#
+short_version = '1.0.1'
+version = '1.0.1'
+full_version = '1.0.1'
+git_revision = 'd944907d5c796be80cbbb167a14c3aaa0ab8d524'
+release = True
+
+if not release:
+    version = full_version
