9a1ea96fd88cedd1ef45ac9b42e7d533bde5aa69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:45 +0000

    Add package arxivtorm/0.1.1/arxivtorm-0.1.1.tar.gz

---
+++ packages/arxivtorm/arxivtorm-0.1.1.tar.gz/arxivtorm-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path 
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+   name='arxivtorm',
+   version='0.1.1',
+   description='Gets papers from ArXiv and puts them on your remarkable',
+   author='Naren Dasan',
+   author_email='naren@narendasan.com',
+   url='https://github.com/narendasan/ArXivToRm',
+   license="NCSA",
+   long_description=long_description,
+   packages=['arxivtorm'],
+   install_requires=['arxiv'],
+   entry_points = {
+              'console_scripts': [
+                  'arxivtorm = arxivtorm.main:main',                  
+              ],              
+          },
+)
