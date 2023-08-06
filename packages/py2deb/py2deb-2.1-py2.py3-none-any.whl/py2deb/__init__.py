851ddd39646f193a06d26d842793ca789e4cf4fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:59 +0000

    Add package py2deb/2.1/py2deb-2.1-py2.py3-none-any.whl

---
+++ packages/py2deb/py2deb-2.1-py2.py3-none-any.whl/py2deb/__init__.py
@@ -0,0 +1,18 @@
+# py2deb: Python to Debian package converter.
+#
+# Authors:
+#  - Arjan Verwer
+#  - Peter Odding <peter.odding@paylogic.com>
+# Last Change: December 16, 2018
+# URL: https://py2deb.readthedocs.io
+
+"""
+The top level :mod:`py2deb` module contains only a version number.
+
+.. data:: __version__
+
+   The version number of the `pydeb` package (a string).
+"""
+
+# Semi-standard module versioning.
+__version__ = '2.1'
