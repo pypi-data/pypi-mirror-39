c4010991b212070e3af55f3e00b7ece23d2acfab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:38 +0000

    Add package prediction-evaluation/0.0.0/prediction_evaluation-0.0.0.tar.gz

---
+++ packages/prediction-evaluation/prediction_evaluation-0.0.0.tar.gz/prediction_evaluation-0.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+with open("requirements.txt") as f:
+	required = f.read().splitlines()
+
+setup(
+name = "prediction_evaluation",
+version="0.0.0",
+description = "evaluation of prediction of binary, multiclass and regression",
+long_description= readme,
+author = "Xinyu Zhang, Yijia Chen, Xiaochi Ge",
+author_email = "mandy_zhang512@gwu.edu",
+url = "https://github.com/MandyZhangxy/DATS6450-final-project.git",
+license = "GNU",
+classifiers=[
+    "Programming Language :: Python :: 2"
+],
+keywords = "binary classification, regression, multiclass, evaluation",
+install_requires = required
+#package_data = {
+#    "sample_data": ["data/"]
+#}
+)
