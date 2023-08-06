fa4f578750007806a7450d0620b6ead64f48964f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:13 +0000

    Add package rotest-reportportal/2.0.0/rotest_reportportal-2.0.0.tar.gz

---
+++ packages/rotest-reportportal/rotest_reportportal-2.0.0.tar.gz/rotest_reportportal-2.0.0/setup.py
@@ -0,0 +1,32 @@
+"""Setup file for handling packaging and distribution."""
+from setuptools import setup
+
+
+setup(
+    name='rotest_reportportal',
+    version="2.0.0",
+    description="Rotest result handler to send data to a ReportPortal system",
+    long_description=open("README.rst").read(),
+    license="MIT",
+    author="gregoil",
+    author_email="gregoil@walla.co.il",
+    url="https://github.com/gregoil/rotest_reportportal",
+    keywords="testing system reportportal unittest",
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,<3.8.0",
+    install_requires=['rotest',
+                      'attrdict',
+                      'pyyaml',
+                      'reportportal_client'],
+    extras_require={
+        "dev": ["flake8", "pylint",
+                "pytest", "pytest-cov",
+                "mock"]
+    },
+    packages=["rotest_reportportal"],
+    entry_points={
+        "rotest.result_handlers":
+            ["reportportal = "
+             "rotest_reportportal:ReportPortalHandler"]
+    },
+    zip_safe=False
+)
