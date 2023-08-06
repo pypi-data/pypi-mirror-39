5499b4a468c8ca39dcd8e87a209a33994e9f3b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:24 +0000

    Add package autodiffpy/0.0.1/autodiffpy-0.0.1-py2.py3-none-any.whl

---
+++ packages/autodiffpy/autodiffpy-0.0.1-py2.py3-none-any.whl/autodiffpy/autodiff.py
@@ -0,0 +1,357 @@
+import numpy as np
+
+
+class autodiff():
+    def __init__(self,name,val,der=1):
+        self.name = name
+        # set val attribute
+        if isinstance(val, np.ndarray):
+            self.val = val
+        elif isinstance(val, list):
+            self.val = np.asarray(val)
+        else:
+            self.val = np.asarray([val])
+
+        self.der = {name:der}
+
+        self.lparent = None
+        self.rparent = None
+        self.back_der = None
+        self.back_partial_der = None
+
+
+    def __str__(self):
+        return f"value: {self.val}\nderivatives:{self.der}"
+
+    # function for back-propagation
+    def backprop(self, backproplist = None):
+        if backproplist == None:
+            backproplist = {}
+            # self.back_der = np.ones(len(self.val))
+            self.back_der = 1
+        if self.lparent:
+            try:
+                self.lparent.back_der = self.back_der*self.lparent.back_partial_der
+                self.lparent.backprop(backproplist)
+            except:
+                pass
+        if self.rparent:
+            try:
+                self.rparent.back_der = self.back_der*self.rparent.back_partial_der
+                self.rparent.backprop(backproplist)
+            except:
+                pass
+        if self.lparent is None and self.rparent is None:
+            backproplist[self.name] = self.back_der
+
+        return backproplist
+
+
+    def __eq__(self, other):
+        if isinstance(other, autodiff) == False:
+            raise ValueError("Error: only autodiff instances can be compared with another.")
+        return (self.val == other.val) and (self.der == other.der)
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __neg__(self):
+        """Allows unary operation of autodiff instance."""
+        anew = autodiff(self.name, -self.val, self.der)
+        for key in self.der:
+            anew.der[key] = -self.der[key]
+        return anew
+
+
+
+    def __mul__(self, other):
+        """Allows multiplication of another autodiff instance, or multiplication of a constant (integer or float)."""
+
+        if isinstance(other, (int, float, autodiff)) == False:
+            raise ValueError("Error: Only integer, float, or autodiff instances can be multiplied.")
+
+        anew = autodiff(self.name, self.val, self.der)
+
+        #Stores for backpropagation functionality
+        anew.lparent = self
+        anew.rparent = other
+
+        #assuming that other is autodiff instance
+        try:
+            anew.val = self.val*other.val
+            for key in np.unique([key for key in self.der] + [key for key in other.der]):
+                if key not in self.der:
+                    anew.der[key]=self.val*other.der[key]
+                elif key not in other.der:
+                    anew.der[key]=self.der[key]*other.val
+                else:
+                    anew.der[key]=self.der[key]*other.val+other.der[key]*self.val
+
+            #set the back partial derivatives that can be used for backpropagation
+            self.back_partial_der = other.val
+            other.back_partial_der = self.val
+
+        # if 'other' is not autodiff instance
+        except AttributeError:
+            # assuming that 'other' is a valid constant
+            for key in self.der:
+                anew.der[key] = other*self.der[key]
+                anew.val = other*self.val
+                self.back_partial_der = other
+
+        return anew
+
+    __rmul__ = __mul__
+
+    def __truediv__(self,other):
+        '''function for left division'''
+
+        if isinstance(other, (int, float, autodiff)) == False:
+            raise ValueError("Error: Only integer, float, or autodiff instances can be divided.")
+
+        anew = autodiff(self.name, self.val, self.der)
+        
+        anew.lparent = self
+        anew.rparent = other
+        
+        try:
+            anew.val = self.val/other.val
+            anew.back_partial_der = 1/other.val
+        
+            for key in np.unique([key for key in self.der] + [key for key in other.der]):
+                if key not in self.der:
+                    anew.der[key]= -self.val*other.der[key]/(other.val**2)
+                elif key not in other.der:
+                    anew.der[key]=self.der[key]/other.val
+                else:
+                    anew.der[key]=0
+                    
+            self.back_partial_der = 1/other.val
+            other.back_partial_der = -self.val/(other.val**2)
+            
+        except AttributeError:
+            for key in self.der:
+                anew.der[key] = self.der[key]/other
+                anew.val = self.val/other
+                anew.back_partial_der = 1/other
+                
+        return anew
+
+
+    def __rtruediv__(self, other):
+
+        '''function for right division, when performing (constant)/(autodiff)'''
+
+        if isinstance(other, (int,float)):
+            anew = autodiff(self.name, self.val, self.der)
+            anew.lparent = self
+            anew.rparent = other
+        
+            for key in self.der:
+                anew.der[key] = -other*self.der[key]/(self.val**2)
+                anew.val = other/self.val
+                
+            self.back_partial_der = -other/(self.val**2)
+            return anew
+        else:
+            raise ValueError("Error: Only integer, float, or autodiff instances can be divided.")
+
+
+
+    def __add__(self, other):
+
+        if isinstance(other, (int, float, autodiff)) == False:
+            raise ValueError("Error: Only integer, float, or autodiff instances can be added.")
+
+
+        #Generate a new autodiff instance copy of self
+        anew = autodiff(self.name, self.val, self.der)
+
+        anew.lparent = self
+        anew.rparent = other
+
+
+        #Tries adding two autodiff instances together
+        try:
+
+            #Add values
+            anew.val = self.val + other.val
+
+            #Calculate derivatives of this addition for all variables so far encountered
+            for key in np.unique([key for key in self.der] + [key for key in other.der]): #Iterate through all unique variables so far encountered
+                #If self has not encountered this variable before (so derivative of self with respect to variable must be 0)
+                if key not in self.der:
+                    anew.der[key] = other.der[key]
+
+                #Else, if opponent has not encountered this variable before (so derivative of self with respect to variable must be 0)
+                elif key not in other.der:
+                    anew.der[key] = self.der[key]
+
+                #Else, if both self and opponent have encountered this variable before
+                else:
+                    anew.der[key] = self.der[key] + other.der[key]
+
+            self.back_partial_der = 1
+            other.back_partial_der = 1
+
+        #Otherwise, if not two autodiff instances:
+        except AttributeError:
+            #Tries adding autodiff instance and number together
+
+            for key in self.der:
+                anew.der[key] = self.der[key]
+                anew.val = other + self.val
+            self.back_partial_der = 1
+
+        #Returns new autodiff instance
+        return anew
+
+
+
+    #FUNCTION: __radd__
+    #PURPOSE: Allows commutative addition.
+    def __radd__(self, other):
+        return self + other
+
+    #FUNCTION: __sub__
+    #PURPOSE: Subtract an autodiff instance or number from a autodiff instance, and calculate the derivatives resulting from this action.
+    def __sub__(self, other):
+        if isinstance(other, (int, float, autodiff)) == False:
+            raise ValueError("Error: Only integer, float, or autodiff instances can be subtracted.")
+
+        #Generate a new autodiff instance copy of self
+        anew = autodiff(self.name, self.val, self.der)
+        anew.lparent = self
+        anew.rparent = other
+        
+        #Tries subtracting two autodiff instances together
+        try:
+            #Subtract values
+            anew.val = self.val - other.val
+
+            #Calculate derivatives of this subtraction for all variables so far encountered
+            for key in np.unique([key for key in self.der] + [key for key in other.der]): #Iterate through all unique variables so far encountered
+                #If self has not encountered this variable before (so derivative of self with respect to variable must be 0)
+                if key not in self.der:
+                    anew.der[key] = -1*other.der[key]
+
+                #Else, if opponent has not encountered this variable before (so derivative of self with respect to variable must be 0)
+                elif key not in other.der:
+                    anew.der[key] = self.der[key]
+
+                #Else, if both self and opponent have encountered this variable before
+                else:
+                    anew.der[key] = self.der[key] - other.der[key]
+                    
+            self.back_partial_der = 1
+            other.back_partial_der = -1
+        #Otherwise, if not two autodiff instances:
+        except AttributeError:
+            #Tries subtracting number from autodiff instance
+
+            for key in self.der:
+                anew.der[key] = self.der[key]
+                anew.val = self.val - other
+                
+            self.back_partial_der = 1
+        #Returns new autodiff instance
+        return anew
+
+
+
+    #FUNCTION: __sub__
+    #PURPOSE: Subtract an autodiff instance or number from a autodiff instance, and calculate the derivatives resulting from this action.
+    def __rsub__(self, other):
+        return (-1*self) + other
+
+
+    #FUNCTION: __pow__
+    #PURPOSE: Raise this autodiff instance to a number or to another autodiff instance, and calculate the derivatives resulting from this action.
+    def __pow__(self, other):
+
+        if isinstance(other, (int, float, autodiff)) == False:
+            raise ValueError("Error: Only integer, float, or autodiff instances can be multiplied.")
+
+        #Generate a new autodiff instance copy of self
+        anew = autodiff(self.name, self.val, self.der)
+
+        anew.lparent = self
+        anew.rparent = other
+
+        #Tries raising this autodiff instance to another autodiff instance
+        try:
+            #Raise values
+            anew.val = self.val**other.val
+
+            #Calculate derivatives of this exponentiation for all variables so far encountered
+            for key in np.unique([key for key in self.der] + [key for key in other.der]): #Iterate through all unique variables so far encountered
+                #If self has not encountered this variable before (so derivative of self with respect to variable must be 0)
+                if key not in self.der:
+                    anew.der[key] = anew.val*(np.log(self.val)*other.der[key])
+
+                #Else, if opponent has not encountered this variable before (so derivative of self with respect to variable must be 0)
+                elif key not in other.der:
+                    anew.der[key] = anew.val*(other.val*self.der[key]/1.0/self.val)
+
+                #Else, if both self and opponent have encountered this variable before
+                else:
+                    anew.der[key] = anew.val*((np.log(self.val)*other.der[key]) + (other.val*self.der[key]/1.0/self.val))
+
+            self.back_partial_der = other.val*self.val**(other.val-1)
+            other.back_partial_der = (self.val**other.val)*np.log(self.val)
+
+        #Otherwise, if not two autodiff instances:
+        except AttributeError:
+            #Tries adding autodiff instance and number together
+            for key in self.der:
+                anew.der[key] = other*(self.val**(other - 1))*self.der[key]
+                anew.val = self.val**other
+            self.back_partial_der = other*self.val**(other-1)
+        #Returns new autodiff instance
+        return anew
+
+
+    #FUNCTION: __rpow__
+    #PURPOSE: Raise this number to an autodiff instance, and calculate the derivatives resulting from this action.
+    def __rpow__(self, other):
+        if isinstance(other, (int, float, autodiff)) == False:
+            raise ValueError("Error: Only integer, float, or autodiff instances can be multiplied.")
+
+        #Generate a new autodiff instance copy of self
+        anew = autodiff(self.name, self.val, self.der)
+
+        #Tries autodiff instance and number together
+        for key in self.der:
+            anew.der[key] = (other**self.val)*np.log(other)*self.der[key]
+            anew.val = other**self.val
+
+        #Return new autodiff instance
+        return anew
+
+
+    # function for returning jacobian matrix values
+    def jacobian(self, order=None):
+        if order is not None: # If specific ordering requested
+            order = list(order)
+            jacobian = [None]*len(order)
+            ii = 0 # For indexing through jacobian
+            try:
+                for key in order:
+                    jacobian[ii] = self.der[key]
+                    ii = ii + 1
+            except KeyError:
+                raise KeyError("Error: variable(s) in order have not been encountered by this autodiff instance.")
+
+        else: # If no specific ordering given
+            jacobian = [None]*len(self.der)
+            order = [None]*len(self.der) # To hold ordering
+            ii = 0 # For indexing through jacobian
+            for key in self.der:
+                order[ii] = key
+                jacobian[ii] = self.der[key]
+                ii = ii + 1
+
+        # Cast the output as an array
+        jacobian = np.asarray(jacobian)
+        # Return jacobian and its ordering
+        return {"jacobian":jacobian, "order":order}
---
+++ packages/autodiffpy/autodiffpy-0.0.1-py2.py3-none-any.whl/tests/autodiff_test.py
@@ -0,0 +1,240 @@
+import pytest
+import sys
+import numpy as np
+
+sys.path.append('..')
+import numpy as np
+from autodiffpy import autodiff as ad
+from autodiffpy import autodiff_math as admath
+
+
+
+def test_eq():
+    x = ad.autodiff('x', 10)
+    y = ad.autodiff('y', 2)
+    f1 = x*y
+    f2 = y*x
+    assert f1 == f2
+
+def test_eq_errortypes():
+    x = ad.autodiff('x', 10)
+    f1 = x + 2
+    f2 = "hey"
+    with pytest.raises(ValueError):
+        f1 == f2
+
+def test_ne():
+    x = ad.autodiff('x', 10)
+    y = ad.autodiff('y', 2)
+    f1 = x*y
+    f2 = y*y
+    assert f1 != f2
+
+def test_ne_errortypes():
+    x = ad.autodiff('x', 10)
+    y = 1
+    f1 = x**2
+    with pytest.raises(ValueError):
+        f1 == y
+
+## Test true division with an autodiff instsance
+def test_truediv_result_ad():
+    x = ad.autodiff('x', 10)
+    y = ad.autodiff('y', 2)
+    f1 = x/y
+    f2 = 2*x/x
+    assert f1.val == 5
+    assert f1.der['x'] == 1/2
+    assert f1.der['y'] == -10/4
+    assert f2.val == 2
+    assert f2.der['x'] == 0
+
+## Test true division with a constant
+def test_truediv_result_const():
+    x = ad.autodiff('x', 10)
+    y = ad.autodiff('y', 2)
+    f2 = x/3
+    assert f2.val == 10/3
+    assert f2.der['x'] == 1/3
+
+## Test true division error types
+def test_truediv_err_types():
+    x = ad.autodiff('x', 10)
+    s = "str"
+    with pytest.raises(ValueError):
+        x/s
+
+def test_rtruediv_err_types():
+    x = ad.autodiff('x', 10)
+    s = "str"
+    with pytest.raises(ValueError):
+        s/x
+
+## Test reverse division with a constant
+def test_rtruediv_result_const():
+    x = ad.autodiff('x', 10)
+    y = ad.autodiff('y', 2)
+    f2 = 3/x
+    assert f2.val == 3/10
+    assert f2.der['x'] == -3/100
+
+
+## Test multiplication with an autodiff instance
+def test_mul_result_ad():
+    x = ad.autodiff('x', 10)
+    y = ad.autodiff('y', 2)
+    f1 = x*y
+    f2 = x*x*x
+    assert f1.val == 20
+    assert f1.der['x'] == 2
+    assert f1.der['y'] == 10
+    assert f2.val == 1000
+    assert f2.der['x'] == 300
+
+## Test multiplication with a constant
+def test_mul_result_const():
+    x = ad.autodiff('x', 10)
+    f2 = 3*x
+    assert f2.val == 30
+    assert f2.der['x'] == 3
+
+## Test multiplication error types
+def test_mul_err_types():
+    x = ad.autodiff('x', 10)
+    s = "str"
+    with pytest.raises(ValueError):
+        x*s
+
+## Test unary function (negative)
+def test_neg_result_single():
+    x = ad.autodiff('x', 10)
+    f = -x
+    assert f.val == -10
+    assert f.der['x'] == -1
+
+## Test addition with a constant and an autodiff instance
+def test_add_result_adandconst():
+    ad1 = ad.autodiff(name="x", val=20.5, der=1)
+    ad2 = ad.autodiff(name="y", val=3, der=1)
+
+    ad3 = ad1 + ad2 + ad1
+    ad4 = ad1 + 5.5 + ad2
+    ad5 = 5.5 + ad1 + ad2
+
+    assert ad3.val == 44
+    assert ad3.der["x"] == 2
+    assert ad3.der["y"] == 1
+
+    assert ad4.val == 29
+    assert ad4.der["x"] == 1
+    assert ad4.der["y"] == 1
+
+    assert ad5.val == 29
+    assert ad5.der["x"] == 1
+    assert ad5.der["y"] == 1
+
+## Test addition error types
+def test_add_err_types():
+    x = ad.autodiff('x', 10)
+    s = "str"
+    with pytest.raises(ValueError):
+        x+s
+
+## Test substraction error types
+def test_sub_err_types():
+    x = ad.autodiff('x', 10)
+    s = "str"
+    with pytest.raises(ValueError):
+        x-s
+
+## Test subtraction with a constant and an autodiff instance
+def test_sub_result_adandconst():
+    ad1 = ad.autodiff(name="x", val=2.5, der=1)
+    ad2 = ad.autodiff(name="y", val=3, der=1)
+
+    ad3 = ad1 - ad2 - ad1
+    ad4 = ad1 - 5.5 - ad2
+    ad5 = 5.5 - ad1 - ad2
+
+    assert ad3.val == -3
+    assert ad3.der["x"] == 0
+    assert ad3.der["y"] == -1
+
+    assert ad4.val == -6
+    assert ad4.der["x"] == 1
+    assert ad4.der["y"] == -1
+
+    assert ad5.val == 0
+    assert ad5.der["x"] == -1
+    assert ad5.der["y"] == -1
+
+## Test power error types
+def test_pow_err_types():
+    x = ad.autodiff('x', 10)
+    s = "str"
+    with pytest.raises(ValueError):
+        x**s
+
+## Test reverse power error types
+def test_revpow_err_types():
+    x = ad.autodiff('x', 10)
+    s = "str"
+    with pytest.raises(ValueError):
+        s**x
+
+## Test power with a constant and an autodiff instance
+def test_pow_result_adandconst():
+    ad1 = ad.autodiff(name="x", val=2, der=1)
+    ad2 = ad.autodiff(name="y", val=3, der=1)
+    ad3 = ad1**(ad2**ad1)
+    ad4 = (ad1**1.5)**ad2
+    ad5 = (1.5**ad1)**ad2
+
+    assert ad3.val == 512
+    assert abs(ad3.der["x"] - 5812.9920480098718094) < 1E-10
+    assert abs(ad3.der["y"] - 2129.3481386801519905) < 1E-10
+
+    assert abs(ad4.val  - 22.627416997969520780) < 1E-10
+    assert abs(ad4.der["x"] - 50.911688245431421756) < 1E-10
+    assert abs(ad4.der["y"] - 23.526195443245132601) < 1E-10
+
+    assert abs(ad5.val - 11.390625) < 1E-10
+    assert abs(ad5.der["x"] - 13.855502991133679740) < 1E-10
+    assert abs(ad5.der["y"] - 9.2370019940891198269) < 1E-10
+
+## Test jacobian() output
+def test_jacobian():
+    x = ad.autodiff('x', 10)
+    y = ad.autodiff('y', 2)
+    f1 = x*y
+    assert f1.jacobian()["order"] == ['x', 'y']
+    assert np.sum(f1.jacobian()["jacobian"] == np.array([2, 10])) == 2
+    assert f1.jacobian(order=['y','x'])["order"] == ['y', 'x']
+    assert np.sum(f1.jacobian(order=['y','x'])["jacobian"] == np.array([10, 2])) == 2
+
+    a = ad.autodiff('a', [1, 2, 3])
+    b = ad.autodiff('b', [-1, -2, -3])
+    c = ad.autodiff('c', [-2, 5, 10])
+    f2 = a*b - c
+    assert f2.jacobian(order=['a', 'b'])["order"] == ['a', 'b']
+    assert np.sum(f2.jacobian(order=['a', 'b'])["jacobian"] == np.array([[-1, -2, -3], [1, 2, 3]])) == 6
+
+    with pytest.raises(KeyError):
+        assert f2.jacobian(order=['a', 'd'])
+
+## Test backprop() output with hyperbolic functions
+def test_backprop_hyperbolic():
+    x = ad.autodiff('x', 1)
+    y = ad.autodiff('y', 2)
+    z = ad.autodiff('z', 3)
+    f1 = admath.sinh(x)*admath.cosh(y)*admath.tanh(z)
+
+    assert pytest.approx(f1.backprop()['x'][0]) == f1.back_der*np.tanh(z.val)*np.cosh(y.val)*np.cosh(x.val)
+    assert pytest.approx(f1.backprop()['y'][0]) == f1.back_der*np.tanh(z.val)*np.sinh(x.val)*np.sinh(y.val)
+
+
+def test_backprop_sincostanlog():
+    x = ad.autodiff('x', 3)
+    f = admath.sin(admath.cos(admath.tan(admath.log(x))))
+    # f.backprop() = {'x': array([-1.38686635])}
+    assert pytest.approx(f.backprop()['x'][0]) == -1.386866349701885
