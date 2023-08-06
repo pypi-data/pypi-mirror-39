3bcbad155f1d41129014b7e06af8b0a141ab89b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:13 +0000

    Add package infi.sgutils/0.1.14/infi.sgutils-0.1.14.tar.gz

---
+++ packages/infi.sgutils/infi.sgutils-0.1.14.tar.gz/infi.sgutils-0.1.14/src/infi/sgutils/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.14"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.1.14)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'd70044b6b3c0786c89026e3581a94bd0337ddf93'
+__git_head_subject__ = u'Finished Release v0.1.14'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-12-20 17:12:34'
