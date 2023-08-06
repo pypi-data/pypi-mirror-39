4c50838f944e775629f3d4171799e2fcf09ff4fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:21 +0000

    Add package libsmraw-python/20181215/libsmraw-python-20181215.tar.gz

---
+++ packages/libsmraw-python/libsmraw-python-20181215.tar.gz/libsmraw-20181215/smrawtools/mount_handle.c
@@ -0,0 +1,543 @@
+/*
+ * Mount handle
+ *
+ * Copyright (C) 2010-2018, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <common.h>
+#include <file_stream.h>
+#include <memory.h>
+#include <narrow_string.h>
+#include <system_string.h>
+#include <types.h>
+#include <wide_string.h>
+
+#include "mount_handle.h"
+#include "smrawtools_libcerror.h"
+#include "smrawtools_libsmraw.h"
+
+/* Creates a mount handle
+ * Make sure the value mount_handle is referencing, is set to NULL
+ * Returns 1 if successful or -1 on error
+ */
+int mount_handle_initialize(
+     mount_handle_t **mount_handle,
+     libcerror_error_t **error )
+{
+	static char *function = "mount_handle_initialize";
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( *mount_handle != NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_VALUE_ALREADY_SET,
+		 "%s: invalid mount handle value already set.",
+		 function );
+
+		return( -1 );
+	}
+	*mount_handle = memory_allocate_structure(
+	                 mount_handle_t );
+
+	if( *mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_MEMORY,
+		 LIBCERROR_MEMORY_ERROR_INSUFFICIENT,
+		 "%s: unable to create mount handle.",
+		 function );
+
+		goto on_error;
+	}
+	if( memory_set(
+	     *mount_handle,
+	     0,
+	     sizeof( mount_handle_t ) ) == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_MEMORY,
+		 LIBCERROR_MEMORY_ERROR_SET_FAILED,
+		 "%s: unable to clear mount handle.",
+		 function );
+
+		goto on_error;
+	}
+	if( libsmraw_handle_initialize(
+	     &( ( *mount_handle )->input_handle ),
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_INITIALIZE_FAILED,
+		 "%s: unable to initialize input handle.",
+		 function );
+
+		goto on_error;
+	}
+	return( 1 );
+
+on_error:
+	if( *mount_handle != NULL )
+	{
+		memory_free(
+		 *mount_handle );
+
+		*mount_handle = NULL;
+	}
+	return( -1 );
+}
+
+/* Frees a mount handle
+ * Returns 1 if successful or -1 on error
+ */
+int mount_handle_free(
+     mount_handle_t **mount_handle,
+     libcerror_error_t **error )
+{
+	static char *function = "mount_handle_free";
+	int result            = 1;
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( *mount_handle != NULL )
+	{
+		if( libsmraw_handle_free(
+		     &( ( *mount_handle )->input_handle ),
+		     error ) != 1 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_FINALIZE_FAILED,
+			 "%s: unable to free input handle.",
+			 function );
+
+			result = -1;
+		}
+		memory_free(
+		 *mount_handle );
+
+		*mount_handle = NULL;
+	}
+	return( result );
+}
+
+/* Signals the mount handle to abort
+ * Returns 1 if successful or -1 on error
+ */
+int mount_handle_signal_abort(
+     mount_handle_t *mount_handle,
+     libcerror_error_t **error )
+{
+	static char *function = "mount_handle_signal_abort";
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( mount_handle->input_handle != NULL )
+	{
+		if( libsmraw_handle_signal_abort(
+		     mount_handle->input_handle,
+		     error ) != 1 )
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_SET_FAILED,
+			 "%s: unable to signal input handle to abort.",
+			 function );
+
+			return( -1 );
+		}
+	}
+	return( 1 );
+}
+
+/* Opens the input of the mount handle
+ * Returns 1 if successful or -1 on error
+ */
+int mount_handle_open_input(
+     mount_handle_t *mount_handle,
+     system_character_t * const * filenames,
+     int number_of_filenames,
+     libcerror_error_t **error )
+{
+	system_character_t **libsmraw_filenames = NULL;
+	static char *function                   = "mount_handle_open_input";
+	size_t first_filename_length            = 0;
+	int result                              = 0;
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( filenames == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid filenames.",
+		 function );
+
+		return( -1 );
+	}
+	if( number_of_filenames <= 0 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_VALUE_ZERO_OR_LESS,
+		 "%s: invalid number of filenames.",
+		 function );
+
+		return( -1 );
+	}
+	if( number_of_filenames == 1 )
+	{
+		first_filename_length = system_string_length(
+		                         filenames[ 0 ] );
+
+#if defined( HAVE_WIDE_SYSTEM_CHARACTER )
+		result = libsmraw_glob_wide(
+		          filenames[ 0 ],
+		          first_filename_length,
+		          &libsmraw_filenames,
+		          &number_of_filenames,
+		          error );
+#else
+		result = libsmraw_glob(
+		          filenames[ 0 ],
+		          first_filename_length,
+		          &libsmraw_filenames,
+		          &number_of_filenames,
+		          error );
+#endif
+		if( result != 1 )
+		{
+			libcerror_error_free(
+			 error );
+
+			number_of_filenames = 1;
+		}
+		else
+		{
+			filenames = (system_character_t * const *) libsmraw_filenames;
+		}
+	}
+#if defined( HAVE_WIDE_SYSTEM_CHARACTER )
+	if( libsmraw_handle_open_wide(
+	     mount_handle->input_handle,
+	     filenames,
+	     number_of_filenames,
+	     LIBSMRAW_OPEN_READ,
+	     error ) != 1 )
+#else
+	if( libsmraw_handle_open(
+	     mount_handle->input_handle,
+	     filenames,
+	     number_of_filenames,
+	     LIBSMRAW_OPEN_READ,
+	     error ) != 1 )
+#endif
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_IO,
+		 LIBCERROR_IO_ERROR_OPEN_FAILED,
+		 "%s: unable to open file(s).",
+		 function );
+
+		if( libsmraw_filenames != NULL )
+		{
+#if defined( HAVE_WIDE_SYSTEM_CHARACTER )
+			libsmraw_glob_wide_free(
+			 libsmraw_filenames,
+			 number_of_filenames,
+			 NULL );
+#else
+			libsmraw_glob_free(
+			 libsmraw_filenames,
+			 number_of_filenames,
+			 NULL );
+#endif
+		}
+		return( -1 );
+	}
+	if( libsmraw_filenames != NULL )
+	{
+#if defined( HAVE_WIDE_SYSTEM_CHARACTER )
+		if( libsmraw_glob_wide_free(
+		     libsmraw_filenames,
+		     number_of_filenames,
+		     error ) != 1 )
+#else
+		if( libsmraw_glob_free(
+		     libsmraw_filenames,
+		     number_of_filenames,
+		     error ) != 1 )
+#endif
+		{
+			libcerror_error_set(
+			 error,
+			 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+			 LIBCERROR_RUNTIME_ERROR_FINALIZE_FAILED,
+			 "%s: unable to free globbed filenames.",
+			 function );
+
+			return( -1 );
+		}
+	}
+	return( 1 );
+}
+
+/* Closes the mount handle
+ * Returns the 0 if succesful or -1 on error
+ */
+int mount_handle_close(
+     mount_handle_t *mount_handle,
+     libcerror_error_t **error )
+{
+	static char *function = "mount_handle_close";
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( libsmraw_handle_close(
+	     mount_handle->input_handle,
+	     error ) != 0 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_IO,
+		 LIBCERROR_IO_ERROR_CLOSE_FAILED,
+		 "%s: unable to close input handle.",
+		 function );
+
+		return( -1 );
+	}
+	return( 0 );
+}
+
+/* Read a buffer from the input handle
+ * Return the number of bytes read if successful or -1 on error
+ */
+ssize_t mount_handle_read_buffer(
+         mount_handle_t *mount_handle,
+         uint8_t *buffer,
+         size_t size,
+         libcerror_error_t **error )
+{
+	static char *function = "mount_handle_read_buffer";
+	ssize_t read_count    = 0;
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	read_count = libsmraw_handle_read_buffer(
+	              mount_handle->input_handle,
+	              buffer,
+	              size,
+	              error );
+
+	if( read_count == -1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_IO,
+		 LIBCERROR_IO_ERROR_READ_FAILED,
+		 "%s: unable to read buffer from input handle.",
+		 function );
+
+		return( -1 );
+	}
+	return( read_count );
+}
+
+/* Seeks a specific offset from the input handle
+ * Return the offset if successful or -1 on error
+ */
+off64_t mount_handle_seek_offset(
+         mount_handle_t *mount_handle,
+         off64_t offset,
+         int whence,
+         libcerror_error_t **error )
+{
+	static char *function = "mount_handle_seek_offset";
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	offset = libsmraw_handle_seek_offset(
+	          mount_handle->input_handle,
+	          offset,
+	          whence,
+	          error );
+
+	if( offset == -1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_IO,
+		 LIBCERROR_IO_ERROR_SEEK_FAILED,
+		 "%s: unable to seek offset in input handle.",
+		 function );
+
+		return( -1 );
+	}
+	return( offset );
+}
+
+/* Retrieves the number of input handles
+ * Returns 1 if successful or -1 on error
+ */
+int mount_handle_get_number_of_input_handles(
+     mount_handle_t *mount_handle,
+     int *number_of_input_handles,
+     libcerror_error_t **error )
+{
+	static char *function = "mount_handle_get_number_of_input_handles";
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( number_of_input_handles == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid number of input handles.",
+		 function );
+
+		return( -1 );
+	}
+	*number_of_input_handles = 1;
+
+	return( 1 );
+}
+
+/* Retrieves the size of the input volume
+ * Returns 1 if successful or -1 on error
+ */
+int mount_handle_get_size(
+     mount_handle_t *mount_handle,
+     size64_t *size,
+     libcerror_error_t **error )
+{
+	static char *function = "mount_handle_get_size";
+
+	if( mount_handle == NULL )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_ARGUMENTS,
+		 LIBCERROR_ARGUMENT_ERROR_INVALID_VALUE,
+		 "%s: invalid mount handle.",
+		 function );
+
+		return( -1 );
+	}
+	if( libsmraw_handle_get_media_size(
+	     mount_handle->input_handle,
+	     size,
+	     error ) != 1 )
+	{
+		libcerror_error_set(
+		 error,
+		 LIBCERROR_ERROR_DOMAIN_RUNTIME,
+		 LIBCERROR_RUNTIME_ERROR_GET_FAILED,
+		 "%s: unable to retrieve size from input handle.",
+		 function );
+
+		return( -1 );
+	}
+	return( 1 );
+}
+
