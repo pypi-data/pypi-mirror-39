3785eca36b50ad87d2d51b092058a16e02db3076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:38 +0000

    Add package pingouin/0.2.2/pingouin-0.2.2.tar.gz

---
+++ packages/pingouin/pingouin-0.2.2.tar.gz/pingouin-0.2.2/setup.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2018 Raphael Vallat
+
+DESCRIPTION = "Pingouin: statistical package for Python"
+LONG_DESCRIPTION = """Pingouin is a statistical Python package based on Pandas.
+"""
+
+DISTNAME = 'pingouin'
+MAINTAINER = 'Raphael Vallat'
+MAINTAINER_EMAIL = 'raphaelvallat9@gmail.com'
+URL = 'https://pingouin-stats.org/index.html'
+LICENSE = 'BSD (3-clause)'
+DOWNLOAD_URL = 'https://github.com/raphaelvallat/pingouin/'
+VERSION = '0.2.2'
+PACKAGE_DATA = {'pingouin.data.icons': ['*.svg']}
+
+INSTALL_REQUIRES = [
+    'numpy>=1.15',
+    'scipy>=1.1',
+    'pandas>=0.23',
+    'matplotlib>=3.0.2',
+    'seaborn>=0.9.0',
+]
+
+PACKAGES = [
+    'pingouin',
+    'pingouin.datasets',
+    'pingouin.external',
+]
+
+CLASSIFIERS = [
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'License :: OSI Approved :: BSD License',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Operating System :: MacOS'
+]
+
+try:
+    from setuptools import setup
+    _has_setuptools = True
+except ImportError:
+    from distutils.core import setup
+
+if __name__ == "__main__":
+
+    setup(name=DISTNAME,
+          author=MAINTAINER,
+          author_email=MAINTAINER_EMAIL,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION,
+          license=LICENSE,
+          url=URL,
+          version=VERSION,
+          download_url=DOWNLOAD_URL,
+          install_requires=INSTALL_REQUIRES,
+          include_package_data=True,
+          packages=PACKAGES,
+          package_data=PACKAGE_DATA,
+          classifiers=CLASSIFIERS,
+          )
