e83506531c76d5551dfe4d38d6ccd77dc986035e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:22 +0000

    Add package pyabf/2.1.1/pyabf-2.1.1.tar.gz

---
+++ packages/pyabf/pyabf-2.1.1.tar.gz/pyabf-2.1.1/pyabf/__init__.py
@@ -0,0 +1,23 @@
+"""
+PyABF - A Python interface to files in the Axon Binary Format (ABF)
+    by Scott Harden
+
+Documentation and code examples, and more can be found at:
+    https://github.com/swharden/pyABF
+"""
+
+import sys
+
+if sys.version_info >= (3, 6):
+    from ._version import __version__
+    from ._version import versionAtLeast
+    from ._version import info
+    from ._version import help
+    from pyabf.abf import ABF
+    from pyabf.atf import ATF
+    from pyabf import stats
+    from pyabf import plot
+    from pyabf import filter
+else:
+    print("WARNING: pyABF is only partially supported on Python versions <3.6")
+    from pyabf.abf import ABF
