c9e42363fd3e117d87990659a252e38bd3aae3a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:16 +0000

    Add package upcloud-api/0.4.3/upcloud-api-0.4.3.tar.gz

---
+++ packages/upcloud-api/upcloud-api-0.4.3.tar.gz/upcloud-api-0.4.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='upcloud-api',
+    version='0.4.3',
+    description='UpCloud API Client',
+    author='Elias Nygren',
+    maintainer='Mika Lackman',
+    maintainer_email='mika.lackman@upcloud.com',
+    url='https://github.com/UpCloudLtd/upcloud-python-api',
+    packages=['upcloud_api', 'upcloud_api.cloud_manager'],
+    download='https://github.com/UpCloudLtd/upcloud-python-api/tarball/v0.4.3',
+    license='MIT',
+    install_requires=[
+        'requests>=2.6.0',
+        'six>=1.9.0'
+    ]
+)
