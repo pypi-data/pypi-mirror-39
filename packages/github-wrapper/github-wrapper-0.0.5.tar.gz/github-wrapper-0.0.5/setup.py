882405e96cb592cba944a212c64b2c3b23171bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:57 +0000

    Add package github-wrapper/0.0.5/github-wrapper-0.0.5.tar.gz

---
+++ packages/github-wrapper/github-wrapper-0.0.5.tar.gz/github-wrapper-0.0.5/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="github-wrapper",
+    version="0.0.5",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/py-nayls/github-wrapper",
+    packages=[
+        "github_wrapper",
+        "github_wrapper.common",
+        "github_wrapper.v3",
+        "github_wrapper.v3.issues",
+        "github_wrapper.v3.pull_requests",
+    ],
+
+    author="Gagarin Svyatoslav",
+    author_email="nayls@bk.ru",
+    maintainer="Gagarin Svyatoslav",
+    maintainer_email="nayls@bk.ru",
+
+    python_requires=">=3.5",
+    install_requires=[
+        "pytest>=4.0.1",
+        "requests>=2.21.0"
+    ],
+
+    classifiers=[
+        "Framework :: Pytest"
+    ]
+)
