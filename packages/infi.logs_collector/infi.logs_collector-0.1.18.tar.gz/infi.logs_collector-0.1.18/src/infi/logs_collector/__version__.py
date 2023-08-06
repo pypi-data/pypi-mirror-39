989364ee0e63703b836e9c1091018990a77487d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:27 +0000

    Add package infi.logs_collector/0.1.18/infi.logs_collector-0.1.18.tar.gz

---
+++ packages/infi.logs_collector/infi.logs_collector-0.1.18.tar.gz/infi.logs_collector-0.1.18/src/infi/logs_collector/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.18"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.1.18)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '330c665701d2f706fffea158cc4da99bb8ddbcf0'
+__git_head_subject__ = u'Finished Release v0.1.18'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-12-20 15:27:00'
