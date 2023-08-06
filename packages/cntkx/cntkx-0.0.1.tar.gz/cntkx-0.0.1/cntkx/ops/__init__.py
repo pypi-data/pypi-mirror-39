f6b5f271d0bf1fe5fb30d531d108bb5d11c92c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:03 +0000

    Add package cntkx/0.0.1/cntkx-0.0.1.tar.gz

---
+++ packages/cntkx/cntkx-0.0.1.tar.gz/cntkx-0.0.1/cntkx/ops/__init__.py
@@ -0,0 +1,80 @@
+import cntk as C
+import numpy as np
+from . import sequence
+
+
+##########################################################################
+# linear ops
+##########################################################################
+
+def cumsum(x, axis: int=-1):
+    """ Calculates the cumulative sum across a static axis
+
+    Arguments:
+        x: input tensor
+        axis (int): static axis of tensor to cumsum over
+
+    Returns:
+        :class:`~cntk.ops.functions.Function`
+    """
+    d = x.shape[axis]
+    u = C.constant(np.triu(np.ones((d,d))).astype(x.dtype))
+    if axis != -1:
+        x = C.swapaxes(x, -1, axis)
+    z = C.times(x, u)
+    if axis != -1:
+        z = C.swapaxes(z, -1, axis)
+    return z
+
+
+##########################################################################
+# mixture density network ops
+##########################################################################
+
+
+@C.typemap
+def gaussian_mdn_coeff(x, nmix: int, ndim: int):
+    """
+    Extracts the coefficients for gaussian mixture density network.
+    Assumes independence between gaussian dimensions.
+
+    Arguments:
+        x: input tensor
+        nmix (int): number of mixture
+        ndim (int): number of dimension of gaussian
+
+    Returns:
+        tuple
+
+    """
+
+    if len(x.shape) != 1:
+        raise ValueError("Must be a 1d tensor, but input has shape {0}".format(x.shape))
+
+    alpha = C.softmax(C.slice(x, 0, 0, nmix), name='alpha')
+    sigma = C.exp(C.slice(x, 0, nmix, 2 * nmix), name='sigma')  # common variance for all components in single gaussian kernel
+    mu = C.reshape(C.slice(x, 0,  2 * nmix, (ndim + 2) * nmix), shape=(nmix, ndim), name='mu')
+    return alpha, mu, sigma
+
+
+def sample_gaussian_mdn(prediction_tensor, nmix: int, ndim: int):
+    """ Constructs sampling nodes from mixture density network outputs
+
+    Arguments:
+        prediction_tensor: input tensor
+        nmix (int): number of mixture
+        ndim (int): number of dimension of gaussian
+
+    Returns:
+        :class:`~cntk.ops.functions.Function`
+
+    """
+    alpha_tensor, mu_tensor, sigma_tensor = gaussian_mdn_coeff(prediction_tensor, nmix=nmix, ndim=ndim)
+
+    alpha_tensor_gumbel = alpha_tensor + C.random.gumbel_like(alpha_tensor)
+    selected_alpha = C.equal(alpha_tensor_gumbel, C.reduce_max(alpha_tensor_gumbel))
+    selected_mu_tensor = C.reduce_sum(mu_tensor * C.expand_dims(selected_alpha, axis=-1), axis=0)
+    selected_sigma_tensor = C.reduce_sum(sigma_tensor * selected_alpha, axis=0)
+
+    sampled = C.random.normal_like(selected_sigma_tensor) * selected_sigma_tensor + selected_mu_tensor
+    return sampled
