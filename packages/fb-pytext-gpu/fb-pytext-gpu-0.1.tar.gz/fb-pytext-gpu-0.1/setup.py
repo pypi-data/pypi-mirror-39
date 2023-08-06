068cd77cf18ef2a72bf2cd4305c7dd01b327d9de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:49 +0000

    Add package fb-pytext-gpu/0.1/fb-pytext-gpu-0.1.tar.gz

---
+++ packages/fb-pytext-gpu/fb-pytext-gpu-0.1.tar.gz/fb-pytext-gpu-0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+import os
+
+from setuptools import setup
+
+
+setup(
+    name="fb-pytext-gpu",
+    version="0.1",
+    description="pytorch modeling framework and model zoo for text models",
+    author="Facebook",
+    author_email="snisarg@fb.com",
+    license="BSD",
+    packages=["pytext"],
+)
