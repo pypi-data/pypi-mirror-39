7606ee623d46705dd28ceeca2d164e215d399449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:10 +0000

    Add package pythumbnail/0.0.2/pythumbnail-0.0.2-py3-none-any.whl

---
+++ packages/pythumbnail/pythumbnail-0.0.2-py3-none-any.whl/pythumbnail/__init__.py
@@ -0,0 +1,5 @@
+# This page intentionally left blank
+from .thumbnail import thumbnail
+
+def read_file(directory, silence = True, tab_to_space = 4):
+    return thumbnail(directory, silence, tab_to_space)
