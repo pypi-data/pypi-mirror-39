a25bf764faa9355f92f95ea04e64703f4c873664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:46 +0000

    Add package FileReadTool/1.1.2/FileReadTool-1.1.2.tar.gz

---
+++ packages/FileReadTool/FileReadTool-1.1.2.tar.gz/FileReadTool-1.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md","r")as fh:
+	long_description = fh.read()
+
+
+setuptools.setup(
+	name = 'FileReadTool',
+	version= '1.1.2',
+	author="Jiaqi Zhang",
+     	author_email="jiaqi3899@gmail.com",
+     	description="self using package",
+     	long_description=long_description,
+     	long_description_content_type="text/markdown",
+     	url="https://github.com/javatechy/dokr",
+     	packages=setuptools.find_packages(),
+     	classifiers=[
+      	   "Programming Language :: Python :: 3",
+      	   "License :: OSI Approved :: MIT License",
+      	   "Operating System :: OS Independent",
+     	],
+)
---
+++ packages/FileReadTool/FileReadTool-1.1.2.tar.gz/FileReadTool-1.1.2/tool/__init__.py
@@ -0,0 +1,4 @@
+name = "FileReadTool"
+
+__all__ = ['isList','isSubList','ReadData','MatrixToList','ListToMatrix']
+__version__ = '1.1'
