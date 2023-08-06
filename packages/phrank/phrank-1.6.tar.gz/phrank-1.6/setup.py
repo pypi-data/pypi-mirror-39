bc4c496167dce8315eac462b986e233f180e084e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:20 +0000

    Add package phrank/1.6/phrank-1.6.tar.gz

---
+++ packages/phrank/phrank-1.6.tar.gz/phrank-1.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='phrank',
+      version='1.6',
+      description='Measuring phenotype set similarity',
+      url='https://bitbucket.org/bejerano/phrank/',
+      author='Bejerano Lab',
+      author_email='kjag@cs.stanford.edu',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
