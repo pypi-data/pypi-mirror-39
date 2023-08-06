f6210a1cd43c35ac3a1a3be4d887d692a71ab3a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:12 +0000

    Add package flask-sharething/0.2.0/flask-sharething-0.2.0.tar.gz

---
+++ packages/flask-sharething/flask-sharething-0.2.0.tar.gz/flask-sharething-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask-sharething',
+    version='0.2.0',
+    url='https://github.com/qileroro/flask-sharething/',
+    license='Apache License, Version 2.0',
+    author='Kevin Wong',
+    author_email='qileroro@qq.com',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    description='Share extensions in multiple flask sub-applications',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.7'
+    ],
+)
