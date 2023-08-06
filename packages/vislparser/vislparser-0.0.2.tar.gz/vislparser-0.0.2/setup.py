b40b0789d13f5a33893deae9373f10506449f78c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:29 +0000

    Add package vislparser/0.0.2/vislparser-0.0.2.tar.gz

---
+++ packages/vislparser/vislparser-0.0.2.tar.gz/vislparser-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vislparser",
+    version="0.0.2",
+    author="José Patrício",
+    author_email="jpegx100@gmail.com",
+    description="Automatic calls to VISL online parser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lplnufpi/vislparser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
