35d99bfd880ae8dfe43852a39da2a8a7e880a215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:57 +0000

    Add package autoclasswrapper/0.2.0/autoclasswrapper-0.2.0-py3-none-any.whl

---
+++ packages/autoclasswrapper/autoclasswrapper-0.2.0-py3-none-any.whl/autoclasswrapper/__init__.py
@@ -0,0 +1,22 @@
+"""autoclasswrapper: Python wrapper for AutoClass clustering."""
+
+__version__ = "0.2.0"
+__copyright__ = "Copyright 2018 Pierre Poulain"
+
+import logging
+
+from .input import (Input,
+                    Dataset,
+                    raise_on_duplicates,
+                    DuplicateColumnNameError,
+                    CastFloat64Error)
+from .output import Output
+from .run import search_autoclass_in_path, get_autoclass_version, Run
+
+log = logging.getLogger(__name__)
+log_handler = logging.StreamHandler()
+log_formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s",
+                                  "%Y-%m-%d %H:%M:%S")
+log_handler.setFormatter(log_formatter)
+log.addHandler(log_handler)
+log.setLevel(logging.DEBUG)
