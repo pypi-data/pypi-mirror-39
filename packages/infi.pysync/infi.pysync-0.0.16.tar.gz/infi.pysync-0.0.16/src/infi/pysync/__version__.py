ffef3a51fb79ae877906e73dc784b21cbc484d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:22 +0000

    Add package infi.pysync/0.0.16/infi.pysync-0.0.16.tar.gz

---
+++ packages/infi.pysync/infi.pysync-0.0.16.tar.gz/infi.pysync-0.0.16/src/infi/pysync/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.16"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.0.16)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '386ee97afade541e7d16f3e0604db83ee891a353'
+__git_head_subject__ = u'Finished Release v0.0.16'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-12-20 17:54:11'
