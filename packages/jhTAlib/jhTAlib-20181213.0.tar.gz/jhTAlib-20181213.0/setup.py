694eca98852e97001094f4afac09b1a4872d7178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:27 +0000

    Add package jhTAlib/20181213.0/jhTAlib-20181213.0.tar.gz

---
+++ packages/jhTAlib/jhTAlib-20181213.0.tar.gz/jhTAlib-20181213.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='jhTAlib',
+    version='20181213.0',
+    description='Technical Analysis Library',
+    keywords='Technical Analysis Library',
+    url='https://github.com/joosthoeks/jhTAlib',
+    author='Joost Hoeks',
+    author_email='joosthoeks@gmail.com',
+    license='GNU',
+    packages=find_packages(),
+    install_requires=[],
+    zip_safe=False
+)
+
