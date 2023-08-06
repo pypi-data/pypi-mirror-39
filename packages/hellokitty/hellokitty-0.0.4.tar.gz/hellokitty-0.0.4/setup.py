6ddc8acebc6da96c7927fb7148c56ad66166571b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:55 +0000

    Add package hellokitty/0.0.4/hellokitty-0.0.4.tar.gz

---
+++ packages/hellokitty/hellokitty-0.0.4.tar.gz/hellokitty-0.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='hellokitty',
+    version='0.0.4',
+    description='Some funny app!',
+    author='Mindey',
+    author_email='mindey@qq.com',
+    url='https://github.com/mindey/hellokitty',
+    packages = find_packages(exclude=['docs', 'tests*']),
+)
