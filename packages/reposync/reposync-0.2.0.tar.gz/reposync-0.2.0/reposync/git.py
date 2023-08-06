45d906ca5e1420ea732bb613988f2f500fa2cf3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:42 +0000

    Add package reposync/0.2.0/reposync-0.2.0.tar.gz

---
+++ packages/reposync/reposync-0.2.0.tar.gz/reposync-0.2.0/reposync/git.py
@@ -0,0 +1,66 @@
+import os
+import subprocess
+
+import git
+
+
+class Git:
+    def __init__(self, config):
+        self.gopath = config.get('gopath')
+        self.scheme = config.get('scheme')
+        self.verbose = config.get('verbose')
+
+    def clone(self, repository):
+        if repository.kind == 'go':
+            self.go_get(repository)
+            return
+
+        print("Cloning", repository.path, "...", end="", flush=True)
+        try:
+            git.Repo.clone_from(self.scheme + repository.url, repository.path)
+        except git.exc.GitCommandError:
+            print("skipped.")
+        else:
+            print("cloned.")
+
+    def pull(self, repository):
+        if repository.kind == 'go':
+            self.go_get(repository)
+            return
+
+        print("Pulling", repository.path, "...", end="", flush=True)
+        try:
+            git.Git(repository.path).pull()
+        except git.exc.GitCommandError:
+            print("failed.")
+        else:
+            print("pulled.")
+
+    # private
+
+    def go_get(self, repository):
+        print("Getting", repository.path, "...", end="", flush=True)
+
+        if os.path.isdir(repository.path):
+            print("skipped.")
+            return
+
+        meta = repository.meta
+        cmd = meta[0] if len(meta) >= 1 else ''
+
+        get = 'go get {} -u {}/{}'.format(self.verbose_flag, repository.url,
+                                          cmd)
+        subprocess.call(get.split())
+
+        mkdir = 'mkdir -p {}'.format(os.path.dirname(repository.path))
+        subprocess.call(mkdir.split())
+
+        ln = 'ln {} -s {}/{} {}'.format(self.verbose_flag,
+                                        self.gopath + '/src', repository.url,
+                                        repository.path)
+        subprocess.call(ln.split())
+
+        print("done.")
+
+    def verbose_flag(self):
+        return '-v' if self.verbose else ''
