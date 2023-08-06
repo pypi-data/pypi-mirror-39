a3fece06b012e87782295c158cc2188ddaf52d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:08:19 +0000

    Add package alyssum/0.0.10/alyssum-0.0.10-py3-none-any.whl

---
+++ packages/alyssum/alyssum-0.0.10-py3-none-any.whl/alyssum/decorators.py
@@ -0,0 +1,22 @@
+def run_only(time=1, message=None):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if wrapper.count != time:
+                wrapper.count += 1
+                return func(*args, **kwargs)
+            else:
+                if message is not None:
+                    print(message)
+        wrapper.count = 0
+        return wrapper
+    return decorator
+
+
+run_once = run_only(1)
+
+
+def run_once_message(message=None):
+    def decorator(func):
+        return run_only(1, message)(func)
+    return decorator
+
