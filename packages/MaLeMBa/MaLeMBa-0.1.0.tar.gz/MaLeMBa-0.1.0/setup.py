034f97a6187e839b95ee950c3c6f7e3423fa1dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:32 +0000

    Add package MaLeMBa/0.1.0/MaLeMBa-0.1.0.tar.gz

---
+++ packages/MaLeMBa/MaLeMBa-0.1.0.tar.gz/MaLeMBa-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MaLeMBa',
+    version='0.1.0',
+    author="Denis Moshensky",
+    author_email="loven7doo@gmail.com",  
+    description="Machine Learning Models Base",
+    url="https://github.com/loven-doo/MaLeMBa",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    package_data={},
+    install_requires=[
+        'numpy >= 1.15.3',
+        'pandas >= 0.23.4',
+        'scipy >= 1.1.0',
+    ],
+    entry_points={
+        'console_scripts': []
+    }
+)
