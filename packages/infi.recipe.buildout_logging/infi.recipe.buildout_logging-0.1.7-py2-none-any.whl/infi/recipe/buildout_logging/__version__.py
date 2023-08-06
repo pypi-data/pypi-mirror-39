0119258ada42efbaa4e103327fea21fb44b091fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:16 +0000

    Add package infi.recipe.buildout_logging/0.1.7/infi.recipe.buildout_logging-0.1.7-py2-none-any.whl

---
+++ packages/infi.recipe.buildout_logging/infi.recipe.buildout_logging-0.1.7-py2-none-any.whl/infi/recipe/buildout_logging/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.7"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.1.7)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '8ae9b93ce9dcc1d29b5118683c185fb73f3dd2dd'
+__git_head_subject__ = u'Finished Release v0.1.7'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2018-12-20 17:21:07'
